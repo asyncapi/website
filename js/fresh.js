fc0495b17222d73c90c5efe9fa002a5896c17ba5
fmvilas <fmvilas@gmail.com>
2019-04-18 18:25:56 +0200

    Add blog

--- js/fresh.js
+++ js/fresh.js
@@ -67,24 +67,4 @@ $(document).ready(function(){
     var $window           = $(window),
         win_height_padded = $window.height() * 1.1,
         isTouch           = Modernizr.touch;
-
-    $window.on('scroll', revealOnScroll);
-
-    function revealOnScroll() {
-        var scrolled = $window.scrollTop();
-        $(".revealOnScroll:not(.animated)").each(function () {
-            var $this     = $(this),
-                offsetTop = $this.offset().top;
-
-            if (scrolled + win_height_padded > offsetTop) {
-                if ($this.data('timeout')) {
-                    window.setTimeout(function(){
-                        $this.addClass('animated ' + $this.data('animation'));
-                    }, parseInt($this.data('timeout'),10));
-                } else {
-                    $this.addClass('animated ' + $this.data('animation'));
-                }
-            }
-        });
-    }
 })
