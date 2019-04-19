3b6a3e6a6a253e633a46a53c78afae282440929b
fmvilas <fmvilas@gmail.com>
2019-04-19 19:00:09 +0200

    Remove preloader

--- themes/hugo-fresh/static/js/fresh.js
+++ themes/hugo-fresh/static/js/fresh.js
@@ -1,11 +1,4 @@
 $(document).ready(function(){
-    
-    //Preloader
-    $(window).on('load', function() { // makes sure the whole site is loaded 
-        $('#status').fadeOut(); // will first fade out the loading animation 
-        $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website. 
-        $('body').delay(350).css({'overflow':'visible'});
-    })
 
     //Mobile menu toggle
     if ($('.navbar-burger').length) {
