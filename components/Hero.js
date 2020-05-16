1bd390c2c3481fdf78297cb30525f3d6257b0624
fmvilas <fmvilas@gmail.com>
2020-05-16 18:32:42 +0200

    Remove .next from repo

--- components/Hero.js
+++ components/Hero.js
@@ -5,7 +5,7 @@ import ArrowRight from './icons/ArrowRight'
 export default function Hero () {
   return (
     <div className="px-2 md:flex md:mt-16 md:px-0">
-      <div className="text-center mb-8 md:mb-0 md:w-1/2 md:text-left">
+      <div className="text-center mb-8 md:flex-1 md:mb-0 md:w-1/2 md:text-left">
         <h1 className="text-primary-800 text-3xl font-bold md:text-4xl md:w-2/3 mb-4">Building the future of event-driven architectures.</h1>
         <h2 className="text-gray-500 text-xl font-normal mb-6">
           Open source tools to easily build and maintain your event-driven architecture.
@@ -13,7 +13,7 @@ export default function Hero () {
         </h2>
         <Button text="Get Started" href="/docs/getting-started" icon={<ArrowRight className="-mb-1 h-5 w-5" />} />
       </div>
-      <div className="text-center md:flex">
+      <div className="text-center md:flex-1 md:flex">
         <EveAndChan className="inline-block sm:w-2/3 md:w-full md:block md:self-center" />
       </div>
     </div>
