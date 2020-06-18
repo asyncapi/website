00d75a1fc10943ae94b23a7caf0e20d8b003653d
fmvilas <fmvilas@gmail.com>
2020-06-18 17:50:22 +0200

    Debugging docs menu

--- components/layout/DocsLayout.js
+++ components/layout/DocsLayout.js
@@ -19,11 +19,12 @@ export default function DocsLayout({ post, navItems = {}, children }) {
 
   const [showMenu, setShowMenu] = useState(false)
 
-  const navigation = navItems.sort((i1, i2) => {
-    const i1Weight = i1.isSection ? (i1.weight + 1) * 1000 : (i1.sectionWeight + 1) * 1000 + (i1.weight || 0)
-    const i2Weight = i2.isSection ? (i2.weight + 1) * 1000 : (i2.sectionWeight + 1) * 1000 + (i2.weight || 0)
-    return i1Weight - i2Weight
-  })
+  // const navigation = navItems.sort((i1, i2) => {
+  //   const i1Weight = i1.isSection ? (i1.weight + 1) * 1000 : (i1.sectionWeight + 1) * 1000 + (i1.weight || 0)
+  //   const i2Weight = i2.isSection ? (i2.weight + 1) * 1000 : (i2.sectionWeight + 1) * 1000 + (i2.weight || 0)
+  //   return i1Weight - i2Weight
+  // })
+  const navigation = navItems
 
   return (
     <DocsContext.Provider value={{ post, navItems }}>
