2457cf071e14da007cb6b32a017d7f494b268059
fmvilas <fmvilas@gmail.com>
2020-06-30 17:16:39 +0200

    Remove TOC line on docs

--- components/layout/DocsLayout.js
+++ components/layout/DocsLayout.js
@@ -41,7 +41,7 @@ export default function DocsLayout({ post, navItems = {}, children }) {
         <div className="hidden md:flex md:flex-shrink-0">
           <div className="flex flex-col w-64 border-r border-gray-200 bg-white">
             <div className="flex-1 flex flex-col pt-5 md:overflow-y-auto md:sticky md:top-0 md:max-h-screen">
-              <ClickableLogo logoClassName="h-8 w-auto ml-3" />
+              <ClickableLogo logoClassName="h-8 w-auto ml-4 mt-0.5" />
               
               <nav className="flex-1 mt-3 pb-8 px-2 bg-white">
                 {
@@ -79,7 +79,7 @@ export default function DocsLayout({ post, navItems = {}, children }) {
               </p>
             </div>
             <div className={`xl:flex ${post.toc && post.toc.length ? 'xl:flex-row-reverse' : ''}`}>
-              <TOC toc={post.toc} className="bg-blue-100 mt-4 p-4 sticky top-0 xl:bg-transparent xl:mt-0 xl:pt-0 xl:pb-8 xl:top-4 xl:overflow-y-auto xl:max-h-(screen-16) xl:w-72 xl:border-l xl:border-gray-200" />
+              <TOC toc={post.toc} className="bg-blue-100 mt-4 p-4 sticky top-0 xl:bg-transparent xl:mt-0 xl:pt-0 xl:pb-8 xl:top-4 xl:overflow-y-auto xl:max-h-(screen-16) xl:w-72" />
               <div className="mt-8 px-4 sm:px-6 xl:px-8 xl:flex-1 xl:max-w-184">
                 <article className="mb-32">
                   <Head
