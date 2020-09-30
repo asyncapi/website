1ccff69ddb52014121692b2aa164fd7eed8275ad
fmvilas <fmvilas@gmail.com>
2020-09-30 11:11:01 +0200

    Fade out coming soon items

--- components/navigation/MenuBlocks.js
+++ components/navigation/MenuBlocks.js
@@ -7,15 +7,15 @@ export default function MenuBlocks ({
     <>
       {
         items.map((item, index) => (
-          <a href={item.href} key={index} className="-m-3 p-3 flex items-start space-x-4 rounded-lg hover:bg-gray-50 transition ease-in-out duration-150">
-            <div className="flex-shrink-0 flex items-center justify-center h-10 w-10 rounded-md bg-primary-500 text-white sm:h-12 sm:w-12 ">
+          <a href={item.comingSoon ? null : item.href} key={index} className="-m-3 p-3 flex items-start space-x-4 rounded-lg hover:bg-gray-50 transition ease-in-out duration-150">
+            <div className={`flex-shrink-0 flex items-center justify-center h-10 w-10 rounded-md bg-primary-500 text-white sm:h-12 sm:w-12 ${item.comingSoon && 'opacity-50'}`}>
               <item.icon className="h-6 w-6" />
             </div>
             <div className="space-y-1">
               <p className="text-base leading-6 font-medium text-gray-900">
-                { item.title } { item.comingSoon && <Label text="Coming soon" /> }
+                <span className={item.comingSoon && 'opacity-50'}>{ item.title }</span> { item.comingSoon && <Label text="Coming soon" /> }
               </p>
-              <p className="text-sm leading-5 text-gray-500">
+              <p className={`text-sm leading-5 text-gray-500 ${item.comingSoon && 'opacity-50'}`}>
                 { item.description }
               </p>
             </div>
