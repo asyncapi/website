8158d3099d187a2b072e001d0f6e31d10f8bde0c
Maciej Urbańczyk <urbanczyk.maciej.95@gmail.com>
2020-10-12 20:21:44 +0200

    feat: add icons and webmanifest

--- components/Head.js
+++ components/Head.js
@@ -21,6 +21,18 @@ export default function HeadComponent({
       <meta httpEquiv="x-ua-compatible" content="ie=edge" />
       <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
       <meta name="description" content={description} />
+
+      {/* Icons and webmanifest */}
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="194x194" href="/favicon-194x194.png" />
+      <link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-192x192.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+      <meta name="msapplication-TileColor" content="#5bbbd5" />
+      <meta name="msapplication-TileImage" content="/mstile-150x150.png" />
+      <meta name="theme-color" content="#2b5797" />
       
       {/* Google / Search Engine Tags */}
       <meta itemProp="name" content={title} />
