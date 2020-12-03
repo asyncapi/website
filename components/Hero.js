80829f823f77763a89b3988c8779841e081e7854
Fran Mendez <fmvilas@gmail.com>
2020-12-03 12:33:21 +0100

    Add hero banner

--- components/Hero.js
+++ components/Hero.js
@@ -2,11 +2,22 @@ import Button from './buttons/Button'
 import ArrowRight from './icons/ArrowRight'
 import OpenInPlaygroundButton from './buttons/OpenInPlaygroundButton'
 import DemoAnimation from './DemoAnimation'
+import YoutubeButton from './buttons/YoutubeButton'
 
 export default function Hero ({ className = '' }) {
   return (
     <div className={`px-2 mt-12 ${className}`}>
       <div className="text-center">
+
+        <div className="bg-gray-50 mb-12 py-28 border border-gray-200">
+          <h1 className="text-6xl font-extrabold">
+            <span className="countdown-text-gradient">5 days...</span>
+          </h1>
+          <p className="mb-2 text-xl text-gray-500">To change things forever. To take AsyncAPI to a whole new level.</p>
+          <p className="text-xl font-bold text-gray-700">Are you ready for the big announcement?</p>
+          <YoutubeButton href="/" text="Watch it live on Youtube" className="mt-4 inline-block" />
+        </div>
+
         <h1 className="text-primary-800 text-3xl font-bold md:text-4xl mb-4">
           Building the future of event-driven architecture.
         </h1>
