7848c8840812f6f2fe9beae36eca5fbaa849a94b
Fran Mendez <fmvilas@gmail.com>
2020-12-03 13:18:29 +0100

    Make announcement component smart

---
+++ components/campaigns/AnnoucementHero.js
@@ -0,0 +1,19 @@
+import AnnouncementRemainingDays from './AnnouncementRamainingDays'
+import YoutubeButton from '../buttons/YoutubeButton'
+
+export default function AnnouncementHero() {
+  const announcementDateTime = '2020-12-08T16:00:00Z'
+
+  if (new Date(announcementDateTime) < new Date()) return null
+
+  return (
+    <div className="bg-gray-50 mb-12 py-28 border border-gray-200">
+      <h1 className="text-6xl">
+        <AnnouncementRemainingDays dateTime={announcementDateTime} />
+      </h1>
+      <p className="mb-2 text-xl text-gray-500">To change things forever. To take AsyncAPI to a whole new level.</p>
+      <p className="text-xl font-bold text-gray-700">Are you ready for the big announcement?</p>
+      <YoutubeButton href="/" text="Watch it live on Youtube" className="mt-4 inline-block" />
+    </div>
+  )
+}
