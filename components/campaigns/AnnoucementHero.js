c96e7e6a81b9c4a907525d77ab7e041021465bf7
Fran Méndez <fmvilas@gmail.com>
2020-12-08 15:59:06 +0100

    chore(blog): add announcement blog post and change banner (#161)
    * Add announcement blog post and change banner
    
    * Point form to root
    
    * Use form 1
    
    * Remove action
    
    * Use hidden field
    
    * Revert back the date and link Postman to its website
    
    * Change announcement date time to 3pm UTC

--- components/campaigns/AnnoucementHero.js
+++ components/campaigns/AnnoucementHero.js
@@ -1,10 +1,35 @@
+import { useRouter } from 'next/router'
 import AnnouncementRemainingDays from './AnnouncementRamainingDays'
 import YoutubeButton from '../buttons/YoutubeButton'
+import Button from '../buttons/Button'
 
 export default function AnnouncementHero({ className = '', small = false }) {
-  const announcementDateTime = '2020-12-08T16:00:00Z'
+  const { pathname } = useRouter()
+  const announcementDateTime = '2020-12-08T15:00:00Z'
+  const announcementDeadlineDateTime = '2020-12-15T16:00:00Z'
 
-  if (new Date(announcementDateTime) < new Date()) return null
+  if (pathname === '/blog/asyncapi-partners-with-postman') return null
+  if (new Date(announcementDeadlineDateTime) < new Date()) return null
+
+  if (new Date(announcementDateTime) < new Date()) {
+    return (
+      <div className={`mb-12 px-4 md:flex md:justify-center ${className}`}>
+        <img className="w-full md:w-auto md:h-64" src="/img/posts/asyncapi-partners-with-postman.png" />
+        <div className="md:inline-flex md:flex-col md:justify-center md:ml-4">
+          <div className="my-2 text-xl text-gray-500">
+            <p className="font-bold text-gray-700">AsyncAPI partners with Postman</p>
+            <p>To take AsyncAPI to a whole new level.</p>
+          </div>
+          <div className="mt-8">
+            <Button href="/blog/asyncapi-partners-with-postman" text="Read the announcement" />
+          </div>
+          <div>
+            <YoutubeButton href="https://youtu.be/Q5ZdPUzYbmc" text="Watch it on Youtube" className="mt-4 inline-block" />
+          </div>
+        </div>
+      </div>
+    )
+  }
 
   return (
     <div className={`mb-12 px-4 ${small ? 'sm:py-8' : 'sm:py-28'} sm:bg-gray-50 sm:border sm:border-gray-200 ${className}`}>
--- pages/jobs/index.js
+++ pages/jobs/index.js
@@ -22,6 +22,7 @@ export default function JobsIndexPage() {
       <Container>
         <NavBar />
       </Container>
+      <AnnouncementHero className="text-center m-4" small={true} />
       <div className="relative pt-8 pb-20 px-4 sm:px-6 lg:pt-12 lg:pb-28 lg:px-8">
         <div className="absolute inset-0">
           <div className="bg-white h-1/3 sm:h-2/3"></div>
@@ -35,7 +36,6 @@ export default function JobsIndexPage() {
               Want to work on a great open-source project with a lovely team and a vibrant community? Come work with us!
             </p>
           </div>
-          <AnnouncementHero className="text-center m-4" small={true} />
           <div className="mt-8 flex flex-col items-stretch sm:rounded-md">
             <ul className="bg-white shadow overflow-hidden divide-y divide-gray-200 sm:w-2/3 sm:self-center">
             {
