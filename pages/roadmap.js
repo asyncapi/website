772824fb377554047b38e33542366f63e1c20a32
Fran Mendez <fmvilas@gmail.com>
2021-03-02 14:46:53 +0100

    Add automatic Roadmap build (wip)

--- pages/roadmap.js
+++ pages/roadmap.js
@@ -1,4 +1,5 @@
 import GenericLayout from '../components/layout/GenericLayout'
+import roadmapData from '../roadmap.json'
 import IconDocuments from '../components/icons/Documents'
 import IconCode from '../components/icons/Code'
 import IconPowerPlug from '../components/icons/PowerPlug'
@@ -66,6 +67,82 @@ export default function RoadmapPage() {
                     We need people to contribute code, ideas, docs, articles, presentations, and more. The sponsors base should also grow along with the community size and the donated money should serve to give back to the community.
                   </p>
                 </div>
+              </div>
+            </div>
+
+            <h3 className="text-center mt-10 text-3xl leading-8 font-normal tracking-tight text-gray-900 sm:text-4xl sm:leading-10">
+              Roadmap
+            </h3>
+            <p className="mt-4 max-w-3xl mx-auto text-center text-xl leading-7 text-gray-500">
+              The key results we want to achieve
+            </p>
+
+            <div className="mt-10 lg:grid lg:grid-cols-3 lg:gap-10">
+              <div>
+                <div className="p-4">
+                  <h3 className="text-md font-semibold uppercase text-center">Now</h3>
+                  <p className="text-center text-sm text-gray-600 mt-2">Our top priority. We're probably working on it right now or starting pretty soon.</p>
+                </div>
+                <ul className="mt-3">
+                  {
+                    roadmapData.keyResults.now.map((kr, index) => (
+                      <li key={index} className="mt-2 flex shadow-sm rounded-md">
+                        <a href={kr.html_url} target="_blank" className="flex-shrink-0 flex items-center justify-center w-4 bg-green-300 text-sm font-medium rounded-l-md">
+                          
+                        </a>
+                        <div className="flex-1 flex items-center justify-between border-t border-r border-b border-gray-200 bg-white rounded-r-md">
+                          <div className="flex-1 px-4 py-2 text-sm">
+                            <a href={kr.html_url} target="_blank" className="block text-gray-900 font-medium hover:text-gray-600">{kr.title}</a>
+                          </div>
+                        </div>
+                      </li>
+                    ))
+                  }
+                </ul>
+              </div>
+              <div>
+                <div className="p-4">
+                  <h3 className="text-md font-semibold uppercase text-center">Later</h3>
+                  <p className="text-center text-sm text-gray-600 mt-2">Our next priority. We'll work on this soon if everything goes as planned.</p>
+                </div>
+                <ul className="mt-3">
+                  {
+                    roadmapData.keyResults.later.map((kr, index) => (
+                      <li key={index} className="mt-2 flex shadow-sm rounded-md">
+                        <a href={kr.html_url} target="_blank" className="flex-shrink-0 flex items-center justify-center w-4 bg-yellow-200 text-sm font-medium rounded-l-md">
+                          
+                        </a>
+                        <div className="flex-1 flex items-center justify-between border-t border-r border-b border-gray-200 bg-white rounded-r-md">
+                          <div className="flex-1 px-4 py-2 text-sm">
+                            <a href={kr.html_url} target="_blank" className="block text-gray-900 font-medium hover:text-gray-600">{kr.title}</a>
+                          </div>
+                        </div>
+                      </li>
+                    ))
+                  }
+                </ul>
+              </div>
+              <div>
+                <div className="p-4">
+                  <h3 className="text-md font-semibold uppercase text-center">Future</h3>
+                  <p className="text-center text-sm text-gray-600 mt-2">Not a priority. We're considering working on this but it's too early to know when.</p>
+                </div>
+                <ul className="mt-3">
+                  {
+                    roadmapData.keyResults.future.map((kr, index) => (
+                      <li key={index} className="mt-2 flex shadow-sm rounded-md">
+                        <a href={kr.html_url} target="_blank" className="flex-shrink-0 flex items-center justify-center w-4 bg-orange-200 text-sm font-medium rounded-l-md">
+                          
+                        </a>
+                        <div className="flex-1 flex items-center justify-between border-t border-r border-b border-gray-200 bg-white rounded-r-md">
+                          <div className="flex-1 px-4 py-2 text-sm">
+                            <a href={kr.html_url} target="_blank" className="block text-gray-900 font-medium hover:text-gray-600">{kr.title}</a>
+                          </div>
+                        </div>
+                      </li>
+                    ))
+                  }
+                </ul>
               </div>
             </div>
           </div>
---
+++ scripts/build-roadmap.js
@@ -0,0 +1,36 @@
+const { writeFileSync } = require('fs')
+const { resolve } = require('path')
+const fetch = require('node-fetch')
+require('dotenv').config({
+  path: resolve(process.cwd(), '.env.local')
+})
+
+async function start() {
+  try {
+    res = await fetch('https://api.zenhub.com/v5/workspaces/5f6492205269c584ae1b576f/issues?epics=1&estimates=1&pipelines=1&repo_ids=296590488', {
+      method: 'get',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Authentication-Token': process.env.ZENHUB_TOKEN,
+      },
+    })
+    const issues = await res.json()
+    const keyResultsNow = issues.filter(iss => iss.labels.length && iss.labels.find(label => label.name === 'Key Result') && iss.pipeline.name === 'Key Results (Now)')
+    const keyResultsLater = issues.filter(iss => iss.labels.length && iss.labels.find(label => label.name === 'Key Result') && iss.pipeline.name === 'Key Results (Later)')
+    const keyResultsFuture = issues.filter(iss => iss.labels.length && iss.labels.find(label => label.name === 'Key Result') && iss.pipeline.name === 'Key Results (Future)')
+    
+    const result = {
+      keyResults: {
+        now: keyResultsNow,
+        later: keyResultsLater,
+        future: keyResultsFuture,
+      },
+    }
+
+    writeFileSync(resolve(__dirname, '..', 'roadmap.json'), JSON.stringify(result, null, '  '))
+  } catch (e) {
+    console.error(e)
+  }
+}
+
+start()
