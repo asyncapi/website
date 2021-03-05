d59df36ee0e2381fe6ee9dbf1c801a96db2851fa
Fran Mendez <fmvilas@gmail.com>
2021-03-05 19:42:50 +0100

    Fix issue link

--- components/roadmap/RoadmapItem.js
+++ components/roadmap/RoadmapItem.js
@@ -12,13 +12,13 @@ export default function RoadmapItem({ item, colorClass, showConnector = true }) 
           </div>
         )}
         <div className="flex flex-1 shadow-sm rounded-md">
-          <a href={item.url} target="_blank" className={`flex-shrink-0 flex items-center justify-center w-4 text-sm font-medium rounded-l-md ${colorClass}`}></a>
+          <a href={item.html_url} target="_blank" className={`flex-shrink-0 flex items-center justify-center w-4 text-sm font-medium rounded-l-md ${colorClass}`}></a>
           <div className="flex-1 flex items-center justify-between border-t border-r border-b border-gray-200 bg-white rounded-r-md">
             <div className="flex flex-1 px-4 py-2 text-sm">
               {
                 item.state === 'closed' && (<div className="text-xs rounded-md h-6 px-1.5 py-0.5 mr-2 uppercase font-semibold bg-gray-100 text-pink-600">Done</div>)
               }
-              <a href={item.url} target="_blank" className="block text-gray-900 font-medium hover:text-gray-600">{item.title}</a>
+              <a href={item.html_url} target="_blank" className="block text-gray-900 font-medium hover:text-gray-600">{item.title}</a>
             </div>
           </div>
         </div>
