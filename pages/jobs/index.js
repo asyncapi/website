59b530ca4761d4dbed6ca998283e48bf891977da
AceTheCreator <devaze007@gmail.com>
2021-04-07 11:31:49 +0100

    feat: :sparkles: feat: Enabling other to post jobs on AsynApi website
    This pull request enables others to share their job offers on the AsyncApi website job page
    
    Resolves: #212

--- pages/jobs/index.js
+++ pages/jobs/index.js
@@ -10,8 +10,7 @@ import AnnouncementHero from "../../components/campaigns/AnnoucementHero"
 import emptyChecker from "../../utils/emptyChecker";
 import Empty from "../../components/illustrations/empty";
 
-// eslint-disable-next-line react/prop-types
-export default function JobsIndexPage({template}) {
+export default function JobsIndexPage() {
   const { navItems } = useContext(JobsContext)
   navItems.map((job) => {
     if(new Date() > new Date(job.closingOn)){
@@ -27,6 +26,7 @@ export default function JobsIndexPage({template}) {
   const onFilter = (data) => {
     setPosts(data);
   }
+  const jobPostUrl = '---%0Atitle%3A%20%27Job%20Title%27%0Adate%3A%20DD%2FMM%2FYYYY%20(current%20date)%0Acategory%3A%20job%20category%0AclosingOn%3A%20MM%2FDD%2FYYYY%0A---%0A%23%23%20About%20the%20team%0ALet%20potential%20employees%20knows%20what%20it%27ll%20feel%20like%20to%20joining%20your%20team%20in%20this%20section%0A%0A%23%23%20TL%3BDR%0AEnter%20list%20of%20perks%20for%20potential%20employees%20in%20this%20section.%0AE.g.%0A*%20%3Amuscle%3A%20Growing%20team%0A*%20%3Ahouse_with_garden%3A%20Fully%20remote%20job%0A*%20%3Amoney_mouth_face%3A%20Great%20salary%20and%20compensation%20package%0A*%20%3Amountain_snow%3A%20Unlimited%20paid%20time%20off%0A%0A%23%23%20About%20the%20job%0ALet%20the%20potential%20employee%20knows%20about%20the%20job%20in%20this%20section%0A%0A%23%23%20About%20youY%0AThe%20Potential%20employees%20would%20love%20to%20know%20more%20about%20you.%20Tell%20them%20more%20about%20you%20in%20this%20section%0A%0A%23%23%20Pay%20and%20benefits%0ALet%20the%20potential%20employees%20knows%20what%20comes%20with%20joining%20your%20team%20in%20this%20section.%0A%0AJoin%20us!%0A'
 
   return (
     <div>
@@ -48,7 +48,7 @@ export default function JobsIndexPage({template}) {
               Want to work on a great open-source project with a lovely team and a vibrant community? Come work with us!
             </p>
             <p className="max-w-2xl mx-auto text-md leading-7 text-gray-400">
-            <span role="img">💡</span> Want to post a job? All job posting must be AsyncAPI-related jobs. <a className="ml-1 text-primary-500 hover:text-primary-400" href={`https://github.com/asyncapi/website/new/master/pages/jobs?value=${template}`} target="_blank" rel="noreferrer">Get started now!</a>
+            <span role="img">💡</span> Want to post a job? All job posting must be AsyncAPI-related jobs. <a className="ml-1 text-primary-500 hover:text-primary-400" href={`https://github.com/asyncapi/website/new/master/pages/jobs?value=${jobPostUrl}`} target="_blank" rel="noreferrer">Get started now!</a>
             </p>
             <p className="max-w-2xl mx-auto text-md leading-7 text-gray-400">Do you want to discuss about your job posting?
             <a className="ml-1 text-primary-500 hover:text-primary-400" href="https://github.com/asyncapi/website/issues/new" target="_blank" rel="noreferrer">Get started here</a>
@@ -77,22 +77,4 @@ export default function JobsIndexPage({template}) {
       <Footer />
     </div>
   )
-}
-
-// eslint-disable-next-line no-unused-vars
-export async function getStaticProps(context) {
-  // eslint-disable-next-line no-undef
-  const fs = require('fs'); 
-  // eslint-disable-next-line no-undef
-  const process = require("process");
-  const template =`${process.cwd()}/template/encoded_job_template.txt`;
-  const rawContent = fs.readFileSync(template, {
-    encoding: "utf-8",
-  });
-  return {
-    props: {
-      template: rawContent
-    }
-  }
-
 }
--- utils/emptyChecker.js
+++ utils/emptyChecker.js
@@ -1,9 +1,8 @@
-// Empty object checker
 const emptyChecker = (obj) => {
     let isDataEmpty = true;
-    // eslint-disable-next-line no-restricted-syntax
+
     for (const key in obj) {
-      // eslint-disable-next-line no-prototype-builtins
+
       if (obj.hasOwnProperty(key)) isDataEmpty = false;
     }
     return isDataEmpty;
