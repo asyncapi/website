142b656cc37872c0fba85dc06cf9e6440d0aa97a
Ace <40604284+AceTheCreator@users.noreply.github.com>
2021-04-08 21:06:02 +0100

    feat: enabling others to share their job offers (#224)
    * added the job posting functionality
    
    * adding filter functionality
    
    * .
    
    * feat: :lipstick: Enabling others to share their job offers
    
    * feat: :lipstick: Enabling others to share their job offers
    
    * feat: :sparkles: Enabling others to share their job offers
    
    feat: Enabling others to share their job offers
    
    * feat: Enabling others to post jobs on AsyncApi
    
    * feat: Enabling others to post jobs on AsyncApi website
    
    * feat: :sparkles: feat: Enabling other to post jobs on AsynApi website
    
    This pull request enables others to share their job offers on the AsyncApi website job page
    
    Resolves: #212
    
    * Delete yarn.lock
    
    * feat: :sparkles: feat: Enabling others to share their job offers
    
    This pull request enables others to share their job offers on the AsyncApi website job page
    
    Resolves: #212
    
    * feat: Enabling others to share their job offers
    
    This pull request enables others to share their job offers on the AsyncApi website job page
    
    Resolves: #212
    
    * feat: :sparkles: add job posting functionality
    
    * feat: :zap: add job posting functionality
    
    This pull request enables others to share their job offers on the AsyncApi website job page
    
    Resolves: #212
    
    * changed port back to default
    
    * updated PR
    
    * Update pages/jobs/index.js
    
    Co-authored-by: Lukasz Gornicki <lpgornicki@gmail.com>
    
    * Update pages/jobs/index.js
    
    Co-authored-by: Fran Méndez <fmvilas@gmail.com>
    
    * Update pages/jobs/index.js
    
    Co-authored-by: Fran Méndez <fmvilas@gmail.com>
    
    * Update templates/job_template.md
    
    Co-authored-by: Fran Méndez <fmvilas@gmail.com>
    
    * Update templates/job_template.md
    
    Co-authored-by: Fran Méndez <fmvilas@gmail.com>
    
    * Update components/navigation/Filter.js
    
    Co-authored-by: Fran Méndez <fmvilas@gmail.com>
    
    * Update styles.css
    
    * Update components/navigation/Filter.js
    
    Co-authored-by: Fran Méndez <fmvilas@gmail.com>
    
    * used the existing select component and got rid of the custom one
    
    * Update components/navigation/Filter.js
    
    Co-authored-by: Fran Méndez <fmvilas@gmail.com>
    
    * Update components/navigation/Filter.js
    
    Co-authored-by: Fran Méndez <fmvilas@gmail.com>
    
    Co-authored-by: Lukasz Gornicki <lpgornicki@gmail.com>
    Co-authored-by: Fran Méndez <fmvilas@gmail.com>

---
+++ components/navigation/Filter.js
@@ -0,0 +1,31 @@
+import {useState} from 'react';
+import PropTypes from 'prop-types';
+import Select from '../form/Select';
+
+export default function Filter({data, onFilter}) {
+    const [state, setState] = useState('Filter');
+    const categories = data.map(data => {
+        return {
+            value: data.category,
+            text : data.category
+        }
+    });
+    const onFilterApply = (value) => {
+        setState(value);
+        const jobs = data.filter((e) => e.category === value);
+        onFilter(jobs);
+    }
+    return (
+        <div class="mb-4">
+            <Select options={categories} 
+            onChange={onFilterApply} selected={state}
+            className='w-56 max-w-sm'
+             />
+        </div>
+    )
+}
+
+Filter.propTypes = {
+    data: PropTypes.array.isRequired,
+    onFilter: PropTypes.func.isRequired,
+}
