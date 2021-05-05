3719013c896a834ccf2f0cc78fa310782a8a5c07
Sergio Moya <1083296+smoya@users.noreply.github.com>
2021-05-05 18:00:49 +0200

    feat: add blog authors links on avatars and names

--- components/AuthorAvatars.js
+++ components/AuthorAvatars.js
@@ -1,12 +1,14 @@
 export default function AuthorAvatars({ authors = [] }) {
   return (
-    authors.map((author, index) => (
-      <img
+    authors.map((author, index) => {
+      let avatar = <img
         key={index}
         title={author.name}
         className={`${index > 0 ? `absolute left-${index * 7} top-0` : `relative mr-${(authors.length - 1) * 7}`} z-${(authors.length - 1 - index) * 10} h-10 w-10 border-2 border-white rounded-full object-cover hover:z-50`}
         src={author.photo}
       />
-    ))
+
+      return author.link ? <a alt={author.name} href={author.link}>{avatar}</a> : {avatar}
+    })
   )
 }
