9106c2a52e3a8df904d5d9bbe7481ac7d948e089
Aayush Kumar Sahu <aayushmau5@gmail.com>
2021-06-01 17:30:04 +0530

    Add redirect markers and remove `latest.md` file

--- components/layout/DocsLayout.js
+++ components/layout/DocsLayout.js
@@ -16,10 +16,7 @@ import AnnouncementHero from '../campaigns/AnnoucementHero'
 
 function generateEditLink(post) {
   if (post.slug.includes('/specifications/')) {
-    if(post.slug.endsWith('/latest')) {
-      return <a target="_blank" rel="noopener noreferrer" href={`https://github.com/asyncapi/spec/blob/master/spec/asyncapi.md`} className="ml-1 underline">Edit this page on Github</a>
-    }
-    return <a target="_blank" rel="noopener noreferrer" href={`https://github.com/asyncapi/spec/blob/${basename(post.slug)}/spec/asyncapi.md`} className="ml-1 underline">Edit this page on Github</a>
+    return <a target="_blank" rel="noopener noreferrer" href={`https://github.com/asyncapi/spec/blob/master/spec/asyncapi.md`} className="ml-1 underline">Edit this page on Github</a>
   } 
   return <a target="_blank" rel="noopener noreferrer" href={`https://github.com/asyncapi/website/blob/master/pages${post.isIndex ? post.slug + '/index' : post.slug}.md`} className="ml-1 underline">Edit this page on Github</a>
 }
