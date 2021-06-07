2d2bece71f5d9a58ee4d2aef0420472a59b2d56e
Aayush Kumar Sahu <aayushmau5@gmail.com>
2021-06-07 18:16:44 +0530

    Add else statement

--- scripts/build-post-list.js
+++ scripts/build-post-list.js
@@ -58,9 +58,10 @@ function walkDirectories(directories, result, sectionWeight = 0, sectionTitle) {
         details.isIndex = fileName.endsWith('/index.md')
         details.slug = details.isIndex ? sectionSlug : slug.replace(/\.md$/, '')
         if(details.slug.includes('/specifications/') && !details.title) {
-          details.title = capitalize(basename(data.slug))
           if(basename(data.slug).startsWith('v')) {
             details.title = capitalize(basename(data.slug).slice(1))
+          } else {
+            details.title = capitalize(basename(data.slug))
           }
         }
         result.push(details)
