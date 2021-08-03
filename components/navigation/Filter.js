711655136e9099e15743818d0861662f6ce05642
Damilola Randolph <damilolarandolph@gmail.com>
2021-08-03 13:50:26 +0000

    Fix duplicate filter dropdown items.

--- components/navigation/Filter.js
+++ components/navigation/Filter.js
@@ -1,31 +1,36 @@
-import {useState} from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import Select from '../form/Select';
 
-export default function Filter({data, onFilter}) {
-    const [state, setState] = useState('Filter');
-    const categories = data.map(data => {
-        return {
-            value: data.category,
-            text : data.category
-        }
-    });
-    const onFilterApply = (value) => {
-        setState(value);
-        const jobs = data.filter((e) => e.category === value);
-        onFilter(jobs);
+export default function Filter({ data, onFilter }) {
+  const [state, setState] = useState('Filter');
+  let foundCategories = new Set();
+  let categories = [];
+
+  data.forEach(data => {
+    if (foundCategories.has(data.category)) {
+      return;
     }
-    return (
-        <div className="mb-4">
-            <Select options={categories} 
-            onChange={onFilterApply} selected={state}
-            className='w-56 max-w-sm'
-             />
-        </div>
-    )
+    foundCategories.add(data.category);
+    categories.push({ value: data.category, text: data.category });
+  })
+
+  const onFilterApply = (value) => {
+    setState(value);
+    const jobs = data.filter((e) => e.category === value);
+    onFilter(jobs);
+  }
+  return (
+    <div className="mb-4">
+      <Select options={categories}
+        onChange={onFilterApply} selected={state}
+        className='w-56 max-w-sm'
+      />
+    </div>
+  )
 }
 
 Filter.propTypes = {
-    data: PropTypes.array.isRequired,
-    onFilter: PropTypes.func.isRequired,
+  data: PropTypes.array.isRequired,
+  onFilter: PropTypes.func.isRequired,
 }
