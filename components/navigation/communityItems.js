c6df4a6646eb5d17e8003663a36fc27c1bfd1d4f
David Boyne <davidboyne@Davids-MBP.lan>
2021-10-08 09:22:04 +0100

    adding contributing section to the navigation

--- components/navigation/communityItems.js
+++ components/navigation/communityItems.js
@@ -2,4 +2,5 @@ export default [
   { text: 'Tools & Services', href: '/docs/community/tooling', description: 'Explore the tools and services our awesome community has created.' },
   { text: 'Github Organization', href: 'https://github.com/asyncapi', target: '_blank', description: 'Want to sneak in the code? Everything we do is open-sourced in our Github organization.' },
   { text: 'Slack Workspace', href: 'https://asyncapi.com/slack-invite', target: '_blank', description: `Need help? Want to share something? Join our Slack workspace. We're nice people :)` },
+  { text: 'Contributing', href: 'https://github.com/asyncapi?type=source#-contribute-to-asyncapi', target: '_blank', description: `We are always welcoming and looking for contributions. If you are interested check out our contribution guide.` },
 ]
