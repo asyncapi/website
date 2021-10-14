59fb982b281eb85375d43268bf54dafdbaea917f
David Boyne <davidboyne123@hotmail.co.uk>
2021-10-14 13:55:59 +0100

    Update pages/index.js
    Co-authored-by: Alejandra Quetzalli  <alejandra.quetzalli@postman.com>

--- pages/index.js
+++ pages/index.js
@@ -177,7 +177,7 @@ function HomePage() {
           }
         </div>
         <div className="my-10 text-center">
-          <Button bgClassName="bg-none border border-gray-200 text-gray-800 hover:text-gray-700 shadow-none" href="/blog" text="Read more" />
+          <Button bgClassName="bg-none border border-gray-200 text-gray-800 hover:text-gray-700 shadow-none" href="/blog" text="View more blog posts" />
         </div>
       </Container>
       <Footer />
