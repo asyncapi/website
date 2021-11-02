9ce7e82cc6fa40c6e7d26e5f46916ce52550b2ca
David Boyne <boyneyy123@gmail.com>
2021-11-02 16:24:08 +0000

    feat: adding why async api section and change to order of sections on homepage (#397) (#406)
    Co-authored-by: Lukasz Gornicki <lpgornicki@gmail.com>
    Co-authored-by: Alejandra Quetzalli  <alejandra.quetzalli@postman.com>

--- components/Hero.js
+++ components/Hero.js
@@ -1,28 +1,40 @@
 import Button from './buttons/Button'
 import ArrowRight from './icons/ArrowRight'
 import OpenInStudioButton from './buttons/OpenInStudioButton'
+import Features from '../components/features'
 import DemoAnimation from './DemoAnimation'
-import AnnouncementHero from './campaigns/AnnoucementHero'
+import AnnouncementHero from '../components/campaigns/AnnoucementHero'
 
-export default function Hero ({ className = '' }) {
+export default function Hero({ className = '' }) {
   return (
-    <div className={`px-2 mt-6 ${className}`}>
+    <div className={`px-2 mt-12 ${className}`}>
       <div className="text-center">
         <AnnouncementHero />
         <h1 className="text-primary-800 text-2xl font-bold md:text-6xl mb-4 leading-snug">
           Building the future of {` `}
-          <span className="text-primary-400 block"> event-driven architecture.</span>
+          <span className="text-primary-400 block md:-mt-4">
+            {" "}
+            Event-Driven Architectures (EDA).
+          </span>
         </h1>
         <h2 className="text-gray-500 text-lg font-normal mb-16 max-w-3xl mx-auto">
-          Open Source tools to easily build and maintain your event-driven architecture.
-          All powered by the AsyncAPI specification, the <strong>industry standard</strong> for defining asynchronous APIs.
+          Open-Source tools to easily build and maintain your event-driven
+          architecture. All powered by the AsyncAPI specification, the {" "}
+          <strong>industry standard</strong> for defining asynchronous APIs.
         </h2>
         <Button className="block md:inline-block" text="Read the docs" href="/docs/getting-started" icon={<ArrowRight className="-mb-1 h-5 w-5" />} />
         <OpenInStudioButton text='Open Studio' />
+        <p className="mt-4 text-xs text-gray-500">
+          Proud to be part of the {" "}
+          <a className="underline" href="https://www.linuxfoundation.org/">
+            Linux Foundation
+          </a>
+        </p>
       </div>
       <div className="mt-8 md:mt-16">
         <DemoAnimation />
       </div>
+      <Features />
     </div>
-  )
+  );
 }
