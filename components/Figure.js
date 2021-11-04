c870a0bba2bc75473cbfe147097d4e1b25f1c427
Dimitrios Dedoussis <dimitrios@dedouss.is>
2021-11-04 07:50:05 +0000

    chore(blog): republish the Socket.IO series of blog posts, part 2 (#437)
    Co-authored-by: Alejandra Quetzalli  <alejandra.quetzalli@postman.com>

--- components/Figure.js
+++ components/Figure.js
@@ -3,7 +3,7 @@ import Caption from './Caption'
 export default function Figure ({ src, caption, widthClass, className }) {
   return (
     <figure className={className}>
-      <img className={`${widthClass || 'w-full'} inline-block`} src={src} />
+      <img className={`${widthClass || 'w-full'} inline-block`} src={src} alt={caption} />
       { caption && (<Caption>{caption}</Caption>) }
     </figure>
   )
