51552c09d1cde981e4ac7d96047f3dd7e48aea22
Matatjahu <urbanczyk.maciej.95@gmail.com>
2021-11-18 12:04:01 +0100

    feat: update link for conference stream (3 day)

--- components/campaigns/AnnoucementHero.js
+++ components/campaigns/AnnoucementHero.js
@@ -9,7 +9,7 @@ export default function AnnouncementHero({ className = '', small = false }) {
 
   return (
     <div className={`bg-gray-50 border border-gray-200 py-6  ${className} ${small ? 'mb-0' : 'mb-12'}`}>
-      <h2 className="text-xl md:text-3xl font-bold countdown-text-gradient">AsyncAPI Conference Day 2 is running!</h2>
+      <h2 className="text-xl md:text-3xl font-bold countdown-text-gradient">AsyncAPI Conference Day 3 is running!</h2>
       <div className='px-4 mt-4 mx-auto max-w-7xl'>
         <YouTube
           id="3EeMHhbwyOQ"
