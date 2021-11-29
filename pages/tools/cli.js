5bd4284c53976a3890c371bb98c53d1f40648eed
David Boyne <davidboyne@Davids-MBP.lan>
2021-11-29 09:52:54 +0000

    fixing cli layout issues on mobile and tablet

--- pages/tools/cli.js
+++ pages/tools/cli.js
@@ -1,38 +1,46 @@
-import GenericLayout from "../../components/layout/GenericLayout";
-import GithubButton from "../../components/buttons/GithubButton";
+import GenericLayout from '../../components/layout/GenericLayout';
+import GithubButton from '../../components/buttons/GithubButton';
 
 import {
   DocumentAddIcon,
   BadgeCheckIcon,
   GlobeIcon,
   CodeIcon,
-} from "@heroicons/react/outline";
+} from '@heroicons/react/outline';
 
-import CodeBlock from "../../components/editor/CodeBlock";
+import CodeBlock from '../../components/editor/CodeBlock';
 
 const features = [
   {
-    name: "New files",
+    name: 'New files',
     description: () =>
-      "Use the CLI tool to quickly create new AsyncAPI files. Select from a range of templates (MQTT, WebSockets, Kafka, and more.)",
+      'Use the CLI tool to quickly create new AsyncAPI files. Select from a range of templates (MQTT, WebSockets, Kafka, and more.)',
     icon: DocumentAddIcon,
   },
   {
-    name: "Validate",
+    name: 'Validate',
     description: () =>
-      "Validate your AsyncAPI documents with the CLI. Quickly get feedback to verify your AsyncAPI document is within the correct format.",
+      'Validate your AsyncAPI documents with the CLI. Quickly get feedback to verify your AsyncAPI document is within the correct format.',
     icon: BadgeCheckIcon,
   },
   {
-    name: "Open Studio",
+    name: 'Open Studio',
     // eslint-disable-next-line react/display-name
-    description: () => <>Got an AsyncAPI file locally? Run <code className=" px-1 py-0.5 bg-primary-700 text-white rounded font-mono text-sm">asyncapi start studio</code> to open our studio in seconds.</>,
+    description: () => (
+      <>
+        Got an AsyncAPI file locally? Run{' '}
+        <code className=" px-1 py-0.5 bg-primary-700 text-white rounded font-mono text-sm">
+          asyncapi start studio
+        </code>{' '}
+        to open our studio in seconds.
+      </>
+    ),
     icon: CodeIcon,
   },
   {
-    name: "Open Source",
+    name: 'Open Source',
     description: () =>
-      "All our tools are open source, feel free to contribute new commands or help evolve our existing ones.",
+      'All our tools are open source, feel free to contribute new commands or help evolve our existing ones.',
     icon: GlobeIcon,
   },
 ];
@@ -42,7 +50,7 @@ export default function CliPage() {
     return (
       <div className="mt-8">
         <GithubButton
-          className="w-full sm:w-auto"
+          className="block mt-2 md:mt-0 md:inline-block w-full sm:w-auto"
           href="https://www.github.com/asyncapi/cli"
         />
       </div>
@@ -50,23 +58,21 @@ export default function CliPage() {
   }
 
   const description =
-    "Create, validate, and explore your AsyncAPI files with our CLI tool.";
-  const image = "/img/social/generator.png";
+    'Create, validate, and explore your AsyncAPI files with our CLI tool.';
+  const image = '/img/social/generator.png';
 
   return (
-    <GenericLayout
-      title="CLI"
-      description={description}
-      image={image}
-      wide
-    >
+    <GenericLayout title="CLI" description={description} image={image} wide>
       <div className="py-12 overflow-hidden">
         <div className="relative max-w-xl mx-auto px-4 sm:px-6 lg:px-8 lg:max-w-screen-xl">
           <div className="relative">
             <h3 className="text-center text-3xl leading-8 font-normal tracking-tight text-gray-900 sm:text-4xl sm:leading-10">
-              Interact with AsyncAPI from the comfort of your CLI.
+              <span className="hidden md:block">
+                Interact with AsyncAPI from the comfort of your CLI
+              </span>
+              <span className="md:hidden">AsyncAPI CLI</span>
             </h3>
-            <p className="mt-4 max-w-3xl mx-auto text-center text-xl leading-7 text-gray-500">
+            <p className="mt-4 max-w-3xl mx-auto text-center text-lg md:text-xl leading-7 text-gray-500">
               {description}
             </p>
           </div>
@@ -94,7 +100,7 @@ export default function CliPage() {
                   className="shadow-lg"
                   codeBlocks={[
                     {
-                      language: "npm",
+                      language: 'npm',
                       code: `npm install -g @asyncapi/cli`,
                     },
                   ]}
@@ -114,7 +120,7 @@ export default function CliPage() {
                       className="shadow-lg"
                       codeBlocks={[
                         {
-                          language: "npm",
+                          language: 'npm',
                           code: `asyncapi new`,
                         },
                       ]}
@@ -124,39 +130,44 @@ export default function CliPage() {
               </div>
             </div>
           </div>
-        </div>
-        <div className="py-12 bg-white mt-20">
-          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-            <h4 className="text-2xl leading-8 font-normal text-gray-900 tracking-tight sm:text-3xl sm:leading-9">
-              Features
-            </h4>
-            <p className="mt-3 text-lg leading-7 text-gray-500 lg:pr-4">
-              Use the AsyncAPI CLI tool to help you create, develop, and
-              maintain your AsyncAPI files.
-            </p>
+          <div className="lg:py-12 bg-white mt-20">
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+              <div className="text-center mb-16">
+                <h4 className="text-2xl leading-8 font-normal text-gray-900 tracking-tight sm:text-3xl sm:leading-9 ">
+                  Features
+                </h4>
+                <p className="mt-3 text-lg leading-7 text-gray-500 lg:pr-4 text-center">
+                  Use the AsyncAPI CLI tool to help you create, develop, and
+                  maintain your AsyncAPI files.
+                </p>
+              </div>
 
-            <div className="mt-10">
-              <dl className=" md:grid md:grid-cols-2 space-y-0">
-                {features.map(({ description: Description, ...feature }) => (
-                  <div key={feature.name} className="relative mb-10">
-                    <dt>
-                      <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-primary-500 text-white">
-                        <feature.icon className="h-6 w-6" aria-hidden="true" />
-                      </div>
-                      <p className="ml-16 text-lg leading-6 font-medium text-gray-900">
-                        {feature.name}
-                      </p>
-                    </dt>
-                    <dd className="mt-2 ml-16 text-base text-gray-500 pr-10">
-                      <Description />
-                    </dd>
-                  </div>
-                ))}
-              </dl>
+              <div className="mt-10">
+                <dl className=" md:grid lg:grid-cols-2 lg:space-y-0">
+                  {features.map(({ description: Description, ...feature }) => (
+                    <div key={feature.name} className="relative mb-10">
+                      <dt>
+                        <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-primary-500 text-white">
+                          <feature.icon
+                            className="h-6 w-6"
+                            aria-hidden="true"
+                          />
+                        </div>
+                        <p className="ml-16 text-lg leading-6 font-medium text-gray-900">
+                          {feature.name}
+                        </p>
+                      </dt>
+                      <dd className="mt-2 ml-16 text-base text-gray-500 pr-10">
+                        <Description />
+                      </dd>
+                    </div>
+                  ))}
+                </dl>
+              </div>
             </div>
           </div>
+          <div className="mt-16 text-center">{renderButtons()}</div>
         </div>
-        <div className="mt-16 text-center">{renderButtons()}</div>
       </div>
     </GenericLayout>
   );
