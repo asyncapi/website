115739d999bca123e5047e8a642bb86577aa84d8
Sambhav Saxena <sambhavsaxena02@gmail.com>
2021-12-02 23:52:47 +0530

    Removed period from headline

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -49,12 +49,12 @@ export default function BlogIndexPage() {
         </div>
         <div className="relative max-w-7xl mx-auto">
           <div className="text-center">
-            <h2 className="text-3xl my-4 leading-9 tracking-tight font-extrabold text-gray-900 sm:text-4xl sm:leading-10">
+            <h2 className="text-3xl leading-9 tracking-tight font-extrabold text-gray-900 sm:text-4xl sm:leading-10">
               Welcome to our blog!
             </h2>
-            {/* <p className="mt-3 max-w-2xl mx-auto text-xl leading-7 text-gray-500 sm:mt-4">
-              Find the latest and greatest stories from our community.
-            </p> */}
+            <p className="mt-3 max-w-2xl mx-auto text-xl leading-7 text-gray-500 sm:mt-4">
+              Find the latest and greatest stories from our community
+            </p>
             <p className="max-w-2xl mx-auto text-md leading-7 text-gray-400">
               Want to publish a blog post? We love community stories.
               <a
