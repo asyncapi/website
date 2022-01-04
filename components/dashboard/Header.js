5e8e44ccc7f7c9c4b6d8575b74106acfc17e9d63
Khuda Dad Nomani <32505158+KhudaDad414@users.noreply.github.com>
2022-01-04 15:27:19 +0500

    Update components/dashboard/Header.js
    Co-authored-by: Fran Méndez <fmvilas@gmail.com>

--- components/dashboard/Header.js
+++ components/dashboard/Header.js
@@ -6,7 +6,7 @@ export default function Header() {
     <div className="flex justify-between">
       <div className="lg:flex lg:justify-between">
         <div className="max-w-xl">
-          <h2 className="text-4xl leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none sm:tracking-tight lg:text-6xl">
+          <h2 className="text-3xl leading-10 font-extrabold text-gray-900 sm:text-4xl sm:leading-none sm:tracking-tight">
             Dashboard
           </h2>
           <p className="mt-5 text-xl leading-7 text-gray-500">
