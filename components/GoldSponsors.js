b9a0a0d28c5cd844608bf3b90e07aba1fe6d9b29
akshatnema <20bcs022@iiitdmj.ac.in>
2022-01-13 21:31:08 +0530

    Added Gold Sponsors

---
+++ components/GoldSponsors.js
@@ -0,0 +1,20 @@
+export default function GoldSponsors({ className = '', showSupportBanner = true }){
+    return(
+        <div className={`text-center ${className}`}>
+            <div className="flex flex-wrap mb-8 items-center justify-center md:px-4">
+            <a
+            href="https://www.redhat.com/"
+            target="_blank"
+            className="block relative text-center w-2/3 px-14 py-4 sm:p-0 sm:w-1/2 md:w-1/3 lg:w-1/5"
+            rel="noopener noreferrer"
+           >
+            <img
+            className="inline-block sm:h-40"
+            src="/img/sponsors/redhat.svg"
+          />
+           </a>
+            </div>
+        </div>
+    )
+
+}
