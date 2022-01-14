61faa99fe9dbc24dc229d9647822acc7fbf41e9d
Khuda Dad Nomani <32505158+KhudaDad414@users.noreply.github.com>
2022-01-14 17:31:25 +0500

    add link to the page

--- components/navigation/communityItems.js
+++ components/navigation/communityItems.js
@@ -3,5 +3,6 @@ export default [
   { text: 'Github Organization', href: 'https://github.com/asyncapi', target: '_blank', description: 'Want to sneak in the code? Everything we do is open-sourced in our Github organization.' },
   { text: 'Slack Workspace', href: 'https://asyncapi.com/slack-invite', target: '_blank', description: `Need help? Want to share something? Join our Slack workspace. We're nice people :)` },
   { text: 'Contributing', href: 'https://github.com/asyncapi?type=source#-contribute-to-asyncapi', target: '_blank', description: `We are always welcoming and looking for contributions. If you are interested check out our contribution guide.` },
+  { text: 'Dashboard', href: '/community/dashboard', target: '_blank', description: `Just need a good first issue to start your contribution journey? or want to see what topics are hot in discussion?` },
   { text: 'Technical Steering Committee', href: '/community/tsc', description: 'Get to know what is a TSC member, how you can become one, and meet our current TSC members.' }
 ]
