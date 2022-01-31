8b10d08dd4bd7c96a368113f7950e468344c0ff7
Jonas Lagoni <jonas-lt@live.dk>
2022-01-31 21:45:35 +0100

    switched version

--- pages/tools/modelina.js
+++ pages/tools/modelina.js
@@ -352,7 +352,7 @@ public class LightMeasured {
 }`;
 
 const playgroundAsyncAPIDocument = {
-  "asyncapi": "2.2.0",
+  "asyncapi": "2.3.0",
   "info": {
     "title": "Streetlights API",
     "version": "1.0.0",
