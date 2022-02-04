6f324adda3d6c03dd3efe49d64a0ff9c7fd0117d
akshatnema <20bcs022@iiitdmj.ac.in>
2022-02-04 23:24:22 +0530

    added Link

--- components/navigation/TutorialList.js
+++ components/navigation/TutorialList.js
@@ -1,6 +1,7 @@
 import { useContext } from 'react'
 import DocsContext from '../../context/DocsContext'
 import IconArrowRight from '../icons/ArrowRight'
+import Link from 'next/link';
 
 export default function TutorialList({ className = '' }) {
   const { post, navItems } = useContext(DocsContext)
@@ -11,10 +12,9 @@ export default function TutorialList({ className = '' }) {
     <div className={`${className} grid grid-cols-1 gap-4 sm:grid-cols-2`}>
       {
         tutorials.map((tuto, index) => (
+          <Link href={tuto.slug} key={index}>
           <a
             className="flex flex-col mt-4 p-6 max-w-lg rounded shadow-md border border-gray-200 text-gray-700 transition-all duration-300 ease-in-out hover:shadow-lg hover:border-gray-300"
-            href={tuto.slug}
-            key={index}
           >
             <h5 className="text-lg font-medium font-sans antialiased mb-2">{tuto.title}</h5>
             <p className="flex-1 mb-2 font-normal font-sans antialiased">{tuto.description}</p>
@@ -23,6 +23,7 @@ export default function TutorialList({ className = '' }) {
               <IconArrowRight className="inline-block h-4" />
             </p>
           </a>
+          </Link>
         ))
       }
     </div>
