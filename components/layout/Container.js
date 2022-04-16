8acdea5db21b77b13c6a983290ac429df4dc3e5c
unknown <bajiraouttamsinha@gmail.com>
2022-04-16 11:56:31 +0530

    fixing tool page responsiveness

--- components/layout/Container.js
+++ components/layout/Container.js
@@ -10,8 +10,10 @@ export default function Container ({
   const commonClassNames = `${flex ? `${cssBreakingPoint}:flex` : 'block'} ${flexReverse ? `${cssBreakingPoint}:flex-row-reverse` : ''} ${className} px-4 sm:px-6 lg:px-8`
   const wideClassNames = `max-w-screen-xl ${commonClassNames}`
   const regularClassNames = `max-w-4xl ${commonClassNames}`
-  const normalClassNames = `${wide ? wideClassNames : regularClassNames} mx-auto`
-  const fluidClassNames = `${commonClassNames}`
+  const normalClassNames = `${commonClassNames}`
+  const fluidClassNames = `${wide ? wideClassNames : regularClassNames} mx-auto`
+  
+  
   return (
     <div className={fluid ? fluidClassNames : normalClassNames}>
       {children}
