823d06cedf2b785c39b8ea19130d9808e692be58
Khudadad Nomani <khudadad414@gmail.com>
2022-04-25 15:12:42 +0500

    typo

--- pages/community/dashboard.js
+++ pages/community/dashboard.js
@@ -13,7 +13,7 @@ export default function Home() {
     >
       <title>AsyncAPI - Dashboard</title>
 
-      <div className="py-16">
+      <div className="pt-16">
         <Header />
         <div className="flex flex-col gap-6 md:flex-row  w-full  mt-8">
           <Table
@@ -26,10 +26,10 @@ export default function Home() {
                 <div
                   id="tooltip-default"
                   role="tooltip"
-                  class="inline-block absolute invisible z-10 py-2 px-3 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 transition-opacity duration-300 tooltip dark:bg-gray-700"
+                  className="inline-block absolute invisible z-10 py-2 px-3 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 transition-opacity duration-300 tooltip dark:bg-gray-700"
                 >
                   Tooltip content
-                  <div class="tooltip-arrow" data-popper-arrow></div>
+                  <div className="tooltip-arrow" data-popper-arrow></div>
                 </div>
                 <span>Hot Topics</span>
               </div>
