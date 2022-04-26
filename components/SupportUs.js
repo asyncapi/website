069243850fae67b93d6ed0c0b5d7aeff1af9ac61
johnson chidera <derajohnson64@gmail.com>
2022-04-26 20:48:26 +0100

    fix: updated restream link

--- components/SupportUs.js
+++ components/SupportUs.js
@@ -69,8 +69,8 @@ export default function SupportUs({ className = '', showSupportBanner = true }) 
         <a
           href="https://restream.io/"
           target="_blank"
-          rel="noopener noreferrer"
           className="block relative text-center w-2/3 px-14 py-4 sm:p-0 sm:w-1/3 md:w-1/3 lg:w-1/5"
+          rel="noopener noreferrer"
         >
           <img
             className="inline-block px-4 sm:h-6"
