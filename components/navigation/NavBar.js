728847414591a1ff1853ca868da3118b770d4f12
Idowu Festus <40045755+Mcnoble1@users.noreply.github.com>
2022-04-26 19:29:24 +0100

    fix: add alternative text for hamburger menu icon for screenreaders

--- components/navigation/NavBar.js
+++ components/navigation/NavBar.js
@@ -53,6 +53,7 @@ export default function NavBar ({
         <div className="-mr-2 -my-2 lg:hidden">
           <button onClick={() => setMobileMenuOpen(true)} type="button" className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
             <svg className="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+              <title>Menu</title>
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"/>
             </svg>
           </button>
