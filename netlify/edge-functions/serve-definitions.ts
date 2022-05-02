b8c463b919eb55ce222809c9b8dee8d546052855
Sergio Moya <1083296+smoya@users.noreply.github.com>
2022-05-02 19:48:37 +0200

    authorization + cleanup headers

--- netlify/edge-functions/serve-definitions.ts
+++ netlify/edge-functions/serve-definitions.ts
@@ -1,6 +1,15 @@
 import type { Context } from "netlify:edge";
 
 export default async (request: Request, context: Context) => {
+
+  // Deleting Origin header, which is involved in the cache policy, so requests can hit GH cache.
+  // Reason: raw.githubusercontent.com responses include vary: Authorization,Accept-Encoding,Origin
+  request.headers.delete("origin");
+
+  // Setting GH Token to increase GH rate limit to 5,000 req/h.
+  request.headers.set("Authorization", "token " + Deno.env.get("GITHUB_TOKEN"));
+
+  // Fetching the definition file
   const response = await context.next();
   
   // Setting proper Content-Type header for JSON Schema files.
