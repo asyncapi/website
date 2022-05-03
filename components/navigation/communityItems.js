712237566b1df4dcb77bd528faaf3e13eb8ab91e
Idowu Festus <40045755+Mcnoble1@users.noreply.github.com>
2022-05-03 22:04:45 +0100

    updated community items with icons

--- components/navigation/communityItems.js
+++ components/navigation/communityItems.js
@@ -1,14 +1,16 @@
-import IconGettingStarted from '../icons/GettingStarted'
-import IconSpec from '../icons/Spec'
-import IconUseCases from '../icons/UseCases'
-import IconTutorials from '../icons/Tutorials'
+import IconTools from '../icons/Tools'
+import IconGithubOrganization from '../icons/GithubOrganization'
+import IconSlack from '../icons/Slack'
+import IconContributing from '../icons/Contributing'
+import IconTSC from '../icons/TSC'
+import IconMeetings from '../icons/Meetings'
 
 
 export default [
-  { icon: IconGettingStarted, title: 'Tools & Services', href: '/docs/community/tooling', description: 'Explore the tools and services our awesome community has created.' },
-  { icon: IconSpec, title: 'Github Organization', href: 'https://github.com/asyncapi', target: '_blank', description: 'Want to sneak in the code? Everything we do is open-sourced in our Github organization.' },
-  { icon: IconUseCases, title: 'Slack Workspce', href: 'https://asyncapi.com/slack-invite', target: '_blank', description: `Need help? Want to share something? Join our Slack workspace. We're nice people :)` },
-  { icon: IconTutorials, title: 'Contributing', href: 'https://github.com/asyncapi?type=source#-contribute-to-asyncapi', target: '_blank', description: `We are always welcoming and looking for contributions. If you are interested check out our contribution guide.` },
-  { icon: IconGettingStarted, title: 'Technical Steering Committee', href: '/community/tsc', description: 'Get to know what is a TSC member, how you can become one, and meet our current TSC members.' },
-  { icon: IconGettingStarted, title: 'Meetings', href: '/community/meetings', description: 'See what meetings are organized under AsyncAPI umbrella and join one of them.' }
+  { icon: IconTools, title: 'Tools & Services', href: '/docs/community/tooling', description: 'Explore the tools and services our awesome community has created.' },
+  { icon: IconGithubOrganization, title: 'Github Organization', href: 'https://github.com/asyncapi', target: '_blank', description: 'Want to sneak in the code? Everything we do is open-sourced in our Github organization.' },
+  { icon: IconSlack, title: 'Slack Workspce', href: 'https://asyncapi.com/slack-invite', target: '_blank', description: `Need help? Want to share something? Join our Slack workspace. We're nice people :)` },
+  { icon: IconContributing, title: 'Contributing', href: 'https://github.com/asyncapi?type=source#-contribute-to-asyncapi', target: '_blank', description: `We are always welcoming and looking for contributions. If you are interested check out our contribution guide.` },
+  { icon: IconTSC, title: 'Technical Steering Committee', href: '/community/tsc', description: 'Get to know what is a TSC member, how you can become one, and meet our current TSC members.' },
+  { icon: IconMeetings, title: 'Meetings', href: '/community/meetings', description: 'See what meetings are organized under AsyncAPI umbrella and join one of them.' }
 ]
