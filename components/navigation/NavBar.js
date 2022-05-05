a4e877022dd48470d3ef88e209f023db23f2ed03
Idowu Festus <40045755+Mcnoble1@users.noreply.github.com>
2022-05-05 18:47:28 +0100

    removed redundant code

--- components/navigation/NavBar.js
+++ components/navigation/NavBar.js
@@ -53,9 +53,7 @@ export default function NavBar ({
           <button onClick={() => setMobileMenuOpen(true)} type="button" className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
             <svg className="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
               <title>Menu</title>
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"/>
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"/>
+               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"/>
             </svg>
           </button>
         </div>
@@ -72,7 +70,6 @@ export default function NavBar ({
 
           <div className="relative">
             <NavItem text="Community" onClick={() => showMenu('community')} hasDropdown />
-            {/* {open === 'community' && <NavMenu items={communityItems} />} */}
             {open === 'community' && <CommunityPanel />}
           </div>
 
