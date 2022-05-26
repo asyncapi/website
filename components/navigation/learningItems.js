5898d3f5453b874ada4006c2ab41746ca476b68f
Alejandra Quetzalli <alejandra.olvera.novack@gmail.com>
2022-05-26 16:09:19 -0700

    removing cli mention from reference content bucket

--- components/navigation/learningItems.js
+++ components/navigation/learningItems.js
@@ -7,5 +7,5 @@ export default [
   { href: '/docs/concepts', icon: IconRocket, className: 'bg-secondary-200', title: 'Concepts', description: 'Our Concepts section defines the concepts of AsyncAPI features and capabilities.' },
   { href: '/docs/tutorials', icon: IconGradCap, className: 'bg-pink-100', title: 'Tutorials', description: 'Our Tutorials section teaches beginner processes with AsyncAPI by doing. ' },
   { href: '/docs/tools', icon: IconPlant, className: 'bg-green-200', title: 'Tools', description: 'Our Tools section documents the AsyncAPI tools ecosystem.' },
-  { href: '/docs/reference', icon: IconPaper, className: 'bg-yellow-200', title: 'Reference', description: `Our Reference section teaches how to set up your development environment, CLI, APIs, and beyond.` }
+  { href: '/docs/reference', icon: IconPaper, className: 'bg-yellow-200', title: 'Reference', description: `Our Reference section documents the AsyncAPI specification.` }
 ]
