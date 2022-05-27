66ff271e5403596986e166943d17f8fdc970453e
Karuna Tata <karunatata63@gmail.com>
2022-05-27 18:26:47 +0530

    fix: description

--- components/navigation/communityItems.js
+++ components/navigation/communityItems.js
@@ -11,7 +11,7 @@ export default [
   { icon: IconGithubOrganization, title: 'Github Organization', href: 'https://github.com/asyncapi', target: '_blank', description: 'Want to sneak in the code? Everything we do is open-sourced in our Github organization.' },
   { icon: IconSlack, title: 'Slack Workspace', href: 'https://asyncapi.com/slack-invite', target: '_blank', description: `Need help? Want to share something? Join our Slack workspace. We're nice people :)` },
   { icon: IconContributing, title: 'Contributing', href: 'https://github.com/asyncapi?type=source#-contribute-to-asyncapi', target: '_blank', description: `We are always welcoming and looking for contributions. If you are interested check out our contribution guide.` },
-  { icon: IconTSC, title: 'Technical Steering Committee', href: '/community/tsc', description: 'Get to know what is a TSC member, how you can become one, and meet our current Ambassadors.' },
-  { icon: IconTSC, title: 'Ambassadors', href: '/community/ambassador', description: 'Get to know what is AsyncAPI ambassador program, how you can become one, and meet our current TSC members.' },
+  { icon: IconTSC, title: 'Technical Steering Committee', href: '/community/tsc', description: 'Get to know what is a TSC member, how you can become one, and meet our current TSC members.' },
+  { icon: IconTSC, title: 'Ambassadors', href: '/community/ambassador', description: 'Get to know what is AsyncAPI ambassador program, how you can become one, and meet our current Ambassadors.' },
   { icon: IconMeetings, title: 'Meetings', href: '/community/meetings', description: 'See what meetings are organized under AsyncAPI umbrella and join one of them.' }
 ]
