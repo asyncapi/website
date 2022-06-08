0a88eff396ace4eb46c2a9e40473df7d2e77775a
derberg <lpgornicki@gmail.com>
2022-06-08 18:03:31 +0200

    fixes after review

--- components/campaigns/AnnoucementHero.js
+++ components/campaigns/AnnoucementHero.js
@@ -1,5 +1,7 @@
 import { useRouter } from 'next/router'
 import Button from '../buttons/Button'
+import Heading from '../typography/Heading';
+import Paragraph from '../typography/Paragraph'
 
 export default function AnnouncementHero({ className = '', small = false}) {
 
@@ -8,10 +10,13 @@ export default function AnnouncementHero({ className = '', small = false}) {
   if (pathname === '/blog/events2021') return null
 
   return (
-    <div className={`bg-gray-50 border border-gray-200 py-6  ${className} ${small ? 'mb-0' : 'mb-12'}`}>
-      <h2 className="text-xl md:text-3xl font-bold countdown-text-gradient">AsyncAPI Conference 2022</h2>
-      <h3 className="text-xl md:text-3xl font-bold countdown-text-gradient">Do you prefer flying over or watching online?</h3>
-
+    <div className={`bg-gray-50 border border-gray-200 py-6 rounded ${className} ${small ? 'mb-4' : 'mb-12'}`}>
+      <Heading level="h2" typeStyle="heading-lg">
+        AsyncAPI Conference 2022
+      </Heading>
+      <Paragraph typeStyle="body-lg">
+        Help us decide on a date and location by filling out a short survey
+      </Paragraph>
       <div className="mt-8 pb-2 space-x-2">
         <Button href="https://conference.asyncapi.com/" target="_blank" text="Share your opinion!" />
       </div>
