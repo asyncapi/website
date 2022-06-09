b6fea8f1d0a2702d62bed4f7855eeb96026f3393
Harish-b-13 <harishbalaji369@gmail.com>
2022-06-09 22:40:50 +0530

    Updated spacing & "Social" layout (small devices)

--- components/Footer.js
+++ components/Footer.js
@@ -13,9 +13,9 @@ import Heading from "./typography/Heading"
 export default function Footer() {
   return (
     <footer className="bg-dark mt-12 margin: 0 auto">
-      <div className="max-w-screen-xl mx-auto py-16 px-4 overflow-hidden sm:px-6 lg:px-8 divide-y divide-cool-gray">
-        <nav className="-mx-5 -my-2 py-10 px-5 mt-2 flex flex-wrap justify-between ">
-          <div className="mr-5 w-full md:w-auto">
+      <div className="max-w-screen-xl mx-auto py-4 sm:py-6 md:py-12 xl:py-16 px-3 overflow-hidden sm:px-6 lg:px-8 divide-y divide-cool-gray">
+        <nav className="-mx-5 -my-2 py-4 sm:py-10 px-6 mt-2 flex flex-wrap justify-between ">
+          <div className="mr-14 w-full md:w-auto">
             <div className="">
               <Link href="/">
                 <a className="cursor-pointer">
@@ -26,18 +26,18 @@ export default function Footer() {
             <div className="">
               {/* <p className="text-lg font-medium text-white mt-8 mb-15"> */}
               <Heading className="mt-12 mb-15 text-white" typeStyle="heading-sm-semibold">
-                Building the future of <br/> Event-Driven Architectures (EDAs).
+                Building the future of <br/> Event-Driven Architectures.
               </Heading>
               {/* </p> */}
             </div>
           </div>
 
         
-          <div className="flex flex-col md:flex-row">
+          <div className="flex flex-col sm:flex-row">
             
             <div className="flex">
 
-              <div className="px-5 md:px-10 md:px-5 ml-5 mb-5">
+              <div className="px-0 lg:px-10 lg:ml-5 mb-5">
                 <div className="py-2">
                   <div className="text-white">
                     <Heading typeStyle="heading-sm-semibold">The Initiative</Heading>
@@ -72,7 +72,7 @@ export default function Footer() {
                 </ul>
               </div>
 
-              <div className="px-5 md:px-10 ml-5 mb-5">
+              <div className="px-15 sm:px-8 sm:ml-10 md:ml-5 mb-5">
                 <div className="py-2">
                   <div className="text-white">
                     <Heading typeStyle="heading-sm-semibold">News</Heading>
@@ -98,75 +98,75 @@ export default function Footer() {
 
             </div>
 
-            <div className="px-5 md:px-10 ml-5 mb-5">
-              <div className="py-2">
+            <div className="px-0 sm:px-10 sm:ml-5 mb-5">
+              <div className="py-2 hidden sm:block">
                 <div className="mr-15 text-white">
                   <Heading typeStyle="heading-sm-semibold">Social</Heading>
                 </div>
               </div>
-              <ul className="justify-start" aria-label="AsyncAPI social media links">
-                <li className="py-2">
+              <ul className="justify-start flex sm:flex-col" aria-label="AsyncAPI social media links">
+                <li className="py-2 mr-3 sm:mr-0">
                   <a href="https://twitter.com/AsyncAPISpec" target="_blank" rel="noopener noreferrer" className="text-gray-500 flex">
                     <div className="">
                       <div className="flex fit-content items-center hover:text-blue-500">
                         <span className="sr-only">Twitter</span>
-                        <IconTwitter className="h-6 w-6" />
-                        <span className="text-cool-gray absolute ml-8 hover:text-blue-500">Twitter</span>
+                        <IconTwitter className="h-8 w-8 sm:h-6 sm:w-6" />
+                        <span className="text-cool-gray absolute ml-8 hover:text-blue-500 hidden sm:block">Twitter</span>
                       </div>
                     </div>
                   </a>
                 </li>
-                <li className="py-2">
+                <li className="py-2 mr-3 sm:mr-0">
                   <a href="https://github.com/asyncapi" target="_blank" rel="noopener noreferrer" className="text-gray-500 flex">
                     <div className="">
                       <div className="flex fit-content items-center  hover:text-gray-700">
                         <span className="sr-only">GitHub</span>
-                        <IconGithub className="h-6 w-6" />
-                        <span className="text-cool-gray absolute ml-8 hover:text-gray-700">GitHub</span>
+                        <IconGithub className="h-8 w-8 sm:h-6 sm:w-6" />
+                        <span className="text-cool-gray absolute ml-8 hover:text-gray-700 hidden sm:block">GitHub</span>
                       </div>
                     </div>
                   </a>
                 </li>
-                <li className="py-2">
+                <li className="py-2 mr-3 sm:mr-0">
                   <a href="https://linkedin.com/company/asyncapi" rel="noopener noreferrer" target="_blank" className="text-gray-500 flex">
                     <div className="">
                       <div className="flex fit-content items-center hover:text-blue-500">
                         <span className="sr-only">LinkedIn</span>
-                        <IconLinkedIn className="h-5 w-5 ml-1" />
-                        <span className="text-cool-gray absolute ml-8 hover:text-blue-500">LinkedIn</span>
+                        <IconLinkedIn className="h-8 w-8 sm:h-5 sm:w-5 ml-1" />
+                        <span className="text-cool-gray absolute ml-8 hover:text-blue-500 hidden sm:block">LinkedIn</span>
                       </div>
                     </div>
                   </a>
                 </li>
-                <li className="py-2">
+                <li className="py-2 mr-3 sm:mr-0">
                   <a href="https://youtube.com/asyncapi" target="_blank" rel="noopener noreferrer" className="text-gray-500 flex">
                     <div className="">
                       <div className="flex fit-content items-center hover:text-red-600">
                         <span className="sr-only">YouTube</span>
-                        <IconYoutubeGray className="h-6 w-6 " /> 
-                        <span className="text-cool-gray absolute ml-8 hover:text-white">YouTube</span>
+                        <IconYoutubeGray className="h-8 w-8 sm:h-6 sm:w-6" /> 
+                        <span className="text-cool-gray absolute ml-8 hover:text-white hidden sm:block">YouTube</span>
                       </div>
                     </div>
                   </a>
                 </li>
-                <li className="py-2">
+                <li className="py-2 mr-3 sm:mr-0">
                   <a href="https://asyncapi.com/slack-invite" target="_blank" rel="noopener noreferrer" className="text-gray-500">
                     <div className="">
                       <div className="flex fit-content items-center hover:text-white">
                         <span className="sr-only">Slack</span>
-                        <IconSlack className="h-6 w-6" /> 
-                        <span className="text-cool-gray absolute ml-8 hover:text-white">Slack</span>
+                        <IconSlack className="h-8 w-8 sm:h-6 sm:w-6" /> 
+                        <span className="text-cool-gray absolute ml-8 hover:text-white hidden sm:block">Slack</span>
                       </div>
                     </div>
                   </a>
                 </li>
-                <li className="py-2">
+                <li className="py-2 mr-3 sm:mr-0">
                   <a href="https://www.twitch.tv/asyncapi" target="_blank" rel="noopener noreferrer" className="text-gray-500">
                     <div className="">
                       <div className="flex fit-content items-center hover:text-purple-500">
                         <span className="sr-only">Twitch</span>
-                        <IconTwitch className="h-6 w-5" /> 
-                        <span className="text-cool-gray absolute ml-8 hover:text-purple-500">Twitch</span>
+                        <IconTwitch className="h-8 w-8 sm:h-6 sm:w-6" /> 
+                        <span className="text-cool-gray absolute ml-8 hover:text-purple-500 hidden sm:block">Twitch</span>
                       </div>
                     </div>
                   </a>
@@ -176,20 +176,18 @@ export default function Footer() {
           </div>
         </nav>
         
-        <div className="md:flex justify-between items-center mt-20 py-10">
-          <div className="">
-            <p className="text-left text-sm leading-6 text-cool-gray">
+        <div className="sm:flex justify-between xl:mt-20 py-12">
+          <div className="w-full sm:w-2/3">
+            <p className="text-left text-base leading-6 mb-3 text-cool-gray">
               Made with <span className="font-mono text-secondary-500">:love:</span> by the AsyncAPI Initiative.
             </p>
-            <p className="text-left text-sm leading-6 text-cool-gray mt-1">
+            <p className="text-left text-sm leading-6 text-cool-gray w-full sm:w-2/3">
               Copyright &copy; AsyncAPI Project a Series of LF Projects, LLC.
-            </p>
-            <p className="text-left text-sm leading-6 text-cool-gray mt-1">
               For web site terms of use, trademark policy and general project policies please see <a href="https://lfprojects.org" className="text-secondary-500 hover:text-white transition ease-in-out duration-300 underline" target="_blank" rel="noopener noreferrer">https://lfprojects.org</a>
             </p>
           </div>
-          <div className="block mt-10 md:mt-0">
-            <p className="text-center text-sm leading-6">
+          <div className="block mt-10 sm:mt-0">
+            <p className="block text-sm leading-6">
               <a href="https://netlify.com" target="_blank" rel="noopener noreferrer">
                 <img src="https://www.netlify.com/img/global/badges/netlify-color-bg.svg" className="inline" alt="Deploys by Netlify"/>
               </a>
