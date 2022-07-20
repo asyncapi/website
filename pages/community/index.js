f2af1c71d6355dc0b1727c5729bf9a9814ddac18
AceTheCreator <devaze007@gmail.com>
2022-07-20 14:55:38 +0100

    successfully created first orbit sequece

--- pages/community/index.js
+++ pages/community/index.js
@@ -2,11 +2,50 @@ import React from 'react';
 import CommunityLayout from '../../components/layout/CommunityLayout';
 
 function CommunityIndexPage() {
-    return (
-      <CommunityLayout title="AsyncAPI Meetings" description="The home for developer communities" wide>
-        <div className="h-100"></div>
-      </CommunityLayout>
-    );
+  return (
+    <CommunityLayout
+      title="AsyncAPI Meetings"
+      description="The home for developer communities"
+      wide
+    >
+      <div className="h-screen">
+        <div className="orbit-container">
+          <div id="first-orbit" className="orbit">
+            <div id="circle-around-first-1" className="circle-around-orbit">
+              <div>
+                <img src="/img/homepage/idea.png" alt="idea" />
+              </div>
+            </div>
+            <div id="circle-around-first-2" className="circle-around-orbit">
+              <img src="/img/homepage/fran.png" alt="idea" />
+            </div>
+            <div id="circle-around-first-3" className="circle-around-orbit">
+              <img src="/img/homepage/ale.jpeg" alt="idea" />
+            </div>
+            <div id="circle-around-first-4" className="circle-around-orbit">
+              <div>
+                <img src="/img/homepage/comment.png" alt="idea" />
+              </div>
+            </div>
+            <div id="circle-around-first-5" className="circle-around-orbit">
+              <div>
+                <img src="/img/homepage/reaction.png" alt="idea" />
+              </div>
+            </div>
+          </div>
+          <div id="uranus-orbit" className="orbit">
+            <div id="uranus"></div>
+          </div>
+          <div id="neptune-orbit" className="orbit">
+            <div id="neptune"></div>
+          </div>
+          {/* <div id="pluto-orbit" className="orbit">
+            <div id="pluto"></div>
+          </div> */}
+        </div>
+      </div>
+    </CommunityLayout>
+  );
 }
 
-export default CommunityIndexPage
+export default CommunityIndexPage;
