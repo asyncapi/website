07ad3b8f5460acf21727fec31c996d734cf14f3d
Harish-b-03 <harishbalaji369@gmail.com>
2022-07-29 20:23:07 +0530

    update - merge conflict solved

--- components/navigation/DocsMobileMenu.js
+++ components/navigation/DocsMobileMenu.js
@@ -12,7 +12,7 @@ export default function DocsMobileMenu({
     <div className="z-60 lg:hidden">
       <div className="relative inset-0 flex z-40">
         <div className=" flex-1 flex flex-col max-w-xs w-full bg-white">
-        <div className='w-full my-4 px-2'>
+            <div className='w-full my-4 px-2'>
               <SearchButton 
                 className="flex w-full items-center text-left text-sm space-x-3 px-3 py-1.5 bg-white hover:bg-secondary-100 border-gray-300 hover:border-secondary-500 border text-gray-700 hover:text-secondary-500 shadow-sm transition-all duration-500 ease-in-out rounded-md"
                 indexName={DOCS_INDEX_NAME}
