96dc7d97f3b9e61b1a065ee12e69a7d476b36c5b
acethecreator <devaze007@gmail.com>
2022-08-17 17:29:23 +0100

    .

--- pages/community/index.js
+++ pages/community/index.js
@@ -8,118 +8,118 @@ import NewsletterSubscribe from '../../components/NewsletterSubscribe';
 
 const orbit1 = [
   {
-    id: 'item',
-    img: '/img/homepage/reaction.png',
-    alt: 'reaction',
+    id: "item",
+    img: "/img/homepage/reaction.png",
+    alt: "reaction",
   },
   {
-    id: 'item',
-    img: '/img/homepage/ale.jpeg',
-    alt: 'ale',
+    id: "item",
+    img: "/img/homepage/ale.jpeg",
+    alt: "ale",
   },
   {
-    id: 'item',
-    img: '/img/homepage/idea.png',
-    alt: 'idea',
+    id: "item",
+    img: "/img/homepage/idea.png",
+    alt: "idea",
   },
   {
-    id: 'item',
-    img: '/img/homepage/lukasz-homepage-slack.jpg',
-    alt: 'lukasz',
+    id: "item",
+    img: "/img/homepage/lukasz-homepage-slack.jpg",
+    alt: "lukasz",
   },
   {
-    id: 'item',
-    img: '/img/homepage/comment.png',
-    alt: 'comment',
+    id: "item",
+    img: "/img/homepage/comment.png",
+    alt: "comment",
   },
   {
-    id: 'item',
-    img: '/img/homepage/fran.png',
-    alt: 'fran',
+    id: "item",
+    img: "/img/homepage/fran.png",
+    alt: "fran",
   },
 ];
 
 const orbit2 = [
   {
-    id: 'item-2',
-    img: '/img/homepage/jonas.jpeg',
-    alt: 'jonas',
+    id: "item-2",
+    img: "/img/homepage/jonas.jpeg",
+    alt: "jonas",
   },
   {
-    id: 'item-2',
-    img: '/img/homepage/slack.png',
-    alt: 'slack',
+    id: "item-2",
+    img: "/img/homepage/slack.png",
+    alt: "slack",
   },
   {
-    id: 'item-2',
-    img: '/img/homepage/missy.jpeg',
-    alt: 'missy',
+    id: "item-2",
+    img: "/img/homepage/missy.jpeg",
+    alt: "missy",
   },
   {
-    id: 'item-2',
-    img: '/img/homepage/check.png',
-    alt: 'check',
+    id: "item-2",
+    img: "/img/homepage/check.png",
+    alt: "check",
   },
   {
-    id: 'item-2',
-    img: '/img/homepage/eve-and-chan.png',
-    alt: 'eve-chan',
+    id: "item-2",
+    img: "/img/homepage/eve-and-chan.png",
+    alt: "eve-chan",
   },
   {
-    id: 'item-2',
-    img: '/img/homepage/maciej.png',
-    alt: 'maciej',
+    id: "item-2",
+    img: "/img/homepage/maciej.png",
+    alt: "maciej",
   },
   {
-    id: 'item-2',
-    img: '/img/homepage/git.png',
-    alt: 'git',
+    id: "item-2",
+    img: "/img/homepage/git.png",
+    alt: "git",
   },
 ];
 
 const orbit3 = [
   {
-    id: 'item-3',
-    img: '/img/homepage/ace.jpeg',
-    alt: 'ace',
+    id: "item-3",
+    img: "/img/homepage/ace.jpeg",
+    alt: "ace",
   },
   {
-    id: 'item-3',
-    img: '/img/homepage/star.png',
-    alt: 'star',
+    id: "item-3",
+    img: "/img/homepage/star.png",
+    alt: "star",
   },
   {
-    id: 'item-3',
-    img: '/img/homepage/barbano.jpeg',
-    alt: 'barbano',
+    id: "item-3",
+    img: "/img/homepage/barbano.jpeg",
+    alt: "barbano",
   },
   {
-    id: 'item-3',
-    img: '/img/homepage/dale.jpeg',
-    alt: 'dale',
+    id: "item-3",
+    img: "/img/homepage/dale.jpeg",
+    alt: "dale",
   },
   {
-    id: 'item-3',
-    img: '/img/homepage/kuda.jpeg',
-    alt: 'kuda',
+    id: "item-3",
+    img: "/img/homepage/kuda.jpeg",
+    alt: "kuda",
   },
   {
-    id: 'item-3',
-    img: '/img/homepage/yash.jpeg',
-    alt: 'yash',
+    id: "item-3",
+    img: "/img/homepage/yash.jpeg",
+    alt: "yash",
   },
   {
-    id: 'item-3',
-    img: '/img/homepage/sergio.jpeg',
-    alt: 'sergio',
+    id: "item-3",
+    img: "/img/homepage/sergio.jpeg",
+    alt: "sergio",
   },
 ];
 
 const quickLinks = [
-  { title: 'Advocate Programs', bg: 'white', color: 'primary-600' },
-  { title: 'Events & Meetups', bg: 'white', color: 'primary-600' },
-  { title: 'Community Members', bg: 'white', color: 'primary-600' },
-  { title: 'Start Contributing', bg: 'white', color: 'primary-600' },
+  { title: "Advocate Programs", bg: "white", color: "primary-600" },
+  { title: "Events & Meetups", bg: "white", color: "primary-600" },
+  { title: "Community Members", bg: "white", color: "primary-600" },
+  { title: "Start Contributing", bg: "white", color: "primary-600" },
 ];
 
 function CommunityIndexPage() {
@@ -146,7 +146,7 @@ function CommunityIndexPage() {
           textColor="text-gray-700"
           className="text-slate-500 text-xs md:text-sm"
         >
-          We're a community of great people who are passionate about AsyncAPI.
+          We"re a community of great people who are passionate about AsyncAPI.
           Join us in building the future of Event Driven APIs by asking
           questions, share ideas and build connection with each other
         </Heading>
@@ -324,7 +324,7 @@ function CommunityIndexPage() {
                   </div>
                   <div
                     className="w-full bg-gray-200 mt-4"
-                    style={{ height: '1px' }}
+                    style={{ height: "1px" }}
                   />
                   <div className="flex mt-2 justify-between">
                     <div className="text-left border-r-2 border-r-gray-400 pr-5">
@@ -367,7 +367,7 @@ function CommunityIndexPage() {
                   </div>
                   <div
                     className="w-full bg-gray-200 mt-4"
-                    style={{ height: '1px' }}
+                    style={{ height: "1px" }}
                   />
                   <div className="flex mt-2 justify-between">
                     <div className="text-left border-r-2 border-r-gray-400 pr-5">
