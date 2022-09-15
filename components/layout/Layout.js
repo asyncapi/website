4836275b41992110fb280c3575bf817b7457521b
acethecreator <devaze007@gmail.com>
2022-09-15 11:59:47 +0100

    completed the community docs section

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -21,60 +21,65 @@ export default function Layout({ children }) {
         {children}
       </DocsLayout>
     )
-  }else if(pathname.startsWith('/community/docs')){
+  }
+  else if (pathname.startsWith('/community/docs/')) {
+    const posts = getAllPosts();
+    const post = getPostBySlug(pathname);
+    return (
+      <CommunityDocsLayout
+        post={post}
+        navItems={posts.filter((p) => p.slug.startsWith('/community/docs/'))}
+      >
+        {children}
+      </CommunityDocsLayout>
+    );
+  } else if (pathname.startsWith('/community/docs')) {
     const posts = getAllPosts();
     return (
       <CommunityDocsContext.Provider
-        value={{ navItems: posts.filter((p) => p.slug.startsWith('/community/docs/')) }}
+        value={{
+          navItems: posts.filter((p) => p.slug.startsWith('/community/docs/')),
+        }}
       >
         {children}
       </CommunityDocsContext.Provider>
     );
-  }
-  else if(pathname.startsWith('/community/docs/')){
-        const posts = getAllPosts();
-        const post = getPostBySlug(pathname);
-    return <CommunityDocsLayout post={post} navItems={posts.filter(p => p.slug.startsWith('/community/docs/'))}>
-      {children}
-    </CommunityDocsLayout>
-  }
-   else if (pathname.startsWith('/blog/')) {
-    const posts = getAllPosts()
-    const post = getPostBySlug(pathname)
+  } else if (pathname.startsWith('/blog/')) {
+    const posts = getAllPosts();
+    const post = getPostBySlug(pathname);
     return (
-      <BlogLayout post={post} navItems={posts.filter(p => p.slug.startsWith('/blog/'))}>
+      <BlogLayout
+        post={post}
+        navItems={posts.filter((p) => p.slug.startsWith('/blog/'))}
+      >
         {children}
       </BlogLayout>
-    )
+    );
   } else if (pathname === '/blog') {
-    const posts = getAllPosts()
+    const posts = getAllPosts();
     return (
-      <BlogContext.Provider value={{ navItems: posts.filter(p => p.slug.startsWith('/blog/')) }}>
+      <BlogContext.Provider
+        value={{ navItems: posts.filter((p) => p.slug.startsWith('/blog/')) }}
+      >
         {children}
       </BlogContext.Provider>
-    )
+    );
   } else if (pathname === '/jobs') {
-    const posts = getAllPosts()
+    const posts = getAllPosts();
     return (
-      <JobsContext.Provider value={{ navItems: posts.filter(p => p.slug.startsWith('/jobs/')) }}>
+      <JobsContext.Provider
+        value={{ navItems: posts.filter((p) => p.slug.startsWith('/jobs/')) }}
+      >
         {children}
       </JobsContext.Provider>
-    )
+    );
   } else if (pathname.startsWith('/jobs/')) {
-    const post = getPostBySlug(pathname)
-    return (
-      <JobsLayout post={post}>
-        {children}
-      </JobsLayout>
-    )
+    const post = getPostBySlug(pathname);
+    return <JobsLayout post={post}>{children}</JobsLayout>;
   } else {
-    const post = getPostBySlug(pathname)
+    const post = getPostBySlug(pathname);
     if (post) {
-      return (
-        <GenericPostLayout post={post}>
-          {children}
-        </GenericPostLayout>
-      )
+      return <GenericPostLayout post={post}>{children}</GenericPostLayout>;
     }
   }
   
--- pages/community/docs/getting-started/index.md
+++ pages/community/docs/getting-started/index.md
@@ -1,7 +1,7 @@
 ---
 title: 'Getting started'
 description: 'The essential guide to start using AsyncAPI. Keep it as your reference for all the operations that you can perform on the API resources.'
-defaultLink: 'community/docs/gettings-started/become-a-tsc-member'
+defaultLink: 'docs/getting-started/become-a-tsc-member'
 weight: 1
 icon: /img/illustrations/icons/book.svg
 ---
