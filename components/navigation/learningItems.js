8c764fd51a3b48246f752320b1d81803eb7ed4f2
Matatjahu <urbanczyk.maciej.95@gmail.com>
2022-10-03 16:06:52 +0200

    change icon

--- components/navigation/learningItems.js
+++ components/navigation/learningItems.js
@@ -8,6 +8,6 @@ export default [
   { href: '/docs/concepts', icon: IconRocket, className: 'bg-secondary-200', title: 'Concepts', description: 'Our Concepts section defines the concepts of AsyncAPI features and capabilities.' },
   { href: '/docs/tutorials', icon: IconGradCap, className: 'bg-pink-100', title: 'Tutorials', description: 'Our Tutorials section teaches beginner processes with AsyncAPI by doing. ' },
   { href: '/docs/tools', icon: IconPlant, className: 'bg-green-200', title: 'Tools', description: 'Our Tools section documents the AsyncAPI tools ecosystem.' },
-  { href: '/docs/guides', icon: IconGuide, className: 'bg-primary-200', iconProps: { style: { stroke: '#E0D1FC' } }, title: 'Guides', description: 'Our Guides section shows at a high level how stuff work.' },
+  { href: '/docs/guides', icon: IconGuide, className: 'bg-primary-200', title: 'Guides', description: 'Our Guides section shows at a high level how stuff work.' },
   { href: '/docs/reference', icon: IconPaper, className: 'bg-yellow-200', title: 'Reference', description: `Our Reference section documents the AsyncAPI specification.` }
 ]
