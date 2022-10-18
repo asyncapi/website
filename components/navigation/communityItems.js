7507565c03391af4981d542249f564e276c36d99
Khuda Dad Nomani <khudadad414@gmail.com>
2022-10-18 14:26:18 +0100

    Icon Updated

--- components/navigation/communityItems.js
+++ components/navigation/communityItems.js
@@ -2,6 +2,7 @@ import IconTools from '../icons/Tools'
 import IconGithubOrganization from '../icons/GithubOrganization'
 import IconSlack from '../icons/Slack'
 import IconContributing from '../icons/Contributing'
+import IconDashboard from '../icons/Dashboard'
 import IconTSC from '../icons/TSC'
 import IconMeetings from '../icons/Meetings'
 import IconNewsroom from '../icons/Newsroom'
@@ -14,7 +15,7 @@ export default [
   { icon: IconSlack, title: 'Slack Workspace', href: 'https://asyncapi.com/slack-invite', target: '_blank', description: `Need help? Want to share something? Join our Slack workspace. We're nice people :)` },
   { icon: IconContributing, title: 'Contributing', href: 'https://github.com/asyncapi?type=source#-contribute-to-asyncapi', target: '_blank', description: `We are always welcoming and looking for contributions. If you are interested check out our contribution guide.` },
   { icon: IconTSC, title: 'Technical Steering Committee', href: '/community/tsc', description: 'Get to know what is a TSC member, how you can become one, and meet our current TSC members.' },
-  { icon: IconNewsroom, title: 'Dashboard',href: '/community/dashboard',target: '_blank', description: `Just need a good first issue to start your contribution journey? or want to see what topics are hot in discussion?`},
+  { icon: IconDashboard, title: 'Dashboard',href: '/community/dashboard',target: '_blank', description: `Just need a good first issue to start your contribution journey? or want to see what topics are hot in discussion?`},
   { icon: IconMeetings, title: 'Meetings', href: '/community/meetings', description: 'See what meetings are organized under AsyncAPI umbrella and join one of them.' },
   { icon: IconNewsroom, title: 'Newsroom', href: '/community/newsroom', description: 'Get upto date with the recent activity in the initiative.' },
  ]
