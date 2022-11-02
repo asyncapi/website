a4e2c1f24429b8102cb1a63953e7c2e92a427d71
Matatjahu <urbanczyk.maciej.95@gmail.com>
2022-11-02 10:25:59 +0100

    feat: add conference banner with YT livestream

--- components/campaigns/AnnoucementHero.js
+++ components/campaigns/AnnoucementHero.js
@@ -1,14 +1,25 @@
 import { useRouter } from 'next/router'
+import YouTube from 'react-youtube-embed'
 import Button from '../buttons/Button'
 import Heading from '../typography/Heading'
-import Paragraph from '../typography/Paragraph'
 
-export default function AnnouncementHero({ className = '', small = false}) {
+function getConferenceDetails() {
+  const day = new Date().getUTCDate();
+  switch (day) {
+    case 3: return { day: 1, ytId: 'NTHsezlKBh8' };  
+    case 4: return { day: 2, ytId: '8khuAfL7TSE' };
+    case 5: return { day: 3, ytId: 'R8PYWXDDZbI' };
+  }
+}
 
-  const { pathname } = useRouter()
+export default function AnnouncementHero({ className = '', small = false }) {
+  const { pathname } = useRouter();
+  if (pathname === '/blog/events2021') return null;
 
-  if (pathname === '/blog/events2021') return null
+  const details = getConferenceDetails();
+  if (!details) return null;
 
+  const { day, ytId } = details;
   return (
     <div
       className={`bg-gray-50 border border-gray-200 py-6 rounded ${className} ${
@@ -20,16 +31,21 @@ export default function AnnouncementHero({ className = '', small = false}) {
         level="h2"
         typeStyle="heading-lg"
       >
-        AsyncAPI Conference 2022
+        AsyncAPI Conference 2022 Day {day} is running!
       </Heading>
-      <Paragraph typeStyle="body-lg">
-        November 3-5, 2022 | Virtual
-      </Paragraph>
+       <div className='px-4 mt-4 mx-auto max-w-7xl'>
+        <YouTube id={ytId} />
+      </div>
       <div className="mt-8 pb-2 space-x-2">
         <Button
-          href="https://conference.asyncapi.com/"
+          href="https://www.asyncapi.com/slack-invite"
           target="_blank"
-          text="Learn more"
+          text="Comment and ask questions"
+        />
+        <Button
+          href="https://conference.asyncapi.com/schedule"
+          target="_blank"
+          text="Check schedule"
         />
       </div>
     </div>
