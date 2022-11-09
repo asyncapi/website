3be8127a7357cd6f950f87635df69defcc58130e
Lukasz Gornicki <lpgornicki@gmail.com>
2022-11-09 10:01:41 +0100

    fix: enable ids in list of features from landing page (#1087)
    Co-authored-by: Alejandra Quetzalli <alejandra.olvera.novack@gmail.com>

--- components/typography/TextLink.js
+++ components/typography/TextLink.js
@@ -3,7 +3,8 @@ export default function TextLink({
     href,
     className,
     target,
-    children
+    children,
+    id
 }) {
 
     const classNames = `text-secondary-500 underline hover:text-gray-800 font-medium transition ease-in-out duration-300 ${className || ''}`
@@ -12,7 +13,7 @@ export default function TextLink({
         <>
             {' '}
             <Link href={href}>
-                <a target={target} rel="noreferrer noopener" className={classNames}>
+                <a id={id} target={target} rel="noreferrer noopener" className={classNames}>
                     {children}
                 </a>
             </Link>
