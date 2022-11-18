019bc575a2fe16f18dda533b8b6751dfc9b8be70
acethecreator <devaze007@gmail.com>
2022-11-18 16:12:15 +0100

    extracted and seperated orbit data

--- pages/community/index.js
+++ pages/community/index.js
@@ -6,115 +6,7 @@ import IconRocket from '../../components/icons/Rocket';
 import NewsletterSubscribe from '../../components/NewsletterSubscribe';
 import GenericLayout from '../../components/layout/GenericLayout';
 import Link from 'next/link';
-
-const orbit1 = [
-  {
-    id: 'item',
-    img: '/img/homepage/reaction.png',
-    alt: 'reaction',
-  },
-  {
-    id: 'item',
-    img: '/img/homepage/ale.jpeg',
-    alt: 'ale',
-  },
-  {
-    id: 'item',
-    img: '/img/homepage/idea.png',
-    alt: 'idea',
-  },
-  {
-    id: 'item',
-    img: '/img/homepage/lukasz-homepage-slack.jpg',
-    alt: 'lukasz',
-  },
-  {
-    id: 'item',
-    img: '/img/homepage/comment.png',
-    alt: 'comment',
-  },
-  {
-    id: 'item',
-    img: '/img/homepage/fran.png',
-    alt: 'fran',
-  },
-];
-
-const orbit2 = [
-  {
-    id: 'item-2',
-    img: '/img/homepage/jonas.jpeg',
-    alt: 'jonas',
-  },
-  {
-    id: 'item-2',
-    img: '/img/homepage/slack.png',
-    alt: 'slack',
-  },
-  {
-    id: 'item-2',
-    img: '/img/homepage/missy.jpeg',
-    alt: 'missy',
-  },
-  {
-    id: 'item-2',
-    img: '/img/homepage/check.png',
-    alt: 'check',
-  },
-  {
-    id: 'item-2',
-    img: '/img/homepage/eve-and-chan.png',
-    alt: 'eve-chan',
-  },
-  {
-    id: 'item-2',
-    img: '/img/homepage/maciej.png',
-    alt: 'maciej',
-  },
-  {
-    id: 'item-2',
-    img: '/img/homepage/git.png',
-    alt: 'git',
-  },
-];
-
-const orbit3 = [
-  {
-    id: 'item-3',
-    img: '/img/homepage/ace.jpeg',
-    alt: 'ace',
-  },
-  {
-    id: 'item-3',
-    img: '/img/homepage/star.png',
-    alt: 'star',
-  },
-  {
-    id: 'item-3',
-    img: '/img/homepage/barbano.jpeg',
-    alt: 'barbano',
-  },
-  {
-    id: 'item-3',
-    img: '/img/homepage/dale.jpeg',
-    alt: 'dale',
-  },
-  {
-    id: 'item-3',
-    img: '/img/homepage/kuda.jpeg',
-    alt: 'kuda',
-  },
-  {
-    id: 'item-3',
-    img: '/img/homepage/yash.jpeg',
-    alt: 'yash',
-  },
-  {
-    id: 'item-3',
-    img: '/img/homepage/sergio.jpeg',
-    alt: 'sergio',
-  },
-];
+import orbitData from '../../config/orbitData.json';
 
 function CommunityIndexPage() {
   const image = '/img/social/website-card.png';
@@ -169,7 +61,7 @@ function CommunityIndexPage() {
       <div className="overflow-hidden orbits">
         <div className="orbit-container">
           <div id="first-orbit" className="orbit">
-            {orbit1.map((orbit) => (
+            {orbitData[0].map((orbit) => (
               <div key={orbit.id} className={orbit.id}>
                 <img src={orbit.img} alt={orbit.alt} className="orbit-img" />
               </div>
@@ -179,14 +71,14 @@ function CommunityIndexPage() {
             </div>
           </div>
           <div id="second-orbit" className="orbit">
-            {orbit2.map((orbit) => (
+            {orbitData[1].map((orbit) => (
               <div key={orbit.id} className={orbit.id}>
                 <img src={orbit.img} alt={orbit.alt} />
               </div>
             ))}
           </div>
           <div id="third-orbit" className="orbit">
-            {orbit3.map((orbit) => (
+            {orbitData[2].map((orbit) => (
               <div key={orbit.id} className={orbit.id}>
                 <img src={orbit.img} alt={orbit.alt} />
               </div>
