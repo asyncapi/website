94b4c020a3fea936efb0ba27f6d0c8ae96e1cd8c
akshatnema <20bcs022@iiitdmj.ac.in>
2022-11-30 19:39:37 +0530

    View Source code button added

--- components/tools/ToolsCard.js
+++ components/tools/ToolsCard.js
@@ -1,11 +1,19 @@
-import {useState} from 'react'
+import { useState, useEffect } from 'react'
 import Heading from '../typography/Heading';
 import Paragraph from '../typography/Paragraph';
 import Tag from './Tags';
 import TextTruncate from 'react-text-truncate';
 
-export default function toolsCard({toolData}) {
+export default function toolsCard({ toolData }) {
   const [showDescription, setShowDescription] = useState(false)
+  const [onGithub, setonGithub] = useState(false)
+
+  useEffect(() => {
+    const url = new URL(toolData.links.repoUrl)
+    if (url.host == 'github.com') setonGithub(true)
+    else setonGithub(false)
+  }, [])
+
   return (
     <div className="border shadow-md rounded-lg">
       <div className="pt-8 px-6">
@@ -13,7 +21,7 @@ export default function toolsCard({toolData}) {
           <div className="flex gap-4 justify-between w-full">
             <Heading typeStyle="heading-sm-semibold">{toolData.title}</Heading>
             <div className='bg-green-100 border border-green-600 text-green-600 p-1 text-center text-xs w-fit h-fit rounded-md'>
-              {toolData.filters.hasCommercial=== false ? 'FREE' : 'PAID'}
+              {toolData.filters.hasCommercial === false ? 'FREE' : 'PAID'}
             </div>
           </div>
           <div className='relative'>
@@ -28,14 +36,14 @@ export default function toolsCard({toolData}) {
               </div>
             </Paragraph>
             {showDescription && <div className="absolute top-0 p-2 z-10 bg-white w-full border border-gray-200 shadow-md">
-                <div className='flex gap-2 cursor-pointer text-gray-500 hover:bg-gray-200 p-1 rounded hover:text-dark w-fit' onClick={() => setShowDescription(false)}>
-                  <img src='/img/illustrations/icons/close-icon.svg' width='10' />
-                  <div className='text-sm'>Close</div>
-                </div>
-                <Paragraph typeStyle="body-sm" className='mt-4'>
-                  {toolData.description}
-                </Paragraph>
-              </div>}
+              <div className='flex gap-2 cursor-pointer text-gray-500 hover:bg-gray-200 p-1 rounded hover:text-dark w-fit' onClick={() => setShowDescription(false)}>
+                <img src='/img/illustrations/icons/close-icon.svg' width='10' />
+                <div className='text-sm'>Close</div>
+              </div>
+              <Paragraph typeStyle="body-sm" className='mt-4'>
+                {toolData.description}
+              </Paragraph>
+            </div>}
           </div>
         </div>
       </div>
@@ -55,33 +63,42 @@ export default function toolsCard({toolData}) {
         <div className="flex gap-2 flex-wrap">
           {toolData.filters.technology.map((item, index) => (
             <Tag key={index}
-            name={item.name}
-            bgColor={item.color}
-            borderColor={item.borderColor}
+              name={item.name}
+              bgColor={item.color}
+              borderColor={item.borderColor}
             />
           ))}
         </div>
       </div>
       <hr className="" />
-      <div className="flex">
-        <a className="w-full text-center py-6 hover:bg-gray-200 cursor-pointer"  href={toolData.links.repoUrl} target='_blank' rel='noreferrer'>
-          <div className="m-auto flex w-fit gap-2">
-            <img src="/img/logos/github-black.svg" className="w-5" />
-            <div className="text-gray-700 text-sm">View on Github</div>
-          </div>
-        </a>
-        {toolData.links.websiteUrl && (
-          <>
-            <div className='border border-gray-200 h-auto'></div>
+      {(toolData.links.repoUrl || toolData.links.websiteUrl) &&
+        <div className="flex">
+          {onGithub ?
             <a className="w-full text-center py-6 hover:bg-gray-200 cursor-pointer" href={toolData.links.repoUrl} target='_blank' rel='noreferrer'>
               <div className="m-auto flex w-fit gap-2">
-                <img src="/img/illustrations/icons/share.svg" className="w-5" />
-                <div className="text-gray-700 text-sm">Visit Website</div>
+                <img src="/img/logos/github-black.svg" className="w-5" />
+                <div className="text-gray-700 text-sm">View on Github</div>
+              </div>
+            </a> :
+            <a className="w-full text-center py-6 hover:bg-gray-200 cursor-pointer" href={toolData.links.repoUrl} target='_blank' rel='noreferrer'>
+              <div className="m-auto flex w-fit gap-2">
+                <div className="text-gray-700 text-sm">View Source Code</div>
               </div>
             </a>
-          </>
-        )} 
-      </div>
+          }
+          {toolData.links.websiteUrl && (
+            <>
+              <div className='border border-gray-200 h-auto'></div>
+              <a className="w-full text-center py-6 hover:bg-gray-200 cursor-pointer" href={toolData.links.repoUrl} target='_blank' rel='noreferrer'>
+                <div className="m-auto flex w-fit gap-2">
+                  <img src="/img/illustrations/icons/share.svg" className="w-5" />
+                  <div className="text-gray-700 text-sm">Visit Website</div>
+                </div>
+              </a>
+            </>
+          )}
+        </div>
+      }
     </div>
   );
 }
