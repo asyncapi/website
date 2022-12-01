7f9cad0396d261cc297cba702821f59d14a18ebb
akshatnema <20bcs022@iiitdmj.ac.in>
2022-12-01 16:14:51 +0530

    added try/catch

--- scripts/tools/tools-object.js
+++ scripts/tools/tools-object.js
@@ -36,7 +36,7 @@ async function convertTools(data) {
   let appendData = {};
   const dataArray = data.items;
 
-  for(var index in categoryList){
+  for (var index in categoryList) {
     appendData[categoryList[index].name] = {
       description: categoryList[index].description,
       toolsList: []
@@ -44,45 +44,49 @@ async function convertTools(data) {
   }
 
   for (let tool of dataArray) {
+    try {
+      if (tool.name === '.asyncapi-tool') {
+        // extracting the reference id of the repository which will be used to extract the path of the .asyncapi-tool file in the Tools repository
+        // ex: for a url = "https://api.github.com/repositories/351453552/contents/.asyncapi-tool?ref=61855e7365a881e98c2fe667a658a0005753d873"
+        // the text (id) present after '=' gives us a reference id for the repo
+        let reference_id = tool.url.split("=")[1];
+        let download_url = `https://raw.githubusercontent.com/${tool.repository.full_name}/${reference_id}/${tool.path}`;
 
-    if (tool.name === '.asyncapi-tool') {
-      // extracting the reference id of the repository which will be used to extract the path of the .asyncapi-tool file in the Tools repository
-      // ex: for a url = "https://api.github.com/repositories/351453552/contents/.asyncapi-tool?ref=61855e7365a881e98c2fe667a658a0005753d873"
-      // the text (id) present after '=' gives us a reference id for the repo
-      let reference_id = tool.url.split("=")[1];
-      let download_url = `https://raw.githubusercontent.com/${tool.repository.full_name}/${reference_id}/${tool.path}`;
-      
-      const { data: toolFileContent } = await axios.get(download_url);
+        const { data: toolFileContent } = await axios.get(download_url);
 
-      //some stuff can be YAML
-      const jsonToolFileContent = convertToJson(toolFileContent)
+        //some stuff can be YAML
+        const jsonToolFileContent = convertToJson(toolFileContent)
 
-      //validating against JSON Schema for tools file
-      const isValid = validate(jsonToolFileContent)
+        //validating against JSON Schema for tools file
+        const isValid = validate(jsonToolFileContent)
 
-      if (isValid) {
-        let repositoryUrl = tool.repository.html_url;
-        let isAsyncAPIrepo = tool.repository.owner.login === "asyncapi";
-        let toolObject = createToolObject(jsonToolFileContent, repositoryUrl, isAsyncAPIrepo);
+        if (isValid) {
+          let repositoryUrl = tool.repository.html_url;
+          let isAsyncAPIrepo = tool.repository.owner.login === "asyncapi";
+          let toolObject = createToolObject(jsonToolFileContent, repositoryUrl, isAsyncAPIrepo);
 
-        jsonToolFileContent.filters.categories.forEach((category) => {
-          const categorySearch = fuse.search(category);
+          jsonToolFileContent.filters.categories.forEach((category) => {
+            const categorySearch = fuse.search(category);
 
-          if (categorySearch.length) {
-            let searchedCategoryName = categorySearch[0].item.name
-            if (!appendData[searchedCategoryName].toolsList.find((element => element === toolObject)))
-              appendData[searchedCategoryName].toolsList.push(toolObject);
-          } else {
-            if (!appendData['Others'].toolsList.find((element => element === toolObject)))
-              appendData['Others'].toolsList.push(toolObject);
-          }
-        });
-      } else {
-        console.error('Invalid .asyncapi-tool file.');
-        console.error(`Located in: ${tool.html_url}`);
-        console.error('Validation errors:', JSON.stringify(validate.errors, null, 2));
-        console.error('Not failing, dropping errors for further investigation');
+            if (categorySearch.length) {
+              let searchedCategoryName = categorySearch[0].item.name
+              if (!appendData[searchedCategoryName].toolsList.find((element => element === toolObject)))
+                appendData[searchedCategoryName].toolsList.push(toolObject);
+            } else {
+              if (!appendData['Others'].toolsList.find((element => element === toolObject)))
+                appendData['Others'].toolsList.push(toolObject);
+            }
+          });
+        } else {
+          console.error('Invalid .asyncapi-tool file.');
+          console.error(`Located in: ${tool.html_url}`);
+          console.error('Validation errors:', JSON.stringify(validate.errors, null, 2));
+          console.error('Not failing, dropping errors for further investigation');
+        }
       }
+    } catch (err) {
+      console.error(err)
+      throw err;
     }
   }
   return appendData;
