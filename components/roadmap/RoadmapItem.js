bf3d617a7685629d9427fcafa1a835baadd88c0f
Fran Mendez <fmvilas@gmail.com>
2023-01-13 19:25:52 +0100

    Add a tooltip indicating an outcome or solution is done.

--- components/roadmap/RoadmapItem.js
+++ components/roadmap/RoadmapItem.js
@@ -71,9 +71,11 @@ function Pill ({
               <a href={item.url} rel="noopener noreferrer" onClick={() => !item.url && item.description && setIsDescriptionVisible(true)} className={`block text-gray-900 text-left font-medium ${item.description || item.url ? 'hover:text-gray-600 cursor-pointer' : 'cursor-default'}`}>
                 {
                   item.done && (
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="inline-block mr-2 -mt-0.5 w-6 h-6 text-green-600">
-                      <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                    </svg>
+                    <span title="Done!">
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="inline-block mr-2 -mt-0.5 w-6 h-6 text-green-600">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                      </svg>
+                    </span>
                   )
                 }
                 <span>{ item.title }</span>
