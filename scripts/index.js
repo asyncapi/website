03ccdd8d690602b0ce27a6ebec1fda7021bd43f3
Fran Mendez <fmvilas@gmail.com>
2023-01-13 18:34:42 +0100

    chore: unlink roadmap from shape-up-process repo

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,7 @@
 const rssFeed = require('./build-rss');
-const buildRoadMap = require('./build-roadmap');
 const buildPostList = require('./build-post-list');
 
 async function start() {
-  buildRoadMap();
   await buildPostList();
   rssFeed(
     'blog',
