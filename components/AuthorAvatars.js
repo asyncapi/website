3fdac5b470c9ab5e3088be9cea23eff9d8b438a9
Shofiya2003 <shofiyabootwala@gmail.com>
2023-02-07 10:53:49 +0530

    created a cloudinary url for author profile image in blog page

--- components/AuthorAvatars.js
+++ components/AuthorAvatars.js
@@ -1,11 +1,28 @@
+import {buildImageUrl } from 'cloudinary-build-url'
+
+
 export default function AuthorAvatars({ authors = [] }) {
   return (
     authors.map((author, index) => {
+      let authorPhoto = `https://www.asyncapi.com${author.photo}`
+      authorPhoto = buildImageUrl(authorPhoto, {
+        cloud: {
+          storageType: 'fetch'
+        },
+        transformations: {
+          resize: {
+            type: 'fill',
+            width: 40,
+            height: 40,
+          },
+          radius: 'max',
+        }
+      });
       let avatar = <img
         key={index}
         title={author.name}
-        className={`${index > 0 ? `absolute left-${index * 7} top-0` : `relative mr-${(authors.length - 1) * 7}`} z-${(authors.length - 1 - index) * 10} h-10 w-10 border-2 border-white rounded-full object-cover hover:z-50`}
-        src={author.photo}
+        className={`${index > 0 ? `absolute left-${index * 7} top-0` : `relative mr-${(authors.length - 1) * 7}`} z-${(authors.length - 1 - index) * 10} border-2 border-white object-cover hover:z-50`}
+        src={authorPhoto}
         loading="lazy"
       />
 
