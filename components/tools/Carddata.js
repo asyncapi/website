80605ade1b7385bc4efdeedf945f63252624cf52
Tusharjain4 <jain0735@gmail.com>
2023-02-07 06:02:33 +0530

    fix: add tooltips to the fields inside Tool Card #1229

---
+++ components/tools/Carddata.js
@@ -0,0 +1,44 @@
+import React, {useEffect, useRef, useState} from 'react'
+import InfoIcon from '../icons/InfoIcon'
+import TextTruncate from 'react-text-truncate';
+
+export const Carddata = (props) => {
+  const { classes, visible, heading, data, read, setRead, setVisible, type } = props
+  const [outsideClick, setOutsideClick] = useState(true)
+  const initial = {
+    lang: false,
+    tech: false,
+    category: false
+  }
+  const domNode = useRef() 
+  useEffect(()=>{
+    let maybeHandler = (event) => {
+      setOutsideClick(true) 
+      if( domNode.current && !domNode.current.contains(event.target)){
+        setOutsideClick(false)
+        setRead(false)
+      }
+    }
+    document.addEventListener("mousedown", maybeHandler);
+    return ()=>{ document.removeEventListener("mousedown", maybeHandler)}
+  },[])
+
+  return (
+    <div className={classes || "text-sm text-gray-500"}>
+      {heading}
+      <span className="group relative"  ref={domNode} >
+        {outsideClick && visible[type] && <span className="w-48 text-xs border z-10 bg-white border-gray-200 shadow-md -left-2/3 absolute translate-x-1/3 -top-4 rounded px-2 py-1">
+          {read ? data : <TextTruncate
+            element="span"
+            line={3}
+            text={data}
+          />}
+          <button className='cursor-pointer text-cyan-600' onClick={() => {setOutsideClick(true) ;setRead(!read) }}>{read ? " Show Less" : " Show More"}</button>
+        </span>}
+        <button onClick={() =>{setVisible({ ...initial, [type]: !visible[type] }) }} className="mx-1" >
+          <InfoIcon />
+        </button>
+      </span>
+    </div>
+  )
+}
--- components/tools/ToolsCard.js
+++ components/tools/ToolsCard.js
@@ -4,12 +4,11 @@ import Paragraph from '../typography/Paragraph';
 import Tag from './Tags';
 import TextTruncate from 'react-text-truncate';
 import Data from "../../scripts/tools/tools-schema.json"
+import { Carddata } from './Carddata';
 
 export default function ToolsCard({ toolData }) {
   const [showDescription, setShowDescription] = useState(false)
   const [showMoreDescription, setShowMoreDescription] = useState(false)
-  const [lang, setLang] = useState(false)
-  const [tech, setTech] = useState(false)
   const [readMore, setReadMore] = useState(false)
   const descriptionRef = useRef(null)
   useEffect(() => {
@@ -25,6 +24,11 @@ export default function ToolsCard({ toolData }) {
     if (url.host == 'github.com') onGit = true
     else onGit = false
   }
+
+  const [visible, setVisible] = useState({
+    lang : false,
+    tech: false,
+  })
 
   return (
     <div className="border shadow-md h-auto flex flex-col rounded-lg">
@@ -62,18 +66,7 @@ export default function ToolsCard({ toolData }) {
       <div className="grow">
         {(toolData?.filters?.language || toolData?.filters?.technology?.length > 0) ? <div className="my-6">
           {toolData.filters.language && <div className="flex flex-col gap-2 mx-6">
-            <div className="text-gray-700 text-sm font-semibold">LANGUAGES
-              <span className="group relative">
-                {lang && <span className="w-48 text-xs border z-10 bg-white border-gray-200 shadow-md -left-2/3 absolute translate-x-1/3 -top-4 rounded px-2 font-light py-1">
-                  {Data.properties.filters.properties.language.description.length >= 130 ?
-                    <div>{!readMore ? Data.properties.filters.properties.language.description.slice(0, 118) + "... " : Data.properties.filters.properties.language.description}<button className='cursor-pointer text-cyan-600' onClick={() => { setReadMore(!readMore) }} >{readMore ? " Show Less" : " Show More"}</button></div> :
-                    Data.properties.filters.properties.language.description}
-                </span>}
-                <button onClick={() => { setLang(!lang); setTech(false) }} className="mx-1" ><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 translate-y-2">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
-                </svg></button>
-              </span>
-            </div>
+          <Carddata  classes= 'text-sm text-gray-700'  heading="LANGUAGE" data = {Data.properties.filters.properties.language.description} type="lang" visible = {visible} setVisible = {setVisible} read={readMore} setRead ={setReadMore} />
             <div className="flex gap-2">
               <Tag
                 name={toolData.filters.language.name}
@@ -83,17 +76,7 @@ export default function ToolsCard({ toolData }) {
             </div>
           </div>}
           {toolData.filters.technology.length > 0 && <div className="flex flex-col gap-2 my-4 mx-6">
-            <div className="text-gray-700 text-sm font-semibold">TECHNOLOGIES
-              <span className="group relative">
-                {tech && <span className="w-48 text-xs bg-white absolute left-1 font-light -top-2 border border-gray-200 shadow-md rounded px-2 py-1 translate-x-8 z-10">
-                  {Data.properties.filters.properties.technology.description}
-                </span>}
-                <button onClick={() => { setTech(!tech); setLang(false) }} className="mx-1" ><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 translate-y-2">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
-                </svg>
-                </button>
-              </span>
-            </div>
+          <Carddata classes= 'text-sm text-gray-700' heading="TECHNOLOGIES" data = {Data.properties.filters.properties.technology.description} type="tech" visible = {visible} setVisible = {setVisible} read={readMore} setRead ={setReadMore} />
             <div className="flex gap-2 flex-wrap">
               {toolData.filters.technology.map((item, index) => (
                 <Tag key={index}
