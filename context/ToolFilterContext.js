38548392b43d157191b5a6f699d4e8bc36721701
Olaleye Blessing <Olayinkablexxy@gmail.com>
2023-04-04 15:10:53 +0100

    feat: persist filters as URL query params on Tools page

--- context/ToolFilterContext.js
+++ context/ToolFilterContext.js
@@ -1,15 +1,32 @@
-import { createContext, useState } from 'react'
+import { useRouter } from 'next/router';
+import { createContext, useEffect, useState } from 'react'
 export const ToolFilterContext = createContext()
 
 export default function ToolFilter({ children }) {
+    const router = useRouter();
     const [isPaid, setisPaid] = useState("all");
     const [isAsyncAPIOwner, setAsyncAPIOwner] = useState(false)
     const [languages, setLanguages] = useState([])
     const [technologies, setTechnologies] = useState([])
     const [categories, setCategories] = useState([])
 
+
+    useEffect(() => {
+      if(!router.isReady) return;
+
+        let { pricing, owned, langs, techs, categories } = router.query;
+
+        setisPaid(pricing || "all")
+        setAsyncAPIOwner(owned === "true")
+        setLanguages(langs?.split(",") || [])
+        setTechnologies(techs?.split(",") || [])
+        setCategories(categories?.split(",") || [])
+
+    }, [router.query])
+    
+
     return (
-        <ToolFilterContext.Provider value={{isPaid, isAsyncAPIOwner, languages, technologies, categories, setisPaid, setAsyncAPIOwner, setLanguages, setTechnologies, setCategories}}>
+        <ToolFilterContext.Provider value={{isPaid, isAsyncAPIOwner, languages, technologies, categories}}>
             {children}
         </ToolFilterContext.Provider>
     )
