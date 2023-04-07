19099182f90a9a4efd1150c1953433bd7e56d844
Mahendra Dani <mahendraunofficial09@gmail.com>
2023-04-07 21:36:22 +0530

    Removed changes

--- pages/community/tsc.js
+++ pages/community/tsc.js
@@ -158,7 +158,6 @@ function UserInfo({ user }) {
               <a
                 href={user.github}
                 className="text-gray-600 hover:text-gray-500"
-                target = "_blank"
               >
                 <span className="sr-only">GitHub</span>
                 <GithubSVG />
@@ -169,7 +168,6 @@ function UserInfo({ user }) {
                 <a
                   href={user.twitter}
                   className="text-gray-600 hover:text-gray-500"
-                  target = "_blank"
                 >
                   <span className="sr-only">Twitter</span>
                   <TwitterSVG />
@@ -181,7 +179,6 @@ function UserInfo({ user }) {
                 <a
                   href={user.linkedin}
                   className="text-gray-600 hover:text-gray-500"
-                  target = "_blank"
                 >
                   <span className="sr-only">LinkedIn</span>
                   <LinkedInSVG />
