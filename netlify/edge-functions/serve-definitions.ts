d805c45f74b8112727f89ff7c79836535668e77f
Lukasz Gornicki <lpgornicki@gmail.com>
2023-05-11 18:31:07 +0200

    fix: make sure function for rewriting request supports % (#1653)

--- netlify/edge-functions/serve-definitions.ts
+++ netlify/edge-functions/serve-definitions.ts
@@ -15,7 +15,7 @@ const URL_DEST_DEFINITIONS = "https://raw.githubusercontent.com/asyncapi/spec-js
 //   Examples: /definitions/asyncapi.yaml OR /schema-store/2.4.0.JSON (uppercase)
 //
 // Non-legitimate requests should not use our GitHub Token and affect the rate limit. Those shouldn't send metrics to NR either as they just add noise.
-const legitimateRequestRegex = /^\/[\w\-]*\/?(?:([\w\-\.]*\/)?([\w\-$\.]*\.json))?$/
+const legitimateRequestRegex = /^\/[\w\-]*\/?(?:([\w\-\.]*\/)?([\w\-$%\.]*\.json))?$/
 
 export default async (request: Request, context: Context) => {
   let rewriteRequest = buildRewrite(request);
