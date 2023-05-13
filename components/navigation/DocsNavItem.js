b70f39fcb9719d8beb84b942f99ecf3228017473
Priyansh61 <21bec080@iiitdmj.ac.in>
2023-05-13 23:24:36 +0530

    added useEffect to scroll the sidebar on active item chagne or on rerender

--- components/navigation/DocsNavItem.js
+++ components/navigation/DocsNavItem.js
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import { useEffect,useRef, useState } from 'react';
 
 function isActiveSlug(slug, activeSlug, sectionSlug) {
   if(slug === '/docs' || (sectionSlug !== undefined && slug === sectionSlug)) {
@@ -14,9 +15,22 @@ function isActiveSlug(slug, activeSlug, sectionSlug) {
 export default function DocsNavItem({ title, slug, href, activeSlug, sectionSlug, onClick = () => {}, defaultClassName = '', inactiveClassName = '', activeClassName = '', bucket }) {
   const isActive = isActiveSlug(slug, activeSlug, sectionSlug);
   const classes = `${isActive ? activeClassName : inactiveClassName} ${defaultClassName} inline-block`;
+  const activeRef = useRef(null);
+    
+  useEffect(() => {
+    if (activeRef.current) {
+      activeRef.current.scrollIntoView({ 
+        behavior: 'auto', 
+        block: 'center', 
+        inline: 'start',
+    }
+    )};
+  }, [activeSlug]);
+
+
 
   return (
-    <div className='inline-block'>
+    <div className='inline-block' ref = {slug === activeSlug ? activeRef : null}>
       <div className={classes}>
         <Link href={href || slug}>
           <a href={href || slug} onClick={onClick}>
