cc433c05eeadf49741ec06e7d52f3dd4b2b5e6fb
Ashish Padhy <100484401+Shurtu-gal@users.noreply.github.com>
2023-05-16 03:58:39 +0530

    feat: refactor case study code

--- pages/casestudies/[id].js
+++ pages/casestudies/[id].js
@@ -1,12 +1,55 @@
-import React from 'react';
-import Link from 'next/link';
-import Paragraph from '../../components/typography/Paragraph';
-import GenericLayout from '../../components/layout/GenericLayout';
-import Heading from '../../components/typography/Heading';
-import CaseStudiesList from '../../config/case-studies.json';
-import { MDXRemote } from 'next-mdx-remote';
-import { getMDXComponents } from '../../components/MDX.js';
-import { serialize } from 'next-mdx-remote/serialize'
+import React from "react";
+import Link from "next/link";
+import Paragraph from "../../components/typography/Paragraph";
+import Heading from "../../components/typography/Heading";
+import CaseStudiesList from "../../config/case-studies.json";
+import { MDXRemote } from "next-mdx-remote";
+import { getMDXComponents } from "../../components/MDX.js";
+import { serialize } from "next-mdx-remote/serialize";
+import CaseStudyLayout from "../../components/layout/CaseStudyLayout";
+
+const renderContent = (content, allComponents, level) => {
+  const typeStyle =
+    level === 0 ? "heading-lg" : level === 1 ? "heading-md" : "heading-sm";
+
+  return content.map((item) => {
+    return (
+      <div
+        className="mt-10"
+        key={item.title}
+        id={item.title
+          .replace(/<|>|"|\\|\/|=/gi, "")
+          .replace(/\s/gi, "-")
+          .toLowerCase()}
+      >
+        <Heading typeStyle={typeStyle} className="mt-8">
+          {item.title}
+        </Heading>
+        {item.content && (
+          <Paragraph typeStyle="body-md" className="my-4">
+            <MDXRemote {...item.content} components={allComponents} />
+          </Paragraph>
+        )}
+        {item.items && (
+          <div className="mt-4 items-center">
+            <div className="flex flex-wrap gap-2">
+              {item.items.map((item) => (
+                <span
+                  key={item}
+                  className="bg-green-100 border border-green-600 text-green-600 p-1 text-center text-xs rounded-md "
+                >
+                  {item}
+                </span>
+              ))}
+            </div>
+            {item.children &&
+              renderContent(item.children, allComponents, level + 1)}
+          </div>
+        )}
+      </div>
+    );
+  });
+};
 
 export async function getStaticProps({ params }) {
   const data = CaseStudiesList.filter((p) => p.id === params.id);
@@ -45,8 +88,7 @@ export async function getStaticPaths() {
   };
 }
 
-
-function Index({ 
+function Index({
   casestudy,
   challenges,
   solution,
@@ -64,235 +106,175 @@ function Index({
   asyncapiDocumentation,
   asyncapiBindings,
   asyncapiTools,
-  additionalResources
+  additionalResources,
 }) {
-  const image = '/img/social/website-card.png';
+  const image = "/img/social/website-card.png";
   const allComponents = getMDXComponents();
   var contacts = casestudy.company.contact
+
+  const content = [
+    {
+      title: "Challenges",
+      content: challenges,
+    },
+    {
+      title: "Solution",
+      content: solution,
+    },
+    {
+      title: "Use Case",
+      content: usecase,
+    },
+    {
+      title: "More Details",
+      items: [
+        `Languages: ${casestudy.technical.languages[0]}`,
+        `Frameworks: ${casestudy.technical.frameworks[0]}`,
+        `Protocols: ${casestudy.technical.protocols[0]}`,
+      ],
+      children: [
+        {
+          title: "Testing strategy",
+          content: testing,
+        },
+        {
+          title: "Approach to code generation",
+          content: codegen,
+        },
+        {
+          title: "Architecture",
+          content: architecture,
+        },
+        {
+          title: "More Details about AsyncAPI",
+          items: [
+            `Version: ${casestudy.asyncapi.versions[0]}`,
+            `Who maintains documents: ${casestudy.asyncapi.maintainers}}`,
+            `Internal users: ${casestudy.asyncapi.audience.internal.toString()}`,
+            `External users: ${casestudy.asyncapi.audience.external.toString()}`,
+          ],
+          children: [
+            {
+              title: "How AsyncAPI documents are stored",
+              content: asyncapiStorage,
+            },
+            {
+              title: "Where maintainers edit AsyncAPI documents",
+              content: asyncapiEditing,
+            },
+            {
+              title: "What extensions are used",
+              content: asyncapiExtensions,
+            },
+            {
+              title: "How documentation is generated",
+              content: asyncapiDocumentation,
+            },
+            {
+              title: "What bindings are used",
+              content: asyncapiBindings,
+            },
+            {
+              title: "What tools are used",
+              content: asyncapiTools,
+            },
+          ],
+        },
+        {
+          title: "Schemas",
+          items: [`Spec: ${casestudy.schemas.description}`],
+          children: [
+            {
+              title: "Storage strategy",
+              content: schemaStorage,
+            },
+            {
+              title: "Schema Registry",
+              content: registry,
+            },
+            {
+              title: "Versioning of schemas",
+              content: versioning,
+            },
+            {
+              title: "Validation of message schemas",
+              content: validation,
+            },
+            {
+              title: "Additional Resources",
+              content: additionalResources,
+            },
+          ],
+        },
+      ],
+    },
+  ];
+
   return (
-    <GenericLayout
+    <CaseStudyLayout
       title="AsyncAPI Case Studies"
       description="The home for all case studies related to AsyncAPI."
+      content={content}
       image={image}
       hideBanner={true}
       wide
     >
-    <div className="px-4 sm:px-6 xl:px-8 xl:flex-1 xl:max-w-5xl">
-      <div className="mt-10 md:mt-20 flex flex-col md:flex-row justify-between items-center">
-        <div className="w-full md:w-[65%]">
-          <Heading typeStyle="heading-xl" className="countdown-text-gradient">
-            {casestudy.company.name}
-          </Heading>
-          <div className='flex flex-wrap gap-1'>
-              {contacts.map((item, index) => (
-                <div key={index}>
-                  <Heading typeStyle="body-lg">
-                    <Link href={item.link}>
-                      <a className="text-md leading-5 font-medium text-gray-900 
-                      hover:underline" target="_blank">
-                        {item.name}{index != contacts.length - 1 ? ', ' : ' '}
-                      </a>
-                    </Link>
-                  </Heading>
-                </div>)
-              )}
-            </div>
-          <div className="mt-4 flex flex-wrap gap-2">
-            <span className="bg-green-100 border border-green-600 text-green-600 p-1 text-center text-xs rounded-md ">
-                Industry: {casestudy.company.industry}
-            </span>
-            <span className="bg-green-100 border border-green-600 text-green-600 p-1 text-center text-xs rounded-md ">
-                Customers: {casestudy.company.customers}
-            </span>
-            <span className="bg-green-100 border border-green-600 text-green-600 p-1 text-center text-xs rounded-md ">
-                Revenue: {casestudy.company.revenue}
-            </span>
-          </div>
-          <div className="mt-10">
-            <Heading typeStyle="body-lg">
-                    {casestudy.company.description}
+      <div className="px-4 sm:px-6 xl:px-8 xl:flex-1 xl:max-w-5xl">
+        <div className="mt-10 md:mt-20 flex flex-col md:flex-row justify-between items-center">
+          <div className="w-full md:w-[65%]">
+            <Heading typeStyle="heading-xl" className="countdown-text-gradient">
+              {casestudy.company.name}
             </Heading>
-            <Heading className="mt-10" typeStyle="body-lg">
-                    tl;dr just go and have a look at
-                    <Link href={'/'+casestudy.asyncapi.fullExample}>
-                      <a className="ml-2 text-secondary-500 underline hover:text-gray-800 font-medium transition ease-in-out duration-300" target="_blank">
-                        full production-used AsyncAPI document
-                      </a>
-                    </Link>
-            </Heading>
-          </div>
-        </div>
-        <div className="mt-10 md:mt-0">
-          <img
-            src={casestudy.company.logo}
-            alt={casestudy.company.name}
-            className="w-[350px] rounded-lg"
-          />
-        </div>
-      </div>
-      <div className="mt-10">
-        <Heading typeStyle="heading-lg" className="mt-8">
-            Challenges
-        </Heading>
-        <Paragraph typeStyle="body-md" className="my-4">
-          <MDXRemote {...challenges} components={allComponents} />
-        </Paragraph>
-      </div>
-      <div className="mt-10">
-        <Heading typeStyle="heading-lg" className="mt-8">
-            Solution
-        </Heading>
-        <Paragraph typeStyle="body-md" className="my-4">
-          <MDXRemote {...solution} components={allComponents} />
-        </Paragraph>
-      </div>
-      <div className="mt-10">
-        <Heading typeStyle="heading-lg" className="mt-8">
-            Use case
-        </Heading>
-        <Paragraph typeStyle="body-md" className="my-4">
-          <MDXRemote {...usecase} components={allComponents} />
-        </Paragraph>
-      </div>
-      <div className="mt-10">
-        <Heading typeStyle="heading-lg" className="mt-8">
-            More details
-        </Heading>
-        <div className="mt-4 items-center">
-          <div className='flex flex-wrap gap-2'>
-          <span className="bg-green-100 border border-green-600 text-green-600 p-1 text-center text-xs rounded-md ">
-            Languages: {casestudy.technical.languages[0]}
-          </span>
-          <span className="bg-green-100 border border-green-600 text-green-600 p-1 text-center text-xs rounded-md ">
-            Frameworks: {casestudy.technical.frameworks[0]}
-          </span>
-          <span className=" bg-green-100 border border-green-600 text-green-600 p-1 text-center text-xs rounded-md ">
-            Protocols: {casestudy.technical.protocols[0]}
-          </span>
-          </div>
-          <div className="mt-10">
-            <Heading typeStyle="heading-md" className="mt-8">
-                Testing strategy
-            </Heading>
-            <Paragraph typeStyle="body-md" className="my-4">
-              <MDXRemote {...testing} components={allComponents} />
-            </Paragraph>
-          </div>
-          <div className="mt-10">
-            <Heading typeStyle="heading-md" className="mt-8">
-                Approach to code generation
-            </Heading>
-            <Paragraph typeStyle="body-md" className="my-4">
-              <MDXRemote {...codegen} components={allComponents} />
-            </Paragraph>
-          </div>
-          <div className="mt-10">
-            <Heading typeStyle="heading-md" className="mt-8">
-              Architecture
-            </Heading>
-            <Paragraph typeStyle="body-md" className="my-4">
-              <MDXRemote {...architecture} components={allComponents} />
-            </Paragraph>
-          </div>
-          <div className="mt-10">
-            <Heading typeStyle="heading-md" className="mt-8">
-              More details about AsyncAPI
-            </Heading>
-            <div className="mt-4  flex flex-wrap gap-2">
+            <div className='flex flex-wrap gap-1' id='Contacts'>
+                {contacts.map((item, index) => (
+                  <div key={index}>
+                    <Heading typeStyle="body-lg">
+                      <Link href={item.link}>
+                        <a className="text-md leading-5 font-medium text-gray-900 
+                        hover:underline" target="_blank">
+                          {item.name}{index != contacts.length - 1 ? ', ' : ' '}
+                        </a>
+                      </Link>
+                    </Heading>
+                  </div>)
+                )}
+              </div>
+            <div className="mt-4 flex flex-wrap gap-2">
               <span className="bg-green-100 border border-green-600 text-green-600 p-1 text-center text-xs rounded-md ">
-                Versions: {casestudy.asyncapi.versions[0]}
+                  Industry: {casestudy.company.industry}
               </span>
-              <span className=" bg-green-100 border border-green-600 text-green-600 p-1 text-center text-xs rounded-md ">
-                Who maintains documents: {casestudy.asyncapi.maintainers}
+              <span className="bg-green-100 border border-green-600 text-green-600 p-1 text-center text-xs rounded-md ">
+                  Customers: {casestudy.company.customers}
               </span>
-              <span className=" bg-green-100 border border-green-600 text-green-600 p-1 text-center text-xs rounded-md ">
-                Internal users: {casestudy.asyncapi.audience.internal.toString()}
-              </span>
-              <span className=" bg-green-100 border border-green-600 text-green-600 p-1 text-center text-xs rounded-md ">
-                External users: {casestudy.asyncapi.audience.external.toString()}
+              <span className="bg-green-100 border border-green-600 text-green-600 p-1 text-center text-xs rounded-md ">
+                  Revenue: {casestudy.company.revenue}
               </span>
             </div>
-            <Heading typeStyle="heading-sm" className="mt-8">
-              How AsyncAPI documents are stored
-            </Heading>
-            <Paragraph typeStyle="body-md" className="my-4">
-              <MDXRemote {...asyncapiStorage} components={allComponents} />
-            </Paragraph>
-            <Heading typeStyle="heading-sm" className="mt-8">
-              Where maintainers edit AsyncAPI documents
-            </Heading>
-            <Paragraph typeStyle="body-md" className="my-4">
-              <MDXRemote {...asyncapiEditing} components={allComponents} />
-            </Paragraph>
-            <Heading typeStyle="heading-sm" className="mt-8">
-              What extensions are used
-            </Heading>
-            <Paragraph typeStyle="body-md" className="my-4">
-              <MDXRemote {...asyncapiExtensions} components={allComponents} />
-            </Paragraph>
-            <Heading typeStyle="heading-sm" className="mt-8">
-              How documentation is generated
-            </Heading>
-            <Paragraph typeStyle="body-md" className="my-4">
-              <MDXRemote {...asyncapiDocumentation} components={allComponents} />
-            </Paragraph>
-            <Heading typeStyle="heading-sm" className="mt-8">
-              What bindings are used
-            </Heading>
-            <Paragraph typeStyle="body-md" className="my-4">
-              <MDXRemote {...asyncapiBindings} components={allComponents} />
-            </Paragraph>
-            <Heading typeStyle="heading-sm" className="mt-8">
-              What tools are used
-            </Heading>
-            <Paragraph typeStyle="body-md" className="my-4">
-              <MDXRemote {...asyncapiTools} components={allComponents} />
-            </Paragraph>
+            <div className="mt-10">
+              <Heading typeStyle="body-lg">
+                      {casestudy.company.description}
+              </Heading>
+              <Heading className="mt-10" typeStyle="body-lg">
+                      tl;dr just go and have a look at
+                      <Link href={'/'+casestudy.asyncapi.fullExample}>
+                        <a className="ml-2 text-secondary-500 underline hover:text-gray-800 font-medium transition ease-in-out duration-300" target="_blank">
+                          full production-used AsyncAPI document
+                        </a>
+                      </Link>
+              </Heading>
+            </div>
           </div>
-          <div className="mt-10">
-            <Heading typeStyle="heading-md" className="mt-8">
-              Schemas
-            </Heading>
-            <div className="mt-4 items-center">
-              <span className=" bg-green-100 border border-green-600 text-green-600 p-1 text-center text-xs rounded-md ">
-                Spec: {casestudy.schemas.description}
-              </span>
-            </div>
-            <Heading typeStyle="heading-sm" className="mt-8">
-              Storage strategy
-            </Heading>
-            <Paragraph typeStyle="body-md" className="my-4">
-              <MDXRemote {...schemaStorage} components={allComponents} />
-            </Paragraph>
-            <Heading typeStyle="heading-sm" className="mt-8">
-              Schema Registry
-            </Heading>
-            <Paragraph typeStyle="body-md" className="my-4">
-              <MDXRemote {...registry} components={allComponents} />
-            </Paragraph>
-            <Heading typeStyle="heading-sm" className="mt-8">
-              Versioning of schemas
-            </Heading>
-            <Paragraph typeStyle="body-md" className="my-4">
-              <MDXRemote {...versioning} components={allComponents} />
-            </Paragraph>
-            <Heading typeStyle="heading-sm" className="mt-8">
-              Validation of message schemas
-            </Heading>
-            <Paragraph typeStyle="body-md" className="my-4">
-              <MDXRemote {...validation} components={allComponents} />
-            </Paragraph>
-            <Heading typeStyle="heading-sm" className="mt-8">
-              Additional resources
-            </Heading>
-            <Paragraph typeStyle="body-md" className="my-4">
-              <MDXRemote {...additionalResources} components={allComponents} />
-            </Paragraph>
+          <div className="mt-10 md:mt-0">
+            <img
+              src={casestudy.company.logo}
+              alt={casestudy.company.name}
+              className="w-[350px] rounded-lg"
+            />
           </div>
         </div>
-      </div>
-    </div>
-    </GenericLayout>
+        {renderContent(content, allComponents, 0)}
+        </div>
+    </CaseStudyLayout>
   );
 }
 
