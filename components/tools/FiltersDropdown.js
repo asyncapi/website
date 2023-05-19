fd8679cf5604ad48d488dbdc98865db3e0c63948
Sambhav Gupta <sambhavgupta0705@gmail.com>
2023-05-19 18:13:31 +0530

    test design

--- components/tools/FiltersDropdown.js
+++ components/tools/FiltersDropdown.js
@@ -13,13 +13,13 @@ export default function FiltersDropdown({dataList=[], checkedOptions=[], setStat
     setStateFunction(tempValueArray)
   }
   return (
-    <div className={twMerge(` border w-full max-h-[6rem] z-10 text-gray-00 text-bold cursor-pointer ${className}`)}>
+    <div className={twMerge(` border w-full max-h-[8rem] z-100 text-gray-00 text-bold cursor-pointer ${className}`)}>
       {dataList.map((data, index) => {
         let checked = checkedOptions.indexOf(data.name)!=-1 ? true : false
         return (
-          <div key={index} className={twMerge(`p-1 pb-0 flex gap-1 cursor-pointer items-start ${checked ? 'bg-gray-100' : ''}`)} onClick={(e) => handleClickOption(e, data.name)}>
+          <div key={index} className={twMerge(`p-1 pb-0 flex gap-1 cursor-pointer items-start ${checked ? 'bg-gray-300' : ''}`)} onClick={(e) => handleClickOption(e, data.name)}>
             
-            <div className='text-sm font-200 -mt-[1px] mb-[1px]'>{data.name}</div>
+            <div className='text-md px-2 py-0.5 font-500 -mt-[1px] mb-[1px]'>{data.name}</div>
         </div>
         )})}
     </div>
