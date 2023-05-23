622c655e345f1a7ae4225b91dc07ad20224ddeaa
Ashutosh <ashutosh_rath@users.noreply.github.com>
2023-05-23 17:34:41 +0530

    feat: add testimonial carousel

--- components/Testimonial.js
+++ components/Testimonial.js
@@ -1,35 +1,104 @@
-import Paragraph from "./typography/Paragraph";
+const testimonials = [
+  {
+    imgUrl: '/img/testimonials/matt-mclarty.jpg',
+    name: 'Matt McLarty',
+    salutation: 'Global Leader of API Strategy at MuleSoft',
+    text: 'Microservices underline the need for event-based communication in distributed architectures. AsyncAPI brings the richness of the REST API ecosystem to asynchronous APIs.',
+  },
+  {
+    imgUrl: '/img/testimonials/bill-doerrfeld.jpg',
+    name: 'Bill Doerrfeld',
+    salutation: 'Editor in Chief at Nordic APIs',
+    text: 'Microservices underline the need for event-based communication in distributed architectures. AsyncAPI brings the richness of the REST API ecosystem to asynchronous APIs.',
+  },
+  {
+    imgUrl: '/img/testimonials/jonathan-schabowsky.jpg',
+    name: 'Jonathan Schabowsky',
+    salutation: 'Sr. Architect, Office of the CTO at Solace',
+    text: "Developers need to be able to quickly and consistently create event-driven applications that provide business value and react to customer needs in realtime. I can't count how many times I've heard developers ask for OpenAPI/Swagger style tools for the asynchronous and event driven world, and that is exactly what the AsyncAPI initiative is making a reality.",
+  },
+  {
+    imgUrl: '/img/testimonials/eric-horesnyi.jpg',
+    name: 'Eric Horesnyi',
+    salutation: 'CEO at Streamdata.io',
+    text: 'We’ve been focusing on event-driven APIs since 2014 and thank the AsyncAPI contributors everyday for driving the community towards common standards.',
+  },
+];
+import React, { useState } from 'react';
 
-export default function Testimonial({
-  className = '',
-  text,
-  authorName,
-  authorDescription,
-  authorAvatar,
-}) {
+const Testimonial = () => {
+  const len = testimonials.length;
+  const [activeIndex, setActive] = useState(0);
+
+  const getStyle = (idx) => {
+    const distanceLeft = idx - activeIndex;
+    const distanceRight =
+      distanceLeft > 0 ? distanceLeft - len : distanceLeft + len;
+    const distance =
+      Math.abs(distanceLeft) > Math.abs(distanceRight)
+        ? distanceRight
+        : distanceLeft;
+
+    const styleObj = {};
+
+    if (distance === 0) {
+      styleObj.left = '33.3%';
+      styleObj.zIndex = 1;
+      styleObj.opacity = 1;
+      styleObj.transform = 'scale(1)';
+    } else {
+      styleObj.left =
+        distance > 0 ? `${12.7 + distance * 40}%` : `${54 + distance * 40}%`;
+    }
+
+    if (Math.abs(distance) >= 2) {
+      styleObj.opacity = 0;
+      styleObj.transform = 'scale(0)';
+    }
+
+    return styleObj;
+  };
+
   return (
-    <li className={`py-4 px-4 sm:px-6 md:flex md:flex-row md:py-4 md:pr-0 ${className}`}>
-      <blockquote className="mt-8 md:flex-grow md:flex md:flex-col">
-        <div className="relative text-lg leading-7 font-medium text-gray-600 md:flex-1">
-          <svg className="absolute top-0 left-0 transform -translate-y-2 h-8 w-8 text-primary-500" fill="currentColor" viewBox="0 0 32 32">
-            <path d="M9.352 4C4.456 7.456 1 13.12 1 19.36c0 5.088 3.072 8.064 6.624 8.064 3.36 0 5.856-2.688 5.856-5.856 0-3.168-2.208-5.472-5.088-5.472-.576 0-1.344.096-1.536.192.48-3.264 3.552-7.104 6.624-9.024L9.352 4zm16.512 0c-4.8 3.456-8.256 9.12-8.256 15.36 0 5.088 3.072 8.064 6.624 8.064 3.264 0 5.856-2.688 5.856-5.856 0-3.168-2.304-5.472-5.184-5.472-.576 0-1.248.096-1.44.192.48-3.264 3.456-7.104 6.528-9.024L25.864 4z" />
-          </svg>
-          <Paragraph className="relative pl-10 text-left">
-            {text}
-          </Paragraph>
-        </div>
-        <footer className="mt-6">
-          <div className="flex">
-            <figure className="flex-shrink-0 inline-flex rounded-full border-2 border-white">
-              <img className="h-12 w-12 rounded-full" src={authorAvatar} alt={authorName}/>
-            </figure>
-            <div className="ml-4 text-left">
-              <p className="text-base leading-6 font-bold text-gray-900">{authorName}</p>
-              <p className="text-base leading-6 font-medium text-primary-500">{authorDescription}</p>
+    <div>
+      <div className="h-[460px] lg:h-[500px] overflow-hidden">
+        {testimonials.map(({ imgUrl, name, salutation, text }, index) => (
+          <div
+            className="h-[440px] lg:h-[480px] w-[35%] transition-all duration-500 absolute opacity-80 scale-90 cursor-pointer select-none flex flex-col items-center justify-center border border-gray-300 rounded-lg bg-white shadow-xl z-1}"
+            key={index}
+            onClick={() => setActive(index)}
+            style={getStyle(index)}
+          >
+            <div className="rounded-full h-16 w-16 lg:h-32 lg:w-32 overflow-hidden mt-[-1rem] mb-2 lg:mb-4 lg:mt-[-2rem]">
+              <img src={imgUrl} className="h-full w-full" alt={name} />
             </div>
+            <p className="text-[14px] lg:text-base lg:leading-6 font-bold text-gray-900 mb-2">
+              {name}
+            </p>
+            <p className="text-[12px] lg:text-base lg:leading-6 font-medium text-primary-500 mb-2 lg:mb-4">
+              {salutation}
+            </p>
+            <p className="h-[200px] text-[12px] px-1 lg:px-6 overflow-clip lg:text-lg">
+              {text}
+            </p>
           </div>
-        </footer>
-      </blockquote>
-    </li>
-  )
-}
+        ))}
+      </div>
+      <div className="h-[20px] flex justify-center items-center">
+        {testimonials.map((value, index) => (
+          <div
+            key={index}
+            className={
+              activeIndex === index
+                ? 'h-[10px] w-[10px] bg-[#c62f2f] rounded-full mr-2 cursor-pointer '
+                : 'h-[10px] w-[10px] rounded-full bg-[#ddd] mr-2 cursor-pointer'
+            }
+            onClick={() => setActive(index)}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Testimonial;
