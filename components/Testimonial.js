db3beea1a484985c268c51dc7a9e44067c8e8c88
Lucif3r-in <ashutosh_rath@users.noreply.github.com>
2023-05-28 17:47:10 +0530

    fix: fix nav arrow && chore: add content to config

--- components/Testimonial.js
+++ components/Testimonial.js
@@ -1,35 +1,10 @@
 import { FaArrowLeft, FaArrowRight } from 'react-icons/fa'
 
-const testimonials = [
-  {
-    imgUrl: '/img/testimonials/matt-mclarty.jpg',
-    name: 'Matt McLarty',
-    salutation: 'Global Leader of API Strategy at MuleSoft',
-    text: 'Microservices underline the need for event-based communication in distributed architectures. AsyncAPI brings the richness of the REST API ecosystem to asynchronous APIs.',
-  },
-  {
-    imgUrl: '/img/testimonials/bill-doerrfeld.jpg',
-    name: 'Bill Doerrfeld',
-    salutation: 'Editor in Chief at Nordic APIs',
-    text: 'Microservices underline the need for event-based communication in distributed architectures. AsyncAPI brings the richness of the REST API ecosystem to asynchronous APIs.',
-  },
-  {
-    imgUrl: '/img/testimonials/jonathan-schabowsky.jpg',
-    name: 'Jonathan Schabowsky',
-    salutation: 'Sr. Architect, Office of the CTO at Solace',
-    text: "Developers need to be able to quickly and consistently create event-driven applications that provide business value and react to customer needs in realtime. I can't count how many times I've heard developers ask for OpenAPI/Swagger style tools for the asynchronous and event driven world, and that is exactly what the AsyncAPI initiative is making a reality.",
-  },
-  {
-    imgUrl: '/img/testimonials/eric-horesnyi.jpg',
-    name: 'Eric Horesnyi',
-    salutation: 'CEO at Streamdata.io',
-    text: 'We’ve been focusing on event-driven APIs since 2014 and thank the AsyncAPI contributors everyday for driving the community towards common standards.',
-  },
-];
+import TestimonialData from '../config/content/Testimonial';
 import React, { useState } from 'react';
 
 const Testimonial = () => {
-  const len = testimonials.length;
+  const len = TestimonialData.length;
   const [activeIndex, setActive] = useState(0);
 
   const getStyle = (idx) => {
@@ -72,12 +47,12 @@ const Testimonial = () => {
 
   return (
     <div className="flex flex-row justify-between items-center gap-3 h-full"> 
-      <div className="h-8 w-8 rounded-full bg-[#ddd] cursor-pointer mb-2" onClick={goToPrevious}>
+      <div className="h-8 w-8 rounded-full bg-[#ddd] cursor-pointer mb-2 z-60" onClick={goToPrevious}>
         <FaArrowLeft className="mt-[20%] m-auto"/>
       </div>
       <div className="flex flex-col">
       <div className="h-[460px] lg:h-[500px]">
-         {testimonials.map(({ imgUrl, name, salutation, text }, index) => (
+         {TestimonialData.map(({ imgUrl, name, salutation, text }, index) => (
            <div
              className="h-[440px] lg:h-[480px] w-[40%] transition-all duration-500 absolute opacity-80 scale-90 cursor-pointer select-none flex flex-col items-center justify-center border border-gray-300 rounded-lg bg-white shadow-xl z-1}"
              key={index}
@@ -102,7 +77,7 @@ const Testimonial = () => {
       </div>
       <div className="h-[20px] flex justify-center items-center">
     
-        {testimonials.map((value, index) => (
+        {TestimonialData.map((value, index) => (
           <div
             key={index}
             className={
@@ -116,7 +91,7 @@ const Testimonial = () => {
       </div>
       </div>
           <div
-            className="h-8 w-8 rounded-full bg-[#ddd] cursor-pointer mb-2"
+            className="h-8 w-8 rounded-full bg-[#ddd] cursor-pointer mb-2 z-60"
             onClick={goToNext}
           >
             <FaArrowRight className="mt-[20%] m-auto"/>
---
+++ config/content/Testimonial.js
@@ -0,0 +1,28 @@
+const TestimonialData = [
+    {
+      imgUrl: '/img/testimonials/matt-mclarty.jpg',
+      name: 'Matt McLarty',
+      salutation: 'Global Leader of API Strategy at MuleSoft',
+      text: 'Microservices underline the need for event-based communication in distributed architectures. AsyncAPI brings the richness of the REST API ecosystem to asynchronous APIs.',
+    },
+    {
+      imgUrl: '/img/testimonials/bill-doerrfeld.jpg',
+      name: 'Bill Doerrfeld',
+      salutation: 'Editor in Chief at Nordic APIs',
+      text: 'Microservices underline the need for event-based communication in distributed architectures. AsyncAPI brings the richness of the REST API ecosystem to asynchronous APIs.',
+    },
+    {
+      imgUrl: '/img/testimonials/jonathan-schabowsky.jpg',
+      name: 'Jonathan Schabowsky',
+      salutation: 'Sr. Architect, Office of the CTO at Solace',
+      text: "Developers need to be able to quickly and consistently create event-driven applications that provide business value and react to customer needs in realtime. I can't count how many times I've heard developers ask for OpenAPI/Swagger style tools for the asynchronous and event driven world, and that is exactly what the AsyncAPI initiative is making a reality.",
+    },
+    {
+      imgUrl: '/img/testimonials/eric-horesnyi.jpg',
+      name: 'Eric Horesnyi',
+      salutation: 'CEO at Streamdata.io',
+      text: 'We’ve been focusing on event-driven APIs since 2014 and thank the AsyncAPI contributors everyday for driving the community towards common standards.',
+    },
+  ];
+
+  export default TestimonialData
