6fdce87f69c383bf8b592d8fb331c1eca1358458
14Richa <onlinericha19@gmail.com>
2023-06-09 21:21:33 +0530

    changed file name to maintainers

--- scripts/build-tsc-members.js
+++ scripts/build-tsc-members.js
@@ -1,8 +1,8 @@
 const fs = require("fs");
 const { convertToJson } = require("./utils");
 
-const inputFile = "config/TSC_MEMBERS.yml";
-const outputFile = "config/TSC_MEMBERS.json";
+const inputFile = "config/Maintainers.yaml";
+const outputFile = "config/Maintainers.json";
 
 try {
   // Read YAML file
