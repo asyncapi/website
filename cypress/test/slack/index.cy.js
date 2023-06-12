088daada5fd3b47520f1e17d383ac34dde173418
reachaadrika <aadrikabhargava12@gmail.com>
2023-06-13 02:53:28 +0530

    adding tests for both components

---
+++ cypress/test/slack/index.cy.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { mount } from 'cypress/react';
+import Slack from '../../../components/slack/index';
+
+describe('Slack Component', () => {
+  it('renders the Slack messages correctly', () => {
+    mount(<Slack />);
+
+    // Verify the first Slack message
+    cy.get('[data-testid="slack-message"]').eq(0).as('message1').should('exist')
+
+    // Verify the second Slack message
+    cy.get('[data-testid="slack-message"]').eq(0).as('message2').should('exist')
+
+    // Verify the third Slack message
+    cy.get('[data-testid="slack-message"]').eq(0).as('message3').should('exist')
+
+    // Verify the fourth Slack message
+    cy.get('[data-testid="slack-message"]').eq(0).as('message4').should('exist')
+  });
+});
