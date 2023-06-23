3039ec3dc654c5f6962556071a51d61ec5d1036c
Tushar123 <jain0735@gmail.com>
2023-06-23 19:10:11 +0530

    fix: filter section is overflowing for small screen in dashboard sect… #1690

--- components/dashboard/table/Filters.js
+++ components/dashboard/table/Filters.js
@@ -65,13 +65,13 @@ export default function Filters({
         {open && (
           <div
             ref={floating}
+            className={`${strategy} ${(x && x > 0) ? `left-[${x}px]` : "left-[14px]"}`}
             style={{
-              position: strategy,
               top: y ?? '',
-              left: x>0? x??'':"14px",
+              left: x > 0 ? x : "",
             }}
           >
-            <div className="bg-white shadow-xl rounded w-[19rem]">
+            <div className="bg-white shadow-xl w-96 rounded max-w-[19rem]">
               <div className="flex p-4">
                 <h4 className="text-base">Filter Issues</h4>
                 <button onClick={() => setOpen(!open)} className="ml-auto">
