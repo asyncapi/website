6cce2f58b9f250d437911393ba396e7f1adbafb2
reachaadrika <aadrikabhargava12@gmail.com>
2023-07-02 23:39:36 +0530

    additional fix

--- cypress/test/tools/Carddata.cy.js
+++ cypress/test/tools/Carddata.cy.js
@@ -25,29 +25,4 @@ describe('Carddata Component', () => {
     cy.get('button').click();
     cy.get('.custom-class').should('exist');
   });
-
-  it('should toggle the read state when "Show More" button is clicked', () => {
-    const props = {
-      className: 'custom-class',
-      visible: {
-        lang: true,
-        tech: false,
-        category: true,
-        pricing: false,
-        ownership: true
-      },
-      heading: <h1>Card Heading</h1>,
-      data: 'Card Data',
-      read: false,
-      setRead: cy.stub(),
-      setVisible: cy.stub(),
-      type: 'lang'
-    };
-    mount(<Carddata { ...props } />);
-    cy.contains('Show More').should('exist').click();
-    cy.get('.custom-class button').then((buttons) => {
-      const showLessButton = buttons.toArray().find((button) => button.innerText.includes('Show Less'));
-      expect(showLessButton).to.exist;
-    });
-  });
 });
