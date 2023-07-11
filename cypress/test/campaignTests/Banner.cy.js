bcd67f7a710f418fd39d6204230b848ed6657da2
reachaadrika <aadrikabhargava12@gmail.com>
2023-07-12 01:02:55 +0530

    fixed tests for Banner

--- cypress/test/campaignTests/Banner.cy.js
+++ cypress/test/campaignTests/Banner.cy.js
@@ -3,62 +3,48 @@ import { mount } from '@cypress/react';
 import Banner from '../../../components/campaigns/Banner';
 
 describe('Banner Component', () => {
-  it('should render the banner when the date is within the valid range', () => {
+  it('should not render the banner when the date is not within the valid range', () => {
     const today = new Date();
     const [day,  month, year] = [today.getUTCDate(),  today.getUTCMonth(),  today.getUTCFullYear()];
     if (year > 2022 || month !== 10 || day < 6) {
       mount(<Banner />);
-      cy.get('[data-testid="banner-main-div"]').should('not.exist');
- 
+      cy.get('[data-testid="Banner-main-div"]').should('not.exist');
+
     } else {
       mount(<Banner />);
-      cy.get('[data-testid="banner-main-div"]').should('be.visible');
-    
+      cy.get('[data-testid="Banner-main-div"]').should('be.visible');
+
     }
+  });
+
+  it('should render the banner when the date is within the valid range', () => {
+    const mockDate = new Date(2021, 10, 12).getTime();
+    cy.clock(mockDate);
+    mount(<Banner />);
+    cy.get('[data-testid="Banner-main-div"]').should('be.visible');
   });
 
   it('should display the correct message when the date is within the valid range', () => {
-    const today = new Date();
-    const [day,  month, year] = [today.getUTCDate(),  today.getUTCMonth(),  today.getUTCFullYear()];
-    if (year > 2022 || month !== 10 || day < 6) {
-      mount(<Banner />);
-      //checking css class that font is medium or not 
-      cy.contains('.font-medium', 'AsyncAPI Conference 2022 has ended').should('not.exist');
-    } else {
-      mount(<Banner />);
-      cy.contains('.font-medium', 'AsyncAPI Conference 2022 has ended').should('be.visible');
-    }
+    const mockDate = new Date(2021, 10, 12).getTime();
+    cy.clock(mockDate);
+    mount(<Banner />);
+    cy.contains('.font-medium', 'AsyncAPI Conference 2022 has ended').should('be.visible');
   });
 
   it('should have a link to the recordings playlist', () => {
-    const today = new Date();
-    const [day,  month, year] = [today.getUTCDate(),  today.getUTCMonth(),  today.getUTCFullYear()]
-    if (year === 2022 && month === 10 && day >= 6) {
-      mount(<Banner />);
-     
-      cy.get('[data-testid="banner-link"]')
-        .should('have.attr', 'href', 'https://www.youtube.com/playlist?list=PLbi1gRlP7pijRiA32SU36hD_FW-2qyPhl')
-        .should('have.attr', 'target', '_blank')
-        .should('have.attr', 'rel', 'noopener noreferrer');
-    } else {
-      mount(<Banner />);
-   
-      cy.get('[data-testid="banner-link"]').should('not.exist');
-    }
+    const mockDate = new Date(2021, 10, 12).getTime();
+    cy.clock(mockDate);
+    mount(<Banner />);
+    cy.get('[data-testid="Banner-link"]')
+      .should('have.attr', 'href', 'https://www.youtube.com/playlist?list=PLbi1gRlP7pijRiA32SU36hD_FW-2qyPhl')
+      .should('have.attr', 'target', '_blank')
+      .should('have.attr', 'rel', 'noopener noreferrer');
   });
-  //check to see if max-w-screen is present or not , for full width element
+
   it('should have the max-w-screen-xl class in the div element', () => {
-    const today = new Date();
-    const [day,  month, year] = [today.getUTCDate(),  today.getUTCMonth(),  today.getUTCFullYear()];
-    if (year === 2022 && month === 10 && day >= 6) {
-      mount(<Banner />);
-      //check for max-w-screen-xl class, since it is an imp check
-      cy.get('.max-w-screen-xl').should('exist');
-     
-    } else {
-      mount(<Banner />);
-      cy.get('.max-w-screen-xl').should('not.exist');
-    }
+    const mockDate = new Date(2021, 10, 12).getTime();
+    cy.clock(mockDate);
+    mount(<Banner />);
+    cy.get('.max-w-screen-xl').should('exist');
   });
 });
-
