e3e69de48fc04061e96455713aa34b843f021801
Aadrika Bhargava <64789514+reachaadrika@users.noreply.github.com>
2023-07-13 18:29:45 +0530

    Update cypress/utils/router.js
    Co-authored-by: Akshat Nema <76521428+akshatnema@users.noreply.github.com>

--- cypress/utils/router.js
+++ cypress/utils/router.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { RouterContext } from 'next/dist/shared/lib/router-context';
-import { useRouter } from 'next/router';
 
 const createRouter = (params) => ({
   route: '/',
