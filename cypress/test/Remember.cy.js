2c22d325d2147aed0dcb0a3ea7f3ea91a4a219ed
Aadrika Bhargava <64789514+reachaadrika@users.noreply.github.com>
2023-07-15 02:51:01 +0530

    Update Remember.cy.js

--- cypress/test/Remember.cy.js
+++ cypress/test/Remember.cy.js
@@ -19,7 +19,7 @@ describe('Remember Component', () => {
           .should('have.length', 1)
           .within(() => {
             cy.get(LightBulb).should('exist');
-            cy.contains('[data-testid="Remember-span"]', title).should('exist');
+            cy.contains('[data-testid="Remember-title"]', title).should('exist');
           });
 
         cy.get('[data-testid="Remember-children"]').should('exist');
@@ -29,12 +29,12 @@ describe('Remember Component', () => {
   it('renders the component with default title and no children', () => {
     const className = 'remember';
     mount(<Remember className={className} />);
-    cy.get('[data-testid="Remember-div"]')
+    cy.get('[data-testid="Remember-main"]')
       .should('exist')
       .within(() => {
         cy.get('[data-testid="Remember-heading"]').should('have.length', 1).within(() => {
           cy.get(LightBulb).should('exist');
-          cy.contains('[data-testid="Remember-span"]', 'Remember').should('exist');
+          cy.contains('[data-testid="Remember-title"]', 'Remember').should('exist');
         });
         cy.get('[data-testid="Remember-children"]').should('not.exist');
       });
