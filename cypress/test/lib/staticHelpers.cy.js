be0c34b5fe3f221101231a5b1c1a91bd27419178
reachaadrika <aadrikabhargava12@gmail.com>
2023-07-16 20:00:58 +0530

    add getEvents tests

--- cypress/test/lib/staticHelpers.cy.js
+++ cypress/test/lib/staticHelpers.cy.js
@@ -1,20 +1,34 @@
 import { mount } from '@cypress/react';
 
 import {
-  getStaticPropsHelper,
-  getStaticPathsHelper,
-  getEvents,
+getEvents
 } from '../../../lib/staticHelpers';
 
 describe('getEvents', () => {
   it('should return sorted events in ascending order', () => {
     const events = [
-      { date: '2023-07-10' },
-      { date: '2023-07-08' },
-      { date: '2023-07-09' },
+      {
+        "title": "Community Meeting",
+        "calLink": "https://www.google.com/calendar/event?eid=czRmMG5maHRsYjduM2g3dmwxMDM1Z3R0NzAgY19xOXRzZWlnbG9tZHNqNm5qdWh2YnB0czExY0Bn",
+        "url": "https://github.com/asyncapi/community/issues/645",
+        "date": "2023-04-04T08:00:00.000Z"
+      },
+      {
+        "title": "Spec 3.0 Meeting",
+        "calLink": "https://www.google.com/calendar/event?eid=djhsdjZvbmRsampvb2tsYzhkZWFyc3FtYTAgY19xOXRzZWlnbG9tZHNqNm5qdWh2YnB0czExY0Bn",
+        "url": "https://github.com/asyncapi/community/issues/649",
+        "date": "2023-04-13T15:00:00.000Z"
+      },
+      {
+        "title": "Community Meeting",
+        "calLink": "https://www.google.com/calendar/event?eid=MzgwdmZiMTc4cnBmbTUzdWVlbmM4aWYyM2MgY19xOXRzZWlnbG9tZHNqNm5qdWh2YnB0czExY0Bn",
+        "url": "https://github.com/asyncapi/community/issues/659",
+        "banner": "https://user-images.githubusercontent.com/40604284/229763606-c0b6ed3b-e120-427c-b87d-357856d92777.png",
+        "date": "2023-04-18T16:00:00.000Z"
+      },
     ];
 
-    cy.wrap(events).as('events'); // Preserve events for further assertions
+    cy.wrap(events).as('events'); 
 
     // Call the getEvents function
     cy.get('@events').then((events) => {
@@ -30,60 +44,6 @@ describe('getEvents', () => {
       for (let i = 0; i < sortedTimestamps.length - 1; i++) {
         expect(sortedTimestamps[i]).to.be.greaterThan(sortedTimestamps[i + 1]);
       }
-    });
-  });
-});
-
-describe('getStaticPropsHelper', () => {
-  it('should return the expected props', () => {
-    // Mock the necessary functions
-    const getSectionItemsStub = cy.stub().returns([{ title: 'Section Item' }]);
-    const getPostBySlugStub = cy.stub().returns({
-      title: 'Test Post',
-      date: '2023-07-12',
-      slug: 'test-post',
-      fullSlug: '/test-post',
-      author: 'John Doe',
-      content: 'Lorem ipsum dolor sit amet',
-      ogImage: 'og-image.jpg',
-      coverImage: 'cover-image.jpg',
-    });
-
-    // Call the getStaticProps function
-    const getStaticProps = getStaticPropsHelper('/test-path');
-    return getStaticProps({ params: { slug: 'test-slug' } }).then((props) => {
-      // Assert the returned props
-      expect(props.post.title).to.equal('Test Post');
-      expect(props.post.content).to.equal('Lorem ipsum dolor sit amet');
-      expect(props.navItems).to.deep.equal([
-        {
-          title: 'Getting started',
-          slug: 'docs/getting-started',
-          items: [{ title: 'Section Item' }],
-        },
-        {
-          title: 'Tutorials',
-          slug: 'docs/tutorials',
-          items: [{ title: 'Section Item' }],
-        },
-      ]);
-
-      // Assert the stubbed function calls
-      expect(getSectionItemsStub).to.be.calledWith('docs/getting-started');
-      expect(getPostBySlugStub).to.be.calledWith(
-        '/test-path',
-        'test-slug',
-        [
-          'title',
-          'date',
-          'slug',
-          'fullSlug',
-          'author',
-          'content',
-          'ogImage',
-          'coverImage',
-        ]
-      );
     });
   });
 });
