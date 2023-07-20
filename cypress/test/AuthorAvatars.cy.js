384ca7357940ecbe55cd7d4933a3aab88e4ed4d9
akshatnema <20bcs022@iiitdmj.ac.in>
2023-07-20 21:59:06 +0530

    Update mock data

--- cypress/test/AuthorAvatars.cy.js
+++ cypress/test/AuthorAvatars.cy.js
@@ -62,6 +62,4 @@ describe('AuthorAvatars', () => {
         });
     });
   });
-
-
 });
--- cypress/test/Figure.cy.js
+++ cypress/test/Figure.cy.js
@@ -4,8 +4,8 @@ import Figure from '../../components/Figure';
 
 describe('Figure', () => {
   it('renders the figure with the image and caption', () => {
-    const src = 'example.jpg';
-    const caption = 'Example Caption';
+    const src = '/img/posts/2020-summary/active-members.webp';
+    const caption = 'Figure 1: Slack active members weekly';
     const widthClass = 'w-50';
     const className = 'custom-class';
     const float = 'left';
@@ -32,7 +32,7 @@ describe('Figure', () => {
     cy.get('[data-testid="Figure-img"]').should('have.attr', 'alt', altOnly);
     cy.get('[data-testid="Figure-img"]').should('have.class', imageClass);
 
-    cy.contains('Caption').should('be.visible');
-    cy.contains('Caption').should('have.text', caption);
+    cy.contains(caption).should('be.visible');
+    cy.contains(caption).should('have.text', caption);
   });
 });
