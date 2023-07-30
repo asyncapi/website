f9143f88951bcbb02d2ec2e8d73621901f08ecd1
reachaadrika <aadrikabhargava12@gmail.com>
2023-07-30 23:45:14 +0530

    add copy to clipboard functionality

--- cypress/test/editor/CodeBlock.cy.js
+++ cypress/test/editor/CodeBlock.cy.js
@@ -2,13 +2,29 @@ import { mount } from 'cypress/react'
 import CodeBlock from '../../../components/editor/CodeBlock'
 
 describe('CodeBlock component', () => {
-  it('should render correctly', () => {
+  beforeEach(() => {
     mount(
       <CodeBlock>
-        {`const message = 'Hello, World!';\nconsole.log(message);`}
+        { `const message = 'Hello, World!';\nconsole.log(message);` }
       </CodeBlock>
     )
-    cy.get('.bg-code-editor-dark').should('exist')
-   
+  });
+
+  it('should render correctly', () => {
+     cy.get('.bg-code-editor-dark').should('exist')
+
   })
+  it('should copy code to clipboard when clicking the copy button', () => {
+    const copiedText = "const message = 'Hello, World!';\r\nconsole.log(message);"
+   
+    cy.get('[data-test="copy-button"]').click({force:true})
+    cy.get('[data-testid="clicked-text"]').should('exist');
+
+    cy.window().then((win) => {
+      cy.document().then((doc) => {
+        cy.wrap(win.navigator.clipboard.readText()).should('eq', copiedText)
+      })
+    })
+  })
+
 })
