42ca5a39ada98d8d1f4497bde84f0d172f218218
reachaadrika <aadrikabhargava12@gmail.com>
2023-07-30 22:46:54 +0530

    change in ToolsList

--- cypress/test/tools/ToolsList.cy.js
+++ cypress/test/tools/ToolsList.cy.js
@@ -3,11 +3,13 @@ import { default as ToolsList } from '../../../components/tools/ToolsList';
 import toolsData from '../../../config/tools.json';
 describe('ToolsList Component', () => {
   it('renders correctly with toolsData', () => {
-    mount(<ToolsList toolsData={ toolsData } />);
+    mount(<ToolsList toolsData={toolsData} />);
     cy.get('[data-testid="ToolsList-main"]').should('exist');
+
     Object.keys(toolsData).forEach((categoryName, index) => {
-      cy.contains(categoryName);
-      cy.contains(toolsData[categoryName].description);
+      console.log('Current Category:', categoryName);
+      cy.contains('[data-testid="ToolsList-main"]', categoryName).should('exist');
+      cy.contains('[data-testid="ToolsList-main"]', toolsData[categoryName].description).should('exist');
     });
   });
 });
