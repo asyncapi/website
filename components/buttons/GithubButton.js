7778d3a7c6766363a2511e8a6ed61b892f18423c
Ansh Goyal <anshgoyal1704@gmail.com>
2023-08-02 12:32:39 +0530

    Update components/buttons/GithubButton.js
    Co-authored-by: Maciej Urbańczyk <urbanczyk.maciej.95@gmail.com>

--- components/buttons/GithubButton.js
+++ components/buttons/GithubButton.js
@@ -2,7 +2,7 @@ import Button from './Button'
 import IconGithub from '../icons/Github'
 
 export default function GithubButton({
-  text = 'Github',
+  text = 'View on Github',
   href = 'https://github.com/asyncapi',
   target = '_blank',
   iconPosition = 'left',
