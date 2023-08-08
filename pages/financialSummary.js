fe9a006449677dad701230a528a05dcc185307fd
Vishvamsinh Vaghela <vaghelavishvamsinh11111@gmail.com>
2023-08-08 12:25:30 +0530

    graph added

--- pages/financialSummary.js
+++ pages/financialSummary.js
@@ -3,6 +3,153 @@ import Heading from "../components/typography/Heading"
 import Container from '../components/layout/Container'
 import Paragraph from '../components/typography/Paragraph'
 import Button from '../components/buttons/Button'
+import React, { useState, useEffect } from 'react'
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts'
+import style from "react-syntax-highlighter/dist/esm/styles/hljs/a11y-dark"
+
+const Expenses = [{
+  Link: "https://opencollective.com/asyncapi/expenses/116776",
+  Category: "AsyncAPI Ambassador",
+  Month: "January",
+  Quater: "Q1",
+  Amount: "68.95"
+}, {
+  Link: "https://opencollective.com/asyncapi/expenses/118171",
+  Category: "Google Season of Docs 2022",
+  Month: "May",
+  Quater: "Q2",
+  Amount: "2000"
+}, {
+  Link: "https://opencollective.com/asyncapi/expenses/18495",
+  Category: "Google Season of Docs 2022",
+  Month: "July",
+  Quater: "Q3",
+  Amount: "1666.67"
+}, {
+  Link: "https://opencollective.com/asyncapi/expenses/18495",
+  Category: "AsyncAPI Bounty",
+  Month: "July",
+  Quater: "Q3",
+  Amount: "1639"
+}, {
+  Link: "https://opencollective.com/asyncapi/expenses/18495",
+  Category: "AsyncAPI Bounty",
+  Month: "August",
+  Quater: "Q3",
+  Amount: "1639"
+}
+]
+
+
+const BarChartComponent = ({ data }) => {
+  // State for selected filters
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [selectedYearQuarter, setSelectedYearQuarter] = useState(null);
+
+  // Prepare data for chart
+  const categoryOptions = [...new Set(data.map((item) => item.Category))];
+
+  // Generate all yearly quarters (Q1, Q2, Q3, Q4) based on data
+  const yearQuarterOptions = [];
+  data.forEach((item) => {
+    const yearQuarter = `${item.Month} - ${item.Quater}`;
+    if (!yearQuarterOptions.includes(yearQuarter)) {
+      yearQuarterOptions.push(yearQuarter);
+    }
+  });
+
+  // Filter data based on selected filters
+  const filteredData = data.filter(
+    (item) =>
+      (!selectedCategory || item.Category === selectedCategory) &&
+      (!selectedYearQuarter || `${item.Month} - ${item.Quater}` === selectedYearQuarter)
+  );
+
+  // Calculate total expenses for each category
+  const totalExpensesByCategory = {};
+  filteredData.forEach((item) => {
+    totalExpensesByCategory[item.Category] =
+      (totalExpensesByCategory[item.Category] || 0) + parseFloat(item.Amount);
+  });
+
+  // Prepare data for chart
+  const chartData = Object.keys(totalExpensesByCategory).map((category) => ({
+    Category: category,
+    Amount: totalExpensesByCategory[category],
+  }));
+
+  const totalExpenses = Object.values(totalExpensesByCategory).reduce(
+    (total, expense) => total + expense,
+    0
+  );
+
+  const [windowWidth, setWindowWidth] = useState(null);
+
+  const handleResize = () => {
+    setWindowWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const barWidth = windowWidth < 900 ? 300 : 800;
+  const barHeight = windowWidth < 900 ? 200 : 400;
+
+  return (
+    <div className="flex justify-center mt-8">
+      <div className="w-full lg:w-2/3 px-4">
+        <div style={{ marginBottom: '20px' }}>
+          <h1 className="text-4xl font-semibold mb-2 my-2">Budget Analysis</h1>
+          <p>Gain insights into the allocation of funds across different categories through our Budget Analysis</p>
+          <h4 className="text-sm font-semibold mb-2 my-4">Total Expenses: ${totalExpenses.toFixed(2)}</h4>
+          {/* Select for category filter */}
+          <select
+            className="w-full p-2 border border-gray-300 rounded-md"
+            style={{ color: "gray", backgroundColor: "#f9f7f3", fontWeight: 600 }}
+            value={selectedCategory}
+            onChange={(e) => setSelectedCategory(e.target.value)}
+          >
+            <option value="">All Categories</option>
+            {categoryOptions.map((category) => (
+              <option key={category} value={category}>
+                {category}
+              </option>
+            ))}
+          </select>
+
+          {/* Select for year/quarter filter */}
+          <select
+            className="w-full mt-2 p-2 border border-gray-300 rounded-md"
+            style={{ color: "gray", backgroundColor: "#f9f7f3", fontWeight: 600 }}
+            value={selectedYearQuarter}
+            onChange={(e) => setSelectedYearQuarter(e.target.value)}
+          >
+            <option value="">Total Yearly Expense</option>
+            {yearQuarterOptions.map((yearQuarter) => (
+              <option key={yearQuarter} value={yearQuarter}>
+                {yearQuarter}
+              </option>
+            ))}
+          </select>
+        </div>
+        {/* Recharts BarChart */}
+        <BarChart width={barWidth} height={barHeight} data={chartData}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="Category" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="Amount" fill="rgba(123,93,211,1)" />
+        </BarChart>
+      </div>
+    </div>
+  );
+};
 
 const cardsData = [
   {
@@ -278,12 +425,12 @@ function SuccessStories() {
 function ContactUs() {
   return (
     <Container wide>
-      <div className="grid lg:grid-cols-9 lg:gap-8 lg:text-center my-8">
+      <div className="grid lg:grid-cols-9 lg:gap-8 lg:text-center my-16">
         <div className="col-start-3 col-span-5">
           <Heading level="h1" typeStyle="heading-md">Interested in getting in touch?</Heading>
           <Paragraph typeStyle="body-sm" className="my-2 max-w-4xl">
             Feel free to contact us if you need more explanation. We are happy to hop on a call and help with
-            onboarding to the project as a sponsor. Write email to <span style={{color:"#8054F2" ,fontWeight:900}}>info@asyncapi.io.</span>
+            onboarding to the project as a sponsor. Write email to <span style={{ color: "#8054F2", fontWeight: 900 }}>info@asyncapi.io.</span>
           </Paragraph>
         </div>
       </div>
@@ -314,7 +461,8 @@ function FinancialSummary() {
         <OtherFormsOfFinancialSupport />
         <ExpenseBreakdown />
         <SuccessStories />
-        <ContactUs/>
+        <BarChartComponent data={Expenses} />
+        <ContactUs />
       </Container>
 
     </GenericLayout>
