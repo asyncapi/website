a5e018e02a98358ce02b0d9cdbd005724b6d5633
Vishvamsinh Vaghela <vaghelavishvamsinh11111@gmail.com>
2023-08-08 15:07:48 +0530

    expense object updated

--- components/FinancialSummary/ExpenseObject.js
+++ components/FinancialSummary/ExpenseObject.js
@@ -1,33 +1,133 @@
 const Expenses = [{
-    Link: "https://opencollective.com/asyncapi/expenses/116776",
     Category: "AsyncAPI Ambassador",
     Month: "January",
     Quater: "Q1",
     Amount: "68.95"
 }, {
-    Link: "https://opencollective.com/asyncapi/expenses/118171",
     Category: "Google Season of Docs 2022",
-    Month: "May",
-    Quater: "Q2",
-    Amount: "2000"
+    Month: "January",
+    Quater: "Q1",
+    Amount: "35.62"
 }, {
-    Link: "https://opencollective.com/asyncapi/expenses/18495",
     Category: "Google Season of Docs 2022",
-    Month: "July",
-    Quater: "Q3",
+    Month: "January",
+    Quater: "Q1",
     Amount: "1666.67"
 }, {
-    Link: "https://opencollective.com/asyncapi/expenses/18495",
+    Category: "AsyncAPI Mentorship 2022",
+    Month: "January",
+    Quater: "Q1",
+    Amount: "1500"
+}, {
+    Category: "AsyncAPI Mentorship 2022",
+    Month: "January",
+    Quater: "Q1",
+    Amount: "1500"
+}, {
+    Category: "AsyncAPI Mentorship 2022",
+    Month: "January",
+    Quater: "Q1",
+    Amount: "1500"
+}, {
+    Category: "Community Manager Salary",
+    Month: "February",
+    Quater: "Q1",
+    Amount: "1000.39"
+}, {
+    Category: "AsyncAPI Mentorship 2022",
+    Month: "February",
+    Quater: "Q1",
+    Amount: "1500"
+}, {
+    Category: "Community Manager Salary",
+    Month: "March",
+    Quater: "Q1",
+    Amount: "2000.39"
+}, {
+    Category: "AsyncAPI Mentorship 2022",
+    Month: "March",
+    Quater: "Q1",
+    Amount: "1500"
+}, {
+    Category: "AsyncAPI Mentorship 2022",
+    Month: "March",
+    Quater: "Q1",
+    Amount: "1500"
+}, {
+    Category: "Community Manager Salary",
+    Month: "April",
+    Quater: "Q2",
+    Amount: "2000.39"
+}, {
+    Category: "Community Manager Salary",
+    Month: "May",
+    Quater: "Q2",
+    Amount: "2000.39"
+}, {
+    Category: "	AsyncAPI Webstore",
+    Month: "May",
+    Quater: "Q2",
+    Amount: "75.11"
+}, {
+    Category: "AsyncAPI Bounty",
+    Month: "May",
+    Quater: "Q2",
+    Amount: "400"
+}, {
+    Category: "Community Manager Salary",
+    Month: "June",
+    Quater: "Q2",
+    Amount: "2000.39"
+}, {
+    Category: "AsyncAPI Bounty",
+    Month: "June",
+    Quater: "Q2",
+    Amount: "200"
+}, {
+    Category: "3rd Party Services",
+    Month: "June",
+    Quater: "Q2",
+    Amount: "28.31"
+}, {
+    Category: "AsyncAPI Bounty",
+    Month: "June",
+    Quater: "Q2",
+    Amount: "200"
+}, {
+    Category: "AsyncAPI Bounty",
+    Month: "June",
+    Quater: "Q2",
+    Amount: "200"
+}, {
+    Category: "AsyncAPI Bounty",
+    Month: "June",
+    Quater: "Q2",
+    Amount: "200"
+}, {
+    Category: "Community Manager Salary",
+    Month: "July",
+    Quater: "Q3",
+    Amount: "2000.39"
+}, {
+    Category: "3rd Party Services",
+    Month: "July",
+    Quater: "Q3",
+    Amount: "1088.27"
+}, {
     Category: "AsyncAPI Bounty",
     Month: "July",
     Quater: "Q3",
-    Amount: "1639"
+    Amount: "400"
 }, {
-    Link: "https://opencollective.com/asyncapi/expenses/18495",
-    Category: "AsyncAPI Bounty",
-    Month: "August",
+    Category: "	AsyncAPI Webstore",
+    Month: "July",
     Quater: "Q3",
-    Amount: "1639"
+    Amount: "15671.63"
+}, {
+    Category: "AsyncAPI Bounty",
+    Month: "July",
+    Quater: "Q3",
+    Amount: "400"
 }
 ]
 
