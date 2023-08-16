2f25ceedd1f493ed5d93a156592f766ac4e6561d
akshatnema <akshatnema.school@gmail.com>
2023-08-16 19:21:37 +0530

    Add tests for AnnouncementHero component

--- cypress/test/campaignTests/AnnouncementHero.cy.js
+++ cypress/test/campaignTests/AnnouncementHero.cy.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { mount } from '@cypress/react';
 import AnnouncementHero from '../../../components/campaigns/AnnoucementHero';
 beforeEach(() => {
+  const mockDate = new Date('2023-05-01T00:00:00Z');
+  cy.clock(mockDate.getTime());
   mount(<AnnouncementHero />);
 });
 
@@ -9,8 +11,30 @@ describe('AnnouncementHero Component', () => {
   it('should render the component when the date is within the valid range', () => {
     const mockDate = new Date(2021, 10, 12).getTime();
     cy.clock(mockDate);
-    cy.get('[data-testid="AnnouncementHero-main-div"]').should('not.exist');
+    cy.get('[data-testid="AnnouncementHero-main-div"]').should('exist');
   });
+
+  //check if announcement rendered is small or large .
+  it('should render a small announcement when "small" prop is true', () => {
+    mount(<AnnouncementHero small />);
+    cy.get('[data-testid="AnnouncementHero-main-div"]').should('have.class', 'mb-4');
+  });
+
+  it('should display the correct event information', () => {
+    // Assert the event details
+    cy.get('[data-testid="AnnouncementHero-main-div"]').contains('AsyncAPI Conf on Tour 2023').should('exist');
+    cy.get('[data-testid="AnnouncementHero-main-div"]').contains('London Edition').should('exist');
+    cy.get('[data-testid="AnnouncementHero-main-div"]').contains('20th of September, 2023 | London, UK').should('exist');
+    cy.contains('Submit a session').should('exist');
+  });
+
+  it('should have a link to submit a session', () => {
+    // Assert the link
+    cy.get('[data-testid="AnnouncementHero-submit-session"]').should('have.attr', 'href', 'https://conference.asyncapi.com/')
+      .should('have.attr', 'target', '_blank')
+      .contains('Submit a session');
+  });
+
   //check if announcement rendered is small or large .
   it('should render a small announcement when "small" prop is true', () => {
     const mockDate = new Date('2023-05-01T00:00:00Z');
