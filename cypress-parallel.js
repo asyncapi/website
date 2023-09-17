a5be9acc85f537343f2df09b29bad87abc34af33
Atharv Chandratre <atharvchandratre@gmail.com>
2023-09-17 11:59:44 -0500

    Trying to see if cypress-tests.yml parallelizes runs

--- cypress-parallel.js
+++ cypress-parallel.js
@@ -4,7 +4,7 @@ const path = require('path');
 const NODE_INDEX = Number(process.argv[2] || 1);
 const NODE_TOTAL = Number(process.argv[3] || 1);
 
-const TEST_FOLDER = './cypress/test';
+const TEST_FOLDER = './cypress/e2e';
 
 console.log(getSpecFiles().join(','))
 
