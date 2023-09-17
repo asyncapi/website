85a40d30f256254c25dc430f3e029281b2ebdd9b
Atharv Chandratre <atharvchandratre@gmail.com>
2023-09-17 11:07:25 -0500

    Trying to see if cypress-tests.yml parallelizes runs

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -15,6 +15,7 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
+require('@shelex/cypress-allure-plugin');
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
