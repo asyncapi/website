c33f17737d6e69d9d21f87d7ba457ea1a9db9107
Atharv Chandratre <atharvchandratre@gmail.com>
2023-09-17 12:37:18 -0500

    Trying to see if cypress-tests.yml parallelizes runs

--- cypress.config.js
+++ cypress.config.js
@@ -6,16 +6,7 @@ module.exports = defineConfig({
       framework: "next",
       bundler: "webpack",
     },
-    video: false,
-    screenshotOnRunFailure: false,
-  },
-
-  e2e: {
-    devServer: {
-      framework: "next",
-      bundler: "webpack",
-    },
-    video: false,
-    screenshotOnRunFailure: false,
+    video : false,
+    screenshotOnRunFailure : false
   },
 });
