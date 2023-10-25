b0b538e939473922a50af0702dcb2d98617343fb
Vishvamsinh Vaghela <vaghelavishvamsinh11111@gmail.com>
2023-10-25 22:02:02 +0530

    code and data updates

--- scripts/adopters/index.js
+++ scripts/adopters/index.js
@@ -1,21 +1,18 @@
-const { readdir, writeFile, readFile } = require('fs').promises;
+const { promises: { readFile, writeFile } } = require('fs');
 const { convertToJson } = require('../utils');
 const { resolve } = require('path');
 
 module.exports = async function buildAdoptersList() {
-    let AdoptersList = [];
     try {
-            const AdoptersContent = await readFile('config/adopters/adopters.yml', 'utf-8');
-            const jsonContent = convertToJson(AdoptersContent);
-            
-            AdoptersList.push(jsonContent);
-            await writeFile(
-                resolve(__dirname, '../../config', 'adopters.json'),
-                JSON.stringify(AdoptersList)
-            )
-        }
-     catch (err) {
-        console.log(err);
-        throw err
+        const AdoptersContent = await readFile('config/adopters/adopters.yml', 'utf-8');
+        const jsonContent = convertToJson(AdoptersContent);
+        
+        await writeFile(
+            resolve(__dirname, '../../config', 'adopters.json'),
+            JSON.stringify([jsonContent])
+        );
+    } catch (err) {
+        console.error(err);
+        throw err;
     }
 };
