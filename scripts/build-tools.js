ee9e1432914b7b64e973cafe2e9d85d6dcc267af
Prince Rajpoot <prince.rajpoot.20@gmail.com>
2023-10-30 01:19:20 +0530

    lint fix

--- scripts/build-tools.js
+++ scripts/build-tools.js
@@ -7,44 +7,44 @@ const fs = require('fs');
 const { resolve } = require('path');
 
 const combineAutomatedAndManualTools = async (automatedTools) => {
-    try {
-        await combineTools(automatedTools, manualTools);
-    } catch (err) {
-        console.log("Error while combining tools:", err);
-        throw err;
-    }
+  try {
+    await combineTools(automatedTools, manualTools);
+  } catch (err) {
+    console.log("Error while combining tools:", err);
+    throw err;
+  }
 };
 
 const buildTools = async () => {
-    try {
-        let githubExtractData = await getData();
-        let automatedTools = await convertTools(githubExtractData);
-        fs.writeFileSync(
-            resolve(__dirname, '../config', 'tools-automated.json'),
-            JSON.stringify(automatedTools, null, '  ')
-        );
-        await combineAutomatedAndManualTools(automatedTools);
-    } catch (err) {
-        console.log(err);
-        throw err;
-    }
+  try {
+    let githubExtractData = await getData();
+    let automatedTools = await convertTools(githubExtractData);
+    fs.writeFileSync(
+        resolve(__dirname, '../config', 'tools-automated.json'),
+        JSON.stringify(automatedTools, null, '  ')
+    );
+    await combineAutomatedAndManualTools(automatedTools);
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
 };
 
 const buildToolsManual = async () => {
-    try {
-        const automatedTools = require('../config/tools-automated.json');
-        await combineAutomatedAndManualTools(automatedTools);
-    } catch (err) {
-        console.log(err);
-        throw err;
-    }
+  try {
+    const automatedTools = require('../config/tools-automated.json');
+    await combineAutomatedAndManualTools(automatedTools);
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
 };
 
 if (require.main === module) {
-    buildTools();
+  buildTools();
 }
 
 module.exports = {
-    buildTools,
-    buildToolsManual
+  buildTools,
+  buildToolsManual
 };
