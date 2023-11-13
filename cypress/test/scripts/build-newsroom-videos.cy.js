35fb90349b43aa3ccb269c60936ecd4768fced99
Atharv Chandratre <atharvchandratre@gmail.com>
2023-11-13 08:32:32 -0600

    Empty Commit to Trigger Pipeline 2

--- cypress/test/scripts/build-newsroom-videos.cy.js
+++ cypress/test/scripts/build-newsroom-videos.cy.js
@@ -37,8 +37,6 @@ describe('Newsroom Videos', () => {
       ],
     };
 
-    const t= "empty";
-
     // Intercept the API request and stub the response
     cy.intercept('GET', 'https://youtube.googleapis.com/youtube/v3/search*', {
       statusCode: 200,
