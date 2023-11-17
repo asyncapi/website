79d3d1652bb6bdbe26163eccd17db2db7b33f716
Vishvamsinh Vaghela <vaghelavishvamsinh11111@gmail.com>
2023-11-17 13:03:53 +0530

    design improvements

--- components/FinancialSummary/OtherFormsComponent.js
+++ components/FinancialSummary/OtherFormsComponent.js
@@ -18,7 +18,7 @@ function OtherFormsComponent() {
                     <div class="grid grid-cols-1 md:grid-cols-3 gap-10 mt-8 mx-3">
 
                         <div class="bg-white rounded-md shadow-md p-4 flex flex-col items-center text-center">
-                            <img src="/img/illustrations/EmployeeInvolvement.webp" alt="Employee involvement" class="w-1/5 h-auto object-cover m-2" />
+                            <img src="/img/illustrations/EmployeeInvolvement.webp" alt="Employee involvement" class="w-1/3 h-auto object-cover m-2" />
                             <h2 class="text-2xl font-semibold my-2">Employee involvement</h2>
                             <p class="text-base text-darkGunMetal mt-1 mx-2">
                                 Assign your employees to contribute to projects under the AsyncAPI Initiative on a regular basis, and we'll welcome them as new maintainers.
@@ -26,7 +26,7 @@ function OtherFormsComponent() {
                         </div>
 
                         <div class="bg-white rounded-md shadow-md p-4 flex flex-col items-center text-center">
-                            <img src="/img/illustrations/ServiceProvision.webp" alt="Service provision" class="w-1/5 h-auto object-cover m-2" />
+                            <img src="/img/illustrations/ServiceProvision.webp" alt="Service provision" class="w-1/3 h-auto object-cover m-2" />
                             <h2 class="text-2xl font-semibold my-2">Service provision</h2>
                             <p class="text-base text-darkGunMetal mt-1 mx-2">
                                 AsyncAPI Initiative relies on numerous tools, many of which incur costs. Your organization can provide services such as hosting or storage to support our efforts.
@@ -34,7 +34,7 @@ function OtherFormsComponent() {
                         </div>
                         
                         <div class="bg-white rounded-md shadow-md p-4 flex flex-col items-center text-center">
-                            <img src="/img/illustrations/EventOrganization.webp" alt="Event organization" class="w-1/5 h-auto object-cover m-2" />
+                            <img src="/img/illustrations/EventOrganization.webp" alt="Event organization" class="w-1/3 h-auto object-cover m-2" />
                             <h2 class="text-2xl font-semibold my-2">Event organization</h2>
                             <p class="text-base text-darkGunMetal mt-1 mx-2">
                                 Host AsyncAPI conferences by sponsoring and organizing events under the AsyncAPI brand at your provided venue.
