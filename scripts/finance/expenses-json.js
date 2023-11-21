56da3a9f10c519b2c6bfd5149c5644576ae9adfd
Vishvamsinh Vaghela <vaghelavishvamsinh11111@gmail.com>
2023-11-21 23:33:31 +0530

    code updated

--- scripts/finance/expenses-json.js
+++ scripts/finance/expenses-json.js
@@ -1,34 +1,28 @@
-const fs = require('fs');
-const yaml = require('yaml');
-const path = require('path');
+const { promises: { readFile, writeFile } } = require('fs');
 const { convertToJson } = require('../utils');
+const { resolve } = require('path');
 
-// Function to read YAML content from a file
-function readYamlFromFile(filePath) {
+module.exports = async function buildFinanceInfoList() {
     try {
-        const yamlContent = fs.readFileSync(filePath, 'utf8');
-        const parsedYaml = yaml.parse(yamlContent); // Parse YAML using the "yaml" package
-        return parsedYaml;
-    } catch (error) {
-        throw new Error('Error reading YAML file:', error.message);
+        const ExpensesContent = await readFile('config/finance/2023/Expenses.yml', 'utf-8');
+        const ExpensesLinkContent = await readFile('config/finance/2023/ExpensesLink.yml', 'utf-8');
+
+        const Expenses = convertToJson(ExpensesContent);
+        const ExpensesLink = convertToJson(ExpensesLinkContent);
+
+        // Write Expenses to a JSON file
+        await writeFile(
+            resolve(__dirname, '../../config/finance/json-data/2023/', 'Expenses.json'),
+            JSON.stringify(Expenses, null, 2)
+        );
+
+        // Write ExpensesLink to a JSON file
+        await writeFile(
+            resolve(__dirname, '../../config/finance/json-data/2023/', 'ExpensesLink.json'),
+            JSON.stringify(ExpensesLink, null, 2)
+        );
+    } catch (err) {
+        console.error(err);
+        throw err;
     }
-}
-
-function buildFinanceInfoList() {
-    
-const ExpensesFilePath = path.join(__dirname, '../../config/finance/2023/Expenses.yml');
-const ExpensesLinkFilePath = path.join(__dirname, '../../config/finance/2023/ExpensesLink.yml');
-
-const ExpensesContent = readYamlFromFile(ExpensesFilePath);
-const ExpensesLinkContent = readYamlFromFile(ExpensesLinkFilePath);
-
-const Expenses = convertToJson(ExpensesContent);
-const ExpensesLink = convertToJson(ExpensesLinkContent);
-
-// Specify the output directory for the JSON files
-const outputDirectory = path.join(__dirname, '../../config/finance/json-data/2023/');
-    fs.writeFileSync(path.join(outputDirectory, 'Expenses.json'), JSON.stringify(Expenses, null, 2));
-    fs.writeFileSync(path.join(outputDirectory, 'ExpensesLink.json'), JSON.stringify(ExpensesLink, null, 2));
-}
-
-module.exports = { buildFinanceInfoList }
+};
