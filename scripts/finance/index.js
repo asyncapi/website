ee13a3581c766a0b0dc1073324449a65bfd0cdac
Vishvamsinh Vaghela <vaghelavishvamsinh11111@gmail.com>
2023-11-23 19:41:03 +0530

    json files issue

--- scripts/finance/index.js
+++ scripts/finance/index.js
@@ -1,4 +1,4 @@
-const { promises: { readFile, writeFile } } = require('fs');
+const { promises: { readFile, writeFile, mkdir } } = require('fs');
 const { convertToJson } = require('../utils');
 const { resolve, dirname } = require('path');
 
@@ -17,16 +17,21 @@ module.exports = async function buildFinanceInfoList() {
         const Expenses = convertToJson(ExpensesContent);
         const ExpensesLink = convertToJson(ExpensesLinkContent);
 
+        // Ensure the directory exists before writing the files
+        const jsonDirectory = resolve(currentDir, 'config', 'finance', 'json-data', '2023');
+        console.log('Ensuring directory exists:', jsonDirectory);
+        await mkdir(jsonDirectory, { recursive: true });
+
         // Write Expenses to a JSON file
-        const expensesJsonPath = resolve(currentDir, 'config', 'finance', 'json-data', '2023', 'Expenses.json');
+        const expensesJsonPath = resolve(jsonDirectory, 'Expenses.json');
         console.log('Writing Expenses to:', expensesJsonPath);
         await writeFile(expensesJsonPath, JSON.stringify(Expenses, null, 2));
 
         // Write ExpensesLink to a JSON file
-        const expensesLinkJsonPath = resolve(currentDir, 'config', 'finance', 'json-data', '2023', 'ExpensesLink.json');
+        const expensesLinkJsonPath = resolve(jsonDirectory, 'ExpensesLink.json');
         console.log('Writing ExpensesLink to:', expensesLinkJsonPath);
         await writeFile(expensesLinkJsonPath, JSON.stringify(ExpensesLink, null, 2));
-        
+
     } catch (err) {
         console.error(err);
         throw err;
