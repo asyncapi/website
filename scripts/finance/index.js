ae5ac3fd486a4aad45edc49224758fb89683440f
Vishvamsinh Vaghela <vaghelavishvamsinh11111@gmail.com>
2023-11-23 19:37:57 +0530

    json files removed from pr

--- scripts/finance/index.js
+++ scripts/finance/index.js
@@ -9,7 +9,9 @@ module.exports = async function buildFinanceInfoList() {
         const expensesPath = resolve(currentDir, 'config', 'finance', '2023', 'Expenses.yml');
         const expensesLinkPath = resolve(currentDir, 'config', 'finance', '2023', 'ExpensesLink.yml');
 
+        console.log('Reading Expenses file from:', expensesPath);
         const ExpensesContent = await readFile(expensesPath, 'utf-8');
+        console.log('Reading ExpensesLink file from:', expensesLinkPath);
         const ExpensesLinkContent = await readFile(expensesLinkPath, 'utf-8');
 
         const Expenses = convertToJson(ExpensesContent);
@@ -17,10 +19,12 @@ module.exports = async function buildFinanceInfoList() {
 
         // Write Expenses to a JSON file
         const expensesJsonPath = resolve(currentDir, 'config', 'finance', 'json-data', '2023', 'Expenses.json');
+        console.log('Writing Expenses to:', expensesJsonPath);
         await writeFile(expensesJsonPath, JSON.stringify(Expenses, null, 2));
 
         // Write ExpensesLink to a JSON file
         const expensesLinkJsonPath = resolve(currentDir, 'config', 'finance', 'json-data', '2023', 'ExpensesLink.json');
+        console.log('Writing ExpensesLink to:', expensesLinkJsonPath);
         await writeFile(expensesLinkJsonPath, JSON.stringify(ExpensesLink, null, 2));
         
     } catch (err) {
