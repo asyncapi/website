15290c395e084eb57c096c9e4ce1a087ddf308fd
Vishvamsinh Vaghela <vaghelavishvamsinh11111@gmail.com>
2023-11-23 19:32:52 +0530

    json files removed from pr

--- scripts/finance/index.js
+++ scripts/finance/index.js
@@ -1,26 +1,28 @@
 const { promises: { readFile, writeFile } } = require('fs');
 const { convertToJson } = require('../utils');
-const { resolve } = require('path');
+const { resolve, dirname } = require('path');
 
 module.exports = async function buildFinanceInfoList() {
     try {
-        const ExpensesContent = await readFile('config/finance/2023/Expenses.yml', 'utf-8');
-        const ExpensesLinkContent = await readFile('config/finance/2023/ExpensesLink.yml', 'utf-8');
+        const currentDir = resolve(__dirname, '../../');
+
+        const expensesPath = resolve(currentDir, 'config', 'finance', '2023', 'Expenses.yml');
+        const expensesLinkPath = resolve(currentDir, 'config', 'finance', '2023', 'ExpensesLink.yml');
+
+        const ExpensesContent = await readFile(expensesPath, 'utf-8');
+        const ExpensesLinkContent = await readFile(expensesLinkPath, 'utf-8');
 
         const Expenses = convertToJson(ExpensesContent);
         const ExpensesLink = convertToJson(ExpensesLinkContent);
 
         // Write Expenses to a JSON file
-        await writeFile(
-            resolve(__dirname, '../../config/finance/json-data/2023/', 'Expenses.json'),
-            JSON.stringify(Expenses, null, 2)
-        );
+        const expensesJsonPath = resolve(currentDir, 'config', 'finance', 'json-data', '2023', 'Expenses.json');
+        await writeFile(expensesJsonPath, JSON.stringify(Expenses, null, 2));
 
         // Write ExpensesLink to a JSON file
-        await writeFile(
-            resolve(__dirname, '../../config/finance/json-data/2023/', 'ExpensesLink.json'),
-            JSON.stringify(ExpensesLink, null, 2)
-        );
+        const expensesLinkJsonPath = resolve(currentDir, 'config', 'finance', 'json-data', '2023', 'ExpensesLink.json');
+        await writeFile(expensesLinkJsonPath, JSON.stringify(ExpensesLink, null, 2));
+        
     } catch (err) {
         console.error(err);
         throw err;
