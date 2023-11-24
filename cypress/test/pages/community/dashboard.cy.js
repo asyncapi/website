eef441d263d30bc688b9e86f0d09e86e9995acb9
Vishvamsinh Vaghela <vaghelavishvamsinh11111@gmail.com>
2023-11-24 10:01:35 +0530

    timeout issue fixed

--- cypress/test/pages/community/dashboard.cy.js
+++ cypress/test/pages/community/dashboard.cy.js
@@ -1,28 +1,31 @@
 import MockApp from "../../../utils/MockApp";
 import DashboardHome from "../../../../pages/community/dashboard";
 import Header from "../../../../components/dashboard/Header";
-import { mount } from '@cypress/react'
-describe('Integration Test for Dashboard ', () => {
+import { mount } from '@cypress/react';
 
-    it('should filter GoodFirstIssues and check dashboard render ', () => {
+describe('Integration Test for Dashboard', () => {
+    it('should filter GoodFirstIssues and check dashboard render', () => {
         mount(
             <MockApp>
                 <DashboardHome />
             </MockApp>
         );
+
         // Click on the first filter option
-        cy.get(Header).should('exist')
-        cy.contains('AsyncAPI - Dashboard')
+        cy.get(Header).should('exist');
+        cy.contains('AsyncAPI - Dashboard');
         cy.get('[data-testid="Filters-img-container"]').click({ force: true });
         cy.get('[data-testid="Filter-menu"]').within(() => {
             cy.get('[data-testid="Select-form"]').eq(0).select('asyncapi/generator', { force: true });
-            cy.get('[data-testid="Select-form"]').eq(1).select('docs')
+            cy.get('[data-testid="Select-form"]').eq(1).select('docs');
         });
-        //check if selected is only displayed
+
+        // Check if selected options are displayed
         cy.contains('asyncapi/generator');
-        cy.contains('docs')
-        //check if this is not selected options are not displayed
-        cy.should('not.contain', 'asyncapi/community');
-        cy.should('not.contain', 'javascript');
+        cy.contains('docs');
+
+        // Check if options that should not be selected are not displayed
+        cy.get('[data-testid="Filter-menu"]').should('not.contain', 'asyncapi/community');
+        cy.get('[data-testid="Filter-menu"]').should('not.contain', 'javascript');
     });
 });
