d9c3264b690d216b8c2c355d1a149021d9bb4c63
derberg <lpgornicki@gmail.com>
2023-12-04 22:54:46 +0100

    feat: update release notes cover and add banner

--- components/campaigns/Banner.js
+++ components/campaigns/Banner.js
@@ -12,9 +12,8 @@ export default function Banner({}) {
   const day = new Date().getUTCDate();
   const month = new Date().getUTCMonth();
   const year = new Date().getUTCFullYear();
-
-  // month=10 is November. Show only between 6-30 November.
-  if (year > 2022 || month !== 10 || day < 6) {
+  // month=11 is December. Show only between 6-30 November.
+  if (year > 2023 && month > 11 && day < 6) {
     return null;
   }
 
@@ -22,27 +21,20 @@ export default function Banner({}) {
     <div className="bg-gray-100">
       <div className="mx-auto max-w-screen-xl py-1 px-3 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between flex-wrap">
-          <div className="flex md:hidden w-0 flex-1 flex items-center text-xs">
-            <p className="font-medium text-gray-700">
-              <span className="md:inline">
-                AsyncAPI Conference 2022 has ended! ⭐️
-              </span>
-            </p>
-          </div>
           <div className="hidden md:flex w-0 flex-1 flex items-center text-xs">
             <p className="font-medium text-gray-700">
               <span className="md:inline">
-                AsyncAPI Conference 2022 has ended. Good news: you can still watch the recording! ⭐️
+                AsyncAPI v3 has landed! ⭐️
               </span>
             </p>
           </div>
           <div className="flex">
             <a
-              href="https://www.youtube.com/playlist?list=PLbi1gRlP7pijRiA32SU36hD_FW-2qyPhl"
+              href="/blog/release-notes-3.0.0"
               className="flex items-center justify-center px-4 py-1 border border-transparent rounded-md shadow-sm text-xs font-medium text-indigo-600 focus:text-indigo-600 bg-white hover:bg-indigo-50"
               target="_blank" rel="noopener noreferrer"
             >
-              See recordings
+              Release notes
             </a>
           </div>
         </div>
