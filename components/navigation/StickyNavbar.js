8dbab97b7cc7151293373b288eff7c370dfb044a
Ashmit JaiSarita Gupta <43639341+devilkiller-ag@users.noreply.github.com>
2023-12-13 05:45:48 +0000

    ✨ Rectify menu bar background

--- components/navigation/StickyNavbar.js
+++ components/navigation/StickyNavbar.js
@@ -1,5 +1,5 @@
 export default function StickyNavbar({children, className=''}) {
-  return <div className={`sticky top-0 w-full bg-white border-b border-gray-300 ${className}`} data-testid="Sticky-div">
+  return <div className={`sticky top-0 w-full bg-white border-b border-gray-300 z-30 ${className}`} data-testid="Sticky-div">
       {children}
   </div>;
 }
