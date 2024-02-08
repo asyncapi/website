655feeb3ba4684add545fc205d061948d8d78477
akshatnema <akshatnema.school@gmail.com>
2024-02-08 18:11:57 +0530

    Update env var name in Head

--- components/Head.js
+++ components/Head.js
@@ -10,7 +10,7 @@ export default function HeadComponent({
   rssTitle = 'RSS Feed for AsyncAPI Initiative Blog',
   rssLink = '/rss.xml',
 }) {
-  const url = process.env.NEXT_PUBLIC_DEPLOY_PRIME_URL || process.env.NEXT_PUBLIC_DEPLOY_URL || 'http://localhost:3000';
+  const url = process.env.DEPLOY_PRIME_URL || process.env.DEPLOY_URL || 'http://localhost:3000';
   const appContext = useContext(AppContext);
   const { path = '' } = appContext || {};
 
