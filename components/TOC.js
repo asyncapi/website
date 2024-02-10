7969cdaa54bca93c5fe2a41ddb2d1723d7441502
Vishvamsinh Vaghela <vaghelavishvamsinh11111@gmail.com>
2024-02-10 23:34:14 +0530

    blog updates

--- components/TOC.js
+++ components/TOC.js
@@ -23,13 +23,13 @@ export default function TOC({
   const [open, setOpen] = useState(false);
 
   return (
-    <div className={twMerge(`${className} ${tocItems.length ? '' : 'hidden'} ${cssBreakingPoint === 'xl' ? 'xl:block' : 'lg:block'} md:top-24 md:max-h-(screen-14) z-20`)} onClick={() => setOpen(!open)}>
+    <div className={twMerge(`${className} dark:bg-slate-600 ${tocItems.length ? '' : 'hidden'} ${cssBreakingPoint === 'xl' ? 'xl:block' : 'lg:block'} md:top-24 md:max-h-(screen-14) z-20`)} onClick={() => setOpen(!open)}>
       <div className={`flex cursor-pointer ${tocItems.length ? '' : 'hidden'} ${cssBreakingPoint === 'xl' ? 'xl:cursor-auto' : 'lg:cursor-auto'} xl:mt-2`}>
-        <h5 className={twMerge(`${open && 'mb-4'} flex-1 text-primary-500 font-medium uppercase tracking-wide text-sm font-sans antialiased ${cssBreakingPoint === 'xl' ? 'xl:mb-4 xl:text-xs xl:text-gray-900 xl:font-bold' : 'lg:mb-4 lg:text-xs lg:text-gray-900 lg:font-bold'}`)} data-testid="TOC-Heading">
+        <h5 className={twMerge(`${open && 'mb-4'} flex-1 dark:text-gray-300 text-primary-500 font-medium uppercase tracking-wide text-sm font-sans antialiased ${cssBreakingPoint === 'xl' ? 'xl:mb-4 xl:text-xs xl:text-gray-900 xl:font-bold' : 'lg:mb-4 lg:text-xs lg:text-gray-900 lg:font-bold'}`)} data-testid="TOC-Heading">
           On this page
         </h5>
         <div className={`text-underline text-center p4 ${cssBreakingPoint === 'xl' ? 'xl:hidden' : 'lg:hidden'}`}>
-          <ArrowRight className={`${ open ? '-rotate-90' : 'rotate-90' } transform transition duration-200 ease-in-out h-6 -mt-0.5 text-primary-500`} />
+          <ArrowRight className={`${ open ? '-rotate-90' : 'rotate-90' } transform transition duration-200 ease-in-out h-6 -mt-0.5 dark:text-gray-300 text-primary-500`} />
         </div>
       </div>
       <div className={`${!open && 'hidden'} ${cssBreakingPoint === 'xl' ? 'xl:block' : 'lg:block'}`}>
@@ -43,7 +43,7 @@ export default function TOC({
           {
             tocItems.map((item, index) => (
               <a 
-                className={`pl-${Math.pow(2, item.lvl-1)} block mb-1 transition duration-100 ease-in-out text-gray-900 font-normal text-sm font-sans antialiased hover:underline`}
+                className={`pl-${Math.pow(2, item.lvl-1)} block mb-1 transition duration-100 ease-in-out dark:text-gray-400 text-gray-900 font-normal text-sm font-sans antialiased hover:underline`}
                 href={`#${item.slug}`}
                 key={index}
                 data-testid="TOC-Link"
