bffc39ebb4a0924eaa975548b6a44aa918185f8e
Vishvamsinh Vaghela <vaghelavishvamsinh11111@gmail.com>
2024-02-13 16:22:32 +0530

    toggle issue fixed

--- components/DarkModeToggle.js
+++ components/DarkModeToggle.js
@@ -4,15 +4,23 @@ import { useState } from 'react';
 const DarkModeToggle = () => {
   const { theme, setTheme } = useTheme();
   const [isDarkMode, setIsDarkMode] = useState(theme === 'dark');
+  const [isClickable, setIsClickable] = useState(true);
 
   const toggleDarkMode = () => {
+    if (!isClickable) return;
+    
+    setIsClickable(false); 
     const newTheme = isDarkMode ? 'light' : 'dark';
     setTheme(newTheme);
     setIsDarkMode(!isDarkMode);
+
+    setTimeout(() => {
+      setIsClickable(true);
+    }, 500); 
   };
 
   return (
-    <button onClick={toggleDarkMode} className="dark-mode-toggle">
+    <button onClick={toggleDarkMode} className="dark-mode-toggle" disabled={!isClickable}>
       {isDarkMode ? (
         <img src="/img/illustrations/sun.webp" alt="Dark Mode" width={27} height={27} />
       ) : (
