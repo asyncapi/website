f8ac53983f80eea2011cb68ecbc42056db4a95ee
Ansh Goyal <anshgoyal1704@gmail.com>
2024-02-18 22:59:39 +0530

    migrate lib/* to ts

---
+++ utils/api.ts
@@ -0,0 +1,54 @@
+import posts from '../config/posts.json';
+import { IPost, Posts } from './types';
+
+/**
+ * Retrieves all posts.
+ * @returns {Object} All posts.
+ */
+export function getAllPosts(): object {
+  return posts;
+}
+
+/**
+ * Retrieves a post by its slug and type.
+ * @param {string} slug - The slug of the post.
+ * @param {string} [type=''] - The type of the post.
+ * @returns {Object | undefined} The post if found, undefined otherwise.
+ */
+export function getPostBySlug(
+  slug: string,
+  type: string = ''
+): IPost | undefined {
+  if (type)
+    return (posts as any)[type as keyof Posts].find(
+      (post: IPost) => post.slug === slug && !post.isSection
+    );
+  else {
+    let item: IPost | undefined;
+    Object.entries(posts).forEach(([key, value]: [string, any]) => {
+      let content;
+      if (key !== 'docsTree') {
+        content = (posts as any)[key as keyof Posts].find(
+          (post: IPost) => post.slug === slug && !post.isSection
+        );
+        if (content) item = content;
+      }
+    });
+    return item;
+  }
+}
+
+/**
+ * Retrieves a document by its slug from structured posts.
+ * @param {Object[]} structuredPosts - The structured posts array.
+ * @param {string} slug - The slug of the document.
+ * @returns {Object | undefined} The document if found, undefined otherwise.
+ */
+export function getDocBySlug(
+  structuredPosts: IPost[],
+  slug: string
+): object | undefined {
+  return structuredPosts.find(
+    (post: IPost) => post.slug === slug && !post.isSection
+  );
+}
