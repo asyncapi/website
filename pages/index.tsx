65040fd50d38765c605c6bfc1dda697b7ee6191b
Vishvamsinh Vaghela <vaghelavishvamsinh11111@gmail.com>
2024-02-20 23:40:05 +0530

    tested components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,4 @@
 import { Inter } from 'next/font/google';
-import Paragraph from '@/components/typography/Paragraph';
-import TextLink from '@/components/typography/TextLink';
-import Heading from '@/components/typography/Heading';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -10,9 +7,7 @@ export default function Home() {
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-      <Heading typeStyle="heading-xl" textColor="text-blue-500">Welcome to my website!</Heading>
-      <Paragraph typeStyle="body-lg" textColor="text-gray-700">This is a sample paragraph.</Paragraph>
-      <TextLink href="https://www.google.com" className="underline">fw</TextLink>
+      Hello World
     </main>
   );
 }
