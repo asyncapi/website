c88263e15e7615a9b9c93360c7f3ecd7c54e2bd6
Ashish Padhy <100484401+Shurtu-gal@users.noreply.github.com>
2024-02-23 16:52:03 +0530

    chore: add preview

---
+++ data/temp.tsx
@@ -0,0 +1,39 @@
+/**
+ * @todo: REMOVE BEFORE MERGE
+ */
+
+export const buttonComps = [
+  'Button.tsx',
+  'DocsButton.tsx',
+  'GitHubIssue.tsx',
+  'GithubButton.tsx',
+  'GoogleCalendarButton.tsx',
+  'ICSFileButton.tsx',
+  'OpenInStudioButton.tsx',
+  'SlackButton.tsx',
+  'SubscribeButton.tsx',
+  'YoutubeButton.tsx',
+];
+
+export const iconComps = [
+  'Ambassador.tsx',    'ArrowDown.tsx',          'ArrowLeft.tsx',
+  'ArrowRight.tsx',    'ArrowUp.tsx',            'AsyncAPI.tsx',
+  'Calendar.tsx',      'Clipboard.tsx',          'Code.tsx',
+  'Community.tsx',     'Conference.tsx',         'Contributing.tsx',
+  'Cross.tsx',         'Dashboard.tsx',          'DocsArrow.tsx',
+  'Documents.tsx',     'Exclamation.tsx',        'Filter.tsx',
+  'Generator.tsx',     'GettingStarted.tsx',     'Github.tsx',
+  'GithubActions.tsx', 'GithubOrganization.tsx', 'GoogleCalendar.tsx',
+  'GradCap.tsx',       'Guide.tsx',              'Home.tsx',
+  'Hub.tsx',           'InfoIcon.tsx',           'LightBulb.tsx',
+  'LinkedIn.tsx',      'Loupe.tsx',              'Meetings.tsx',
+  'Migration.tsx',     'Modelina.tsx',           'NavItemDropdown.tsx',
+  'Newsroom.tsx',      'Paper.tsx',              'Parser.tsx',
+  'Plant.tsx',         'Plugins.tsx',            'PowerPlug.tsx',
+  'QuestionMark.tsx',  'React.tsx',              'Rocket.tsx',
+  'Search.tsx',        'Slack.tsx',              'Spec.tsx',
+  'Subscribe.tsx',     'TSC.tsx',                'Tools.tsx',
+  'Tutorials.tsx',     'Twitch.tsx',             'Twitter.tsx',
+  'UseCases.tsx',      'Users.tsx',              'Webinar.tsx',
+  'YouTube.tsx',       'YouTubeGray.tsx'
+]
