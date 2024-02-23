47d32f67b584f918b38685191e8ad312ec1edb08
Keshav <keshav2021csds116@abesit.edu.in>
2024-02-23 07:21:32 +0530

    Fix: Job closing date

--- components/JobSummary.js
+++ components/JobSummary.js
@@ -28,14 +28,23 @@ export default function JobSummary({ job, className = '' }) {
         <dl>
           <dt className="sr-only">Closing on</dt>
           <dd className="flex mt-1 text-sm text-gray-900">
-            <svg viewBox="0 0 20 20" fill="currentColor" className="h-5 w-5 text-gray-400"><path fillRule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clipRule="evenodd"></path></svg>
+            <svg viewBox="0 0 20 20" fill="currentColor" className="h-5 w-5 text-gray-400">
+              <path fillRule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clipRule="evenodd"></path>
+            </svg>
             <span className="ml-4">
-              Closing on&nbsp;
-              <time dateTime={job.closingOn}>{new Date(job.closingOn).toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' })}</time>
+              {new Date(job.closingOn) < new Date() ? (
+                <>Closed on&nbsp;</>
+              ) : (
+                <>Closing on&nbsp;</>
+              )}
+              <time dateTime={job.closingOn}>
+                {new Date(job.closingOn).toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' })}
+              </time>
             </span>
           </dd>
         </dl>
       </div>
+
 
       <div className="hidden lg:block border-t border-gray-200 px-4 py-5 sm:px-6">
         <ApplyJobButton job={job} className="block text-center" />
