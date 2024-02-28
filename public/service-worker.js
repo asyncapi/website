c423eb8bf521c0114ef5d957f95ea83d9918244e
Durgesh kumar prajapati <98798977+Durgesh4993@users.noreply.github.com>
2024-02-29 05:28:29 +0530

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,19 +1,8 @@
-self.addEventListener('install', function (event) {
-    self.skipWaiting();
-});
+self.addEventListener('install', event => self.skipWaiting());
 
-self.addEventListener("activate", function (e) {
-
+self.addEventListener("activate", event => {
     self.registration.unregister()
-        .then(function () {
-
-            return self.clients.matchAll();
-
-        })
-        .then(function (clients) {
-
-            clients.forEach(client => client.navigate(client.url));
-
-        });
-
+        .then(() => self.clients.matchAll())
+        .then(clients => clients.forEach(client => client.navigate(client.url)));
 });
+
