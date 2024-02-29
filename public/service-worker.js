cbd9f03d049bc80eaa25c3cab9efc31a85238e5a
Durgesh kumar prajapati <98798977+Durgesh4993@users.noreply.github.com>
2024-02-29 05:30:00 +0530

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,8 +1,19 @@
-self.addEventListener('install', event => self.skipWaiting());
-
-self.addEventListener("activate", event => {
-    self.registration.unregister()
-        .then(() => self.clients.matchAll())
-        .then(clients => clients.forEach(client => client.navigate(client.url)));
+// Service worker installation event
+self.addEventListener('install', event => {
+    // Skip waiting to activate the service worker immediately
+    self.skipWaiting();
 });
 
+// Service worker activation event
+self.addEventListener("activate", event => {
+    // Unregister the current service worker
+    self.registration.unregister()
+        .then(() => {
+            // Retrieve all active clients associated with the service worker
+            return self.clients.matchAll();
+        })
+        .then(clients => {
+            // For each client, navigate to the same URL to reload the page
+            clients.forEach(client => client.navigate(client.url));
+        });
+});
