8a059b84506c03c718cca467a622203df415a709
devilkiller-ag <ashmitgupta.official@gmail.com>
2024-03-04 16:54:57 +0530

    converted docs card to typescript

--- components/docs/DocsCards.tsx
+++ components/docs/DocsCards.tsx
@@ -1,31 +1,42 @@
+import React from 'react';
 import Link from 'next/link';
 import Heading from '../typography/Heading';
 import Paragraph from '../typography/Paragraph';
 
 import { buckets } from '../data/buckets';
 
-export function DocsCards() {
+type IconType = (props: { className: string }) => JSX.Element;
+
+interface CardProps {
+  title: string;
+  description: string;
+  link: string;
+  className: string;
+  Icon: IconType;
+}
+
+export const DocsCards: React.FC = () => {
   return (
     <div className='grid gap-4 grid-cols-1 sm:grid-cols-2' data-testid="Docs-main-div" >
       {buckets.map(card => (
         <Card key={card.title} {...card} />
-
       ))}
 
     </div>
   );
 }
 
-function Card({ title, description, link, className, Icon }) {
+const Card: React.FC<CardProps> = ({ title, description, link, className, Icon }) => {
   return (
     <Link href={link}>
       <a href={link} className='cursor-pointer' data-testid="Docs-link">
         <div className="h-full border border-gray-200 shadow-md hover:shadow-lg transition-all duration-300 ease-in-out rounded-lg p-6">
           <div data-testid="Docs-div-contents">
-            <Heading 
+            <Heading
               level="h3"
               typeStyle="heading-sm-semibold"
               className='pb-4 border-b border-gray-300'
+              id={title}
             >
               <div className='flex flex-row items-center'>
                 <div className={`flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-lg ${className} text-gray-900 sm:h-12 sm:w-12`} data-testid="Docs-icon">
@@ -34,7 +45,7 @@ function Card({ title, description, link, className, Icon }) {
                 <span className='ml-4'>{title}</span>
               </div>
             </Heading>
-            <Paragraph typeStyle="body-sm" className="mt-5">
+            <Paragraph typeStyle="body-sm" className="mt-5" fontWeight="light">
               {description}
             </Paragraph>
           </div>
