a8bb682d6a2aedd7db48e6b21d58bafcec015e04
devilkiller-ag <ashmitgupta.official@gmail.com>
2024-03-09 12:12:40 +0530

    fix lint error

--- components/docs/DocsCards.tsx
+++ components/docs/DocsCards.tsx
@@ -21,28 +21,28 @@ interface CardProps {
 const Card: React.FC<CardProps> = ({ title, description, link, className, Icon }) => {
   return (
     <Link href={link} className='cursor-pointer' data-testid='Docs-link'>
-        <div className='h-full rounded-lg border border-gray-200 p-6 shadow-md transition-all duration-300 ease-in-out
+      <div className='h-full rounded-lg border border-gray-200 p-6 shadow-md transition-all duration-300 ease-in-out
           hover:shadow-lg'>
-          <div data-testid='Docs-div-contents'>
-            <Heading
-              level={HeadingLevel.h3}
-              typeStyle={HeadingTypeStyle.smSemibold}
-              className='border-b border-gray-300 pb-4'
-              id={title}
-            >
-              <div className='flex flex-row items-center'>
-                <div className={`flex size-12 shrink-0 items-center justify-center rounded-lg ${className}
+        <div data-testid='Docs-div-contents'>
+          <Heading
+            level={HeadingLevel.h3}
+            typeStyle={HeadingTypeStyle.smSemibold}
+            className='border-b border-gray-300 pb-4'
+            id={title}
+          >
+            <div className='flex flex-row items-center'>
+              <div className={`flex size-12 shrink-0 items-center justify-center rounded-lg ${className}
                   text-gray-900 sm:size-12`} data-testid='Docs-icon'>
-                  <Icon className='size-6' />
-                </div>
-                <span className='ml-4'>{title}</span>
+                <Icon className='size-6' />
               </div>
-            </Heading>
-            <Paragraph typeStyle={ParagraphTypeStyle.sm} className='mt-5' fontWeight='light'>
-              {description}
-            </Paragraph>
-          </div>
+              <span className='ml-4'>{title}</span>
+            </div>
+          </Heading>
+          <Paragraph typeStyle={ParagraphTypeStyle.sm} className='mt-5' fontWeight='light'>
+            {description}
+          </Paragraph>
         </div>
+      </div>
     </Link>
   );
 };
