e528e52cfc09ec3008937bf56e0b5b8cb2a5c7a8
Vishvamsinh Vaghela <vaghelavishvamsinh11111@gmail.com>
2024-03-10 17:19:47 +0530

    added link.tsx

--- utils/i18nPaths.ts
+++ utils/i18nPaths.ts
@@ -1,12 +1,17 @@
-const i18nPaths = {
+interface I18nPaths {
+    [key: string]: string[];
+  }
+  
+  const i18nPaths: I18nPaths = {
     en: [
-        "", //Homepage Route
-        "/tools/cli"
+      "", // Homepage Route
+      "/tools/cli"
     ],
     de: [
-        "", //Homepage Route
-        "/tools/cli"
+      "", // Homepage Route
+      "/tools/cli"
     ]
-};
-
-export default i18nPaths;
+  };
+  
+  export default i18nPaths;
+  
