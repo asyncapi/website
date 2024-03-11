8f7d924ad682fae0d43e9fc67aebe5b25ec19e76
devilkiller-ag <ashmitgupta.official@gmail.com>
2024-03-11 14:46:22 +0530

    migrate sponsors component to typescript

---
+++ components/sponsors/GoldSponsors.tsx
@@ -0,0 +1,42 @@
+import { goldSponsors } from './GoldSponsorsList';
+
+interface GoldSponsorsProps {
+  className: string;
+  showSupportBanner: boolean;
+};
+
+/**
+ * This component displays Gold Sponsors.
+ * @param {GoldSponsorsProps} props - The props for GoldSponsors component.
+ * @param {string} props.className - Additional CSS classes for styling.
+ * @param {boolean} props.showSupportBanner - Indicates whether support banner should be displayed.
+ */
+export default function GoldSponsors({
+  className = '',
+  // eslint-disable-next-line no-unused-vars, unused-imports/no-unused-vars
+  showSupportBanner = true
+}: GoldSponsorsProps): React.ReactNode {
+  return (
+    <div className={`text-center ${className}`}>
+      <div className='mb-8 flex flex-wrap items-center justify-center md:px-4'>
+        {goldSponsors.map((sponsor, index) => (
+          <a
+            key={index}
+            href={sponsor.website}
+            target='_blank'
+            className='relative block w-2/3 p-4 text-center sm:w-1/2 sm:p-0 md:w-1/3 lg:w-1/5'
+            rel='noopener noreferrer'
+            data-testid='GoldSponsors-link'
+          >
+            <img
+              className={sponsor.imageClass}
+              src={sponsor.imageSrc}
+              alt={sponsor.altText}
+              data-testid='GoldSponsors-img'
+            />
+          </a>
+        ))}
+      </div>
+    </div>
+  );
+}
