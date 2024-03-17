3ff7946649b35bacd86306bfed7baba6942b06e9
Vishal <vishalsharma05052002@gmail.com>
2024-03-17 16:37:33 +0530

    fix: Navigation to correct heading in tools section

--- components/tools/ToolsList.js
+++ components/tools/ToolsList.js
@@ -1,12 +1,25 @@
 import ToolsCard from './ToolsCard';
+import { useRef, useEffect } from 'react';
 import Heading from '../typography/Heading'
 import Paragraph from '../typography/Paragraph'
 export default function toolsList({ toolsData }) {
+  const categoryRefs = useRef({});
+
+  useEffect(() => {
+    const hash = window.location.hash;
+    if (hash) {
+      const decodedHash = decodeURIComponent(hash.slice(1));
+      const categoryRef = categoryRefs.current[decodedHash];
+      if (categoryRef) {
+        categoryRef.scrollIntoView({ behavior: 'smooth', block: 'start' });
+      }
+    }
+  }, []);
   return (
     <div className="" data-testid="ToolsList-main" >
       {Object.keys(toolsData).map((categoryName, index) => {
         if(toolsData[categoryName].toolsList.length > 0) return (
-        <div className='my-8' key={index} id={categoryName}>
+          <div className='my-8' key={index} id={categoryName} ref={el => categoryRefs.current[categoryName] = el}>
           <Heading typeStyle='heading-md-semibold' className='my-2' >
             {categoryName}
           </Heading>
