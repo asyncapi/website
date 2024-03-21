8d23a1b1b58c866e91befd2de4210c9e8fc69e18
devilkiller-ag <ashmitgupta.official@gmail.com>
2024-03-21 09:37:04 +0530

    migrated figure component to typescript

--- components/Caption.tsx
+++ components/Caption.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
 interface CaptionProps {
-  children: React.ReactNode;
+  children: string;
 }
 
 /**
  * @description This component displays textual captions.
  *
  * @param {CaptionProps} props - The props for the Caption component.
- * @param {React.ReactNode} props.children - The content to be displayed as the caption.
+ * @param {string} props.children - The content to be displayed as the caption.
  */
 export default function Caption({ children } : CaptionProps) {
   return (
