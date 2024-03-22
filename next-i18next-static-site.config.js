105b4a79cea3859675b2cd2b4018ff3bf4babcec
Vishal <vishalsharma05052002@gmail.com>
2024-03-22 18:29:53 +0530

    Revert "feat: add translation for AsyncApi Homepage in Hindi"
    This reverts commit 26942ca72e1ac01c97e1d8d2c0c064c20a1838fd.

--- next-i18next-static-site.config.js
+++ next-i18next-static-site.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     i18n: {
-        languages: ["en", "de","hi"],
+        languages: ["en", "de"],
         defaultLanguage: "en",
         namespaces: ["landing-page", "common", "tools"],
         defaultNamespace: "landing-page",
