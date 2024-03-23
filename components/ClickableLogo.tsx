b2fe18900c0973c222f958ed3e413eb851e1bb17
Vishvamsinh Vaghela <vaghelavishvamsinh11111@gmail.com>
2024-03-23 22:42:31 +0530

    build fix

--- components/ClickableLogo.tsx
+++ components/ClickableLogo.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 import AsyncAPILogo from './AsyncAPILogo';
 
 interface IClickableLogoProps {
-  href: string;
+  href?: string;
   className?: string;
   logoClassName?: string;
 }
