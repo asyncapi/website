ce3446d32af4ab7bf733b4422c912c745d220139
Ashish Padhy <100484401+Shurtu-gal@users.noreply.github.com>
2024-03-28 20:20:22 +0530

    feat: migrate layout and campaigns (#2743)
    Co-authored-by: Akshat Nema <76521428+akshatnema@users.noreply.github.com>%0ACo-authored-by: akshatnema <akshatnema.school@gmail.com>%0ACo-authored-by: aks-nema <akshatnema@tummee.com>%0ACo-authored-by: Ansh Goyal <anshgoyal1704@gmail.com>

---
+++ components/layout/GenericWideLayout.tsx
@@ -0,0 +1,41 @@
+// import AnnouncementHero from '../campaigns/AnnoucementHero';
+import Head from '../Head';
+import Row from './Row';
+
+interface IGenericWideLayoutProps {
+  title: string;
+  description: string;
+  image: string;
+  children?: React.ReactNode;
+  wide?: boolean;
+}
+
+/**
+ * @description The generic layout with the content
+ * @param props.title - The title of the page
+ * @param props.description - The description of the page
+ * @param props.image - The image of the page
+ * @param props.children - The content of the page
+ * @param props.wide - The width of the page
+ */
+export default function GenericWideLayout({
+  title,
+  description,
+  image,
+  children
+  // wide = true
+}: IGenericWideLayoutProps) {
+  if (!title || !description || !image) {
+    throw new Error('Props `title`, `description`, and `image` are required at GenericLayout component.');
+  }
+
+  return (
+    <>
+      <Head title={title} description={description} image={image} />
+      <Row>
+        {/* <AnnouncementHero className='m-4 text-center' small={true} /> */}
+        {children}
+      </Row>
+    </>
+  );
+}
