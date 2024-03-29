4b36cf8defa0bb1a2951ae23b7e51455cabd5680
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2024-03-29 23:33:40 +0530

    Format code

--- components/FinancialSummary/BarChartComponent.js
+++ components/FinancialSummary/BarChartComponent.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef,useMemo } from 'react';
+import React, { useState, useEffect, useRef, useMemo } from 'react';
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 
 import { getUniqueCategories } from '../../lib/getUniqueCategories';
@@ -11,17 +11,17 @@ import { getUniqueCategories } from '../../lib/getUniqueCategories';
  * @returns {JSX.Element} The rendered CustomTooltip component.
  */
 const CustomTooltip = ({ active, payload }) => {
-    if (active && payload && payload.length) {
-        const data = payload[0].payload;
-        return (
-            <div className="bg-opacity-90 bg-white border border-gray-300 p-2 shadow-md rounded-md">
-                <p className="text-14 font-bold mb-1">{data.Category}</p>
-                <p className="text-12 text-gray-900">${data.Amount.toFixed(2)}</p>
-                <p>Click the bar to learn more</p>
-            </div>
-        );
-    }
-    return null;
+	if (active && payload && payload.length) {
+		const data = payload[0].payload;
+		return (
+			<div className="bg-opacity-90 bg-white border border-gray-300 p-2 shadow-md rounded-md">
+				<p className="text-14 font-bold mb-1">{data.Category}</p>
+				<p className="text-12 text-gray-900">${data.Amount.toFixed(2)}</p>
+				<p>Click the bar to learn more</p>
+			</div>
+		);
+	}
+	return null;
 };
 
 /**
@@ -41,12 +41,12 @@ const currentYear = String(new Date().getFullYear());
  */
 
 const Card = ({ month, data, links }) => {
-    return (
-        <div className="bg-slate-100 shadow-lg rounded-lg p-4 flex flex-col h-56 overflow-hidden">
-            <div className="text-lg font-semibold mb-4">{month}</div>
-            <div className="flex flex-col overflow-x-auto overflow-y-auto">
-                {data.map((item, index) => (
-                    <div key={index} className="flex justify-between">
+	return (
+		<div className="bg-slate-100 shadow-lg rounded-lg p-4 flex flex-col h-56 overflow-hidden">
+			<div className="text-lg font-semibold mb-4">{month}</div>
+			<div className="flex flex-col overflow-x-auto overflow-y-auto">
+				{data.map((item, index) => (
+					<div key={index} className="flex justify-between">
 						<div className="text-sm m-2" onClick={(e) => {
 							const category = item.Category;
 							const matchedLinkObject = links.find(obj => obj.category === category);
@@ -56,12 +56,12 @@ const Card = ({ month, data, links }) => {
 						}}>
 							{item.Category}
 						</div>
-                        <div className="text-sm m-2">${item.Amount}</div>
-                    </div>
-                ))}
-            </div>
-        </div>
-    );
+						<div className="text-sm m-2">${item.Amount}</div>
+					</div>
+				))}
+			</div>
+		</div>
+	);
 };
 
 /**
@@ -71,16 +71,16 @@ const Card = ({ month, data, links }) => {
  * @param {any[]} props.ExpensesLinks
  * @returns {JSX.Element} The rendered ExpensesCard component.
  */
-const ExpensesCard = ({Expenses,ExpensesLinks}) => {
-    return (
-        <div className="overflow-x-auto">
-            <div className="grid grid-flow-col auto-cols-max gap-4 p-4">
-                {Object.keys(Expenses).map((month, index) => (
-                    <Card key={index} month={month} data={Expenses[month]} links={ExpensesLinks} />
-                ))}
-            </div>
-        </div>
-    );
+const ExpensesCard = ({ Expenses, ExpensesLinks }) => {
+	return (
+		<div className="overflow-x-auto">
+			<div className="grid grid-flow-col auto-cols-max gap-4 p-4">
+				{Object.keys(Expenses).map((month, index) => (
+					<Card key={index} month={month} data={Expenses[month]} links={ExpensesLinks} />
+				))}
+			</div>
+		</div>
+	);
 };
 
 /**
@@ -89,131 +89,131 @@ const ExpensesCard = ({Expenses,ExpensesLinks}) => {
  * @returns {JSX.Element} The rendered BarChartComponent component.
  */
 const BarChartComponent = () => {
-    // State for selected filters
-    const [selectedCategory, setSelectedCategory] = useState("All Categories");
-    const [selectedMonth, setSelectedMonth] = useState("All Months");
-    const [windowWidth, setWindowWidth] = useState(null);
+	// State for selected filters
+	const [selectedCategory, setSelectedCategory] = useState("All Categories");
+	const [selectedMonth, setSelectedMonth] = useState("All Months");
+	const [windowWidth, setWindowWidth] = useState(null);
 
-	const [Expenses,setExpenses] = useState([]);
-	const [ExpensesLink,setExpensesLink] = useState(null);
-	
+	const [Expenses, setExpenses] = useState([]);
+	const [ExpensesLink, setExpensesLink] = useState(null);
+
 
 
 	// Get unique categories and months from the Expenses data
-	const categories = useMemo(()=>getUniqueCategories(Expenses),[Expenses]);
-    const months = useMemo(()=>Object.keys(Expenses),[Expenses]);
+	const categories = useMemo(() => getUniqueCategories(Expenses), [Expenses]);
+	const months = useMemo(() => Object.keys(Expenses), [Expenses]);
 
-    // Filter the expenses data based on selectedCategory and selectedMonth
-    const filteredData = useMemo(()=>Object.entries(Expenses).flatMap(([month, entries]) =>
-        (selectedMonth === "All Months" || selectedMonth === month) ?
-            entries.filter(entry =>
-                selectedCategory === "All Categories" || entry.Category === selectedCategory
-            )
-            : []
-    ),[selectedCategory,selectedMonth,Expenses]);
+	// Filter the expenses data based on selectedCategory and selectedMonth
+	const filteredData = useMemo(() => Object.entries(Expenses).flatMap(([month, entries]) =>
+		(selectedMonth === "All Months" || selectedMonth === month) ?
+			entries.filter(entry =>
+				selectedCategory === "All Categories" || entry.Category === selectedCategory
+			)
+			: []
+	), [selectedCategory, selectedMonth, Expenses]);
 
-    // Calculate total amount for the filtered data
-    const totalAmount = filteredData.reduce((total, entry) => total + parseFloat(entry.Amount), 0);
+	// Calculate total amount for the filtered data
+	const totalAmount = filteredData.reduce((total, entry) => total + parseFloat(entry.Amount), 0);
 
-    const categoryAmounts = {};
-    filteredData.forEach(entry => {
-        if (categoryAmounts[entry.Category]) {
-            categoryAmounts[entry.Category] += parseFloat(entry.Amount);
-        } else {
-            categoryAmounts[entry.Category] = parseFloat(entry.Amount);
-        }
-    });
+	const categoryAmounts = {};
+	filteredData.forEach(entry => {
+		if (categoryAmounts[entry.Category]) {
+			categoryAmounts[entry.Category] += parseFloat(entry.Amount);
+		} else {
+			categoryAmounts[entry.Category] = parseFloat(entry.Amount);
+		}
+	});
 
-    // Prepare chartData from the aggregated categoryAmounts
-    const chartData = Object.keys(categoryAmounts).map(category => ({
-        Category: category,
-        Amount: categoryAmounts[category],
-    }));
+	// Prepare chartData from the aggregated categoryAmounts
+	const chartData = Object.keys(categoryAmounts).map(category => ({
+		Category: category,
+		Amount: categoryAmounts[category],
+	}));
 
-    // Create a ref for the handleResize function
-    const handleResizeRef = useRef(null);
+	// Create a ref for the handleResize function
+	const handleResizeRef = useRef(null);
 
-    // Define the handleResize function
-    handleResizeRef.current = () => {
-        setWindowWidth(window.innerWidth);
-    };
+	// Define the handleResize function
+	handleResizeRef.current = () => {
+		setWindowWidth(window.innerWidth);
+	};
 
-    // Update the window width when the component mounts and when the window is resized
-    useEffect(() => {
-        // Initial width
-        handleResizeRef.current();
+	// Update the window width when the component mounts and when the window is resized
+	useEffect(() => {
+		// Initial width
+		handleResizeRef.current();
 
-        // Listen for window resize events
-        window.addEventListener("resize", handleResizeRef.current);
+		// Listen for window resize events
+		window.addEventListener("resize", handleResizeRef.current);
 
-		const importComponents = async ()=>{
+		const importComponents = async () => {
 			const Expenses = await import(`../../config/finance/json-data/${currentYear}/Expenses.json`);
 			const ExpensesLink = await import(`../../config/finance/json-data/${currentYear}/ExpensesLink.json`);
-				setExpenses(Expenses.default);
-				setExpensesLink(ExpensesLink.default);
-			}
-			importComponents();
-		
+			setExpenses(Expenses.default);
+			setExpensesLink(ExpensesLink.default);
+		}
+		importComponents();
 
-        // Clean up the event listener when the component unmounts
-        return () => {
-            window.removeEventListener("resize", handleResizeRef.current);
-        };
-    }, []);
 
-    const barWidth = windowWidth < 900 ? null : 800;
-    const barHeight = windowWidth < 900 ? null : 400;
+		// Clean up the event listener when the component unmounts
+		return () => {
+			window.removeEventListener("resize", handleResizeRef.current);
+		};
+	}, []);
 
-    return (
-        <div className="flex justify-center items-center sm:px-6 lg:px-8 mt-8">
-            <div className="w-full lg:w-2/3 px-4 text-center">
-                <div className='mb-5'>
-                    <h1 id="budget-analysis" className="text-3xl font-semibold mb-4 my-2">Budget Analysis</h1>
-                    <p>Gain insights into the allocation of funds across different categories through our Budget Analysis</p>
-                    <div className="flex flex-col md:flex-row justify-between my-4 md:items-center md:justify-between md:m-8">
-                        <div className="my-2">
-                            <p className="text-center sm:text-left">Expenses</p>
-                            <p className="text-center sm:text-left mt-1 text-xl font-semibold">${totalAmount.toFixed(2)}</p>
-                        </div>
-                        <div className="md:flex space-x-4">
+	const barWidth = windowWidth < 900 ? null : 800;
+	const barHeight = windowWidth < 900 ? null : 400;
 
-                        <div className="mx-auto">
-                            {/* Select for category filter */}
-                            <select
-                                className="p-2 m-1 border text-gray-600 font-semibold border-gray-600 rounded-md bg-white text-violet text-xs w-full sm:w-auto md:w-48"
-                                value={selectedCategory}
-                                onChange={(e) => setSelectedCategory(e.target.value)}
-                            >
-                                <option value="All Categories">All Categories</option>
-                                {categories.map(category => (
-                                    <option key={category} value={category}>{category}</option>
-                                ))}
-                            </select>
+	return (
+		<div className="flex justify-center items-center sm:px-6 lg:px-8 mt-8">
+			<div className="w-full lg:w-2/3 px-4 text-center">
+				<div className='mb-5'>
+					<h1 id="budget-analysis" className="text-3xl font-semibold mb-4 my-2">Budget Analysis</h1>
+					<p>Gain insights into the allocation of funds across different categories through our Budget Analysis</p>
+					<div className="flex flex-col md:flex-row justify-between my-4 md:items-center md:justify-between md:m-8">
+						<div className="my-2">
+							<p className="text-center sm:text-left">Expenses</p>
+							<p className="text-center sm:text-left mt-1 text-xl font-semibold">${totalAmount.toFixed(2)}</p>
+						</div>
+						<div className="md:flex space-x-4">
 
-                            {/* Select for month filter */}
-                            <select
-                                className="p-2 m-1 pr-8 border border-gray-600 rounded-md bg-violet text-white font-semibold text-xs w-full sm:w-auto md:w-48"
-                                value={selectedMonth}
-                                onChange={(e) => setSelectedMonth(e.target.value)}
-                            >
-                                <option value="All Months">All Months</option>
-                                {months.map(month => (
-                                    <option key={month} value={month}>{month}</option>
-                                ))}
-                            </select>
-                            </div>
-                        </div>
-                    </div>
+							<div className="mx-auto">
+								{/* Select for category filter */}
+								<select
+									className="p-2 m-1 border text-gray-600 font-semibold border-gray-600 rounded-md bg-white text-violet text-xs w-full sm:w-auto md:w-48"
+									value={selectedCategory}
+									onChange={(e) => setSelectedCategory(e.target.value)}
+								>
+									<option value="All Categories">All Categories</option>
+									{categories.map(category => (
+										<option key={category} value={category}>{category}</option>
+									))}
+								</select>
 
-                </div>
-                {/* Recharts BarChart */}
-               <div className='flex justify-center'> 
-               <BarChart width={barWidth} height={barHeight} data={chartData}>
-                    <CartesianGrid strokeDasharray="3 3" />
-                    <YAxis tickFormatter={(value) => `$${value}`} />
-                    <Tooltip content={<CustomTooltip />} />
-                    <Legend />
-					
+								{/* Select for month filter */}
+								<select
+									className="p-2 m-1 pr-8 border border-gray-600 rounded-md bg-violet text-white font-semibold text-xs w-full sm:w-auto md:w-48"
+									value={selectedMonth}
+									onChange={(e) => setSelectedMonth(e.target.value)}
+								>
+									<option value="All Months">All Months</option>
+									{months.map(month => (
+										<option key={month} value={month}>{month}</option>
+									))}
+								</select>
+							</div>
+						</div>
+					</div>
+
+				</div>
+				{/* Recharts BarChart */}
+				<div className='flex justify-center'>
+					<BarChart width={barWidth} height={barHeight} data={chartData}>
+						<CartesianGrid strokeDasharray="3 3" />
+						<YAxis tickFormatter={(value) => `$${value}`} />
+						<Tooltip content={<CustomTooltip />} />
+						<Legend />
+
 						{
 							ExpensesLink &&
 							<Bar
@@ -232,13 +232,13 @@ const BarChartComponent = () => {
 								}}
 							/>
 						}
-					
-                </BarChart>
-                </div>
-                {windowWidth < 900 ? Expenses && ExpensesLink && <ExpensesCard Expenses={Expenses} ExpensesLinks={ExpensesLink} /> : null}
-            </div>
-        </div>
-    );
+
+					</BarChart>
+				</div>
+				{windowWidth < 900 ? Expenses && ExpensesLink && <ExpensesCard Expenses={Expenses} ExpensesLinks={ExpensesLink} /> : null}
+			</div>
+		</div>
+	);
 };
 
 export default BarChartComponent;
--- scripts/finance/index.js
+++ scripts/finance/index.js
@@ -3,32 +3,32 @@ const { convertToJson } = require('../utils');
 const { resolve, dirname } = require('path');
 
 module.exports = async function buildFinanceInfoList() {
-    try {
-        const currentDir = resolve(__dirname, '../../');
-		    const currentYear = String(new Date().getFullYear());
+	try {
+		const currentDir = resolve(__dirname, '../../');
+		const currentYear = String(new Date().getFullYear());
 
-        const expensesPath = resolve(currentDir, 'config', 'finance', currentYear, 'Expenses.yml');
-        const expensesLinkPath = resolve(currentDir, 'config', 'finance', currentYear, 'ExpensesLink.yml');
+		const expensesPath = resolve(currentDir, 'config', 'finance', currentYear, 'Expenses.yml');
+		const expensesLinkPath = resolve(currentDir, 'config', 'finance', currentYear, 'ExpensesLink.yml');
 
-        const ExpensesContent = await readFile(expensesPath, 'utf-8');
-        const ExpensesLinkContent = await readFile(expensesLinkPath, 'utf-8');
+		const ExpensesContent = await readFile(expensesPath, 'utf-8');
+		const ExpensesLinkContent = await readFile(expensesLinkPath, 'utf-8');
 
-        const Expenses = convertToJson(ExpensesContent);
-        const ExpensesLink = convertToJson(ExpensesLinkContent);
+		const Expenses = convertToJson(ExpensesContent);
+		const ExpensesLink = convertToJson(ExpensesLinkContent);
 
-        // Ensure the directory exists before writing the files
-        const jsonDirectory = resolve(currentDir, 'config', 'finance', 'json-data', currentYear);
-        await mkdir(jsonDirectory, { recursive: true });
+		// Ensure the directory exists before writing the files
+		const jsonDirectory = resolve(currentDir, 'config', 'finance', 'json-data', currentYear);
+		await mkdir(jsonDirectory, { recursive: true });
 
-        // Write Expenses to a JSON files
-        const expensesJsonPath = resolve(jsonDirectory, 'Expenses.json');
-        await writeFile(expensesJsonPath, JSON.stringify(Expenses, null, 2));
+		// Write Expenses to a JSON files
+		const expensesJsonPath = resolve(jsonDirectory, 'Expenses.json');
+		await writeFile(expensesJsonPath, JSON.stringify(Expenses, null, 2));
 
-        const expensesLinkJsonPath = resolve(jsonDirectory, 'ExpensesLink.json');
-        await writeFile(expensesLinkJsonPath, JSON.stringify(ExpensesLink, null, 2));
+		const expensesLinkJsonPath = resolve(jsonDirectory, 'ExpensesLink.json');
+		await writeFile(expensesLinkJsonPath, JSON.stringify(ExpensesLink, null, 2));
 
-    } catch (err) {
-        console.error(err);
-        throw err;
-    }
+	} catch (err) {
+		console.error(err);
+		throw err;
+	}
 };
