d01560fd29e95bdba84316ed08556b67d3d1b025
Ashmit JaiSarita Gupta <43639341+devilkiller-ag@users.noreply.github.com>
2024-04-07 12:55:16 +0530

    feat: migrate leftover components to typescript (#2825)

---
+++ components/NewsletterSubscribe.tsx
@@ -0,0 +1,161 @@
+import { useState } from 'react';
+
+import { ButtonType } from '@/types/components/buttons/ButtonPropsType';
+import { HeadingLevel, HeadingTypeStyle } from '@/types/typography/Heading';
+
+import { useTranslation } from '../utils/i18n';
+import Button from './buttons/Button';
+import Loader from './Loader';
+import Heading from './typography/Heading';
+import Paragraph from './typography/Paragraph';
+import TextLink from './typography/TextLink';
+
+enum FormStatus {
+  NORMAL = 'normal',
+  LOADING = 'loading',
+  SUCCESS = 'success',
+  ERROR = 'error'
+}
+
+interface NewsletterSubscribeProps {
+  className?: string;
+  dark?: boolean;
+  title?: string;
+  subtitle?: string;
+  type?: string;
+}
+
+/**
+ * @description This component displays Newsletter Subscribe component.
+ *
+ * @param {NewsletterSubscribeProps} props - The props for the Newsletter Subscribe component.
+ * @param {string} props.className - CSS class for styling the card.
+ * @param {boolean} props.dark - If true, the theme of the component will be dark.
+ * @param {string} props.title - The title of the Subscribe card.
+ * @param {string} props.subtitle - The subtitle of the Subscribe card.
+ * @param {string} props.type - The type of subscription.
+ */
+export default function NewsletterSubscribe({
+  className = 'p-8 text-center',
+  dark = false,
+  title = 'Subscribe to our newsletter to receive news about AsyncAPI.',
+  subtitle = 'We respect your inbox. No spam, promise ✌️',
+  type = 'Newsletter'
+}: NewsletterSubscribeProps) {
+  const [email, setEmail] = useState<string>('');
+  const [name, setName] = useState<string>('');
+  const [status, setStatus] = useState<FormStatus>(FormStatus.NORMAL);
+
+  const { t } = useTranslation('common');
+
+  const headTextColor = dark ? 'text-white' : '';
+  const paragraphTextColor = dark ? 'text-gray-300' : '';
+
+  const setFormStatus = (formResponse: FormStatus) => {
+    setStatus(formResponse);
+    setTimeout(() => {
+      setStatus(FormStatus.NORMAL);
+    }, 10000);
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    setStatus(FormStatus.LOADING);
+    event.preventDefault();
+    const data = {
+      name,
+      email,
+      interest: type
+    };
+
+    fetch('/.netlify/functions/newsletter_subscription', {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then((res) => {
+        if (res.status === 200) {
+          setFormStatus(FormStatus.SUCCESS);
+        } else {
+          setFormStatus(FormStatus.ERROR);
+        }
+
+        return res.json();
+      })
+      // eslint-disable-next-line @typescript-eslint/no-shadow, no-console
+      .then((data) => console.log(data));
+  };
+
+  if (status === FormStatus.SUCCESS) {
+    return (
+      <div className={className} data-testid='NewsletterSubscribe-main'>
+        <Heading level={HeadingLevel.h3} textColor={headTextColor} typeStyle={HeadingTypeStyle.lg} className='mb-4'>
+          {t('newsletterCTA.successTitle')}
+        </Heading>
+        <Paragraph className='mb-8' textColor={paragraphTextColor}>
+          {t('newsletterCTA.subtitle')}
+        </Paragraph>
+      </div>
+    );
+  }
+
+  if (status === FormStatus.ERROR) {
+    return (
+      <div className={className} data-testid='NewsletterSubscribe-main'>
+        <Heading level={HeadingLevel.h3} textColor={headTextColor} typeStyle={HeadingTypeStyle.lg} className='mb-4'>
+          {t('newsletterCTA.errorTitle')}
+        </Heading>
+        <Paragraph className='mb-8' textColor={paragraphTextColor}>
+          {t('newsletterCTA.errorSubtitle')}{' '}
+          <TextLink href='https://github.com/asyncapi/website/issues/new?template=bug.md' target='_blank'>
+            {t('newsletterCTA.errorLinkText')}
+          </TextLink>
+        </Paragraph>
+      </div>
+    );
+  }
+
+  return (
+    <div className={className} data-testid='NewsletterSubscribe-main'>
+      <Heading level={HeadingLevel.h3} textColor={headTextColor} typeStyle={HeadingTypeStyle.lg} className='mb-4'>
+        {title}
+      </Heading>
+      <Paragraph className='mb-8' textColor={paragraphTextColor}>
+        {subtitle}
+      </Paragraph>
+      {status === 'loading' ? (
+        <Loader dark={dark} />
+      ) : (
+        <form className='flex flex-col gap-4 md:flex-row' onSubmit={handleSubmit}>
+          <input
+            type='text'
+            name='name'
+            placeholder={t('newsletterCTA.nameInput')}
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            className='form-input block w-full rounded-md sm:text-sm sm:leading-5 md:mt-0 md:flex-1'
+            required
+            data-testid='NewsletterSubscribe-text-input'
+          />
+          <input
+            type='email'
+            name='email'
+            placeholder={t('newsletterCTA.emailInput')}
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className='form-input mt-2 block w-full rounded-md sm:text-sm sm:leading-5 md:mt-0 md:flex-1'
+            required
+            data-testid='NewsletterSubscribe-email-input'
+          />
+          <Button
+            type={ButtonType.SUBMIT}
+            text={t('newsletterCTA.subscribeBtn')}
+            className='mt-2 w-full md:mr-2 md:mt-0 md:flex-1'
+            href=''
+          />
+        </form>
+      )}
+    </div>
+  );
+}
