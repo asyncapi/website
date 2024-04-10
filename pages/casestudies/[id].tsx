13f4d86a7dae7fe83a45290206030c286617a83b
Vishvamsinh Vaghela <vaghelavishvamsinh11111@gmail.com>
2024-04-10 17:56:04 +0530

    lint fix

--- pages/casestudies/[id].tsx
+++ pages/casestudies/[id].tsx
@@ -1,16 +1,19 @@
-import Link from "next/link";
-import Paragraph from "../../components/typography/Paragraph";
-import Heading from "../../components/typography/Heading";
-import CaseStudiesList from "../../config/case-studies.json";
-import { MDXRemote, MDXRemoteSerializeResult } from "next-mdx-remote";
-import { getMDXComponents } from "../../components/MDX";
-import { serialize } from "next-mdx-remote/serialize";
-import GenericLayout from "../../components/layout/GenericLayout";
-import CaseTOC from "../../components/CaseTOC";
-import { generateCaseStudyContent } from "../../utils/staticHelpers";
-import { ParagraphTypeStyle } from "@/types/typography/Paragraph";
-import { HeadingTypeStyle } from "@/types/typography/Heading";
-import { ICaseStudy } from "@/types/post";
+import Link from 'next/link';
+import type { MDXRemoteSerializeResult } from 'next-mdx-remote';
+import { MDXRemote } from 'next-mdx-remote';
+import { serialize } from 'next-mdx-remote/serialize';
+
+import type { ICaseStudy } from '@/types/post';
+import { HeadingTypeStyle } from '@/types/typography/Heading';
+import { ParagraphTypeStyle } from '@/types/typography/Paragraph';
+
+import CaseTOC from '../../components/CaseTOC';
+import GenericLayout from '../../components/layout/GenericLayout';
+import { getMDXComponents } from '../../components/MDX';
+import Heading from '../../components/typography/Heading';
+import Paragraph from '../../components/typography/Paragraph';
+import CaseStudiesList from '../../config/case-studies.json';
+import { generateCaseStudyContent } from '../../utils/staticHelpers';
 
 interface IndexProps {
   casestudy: ICaseStudy;
@@ -38,26 +41,43 @@ const renderContent = (
   allComponents: Record<string, React.ComponentType<any>>,
   level: number
 ): JSX.Element[] => {
-  const typeStyle =
-    level === 0 ? HeadingTypeStyle.lg : level === 1 ? HeadingTypeStyle.md : HeadingTypeStyle.sm;
+  let typeStyle;
+
+  if (level === 0) {
+    typeStyle = HeadingTypeStyle.lg;
+  } else if (level === 1) {
+    typeStyle = HeadingTypeStyle.md;
+  } else {
+    typeStyle = HeadingTypeStyle.sm;
+  }
 
   return content.map((item) => {
     return (
-      <div className="mt-10" key={item.title}>
-        <Heading typeStyle={typeStyle} className="mt-8" id={item.title.replace(/<|>|"|\\|\/|=/gi, "").replace(/\s/gi, "-").toLowerCase()}>
+      <div className='mt-10' key={item.title}>
+        <Heading
+          typeStyle={typeStyle}
+          className='mt-8'
+          id={item.title
+            .replace(/<|>|"|\\|\/|=/gi, '')
+            .replace(/\s/gi, '-')
+            .toLowerCase()}
+        >
           {item.title}
         </Heading>
         {item.content && (
-          <Paragraph typeStyle={ParagraphTypeStyle.md} className="my-4">
+          <Paragraph typeStyle={ParagraphTypeStyle.md} className='my-4'>
             <MDXRemote {...item.content} components={allComponents} />
           </Paragraph>
         )}
         {item.items && (
-          <div className="mt-4 items-center">
-            <div className="flex flex-wrap gap-2">
-              {item.items.map((item: string) => (
-                <span key={item} className="bg-green-100 border border-green-600 text-green-600 p-1 text-center text-xs rounded-md ">
-                  {item}
+          <div className='mt-4 items-center'>
+            <div className='flex flex-wrap gap-2'>
+              {item.items.map((subItem: string) => (
+                <span
+                  key={subItem}
+                  className='rounded-md border border-green-600 bg-green-100 p-1 text-center text-xs text-green-600 '
+                >
+                  {subItem}
                 </span>
               ))}
             </div>
@@ -69,6 +89,11 @@ const renderContent = (
   });
 };
 
+/**
+ * @description Fetches static props for the page.
+ * @param {object} params - Parameters object containing the id.
+ * @param {string} params.id - The id of the case study..
+ */
 export async function getStaticProps({ params }: { params: { id: string } }) {
   const data = CaseStudiesList.filter((p: { id: string }) => p.id === params.id);
 
@@ -91,18 +116,22 @@ export async function getStaticProps({ params }: { params: { id: string } }) {
       asyncapiDocumentation: await serialize(data[0].asyncapi.documentation),
       asyncapiBindings: await serialize(data[0].asyncapi.bindings),
       asyncapiTools: await serialize(data[0].asyncapi.tools),
-      additionalResources: await serialize(data[0].additionalResources),
-    },
+      additionalResources: await serialize(data[0].additionalResources)
+    }
   };
 }
 
+/**
+ * @description Retrieves the static paths for the page.
+ */
 export async function getStaticPaths() {
   const paths = CaseStudiesList.map((study: { id: string }) => ({
-    params: { id: study.id },
+    params: { id: study.id }
   }));
+
   return {
     paths,
-    fallback: false,
+    fallback: false
   };
 }
 
@@ -124,9 +153,9 @@ const Index: React.FC<IndexProps> = ({
   asyncapiDocumentation,
   asyncapiBindings,
   asyncapiTools,
-  additionalResources,
+  additionalResources
 }) => {
-  const image = "/img/social/website-card.png";
+  const image = '/img/social/website-card.png';
   const allComponents = getMDXComponents();
   const contacts = casestudy.company.contact;
 
@@ -148,63 +177,77 @@ const Index: React.FC<IndexProps> = ({
     asyncapiBindings,
     asyncapiTools,
     additionalResources,
-    casestudy,
+    casestudy
   });
 
   return (
-    <GenericLayout title="AsyncAPI Case Studies" description="The home for all case studies related to AsyncAPI." image={image} hideBanner={true} wide>
-      <div className="max-w-screen lg:flex-row-reverse lg:flex lg:justify-between">
-        <CaseTOC toc={content} cssBreakingPoint="lg" className="lg:flex-1 bg-blue-100 mt-4 p-4 sticky top-20 overflow-y-auto max-h-screen lg:bg-transparent lg:mt-2 lg:pt-0 lg:pb-8 lg:top-24 lg:max-h-(screen-16) lg:border-l lg:border-gray-200 lg:min-w-[265px] lg:max-w-72 lg:-mr-14" />
-        <div className="px-4 sm:px-6 xl:px-0 lg:flex-1 lg:max-w-[812px] xl:max-w-5xl case-study">
-          <div className="mt-10 md:mt-20 flex flex-col md:flex-row justify-between items-center">
-            <div className="w-full md:w-[65%]">
-              <Heading typeStyle={HeadingTypeStyle.xl} className="countdown-text-gradient">
+    <GenericLayout
+      title='AsyncAPI Case Studies'
+      description='The home for all case studies related to AsyncAPI.'
+      image={image}
+      hideBanner={true}
+      wide
+    >
+      <div className='max-w-screen lg:flex lg:flex-row-reverse lg:justify-between'>
+        <CaseTOC
+          toc={content}
+          cssBreakingPoint='lg'
+          className='sticky top-20 mt-4 max-h-screen overflow-y-auto bg-blue-100 p-4 lg:top-24 lg:-mr-14 lg:mt-2 lg:max-h-(screen-16) lg:min-w-[265px] lg:max-w-72 lg:flex-1 lg:border-l lg:border-gray-200 lg:bg-transparent lg:pb-8 lg:pt-0'
+        />
+        <div className='case-study px-4 sm:px-6 lg:max-w-[812px] lg:flex-1 xl:max-w-5xl xl:px-0'>
+          <div className='mt-10 flex flex-col items-center justify-between md:mt-20 md:flex-row'>
+            <div className='w-full md:w-[65%]'>
+              <Heading typeStyle={HeadingTypeStyle.xl} className='countdown-text-gradient'>
                 {casestudy.company.name}
               </Heading>
-              <div className="flex flex-wrap gap-1" id="Contacts">
+              <div className='flex flex-wrap gap-1' id='Contacts'>
                 {contacts.map((item, index) => (
                   <div key={index}>
                     <Heading typeStyle={HeadingTypeStyle.lg}>
                       <Link
                         href={item.link}
-                        className="text-md leading-5 font-medium text-gray-900 hover:underline"
-                        target="_blank">
-
-                        {item.name}
-                        {index !== contacts.length - 1 ? ", " : " "}
-
+                        className='text-md font-medium leading-5 text-gray-900 hover:underline'
+                        target='_blank'
+                      >
+                        <span>
+                          {item.name}
+                          {index !== contacts.length - 1 && ', '}
+                        </span>
                       </Link>
                     </Heading>
                   </div>
                 ))}
               </div>
-              <div className="mt-4 flex flex-wrap gap-2">
-                <span className="bg-green-100 border border-green-600 text-green-600 p-1 text-center text-xs rounded-md ">
+              <div className='mt-4 flex flex-wrap gap-2'>
+                <span className='rounded-md border border-green-600 bg-green-100 p-1 text-center text-xs text-green-600 '>
                   Industry: {casestudy.company.industry}
                 </span>
-                <span className="bg-green-100 border border-green-600 text-green-600 p-1 text-center text-xs rounded-md ">
+                <span className='rounded-md border border-green-600 bg-green-100 p-1 text-center text-xs text-green-600 '>
                   Customers: {casestudy.company.customers}
                 </span>
-                <span className="bg-green-100 border border-green-600 text-green-600 p-1 text-center text-xs rounded-md ">
+                <span className='rounded-md border border-green-600 bg-green-100 p-1 text-center text-xs text-green-600 '>
                   Revenue: {casestudy.company.revenue}
                 </span>
               </div>
-              <div className="mt-10">
+              <div className='mt-10'>
                 <Heading typeStyle={HeadingTypeStyle.lg}>{casestudy.company.description}</Heading>
-                <Heading className="mt-10" typeStyle={HeadingTypeStyle.lg}>
+                <Heading className='mt-10' typeStyle={HeadingTypeStyle.lg}>
                   tl;dr just go and have a look at
                   <Link
-                    href={"/" + casestudy.asyncapi.fullExample}
-                    className="ml-2 text-secondary-500 underline hover:text-gray-800 font-medium transition ease-in-out duration-300"
-                    target="_blank">
-                    
-                      full production-used AsyncAPI document
-                    
+                    href={`/${casestudy.asyncapi.fullExample}`}
+                    className='ml-2 font-medium text-secondary-500 underline transition duration-300 ease-in-out hover:text-gray-800'
+                    target='_blank'
+                  >
+                    full production-used AsyncAPI document
                   </Link>
                 </Heading>
               </div>
             </div>
-            <img src={casestudy.company.logo} alt={casestudy.company.name} className="w-[250px] mt-5 md:mt-0 mx-auto rounded-lg" />
+            <img
+              src={casestudy.company.logo}
+              alt={casestudy.company.name}
+              className='mx-auto mt-5 w-[250px] rounded-lg md:mt-0'
+            />
           </div>
           {renderContent(content, allComponents, 0)}
         </div>
