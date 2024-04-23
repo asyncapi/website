78457e419b6bb4cf3c1cadbd1e41ce3f5b19e185
Vishvamsinh Vaghela <vaghelavishvamsinh11111@gmail.com>
2024-04-23 15:55:13 +0530

    lint fix

--- pages/404.tsx
+++ pages/404.tsx
@@ -25,7 +25,7 @@ export default function ErrorPage() {
           </header>
         </div>
         <div>
-          <img src={image} className='mx-auto w-52' />
+          <img src={image} alt='Not Found' className='mx-auto w-52' />
         </div>
       </div>
 
