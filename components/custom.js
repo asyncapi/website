05e7b947e1355a27051c431fafeb9311bb625732
Sambhav Gupta <sambhavgupta0705@gmail.com>
2024-04-26 11:11:30 +0530

    custom component

---
+++ components/custom.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import HeadComponent from './Head'
+import HomePage from '../pages'
+import Footer from '../components/footer/Footer'
+
+const Custom = () => {
+  return (
+    <div>
+      <HeadComponent/>
+      <HomePage/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default Custom
