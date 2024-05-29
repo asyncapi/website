6e2d4a18eef18dd4f8725c332eb8f2c727231e46
acethecreator <devaze007@gmail.com>
2024-05-29 14:22:45 +0100

    fix type failure when passing schema

--- components/docs/Visualizer.tsx
+++ components/docs/Visualizer.tsx
@@ -2,10 +2,14 @@ import React from 'react'
 import Schyma from 'schyma';
 import 'schyma/dist/esm/style.css'
 import schema from '../../config/3.0.0.json';
+import { JSONSchema7Object } from 'json-schema';
+
+const typeSchema = schema as unknown as JSONSchema7Object
+
 
 function Visualizer() {
     return (
-        <Schyma title="AsyncAPI Specification" description="The AsyncAPI Specification defines a set of fields that can be used in an AsyncAPI document to describe an application's API. The document may reference other files for additional details or shared fields, but it is typically a single, primary document that encapsulates the API description." schema={schema} />
+        <Schyma title="AsyncAPI Specification" description="The AsyncAPI Specification defines a set of fields that can be used in an AsyncAPI document to describe an application's API. The document may reference other files for additional details or shared fields, but it is typically a single, primary document that encapsulates the API description." schema={typeSchema} />
   )
 }
 
