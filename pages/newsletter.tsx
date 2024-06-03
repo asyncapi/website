28f6aaf2b73cec6b0835f7afa02bf51d18b1d43a
akshatnema <akshatnema.school@gmail.com>
2024-06-03 18:19:22 +0530

    Added newsletter page inside website

---
+++ pages/newsletter.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Head from "@/components/Head";
+import Container from '@/components/layout/Container'
+import NewsletterSubscribe from '@/components/NewsletterSubscribe'
+
+export default function NewsletterIndexPage() {
+  return (
+    <div>
+      <Head title="Newsletter"/>
+      <div className="py-12 mt-12">
+        <Container wide>
+          <NewsletterSubscribe />
+        </Container>
+      </div>
+    </div>
+  );
+}
