d1d7649d2c73cffe688f00bbf5189fa5a4a75543
akshatnema <akshatnema.school@gmail.com>
2024-06-04 15:25:09 +0530

    lint fixed in newsletter page

--- pages/newsletter.tsx
+++ pages/newsletter.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import Head from "@/components/Head";
-import Container from '@/components/layout/Container'
-import NewsletterSubscribe from '@/components/NewsletterSubscribe'
+
+import Head from '@/components/Head';
+import Container from '@/components/layout/Container';
+import NewsletterSubscribe from '@/components/NewsletterSubscribe';
 
 export default function NewsletterIndexPage() {
   return (
     <div>
-      <Head title="Newsletter"/>
-      <div className="py-12 mt-12">
+      <Head title='Newsletter' />
+      <div className='mt-12 py-12'>
         <Container wide>
           <NewsletterSubscribe />
         </Container>
