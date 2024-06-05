8bf5af94b1916779d8bb3ffe6aa8a41884cc0caf
Vishvamsinh Vaghela <vaghelavishvamsinh11111@gmail.com>
2024-06-05 23:01:42 +0530

    utils test updated

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -1,27 +1,47 @@
-const {convertToJson} = require("../scripts/utils")
+const { convertToJson } = require("../scripts/utils");
 
 describe('convertToJson', () => {
   test('should return JSON object if input is valid JSON string', () => {
     const jsonString = '{"name": "AsyncAPI", "age": 5}';
-    const result = convertToJson(jsonString);
+    let result;
+    try {
+      result = convertToJson(jsonString);
+    } catch (error) {
+      result = error;
+    }
     expect(result).toEqual({ name: "AsyncAPI", age: 5 });
   });
 
   test('should return JavaScript object if input is valid YAML string', () => {
     const yamlString = 'name: AsyncAPI\nage: 5';
-    const result = convertToJson(yamlString);
+    let result;
+    try {
+      result = convertToJson(yamlString);
+    } catch (error) {
+      result = error;
+    }
     expect(result).toEqual({ name: "AsyncAPI", age: 5 });
   });
 
   test('should return input if input is already an object', () => {
     const obj = { name: "AsyncAPI", age: 5 };
-    const result = convertToJson(obj);
+    let result;
+    try {
+      result = convertToJson(obj);
+    } catch (error) {
+      result = error;
+    }
     expect(result).toBe(obj);
   });
 
   test('should return null if input is invalid JSON and invalid YAML', () => {
     const invalidString = 'name: AsyncAPI, age: five';
-    const result = convertToJson(invalidString);
+    let result;
+    try {
+      result = convertToJson(invalidString);
+    } catch (error) {
+      result = error;
+    }
     expect(result).toBeNull();
   });
 
