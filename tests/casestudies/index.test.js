abe7c8b2169b378a7452778fd9c7182f7a537009
Vishvamsinh Vaghela <vaghelavishvamsinh11111@gmail.com>
2024-06-11 20:11:57 +0530

    added test for case studies

---
+++ tests/casestudies/index.test.js
@@ -0,0 +1,58 @@
+const { readdir, writeFile, readFile } = require('fs').promises;
+const { convertToJson } = require('../../scripts/utils');
+const { resolve } = require('path');
+const buildCaseStudiesList = require('../../scripts/casestudies/index');
+const {caseStudyContentyaml,caseStudyContentJson} = require('../fixtures/caseStudyData')
+
+jest.mock('fs', () => ({
+  promises: {
+    readdir: jest.fn(),
+    writeFile: jest.fn(),
+    readFile: jest.fn(),
+  },
+}));
+
+jest.mock('../../scripts/utils', () => ({
+  convertToJson: jest.fn(),
+}));
+
+describe('buildCaseStudiesList', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test('should read files, convert to JSON, and write to case-studies.json', async () => {
+    const dirWithCaseStudy = 'config/casestudies';
+    const files = ['casestudy1.yml', 'casestudy2.yml'];
+    const caseStudyContent1 = caseStudyContentyaml;
+    const caseStudyContent2 = caseStudyContentyaml;
+    const jsonContent1 = caseStudyContentJson;
+    const jsonContent2 = caseStudyContentJson;
+    const caseStudiesJsonPath = resolve(__dirname, '../../config', 'case-studies.json');
+
+    readdir.mockResolvedValue(files);
+    readFile.mockResolvedValueOnce(caseStudyContent1).mockResolvedValueOnce(caseStudyContent2);
+    convertToJson.mockReturnValueOnce(jsonContent1).mockReturnValueOnce(jsonContent2);
+
+    await buildCaseStudiesList();
+
+    expect(readdir).toHaveBeenCalledWith(dirWithCaseStudy);
+    expect(readFile).toHaveBeenCalledWith(`${dirWithCaseStudy}/casestudy1.yml`, 'utf-8');
+    expect(readFile).toHaveBeenCalledWith(`${dirWithCaseStudy}/casestudy2.yml`, 'utf-8');
+    expect(convertToJson).toHaveBeenCalledWith(caseStudyContent1);
+    expect(convertToJson).toHaveBeenCalledWith(caseStudyContent2);
+    expect(writeFile).toHaveBeenCalledWith(caseStudiesJsonPath, JSON.stringify([jsonContent1]));
+    expect(writeFile).toHaveBeenCalledWith(caseStudiesJsonPath, JSON.stringify([jsonContent1, jsonContent2]));
+  });
+
+  test('should log and throw an error if an error occurs', async () => {
+    const error = new Error('Test error');
+    readdir.mockRejectedValue(error);
+    const consoleErrorSpy = jest.spyOn(console, 'error').mockImplementation(() => {});
+
+    await expect(buildCaseStudiesList()).rejects.toThrow(error);
+
+    expect(consoleErrorSpy).toHaveBeenCalledWith(error);
+    consoleErrorSpy.mockRestore();
+  });
+});
---
+++ tests/fixtures/caseStudyData.js
@@ -0,0 +1,19 @@
+module.exports = {
+    caseStudyContentyaml: `
+id: adeogroup
+company:
+  name: Adeo Group
+  description: Adeo owns different brands in the retail industry focused on home improvement and DIY markets, like Leroy Merlin.
+  customers: 500M
+  industry: Retail
+`,
+    caseStudyContentJson: {
+        id: 'adeogroup',
+        company: {
+            name: 'Adeo Group',
+            description: 'Adeo owns different brands in the retail industry focused on home improvement and DIY markets, like Leroy Merlin.',
+            customers: '500M',
+            industry: 'Retail'
+        }
+    }
+}
