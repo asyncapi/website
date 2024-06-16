0d29b559dc50ca0c8630ccc35f22d71ce9cfe303
Akshat Nema <76521428+akshatnema@users.noreply.github.com>
2024-06-16 09:22:21 +0530

    Update .storybook/main.ts

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -8,7 +8,6 @@ const config: StorybookConfig = {
     "@storybook/addon-onboarding",
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@chromatic-com/storybook",
     "@storybook/addon-interactions",
   ],
   docs: {
