b27a0956487fe368f94f47f710145f4733089c21
Ansh Goyal <anshgoyal1704@gmail.com>
2024-06-16 04:54:26 +0000

    fix utils.test.js

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -1,32 +1,24 @@
 const { convertToJson } = require("../scripts/utils");
-const {jsonString,yamlString,jsonObject,invalidString} = require("./fixtures/utilsData")
+const { jsonString, yamlString, jsonObject, invalidString } = require("./fixtures/utilsData")
 
 describe('convertToJson', () => {
   test('should return JSON object if input is valid JSON string', () => {
-    try {
     expect(convertToJson(jsonString)).toEqual(jsonObject);
-    } catch (err) {
-      console.error(err);
-    }
   });
 
   test('should return JavaScript object if input is valid YAML string', () => {
-    try {
     expect(convertToJson(yamlString)).toEqual(jsonObject);
-    } catch (err) {
-      console.error(err);
-    }
   });
 
   test('should return input if input is already an object', () => {
-    try {
     expect(convertToJson(jsonObject)).toBe(jsonObject);
-    } catch (err) {
-      console.error(err)
-    }
   });
 
   test('should return null if input is invalid JSON and invalid YAML', () => {
-    expect(() => convertToJson(invalidString)).toThrow('Invalid content format');
+    try {
+      convertToJson(invalidString);
+    } catch (error) {
+      expect(error.message).toBe('Invalid content format');
+    }
   });
 });
