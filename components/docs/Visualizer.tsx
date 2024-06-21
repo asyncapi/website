46a1060cd5a4bdd1621dd392edf296cd264b42a9
acethecreator <devaze007@gmail.com>
2024-06-21 12:05:34 +0100

    added metatag to the visualizer component

--- components/docs/Visualizer.tsx
+++ components/docs/Visualizer.tsx
@@ -5,19 +5,28 @@ import React from 'react';
 import Schyma from 'schyma';
 
 import schema from '../../config/3.0.0.json';
+import Head from '../Head';
 
 const typeSchema = schema as unknown as JSONSchema7Object;
+
+interface IGenericLayoutProps {
+  title: string;
+  description: string;
+}
 
 /**
  * @description This component renders the spec explorer.
  */
-function Visualizer() {
+function Visualizer({ title, description }: IGenericLayoutProps) {
   return (
-    <Schyma
-      title='AsyncAPI Specification'
-      description="The AsyncAPI Specification defines a set of fields that can be used in an AsyncAPI document to describe an application's API. The document may reference other files for additional details or shared fields, but it is typically a single, primary document that encapsulates the API description."
-      schema={typeSchema}
-    />
+    <div>
+      <Head title={title} description={description} />
+      <Schyma
+        title='AsyncAPI Specification'
+        description="The AsyncAPI Specification defines a set of fields that can be used in an AsyncAPI document to describe an application's API. The document may reference other files for additional details or shared fields, but it is typically a single, primary document that encapsulates the API description."
+        schema={typeSchema}
+      />
+    </div>
   );
 }
 
