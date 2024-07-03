32ef1c008059a2b82b0b9f155d0c40d4a49aabfe
Vishvamsinh Vaghela <vaghelavishvamsinh11111@gmail.com>
2024-07-03 10:16:09 +0530

    fix jest config

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,5 @@
 module.exports = {
-<<<<<<< HEAD
-<<<<<<< HEAD
-    verbose: true, // display individual test results with the test suite hierarchy
-    collectCoverage: true, // collect test coverage information\
-    collectCoverageFrom: ['scripts/**/*.js']
-};
-  
-=======
   verbose: true, // display individual test results with the test suite hierarchy
   collectCoverage: true, // collect test coverage information\
   collectCoverageFrom: ['scripts/**/*.js']
 };
->>>>>>> master
-=======
-  verbose: true, // display individual test results with the test suite hierarchy
-  collectCoverage: true, // collect test coverage information\
-  collectCoverageFrom: ['scripts/**/*.js']
-};
->>>>>>> caseStudiesTest
