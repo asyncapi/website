8e99276c1948281b0850bae445b87cc841a9ecc0
Ashmit JaiSarita Gupta <43639341+devilkiller-ag@users.noreply.github.com>
2024-07-07 12:34:31 +0530

    feat: added stories for icons, logos, and colors (#3078)

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,6 +2,7 @@ import type { StorybookConfig } from "@storybook/nextjs";
 
 const config: StorybookConfig = {
   stories: [
+    "../components/**/*.mdx",
     "../components/**/*.stories.@(js|jsx|mjs|ts|tsx)",
   ],
   addons: [
