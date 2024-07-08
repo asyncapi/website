a59c1725496ec1ff595a520c9d7ea63a7b43d3c3
acethecreator <devaze007@gmail.com>
2024-07-08 14:20:17 +0100

    added suggested

--- components/docs/Visualizer.tsx
+++ components/docs/Visualizer.tsx
@@ -5,22 +5,15 @@ import React from 'react';
 import Schyma from 'schyma';
 
 import schema from '../../config/3.0.0.json';
-import Head from '../Head';
 
 const typeSchema = schema as unknown as JSONSchema7Object;
-
-interface IGenericExplorerProps {
-  title: string;
-  description: string;
-}
 
 /**
  * @description This component renders the spec explorer.
  */
-function Visualizer({ title, description }: IGenericExplorerProps) {
+function Visualizer() {
   return (
     <div>
-      <Head title={title} description={description} />
       <Schyma
         title='AsyncAPI Specification'
         description="The AsyncAPI Specification defines a set of fields that can be used in an AsyncAPI document to describe an application's API. The document may reference other files for additional details or shared fields, but it is typically a single, primary document that encapsulates the API description."
