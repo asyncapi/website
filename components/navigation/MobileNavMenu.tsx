d8778340ce98ea93aaf8ec72049592c94d5f4cfe
TenzDelek <tibetdelek@gmail.com>
2024-07-24 22:42:16 +0530

    text size from base to sm

--- components/navigation/MobileNavMenu.tsx
+++ components/navigation/MobileNavMenu.tsx
@@ -135,7 +135,7 @@ export default function MobileNavMenu({ onClickClose = () => {}, uniqueLangs, ch
           <div className='grid gap-4'>
           <div>
                 <h4 className='mb-4 flex justify-between font-medium text-gray-800'>
-                  <a className='cursor-pointer flex gap-2 items-center'>Language <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
+                  <a className='cursor-pointer flex gap-x-2 items-center'>Language <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
   <path strokeLinecap="round" strokeLinejoin="round" d="m10.5 21 5.25-11.25L21 21m-9-3h7.5M3 5.621a48.474 48.474 0 0 1 6-.371m0 0c1.12 0 2.233.038 3.334.114M9 5.25V3m3.334 2.364C11.176 10.658 7.69 15.08 3 17.502m9.334-12.138c.896.061 1.785.147 2.666.257m-4.589 8.495a18.023 18.023 0 0 1-3.827-5.802" />
 </svg>
 </a>
@@ -146,7 +146,7 @@ export default function MobileNavMenu({ onClickClose = () => {}, uniqueLangs, ch
                     <button
                       key={lang.key}
                       onClick={() => changeLanguage(lang.value.toLowerCase(), true)}
-                      className='mb-4 ml-2 block rounded-lg py-1 text-base font-medium leading-6 text-start  w-full text-gray-700 transition duration-150 ease-in-out hover:bg-gray-50'
+                      className='mb-4 ml-2 block rounded-lg py-1 text-sm font-medium leading-6 text-start  w-full text-gray-700 transition duration-150 ease-in-out hover:bg-gray-50'
                       data-testid='MobileNav-language-item'
                     >
                       {lang.text} 
