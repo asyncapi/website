0e2a7098a45f7bfd3446951937182dbbfca23c4c
Vishvamsinh Vaghela <vaghelavishvamsinh11111@gmail.com>
2024-07-26 09:55:02 +0530

    lint fix

--- components/helpers/read-yaml-file.ts
+++ components/helpers/read-yaml-file.ts
@@ -1,5 +1,12 @@
 import fs from 'fs/promises';
 
+/**
+ * Reads a YAML file from the public directory and returns its content as a formatted string.
+ *
+ * @param {string} fileName - The name of the YAML file to read.
+ * @returns {Promise<string>} The content of the YAML file formatted as a string with YAML syntax highlighting.
+ * @throws {Error} If there is an error reading the file.
+ */
 export default async function readYamlFile(fileName: string) {
   try {
     const data = await fs.readFile(`./public/${fileName}`, 'utf-8');
@@ -9,4 +16,4 @@ export default async function readYamlFile(fileName: string) {
   } catch (error: any) {
     throw new Error(`Error: something went wrong while reading ${fileName} file: ${error.message}`);
   }
-};
+}
