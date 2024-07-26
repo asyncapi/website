60b7a56cb3f15bc74026535465b142a8881e305d
TenzDelek <tibetdelek@gmail.com>
2024-07-26 16:01:53 +0530

    make default export for single file for lint

--- components/helpers/read-yaml-file.ts
+++ components/helpers/read-yaml-file.ts
@@ -1,6 +1,6 @@
 import fs from 'fs/promises';
 
-export const readYamlFile = async (fileName: string) => {
+const readYamlFile = async (fileName: string) => {
   try {
     const data = await fs.readFile(`./public/${fileName}`, 'utf-8');
     const yamlString = `\`\`\`yaml\n${data}\`\`\``;
@@ -10,3 +10,5 @@ export const readYamlFile = async (fileName: string) => {
     throw new Error(`Error: something went wrong while reading ${fileName} file: ${error.message}`);
   }
 };
+
+export default readYamlFile;
