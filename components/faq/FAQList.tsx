88f443f7d4554614c683100d60b75152af82a625
devilkiller-ag <ashmitgupta.official@gmail.com>
2024-07-28 19:28:27 +0530

    rectified faq content spacing

--- components/faq/FAQList.tsx
+++ components/faq/FAQList.tsx
@@ -15,8 +15,8 @@ export const faqList: FAQ[] = [
     title: 'What protocols does it support?',
     content: (
       <div>
-        AsyncAPI is protocol- agnostic, so you can use it for APIs that work over any protocol(e.g., AMQP, MQTT,
-        WebSockets, Kafka, STOMP, HTTP, Mercure, etc).For more information, refer to the
+        AsyncAPI is protocol - agnostic, so you can use it for APIs that work over any protocol(e.g., AMQP, MQTT,
+        WebSockets, Kafka, STOMP, HTTP, Mercure, etc). For more information, refer to the{' '}
         <a
           href='https://www.asyncapi.com/docs/reference/specification/latest#serverBindingsObject'
           className='border-b border-secondary-400 font-semibold text-gray-900 transition duration-300 ease-in-out hover:border-secondary-500'
@@ -32,8 +32,9 @@ export const faqList: FAQ[] = [
     content: (
       <div>
         <p>
-          AsyncAPI users are those who implement and maintain event - driven architecture.For example, people that write
-          backend API using WebSocket, or people that maintain communication between their microservices using Kafka.
+          AsyncAPI users are those who implement and maintain event - driven architecture. For example, people that
+          write backend API using WebSocket, or people that maintain communication between their microservices using
+          Kafka.
         </p>
       </div>
     )
@@ -42,9 +43,9 @@ export const faqList: FAQ[] = [
     title: 'What is the AsyncAPI Community?',
     content: (
       <div>
-        It’s the core of the initiative.The AsyncAPI community contributes to the development of the tool, it promotes
+        It’s the core of the initiative. The AsyncAPI community contributes to the development of the tool, it promotes
         access and distribution of the specification allowing freedom of use, study, copying, modification, and
-        redistribution to anyone who wishes to do so.The cooperation between these people in all areas of software
+        redistribution to anyone who wishes to do so. The cooperation between these people in all areas of software
         production generates a substantial improvement in the quality of the software, as well as greater dissemination
         and sustainability over time, and prioritizing the benefit of society over any other.
       </div>
@@ -54,8 +55,8 @@ export const faqList: FAQ[] = [
     title: 'Who can use it?',
     content: (
       <div>
-        Anyone.All projects under AsyncAPI Initiative are part of the Linux Foundation, licensed under the Apache 2.0
-        license.It’s open to use and contribution.
+        Anyone. All projects under AsyncAPI Initiative are part of the Linux Foundation, licensed under the Apache 2.0
+        license. It’s open to use and contribution.
       </div>
     )
   },
@@ -70,7 +71,7 @@ export const faqList: FAQ[] = [
               className='border-b border-secondary-400 transition duration-300 ease-in-out hover:border-secondary-500'
             >
               Official AsyncAPI Documentation
-            </Link>
+            </Link>{' '}
           </li>
           <li className='py-1'>
             <a
@@ -78,7 +79,7 @@ export const faqList: FAQ[] = [
               className='border-b border-secondary-400 transition duration-300 ease-in-out hover:border-secondary-500'
             >
               Presentation by Fran Méndez
-            </a>
+            </a>{' '}
           </li>
         </ul>
       </div>
