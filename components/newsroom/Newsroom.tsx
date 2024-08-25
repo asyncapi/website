b7e3f00585df2fca4ff466d93fe94775403a2b58
Taufiq <taufiq9.2004@gmail.com>
2024-08-25 17:35:02 +0530

    fix:tweetLimit in community/newsroom isn't been reflected(#3132)

--- components/newsroom/Newsroom.tsx
+++ components/newsroom/Newsroom.tsx
@@ -78,7 +78,7 @@ export default function Newsroom() {
           </div>
           <div className='w-full px-2 md:w-1/2 md:pl-4 md:pr-0'>
             <div className='mx-auto mt-8 w-full rounded-xl shadow-md md:mt-0' data-testid='Newsroom-Twitter'>
-              <TwitterTimelineEmbed sourceType='profile' screenName='AsyncAPISpec' options={{ tweetLimit: '2' }} />
+              <TwitterTimelineEmbed sourceType='profile' screenName='AsyncAPISpec' options={{ tweetLimit: 2 }} />
             </div>
           </div>
         </div>
