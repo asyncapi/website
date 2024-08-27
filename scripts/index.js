a21b8dc3fcd83f3e918ecfc40bdb674f56f760ae
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2024-08-27 21:36:01 +0530

    chore: add logic to find the foldername with latest year

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,5 @@
 const { resolve } = require('path');
+const fs = require('fs');
 const rssFeed = require('./build-rss');
 const buildPostList = require('./build-post-list');
 const buildCaseStudiesList = require('./casestudies');
@@ -10,11 +11,27 @@ async function start() {
   rssFeed('blog', 'AsyncAPI Initiative Blog RSS Feed', 'AsyncAPI Initiative Blog', 'rss.xml');
   await buildCaseStudiesList('config/casestudies', resolve(__dirname, '../config', 'case-studies.json'));
   await buildAdoptersList();
+  const financeDir = resolve('.', 'config', 'finance');
+
+  // loop through all the files finance in directory and find the latest year to build the finance info list
+  const yearList = fs
+    .readdirSync(financeDir)
+    // filter out any files that are not numbers
+    .filter((file) => {
+      return !Number.isNaN(parseFloat(file));
+    })
+    // sort the years in descending order
+    .sort((a, b) => {
+      return parseFloat(b) - parseFloat(a);
+    });
+
+  const latestYear = yearList[0];
+
   await buildFinanceInfoList({
     currentDir: '.',
     configDir: 'config',
     financeDir: 'finance',
-    year: String(new Date().getFullYear()),
+    year: latestYear,
     jsonDataDir: 'json-data'
   });
 }
