978b0cd452c90a99a453186229084c3a1e07df12
George Lifin Rell <georgelifinrell@gmail.com>
2024-09-08 14:14:29 +0530

    fix: issue 3196 remove jobs from footer

--- components/footer/FooterList.ts
+++ components/footer/FooterList.ts
@@ -64,10 +64,11 @@ export const initiativeLinks: InitiativeLink[] = [
     label: 'Shop',
     url: 'https://www.store.asyncapi.com/'
   },
-  {
-    label: 'Jobs',
-    url: '/jobs'
-  },
+  // development in progress for Jobs
+  // {
+  //   label: 'Jobs',
+  //   url: '/jobs'
+  // },
   {
     label: 'Brand',
     url: 'https://github.com/asyncapi/brand/blob/master/brand-guidelines/README.md'
