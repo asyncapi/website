829c653a764b004abdaaef41634234a87f554f89
Vishvamsinh Vaghela <vaghelavishvamsinh11111@gmail.com>
2024-09-14 18:27:26 +0530

    jest config update

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   verbose: true,
   collectCoverage: true,
-  collectCoverageFrom: ['scripts/**/*.js'],
   coverageReporters: ['text', 'lcov', 'json-summary'],
   coverageDirectory: 'coverage',
   // To disallow netlify edge function tests from running
