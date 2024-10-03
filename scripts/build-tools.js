e56289b06b35def041fe3c283ceaa38dcffa63c6
Vishvamsinh Vaghela <90895835+vishvamsinh28@users.noreply.github.com>
2024-10-03 07:56:07 +0530

    feat: add tests for build tools script (#3245)
    Co-authored-by: Ansh Goyal <anshgoyal1704@gmail.com>

--- scripts/build-tools.js
+++ scripts/build-tools.js
@@ -1,24 +1,33 @@
 const { getData } = require('./tools/extract-tools-github');
 const { convertTools } = require('./tools/tools-object');
 const { combineTools } = require('./tools/combine-tools');
-const manualTools = require('../config/tools-manual.json')
-
 const fs = require('fs');
 const { resolve } = require('path');
 
-const buildTools = async () => {
+const buildTools = async (automatedToolsPath, manualToolsPath, toolsPath, tagsPath) => {
   try {
     let githubExtractData = await getData();
     let automatedTools = await convertTools(githubExtractData);
+
     fs.writeFileSync(
-      resolve(__dirname, '../config', 'tools-automated.json'),
+      automatedToolsPath,
       JSON.stringify(automatedTools, null, '  ')
     );
-    await combineTools(automatedTools, manualTools);
+
+    await combineTools(automatedTools, require(manualToolsPath), toolsPath, tagsPath);
   } catch (err) {
-    console.log(err);
-    throw err
+    throw new Error(`An error occurred while building tools: ${err.message}`);
   }
 };
 
-buildTools();
+/* istanbul ignore next */
+if (require.main === module) {
+  const automatedToolsPath = resolve(__dirname, '../config', 'tools-automated.json');
+  const manualToolsPath = resolve(__dirname, '../config', 'tools-manual.json');
+  const toolsPath = resolve(__dirname, '../config', 'tools.json');
+  const tagsPath = resolve(__dirname, '../config', 'all-tags.json');
+
+  buildTools(automatedToolsPath, manualToolsPath, toolsPath, tagsPath);
+}
+
+module.exports = { buildTools };
