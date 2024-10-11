89c29fb58b1a77011f54db7b4177dad3a5d6bcb1
Vishvamsinh Vaghela <vaghelavishvamsinh11111@gmail.com>
2024-10-11 13:30:31 +0530

    excluding scripts with import errors

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,9 @@ module.exports = {
   verbose: true,
   collectCoverage: true,
   collectCoverageFrom: ['scripts/**/*.js'],
+  coveragePathIgnorePatterns: [
+    'scripts/compose.js','scripts/dashboard'
+  ],
   // To disallow netlify edge function tests from running
   testMatch: ['**/tests/**/*.test.*', '!**/netlify/**/*.test.*'],
 };
