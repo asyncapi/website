d31a06fb9e2092d9f9280cf431986aff98bad24d
Vishvamsinh Vaghela <vaghelavishvamsinh11111@gmail.com>
2024-10-16 19:11:27 +0530

    commit to rerun workflow

--- scripts/build-tools.js
+++ scripts/build-tools.js
@@ -22,6 +22,7 @@ const buildTools = async (automatedToolsPath, manualToolsPath, toolsPath, tagsPa
 
 /* istanbul ignore next */
 if (require.main === module) {
+  
   const automatedToolsPath = path.join(__dirname, '../config', 'tools-automated.json');
   const manualToolsPath = path.join(__dirname, '../config', 'tools-manual.json');
   const toolsPath = path.join(__dirname, '../config', 'tools.json');
