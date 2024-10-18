fe9c3dafc128a074eab3a771962d060b58e9a120
Vishvamsinh Vaghela <vaghelavishvamsinh11111@gmail.com>
2024-10-18 14:41:12 +0530

    revert uncessary changes

--- scripts/build-post-list.js
+++ scripts/build-post-list.js
@@ -1,5 +1,5 @@
 const { readdirSync, statSync, existsSync, readFileSync, writeFileSync } = require('fs')
-const { resolve, basename } = require('path')
+const { basename } = require('path')
 const frontMatter = require('gray-matter')
 const toc = require('markdown-toc')
 const { slugify } = require('markdown-toc/lib/utils')
