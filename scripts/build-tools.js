4540530a037894b5c9bc1164c68e1d7d34d57c57
Vishvamsinh Vaghela <vaghelavishvamsinh11111@gmail.com>
2024-10-19 11:05:32 +0530

    added retry mechanicasm for enoent errors

--- scripts/build-tools.js
+++ scripts/build-tools.js
@@ -14,16 +14,31 @@ const buildTools = async (automatedToolsPath, manualToolsPath, toolsPath, tagsPa
     if (!fs.existsSync(automatedDir)) {
       fs.mkdirSync(automatedDir, { recursive: true });
     }
-    fs.writeFileSync(
-      automatedToolsPath,
-      JSON.stringify(automatedTools, null, '  ')
-    );
+
+    await retryWriteFile(automatedToolsPath, JSON.stringify(automatedTools, null, '  '));
 
     await combineTools(automatedTools, require(manualToolsPath), toolsPath, tagsPath);
   } catch (err) {
     throw new Error(`An error occurred while building tools: ${err.message}`);
   }
 };
+
+async function retryWriteFile(filePath, data, retries = 3, delay = 1000) {
+  for (let attempt = 0; attempt < retries; attempt++) {
+    try {
+      fs.writeFileSync(filePath, data);
+      console.log(`File written successfully to ${filePath}`);
+      break;
+    } catch (err) {
+      if (err.code === 'ENOENT') {
+        console.error(`ENOENT error on attempt ${attempt + 1}. Retrying in ${delay}ms...`);
+        await new Promise((resolve) => setTimeout(resolve, delay));
+      } else {
+        throw err;
+      }
+    }
+  }
+}
 
 /* istanbul ignore next */
 if (require.main === module) {
