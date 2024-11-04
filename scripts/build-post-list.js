ec110dcb5ebafdef952d01af23c1a4c611e26b9a
Vishvamsinh Vaghela <vaghelavishvamsinh11111@gmail.com>
2024-11-04 20:36:34 +0530

    feqfeq

--- scripts/build-post-list.js
+++ scripts/build-post-list.js
@@ -1,44 +1,40 @@
-const { readdirSync, statSync, existsSync, readFileSync, writeFileSync } = require('fs')
-const { basename } = require('path')
-const frontMatter = require('gray-matter')
-const toc = require('markdown-toc')
-const { slugify } = require('markdown-toc/lib/utils')
-const readingTime = require('reading-time')
-const { markdownToTxt } = require('markdown-to-txt')
-const { buildNavTree, addDocButtons } = require('./build-docs')
+const { readdirSync, statSync, existsSync, readFileSync, writeFileSync } = require('fs');
+const path = require('path');
+const frontMatter = require('gray-matter');
+const toc = require('markdown-toc');
+const { slugify } = require('markdown-toc/lib/utils');
+const readingTime = require('reading-time');
+const { markdownToTxt } = require('markdown-to-txt');
+const { buildNavTree, addDocButtons } = require('./build-docs');
 
-let specWeight = 100
+let specWeight = 100;
 const result = {
   docs: [],
   blog: [],
   about: [],
   docsTree: {}
-}
-const releaseNotes = []
+};
+const releaseNotes = [];
 
 const addItem = (details) => {
-  if (details.slug.startsWith('/docs'))
-    result["docs"].push(details)
-  else if (details.slug.startsWith('/blog'))
-    result["blog"].push(details)
-  else if (details.slug.startsWith('/about'))
-    result["about"].push(details)
-  else { }
-}
+  if (details.slug.startsWith('/docs')) result["docs"].push(details);
+  else if (details.slug.startsWith('/blog')) result["blog"].push(details);
+  else if (details.slug.startsWith('/about')) result["about"].push(details);
+};
 
 async function buildPostList(postDirectories, basePath, writeFilePath) {
   try {
     if (postDirectories.length === 0) {
       throw new Error('Error while building post list: No post directories provided');
     }
-    walkDirectories(postDirectories, result, basePath)
-    const treePosts = buildNavTree(result["docs"].filter((p) => p.slug.startsWith('/docs/')))
-    result["docsTree"] = treePosts
-    result["docs"] = addDocButtons(result["docs"], treePosts)
+    walkDirectories(postDirectories, result, basePath);
+    const treePosts = buildNavTree(result["docs"].filter((p) => p.slug.startsWith('/docs/')));
+    result["docsTree"] = treePosts;
+    result["docs"] = addDocButtons(result["docs"], treePosts);
     if (process.env.NODE_ENV === 'production') {
       // console.log(inspect(result, { depth: null, colors: true }))
     }
-    writeFileSync(writeFilePath, JSON.stringify(result, null, '  '))
+    writeFileSync(writeFilePath, JSON.stringify(result, null, '  '));
   } catch (error) {
     throw new Error(`Error while building post list: ${error.message}`);
   }
@@ -46,118 +42,108 @@ async function buildPostList(postDirectories, basePath, writeFilePath) {
 
 function walkDirectories(directories, result, basePath, sectionWeight = 0, sectionTitle, sectionId, rootSectionId) {
   for (let dir of directories) {
-    let directory = dir[0]
-    let sectionSlug = dir[1] || ''
-    let files = readdirSync(directory);
+    const directory = path.normalize(dir[0]);
+    const sectionSlug = dir[1] || '';
+    const files = readdirSync(directory);
 
     for (let file of files) {
-      let details
-      const fileName = [directory, file].join('/')
-      const fileNameWithSection = [fileName, '_section.mdx'].join('/')
-      const slug = fileName.replace(new RegExp(`^${basePath}`), '')
+      let details;
+      const fileName = path.join(directory, file);
+      const fileNameWithSection = path.join(fileName, '_section.mdx');
+      const slug = fileName.replace(new RegExp(`^${basePath.replace(/\\/g, '\\\\')}`), '').replace(/\\/g, '/'); // Ensure forward slashes for slugs
       const slugElements = slug.split('/');
+
       if (isDirectory(fileName)) {
         if (existsSync(fileNameWithSection)) {
-          // Passing a second argument to frontMatter disables cache. See https://github.com/asyncapi/website/issues/1057
-          details = frontMatter(readFileSync(fileNameWithSection, 'utf-8'), {}).data
-          details.title = details.title || capitalize(basename(fileName))
+          details = frontMatter(readFileSync(fileNameWithSection, 'utf-8'), {}).data;
+          details.title = details.title || capitalize(path.basename(fileName));
         } else {
-          details = {
-            title: capitalize(basename(fileName)),
-          }
+          details = { title: capitalize(path.basename(fileName)) };
         }
-        details.isSection = true
+        details.isSection = true;
         if (slugElements.length > 3) {
-          details.parent = slugElements[slugElements.length - 2]
-          details.sectionId = slugElements[slugElements.length - 1]
+          details.parent = slugElements[slugElements.length - 2];
+          details.sectionId = slugElements[slugElements.length - 1];
         }
         if (!details.parent) {
-          details.isRootSection = true
-          details.rootSectionId = slugElements[slugElements.length - 1]
+          details.isRootSection = true;
+          details.rootSectionId = slugElements[slugElements.length - 1];
         }
-        details.sectionWeight = sectionWeight
-        details.slug = slug
-        addItem(details)
-        const rootId = details.parent || details.rootSectionId
-        walkDirectories([[fileName, slug]], result, basePath, details.weight, details.title, details.sectionId, rootId)
+        details.sectionWeight = sectionWeight;
+        details.slug = slug;
+        addItem(details);
+        const rootId = details.parent || details.rootSectionId;
+        walkDirectories([[fileName, slug]], result, basePath, details.weight, details.title, details.sectionId, rootId);
       } else if (file.endsWith('.mdx') && !fileName.endsWith('/_section.mdx')) {
-        const fileContent = readFileSync(fileName, 'utf-8')
-        // Passing a second argument to frontMatter disables cache. See https://github.com/asyncapi/website/issues/1057
-        const { data, content } = frontMatter(fileContent, {})
-        details = data
-        details.toc = toc(content, { slugify: slugifyToC }).json
-        details.readingTime = Math.ceil(readingTime(content).minutes)
-        details.excerpt = details.excerpt || markdownToTxt(content).substr(0, 200)
-        details.sectionSlug = sectionSlug || slug.replace(/\.mdx$/, '')
-        details.sectionWeight = sectionWeight
-        details.sectionTitle = sectionTitle
-        details.sectionId = sectionId
-        details.rootSectionId = rootSectionId
-        details.id = fileName
-        details.isIndex = fileName.endsWith('/index.mdx')
-        details.slug = details.isIndex ? sectionSlug : slug.replace(/\.mdx$/, '')
+        const fileContent = readFileSync(fileName, 'utf-8');
+        const { data, content } = frontMatter(fileContent, {});
+        details = data;
+        details.toc = toc(content, { slugify: slugifyToC }).json;
+        details.readingTime = Math.ceil(readingTime(content).minutes);
+        details.excerpt = details.excerpt || markdownToTxt(content).substr(0, 200);
+        details.sectionSlug = sectionSlug || slug.replace(/\.mdx$/, '');
+        details.sectionWeight = sectionWeight;
+        details.sectionTitle = sectionTitle;
+        details.sectionId = sectionId;
+        details.rootSectionId = rootSectionId;
+        details.id = fileName;
+        details.isIndex = fileName.endsWith('/index.mdx');
+        details.slug = details.isIndex ? sectionSlug : slug.replace(/\.mdx$/, '');
+
         if (details.slug.includes('/reference/specification/') && !details.title) {
-          const fileBaseName = basename(data.slug)  // ex. v2.0.0 | v2.1.0-next-spec.1
-          const fileName = fileBaseName.split('-')[0] // v2.0.0 | v2.1.0
-          details.weight = specWeight--
+          const fileBaseName = path.basename(data.slug);
+          const fileName = fileBaseName.split('-')[0];
+          details.weight = specWeight--;
 
           if (fileName.startsWith('v')) {
-            details.title = capitalize(fileName.slice(1))
+            details.title = capitalize(fileName.slice(1));
           } else {
-            details.title = capitalize(fileName)
+            details.title = capitalize(fileName);
           }
 
           if (releaseNotes.includes(details.title)) {
-            details.releaseNoteLink = `/blog/release-notes-${details.title}`
+            details.releaseNoteLink = `/blog/release-notes-${details.title}`;
           }
 
           if (fileBaseName.includes('next-spec') || fileBaseName.includes('next-major-spec')) {
-            details.isPrerelease = true
-            // this need to be separate because the `-` in "Pre-release" will get removed by `capitalize()` function
-            details.title += " (Pre-release)"
+            details.isPrerelease = true;
+            details.title += " (Pre-release)";
           }
           if (fileBaseName.includes('explorer')) {
-            details.title += " - Explorer"
+            details.title += " - Explorer";
           }
         }
 
-        // To create a list of available ReleaseNotes list, which will be used to add details.releaseNoteLink attribute.
         if (file.startsWith("release-notes") && dir[1] === "/blog") {
-          const fileName_without_extension = file.slice(0, -4)
-          // removes the file extension. For example, release-notes-2.1.0.md -> release-notes-2.1.0
-          const version = fileName_without_extension.slice(fileName_without_extension.lastIndexOf("-") + 1)
-
-          // gets the version from the name of the releaseNote .md file (from /blog). For example, version = 2.1.0 if fileName_without_extension = release-notes-2.1.0
-          releaseNotes.push(version)
-          // releaseNotes is the list of all available releaseNotes
+          const fileNameWithoutExtension = path.basename(file, path.extname(file));
+          const version = fileNameWithoutExtension.slice(fileNameWithoutExtension.lastIndexOf("-") + 1);
+          releaseNotes.push(version);
         }
 
-        addItem(details)
+        addItem(details);
       }
     }
   }
 }
 
 function slugifyToC(str) {
-  let slug
-  // Try to match heading ids like {# myHeadingId}
-  const headingIdMatch = str.match(/[\s]?\{\#([\w\d\-_]+)\}/)
+  let slug;
+  const headingIdMatch = str.match(/[\s]?\{\#([\w\d\-_]+)\}/);
   if (headingIdMatch && headingIdMatch.length >= 2) {
-    slug = headingIdMatch[1]
+    slug = headingIdMatch[1];
   } else {
-    // Try to match heading ids like {<a name="myHeadingId"/>}
-    const anchorTagMatch = str.match(/[\s]*<a[\s]+name="([\w\d\s\-_]+)"/)
-    if (anchorTagMatch && anchorTagMatch.length >= 2) slug = anchorTagMatch[1]
+    const anchorTagMatch = str.match(/[\s]*<a[\s]+name="([\w\d\s\-_]+)"/);
+    if (anchorTagMatch && anchorTagMatch.length >= 2) slug = anchorTagMatch[1];
   }
-  return slug || slugify(str, { firsth1: true, maxdepth: 6 })
+  return slug || slugify(str, { firsth1: true, maxdepth: 6 });
 }
 
 function isDirectory(dir) {
-  return statSync(dir).isDirectory()
+  return statSync(dir).isDirectory();
 }
 
 function capitalize(text) {
-  return text.split(/[\s\-]/g).map(word => `${word[0].toUpperCase()}${word.substr(1)}`).join(' ')
+  return text.split(/[\s\-]/g).map(word => `${word[0].toUpperCase()}${word.substr(1)}`).join(' ');
 }
 
-module.exports = { slugifyToC, buildPostList }
+module.exports = { slugifyToC, buildPostList };
