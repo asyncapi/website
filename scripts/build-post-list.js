8f26f50845323651035b5a7eb8dd4eb7306bd93c
Vishvamsinh Vaghela <vaghelavishvamsinh11111@gmail.com>
2024-11-04 23:31:12 +0530

    fwqfqw

--- scripts/build-post-list.js
+++ scripts/build-post-list.js
@@ -97,7 +97,7 @@ function walkDirectories(directories, result, basePath, sectionWeight = 0, secti
         details.sectionTitle = sectionTitle
         details.sectionId = sectionId
         details.rootSectionId = rootSectionId
-        details.id = fileName
+        details.id = slug
         details.isIndex = fileName.endsWith(join('index.mdx'))
         details.slug = details.isIndex ? sectionSlug : slug.replace(/\.mdx$/, '')
         if (details.slug.includes('/reference/specification/') && !details.title) {
