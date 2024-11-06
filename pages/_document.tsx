1e2f9e4c856391027b4936dc984df00f531cfa2a
lakshaydiwan <laxhay666@gmail.com>
2024-11-06 22:50:10 +0530

    fix: Improving the lighthouse Score#3356

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,7 +1,9 @@
 import Document, { Head, Html, Main, NextScript } from 'next/document';
 import React from 'react';
+import Script from 'next/script';
 
 import i18nextConfig from '../next-i18next.config';
+
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: any) {
@@ -18,12 +20,13 @@ class MyDocument extends Document {
       <Html lang={currentLocale as string}>
         <Head>
           {/* Load Work Sans font */}
-          <link rel='preconnect' href='https://fonts.googleapis.com' />
-          <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='anonymous' />
+          <link rel="preconnet" href='https://fonts.googleapis.com' />
+          <link rel="preconnect" href='https://fonts.gstatic.com' crossOrigin='anonymous' />
           {/* eslint-disable-next-line max-len */}
+          <link rel='preload'href='https://cdn.jsdelivr.net/gh/tonsky/FiraCode@4/distr/fira_code.css' />
           <link
+            rel="preload"
             href='https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&family=Work+Sans:wght@200;300;400;500;600;700;800;900&display=swap'
-            rel='stylesheet'
           />
 
           {/* Icons */}
@@ -31,8 +34,7 @@ class MyDocument extends Document {
           <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
           <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
           <link rel='icon' type='image/png' sizes='194x194' href='/favicon-194x194.png' />
-
-          <script async defer src='https://buttons.github.io/buttons.js'></script>
+          <Script async defer src='https://buttons.github.io/buttons.js' strategy='lazyOnload'></Script>
         </Head>
         <body>
           <Main />
