9e5b37d905932e4624a8928e60b019764a512d58
Vishvamsinh Vaghela <vaghelavishvamsinh11111@gmail.com>
2024-11-14 17:36:31 +0530

    codecov fix

--- scripts/hello.js
+++
@@ -1,5 +0,0 @@
-function hello(){
-    return "hello"
-}
-
-module.exports = hello
--- tests/hello.test.js
+++
@@ -1,8 +0,0 @@
-const hello = require("../scripts/hello")
-
-describe('hello function', () => {
-    test('should test hello', ()=>{
-        let h1 = hello();
-        expect(h1).toBe("hello")
-    })
-});
