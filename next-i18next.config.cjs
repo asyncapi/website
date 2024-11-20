fd80ef2e9180c39dcebfd23d927fe85a82ba8eed
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2024-11-21 00:43:25 +0530

    add new path config

--- next-i18next.config.cjs
+++ next-i18next.config.cjs
@@ -1,4 +1,8 @@
-const config = {
+// The file is required to be named next-i18next.config.cjs so we can use it in next.config.js.
+// https://github.com/i18next/next-i18next/issues/2185#issuecomment-1618307556
+process.env.I18NEXT_DEFAULT_CONFIG_PATH = './next-i18next.config.cjs';
+
+module.exports = {
   i18n: {
     locales: ['en', 'de'],
     defaultLocale: 'en',
@@ -7,4 +11,3 @@ const config = {
     react: { useSuspense: false } // this line
   }
 };
-export default config;
