a8c12ea12cd4bfa370057f586daf4519544638a0
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2024-11-21 12:36:41 +0530

    add types

--- scripts/utils.ts
+++ scripts/utils.ts
@@ -1,6 +1,6 @@
 import yaml from 'yaml';
 
-function convertToJson(contentYAMLorJSON) {
+function convertToJson(contentYAMLorJSON: string) {
   // Axios handles conversion to JSON by default, if data returned from the server allows it
   // So if returned content is not a string (not YAML), we just return JSON back
   if (typeof contentYAMLorJSON !== 'string') {
@@ -10,11 +10,13 @@ function convertToJson(contentYAMLorJSON) {
   // Check if the content is valid JSON before attempting to parse as YAML
   try {
     const jsonContent = JSON.parse(contentYAMLorJSON);
+
     return jsonContent;
   } catch (jsonError) {
     // If it's not valid JSON, try parsing it as YAML
     try {
       const yamlContent = yaml.parse(contentYAMLorJSON);
+
       return yamlContent;
     } catch (yamlError) {
       // If parsing as YAML also fails, throw an error
