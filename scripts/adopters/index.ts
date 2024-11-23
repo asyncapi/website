24ff781ed51f205e0552361e0349eb38ff729559
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2024-11-23 21:11:54 +0530

    refactor dir variables

--- scripts/adopters/index.ts
+++ scripts/adopters/index.ts
@@ -3,9 +3,10 @@ import { fileURLToPath } from 'url';
 
 import { writeJSON } from '../utils/readAndWriteJson';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+
+const currentFilePath = fileURLToPath(import.meta.url);
+const currentDirPath = dirname(currentFilePath);
 
 export async function buildAdoptersList() {
-  writeJSON('config/adopters.yml', resolve(__dirname, '../../config', 'adopters.json'));
+  writeJSON('config/adopters.yml', resolve(currentDirPath, '../../config', 'adopters.json'));
 }
