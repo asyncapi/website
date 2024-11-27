c5b5c9ffa8f980b73c3e6d350bc688543ee6b8be
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2024-11-27 16:30:22 +0530

    refactor __dirname variable

--- scripts/build-tools.ts
+++ scripts/build-tools.ts
@@ -1,10 +1,14 @@
 import assert from 'assert';
 import fs from 'fs-extra';
-import { resolve } from 'path';
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
 
 import { combineTools } from './tools/combine-tools';
 import { getData } from './tools/extract-tools-github';
 import { convertTools } from './tools/tools-object';
+
+const currentFilePath = fileURLToPath(import.meta.url);
+const currentDirPath = dirname(currentFilePath);
 
 const buildTools = async (automatedToolsPath: string, manualToolsPath: string, toolsPath: string, tagsPath: string) => {
   try {
@@ -22,11 +26,11 @@ const buildTools = async (automatedToolsPath: string, manualToolsPath: string, t
 };
 
 /* istanbul ignore next */
-if (require.main === module) {
-  const automatedToolsPath = resolve(__dirname, '../config', 'tools-automated.json');
-  const manualToolsPath = resolve(__dirname, '../config', 'tools-manual.json');
-  const toolsPath = resolve(__dirname, '../config', 'tools.json');
-  const tagsPath = resolve(__dirname, '../config', 'all-tags.json');
+if (process.argv[1] === fileURLToPath(import.meta.url)) {
+  const automatedToolsPath = resolve(currentDirPath, '../config', 'tools-automated.json');
+  const manualToolsPath = resolve(currentDirPath, '../config', 'tools-manual.json');
+  const toolsPath = resolve(currentDirPath, '../config', 'tools.json');
+  const tagsPath = resolve(currentDirPath, '../config', 'all-tags.json');
 
   buildTools(automatedToolsPath, manualToolsPath, toolsPath, tagsPath);
 }
