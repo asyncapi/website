95df3b9e3b779bcb2d73022fa6e353d3b5ce9536
Balraj Dhakad <158481965+BalrajDhakad@users.noreply.github.com>
2024-12-19 19:20:44 +0530

    Update GoogleCalendarButton.tsx removed text

--- components/buttons/GoogleCalendarButton.tsx
+++ components/buttons/GoogleCalendarButton.tsx
@@ -19,7 +19,6 @@ interface IGoogleCalendarButtonProps extends IButtonDefaultProps {}
  * @param {string} props.className - The class name to be applied to the button.
  */
 export default function GoogleCalendarButton({
-  text,
   href,
   target = '_blank',
   iconPosition = ButtonIconPosition.LEFT,
