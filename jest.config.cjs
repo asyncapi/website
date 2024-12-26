4820f6a1df4c116706d2dbd2158335f102880d83
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2024-12-26 19:45:30 +0530

    chore: update Jest configuration to set test environment to Node

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,6 +1,7 @@
 module.exports = {
+	testEnvironment: 'node',
   transform: {
-    '^.+\\.tsx?$': 'ts-jest'
+    "^.+.tsx?$": ["ts-jest",{}],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   verbose: true,
