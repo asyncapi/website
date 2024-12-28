fb22081372a1af8aaa3fc2aa5ebb726bff5722d0
Zeel Rajodiya <jeelrajodiyajeel@gmail.com>
2024-12-28 09:36:28 +0000

    refactor: update Jest configuration for improved TypeScript support

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,9 +1,4 @@
 module.exports = {
-	testEnvironment: 'node',
-  transform: {
-    "^.+.tsx?$": ["ts-jest",{useESM: true}],
-  },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   verbose: true,
   collectCoverage: true,
   coverageReporters: ['text', 'lcov', 'json-summary'],
@@ -12,6 +7,9 @@ module.exports = {
   coveragePathIgnorePatterns: ['scripts/compose.ts'],
   // To disallow netlify edge function tests from running
   testMatch: ['**/tests/**/*.test.*', '!**/netlify/**/*.test.*'],
-  // Enable ES module support
-  extensionsToTreatAsEsm: ['.ts', '.tsx'],
+  transform: {
+    "^.+\\.[tj]sx?$": ["ts-jest",{
+		"tsconfig": "tsconfig.json"
+	}],
+  },
 };
