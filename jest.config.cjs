ec823ddb83337ae4ff17be872de1d93c05e3906a
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2025-01-01 22:53:02 +0530

    refactor: simplify Jest configuration by removing JSON transform settings and adjusting transformIgnorePatterns

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,21 +1,14 @@
 const { createJsWithTsEsmPreset } = require('ts-jest');
 
-const JsWithTsEsm = createJsWithTsEsmPreset({
-  tsconfig: {
-    resolveJsonModule: true
-  }
-});
+const JsWithTsEsm = createJsWithTsEsmPreset();
 module.exports = {
   ...JsWithTsEsm,
-  transform: {
-    '^.+\\.json$': ['ts-jest', { useESM: true }],
-    ...JsWithTsEsm.transform
-  },
   verbose: true,
   collectCoverage: true,
   coverageReporters: ['text', 'lcov', 'json-summary'],
   coverageDirectory: 'coverage',
   collectCoverageFrom: ['scripts/**/*.ts'],
   coveragePathIgnorePatterns: ['scripts/compose.ts', 'scripts/tools/categorylist.ts', 'scripts/tools/tags-color.ts'],
-  testMatch: ['**/tests/**/*.test.*', '!**/netlify/**/*.test.*']
+  testMatch: ['**/tests/**/*.test.*', '!**/netlify/**/*.test.*'],
+  transformIgnorePatterns: ['node_modules/', '\\.json$']
 };
