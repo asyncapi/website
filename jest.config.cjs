1bb48b6d1b39fcf7c550589c58939005e5b81f9c
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2025-01-01 22:19:24 +0530

    refactor: simplify Jest configuration by utilizing ts-jest preset and removing unnecessary transform settings

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,14 +1,12 @@
+const { createJsWithTsEsmPreset } = require('ts-jest');
+
 module.exports = {
+  ...createJsWithTsEsmPreset(),
   verbose: true,
   collectCoverage: true,
   coverageReporters: ['text', 'lcov', 'json-summary'],
   coverageDirectory: 'coverage',
   collectCoverageFrom: ['scripts/**/*.ts'],
   coveragePathIgnorePatterns: ['scripts/compose.ts', 'scripts/tools/categorylist.ts', 'scripts/tools/tags-color.ts'],
-  testMatch: ['**/tests/**/*.test.*', '!**/netlify/**/*.test.*'],
-  transform: {
-    '^.+\\.tsx?$': ['ts-jest', {}],
-    '^.+\\.json$': ['ts-jest', { useESM: true }]
-  },
-  extensionsToTreatAsEsm: ['.ts', '.tsx']
+  testMatch: ['**/tests/**/*.test.*', '!**/netlify/**/*.test.*']
 };
