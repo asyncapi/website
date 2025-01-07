2710835e1bb6fbe6ee92e9661c922266cfc965a6
rahulrana701 <rahulrana2001a@gmail.com>
2025-01-07 11:55:14 +0530

    Add scrollable Community dropdown to display all content

--- components/navigation/FlyoutMenu.tsx
+++ components/navigation/FlyoutMenu.tsx
@@ -14,12 +14,12 @@ interface FlyoutProps {
 export default function Flyout({ items = [] }: FlyoutProps) {
   return (
     <div
-      className='absolute z-50 -ml-4 w-screen max-w-md pt-3 md:ml-12 md:-translate-x-1/2 lg:left-1/2 lg:max-w-3xl lg:-translate-x-1/2'
-      data-testid='Flyout-main'
+      className="absolute z-50 -ml-4 w-screen max-w-md pt-3 md:ml-12 md:-translate-x-1/2 lg:left-1/2 lg:max-w-3xl lg:-translate-x-1/2"
+      data-testid="Flyout-main"
     >
-      <div className='rounded-lg shadow-lg'>
-        <div className='shadow-xs overflow-hidden rounded-lg'>
-          <div className='relative z-20 grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8 lg:grid-cols-2'>
+      <div className="rounded-lg shadow-lg">
+        <div className="shadow-xs overflow-hidden rounded-lg">
+          <div className="relative z-20 grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8 lg:grid-cols-2 max-h-96 min-h-40 overflow-y-auto scrollbar-thin scrollbar-thumb-white scrollbar-track-transparent">
             <MenuBlocks items={items} />
           </div>
         </div>
