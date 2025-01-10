e2f805346a8acc9cbdf936f5788cee208d1616ed
Sonalilipsa17375 <sonalipatra20.11.2003@gmail.com>
2025-01-10 19:01:21 +0530

    changed height and spacing issues

--- components/CaseStudyCard.tsx
+++ components/CaseStudyCard.tsx
@@ -19,11 +19,11 @@ export default function CaseStudyCard({ studies = [] }: ICaseStudyCardProps) {
   }
 
   return (
-    <div className='flex flex-wrap pt-10 lg:grid lg:grid-cols-3 lg:gap-8 lg:text-center'>
+    <div className='flex flex-wrap pt-10 lg:grid lg:grid-cols-3 lg:gap-8 lg:text-center gap-y-3'>
       {studies.map((study, index) => (
         <a key={index} href={`casestudies/${study.id}`}>
           <div
-            className='max-w-sm overflow-hidden rounded-md border border-gray-200 bg-white p-4'
+            className='max-w-sm h-full overflow-hidden min-h-[300px] rounded-md border border-gray-200 bg-white p-4' 
             data-testid='CaseStudyCard-main'
           >
             <span className='mr-2'>
