869e249c9258b7653216def18df70eba03a48621
Sonalilipsa17375 <sonalipatra20.11.2003@gmail.com>
2025-01-10 21:41:14 +0530

    fixed some failed test issues

--- components/CaseStudyCard.tsx
+++ components/CaseStudyCard.tsx
@@ -19,13 +19,10 @@ export default function CaseStudyCard({ studies = [] }: ICaseStudyCardProps) {
   }
 
   return (
-    <div className='flex flex-wrap pt-10 lg:grid lg:grid-cols-3 lg:gap-8 lg:text-center gap-y-3'>
+    <div className='flex flex-wrap pt-10 lg:grid lg:grid-cols-3 lg:gap-8 lg:text-center gap-3'>
       {studies.map((study, index) => (
         <a key={index} href={`casestudies/${study.id}`}>
-          <div
-            className='max-w-sm h-full overflow-hidden min-h-[300px] rounded-md border border-gray-200 bg-white p-4' 
-            data-testid='CaseStudyCard-main'
-          >
+          <div className='max-w-sm h-full overflow-hidden min-h-[300px] rounded-md border border-gray-200 bg-white p-4'data-testid='CaseStudyCard-main'>
             <span className='mr-2'>
               <img className='m-auto h-16' src={study.company.logo} alt={study.company.name} />
             </span>
