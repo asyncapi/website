563ca7974c45fd098cf2e77c07b771901165a330
Anmol yadav <anmolyadav03unnao@gmail.com>
2025-01-22 16:15:49 +0530

    removed swags and goodies card

--- components/community/Card.tsx
+++ components/community/Card.tsx
@@ -20,6 +20,7 @@ interface CardProps {
   btnText?: string;
   btnBg?: string;
   link: string;
+  customClasses?: string;
 }
 
 /**
@@ -45,30 +46,37 @@ export default function Card({
   bg,
   btnText = '',
   btnBg = '',
-  link
+  link,
+  customClasses,
 }: CardProps) {
   if (type === CardType.SMALL) {
     return (
       <Link href={link} target={link.includes('http') ? '_blank' : undefined}>
         <div
-          className={`w-full cursor-pointer rounded border border-[#ad20e2] p-3 shadow-xl ${bg}`}
-          data-testid='Card-small-bg'
+          className={`w-full cursor-pointer rounded border border-[#ad20e2] p-3 shadow-xl ${bg} ${customClasses}`}
+          data-testid="Card-small-bg"
         >
-          <div className='flex w-min justify-between rounded-xl bg-gray-100 p-2 text-center text-xs'>
-            <span>{icon}</span> <span className='ml-[5px]'>{tagline}</span>
+          <div className="flex w-min justify-between rounded-xl bg-gray-100 p-2 text-center text-xs">
+            <span>{icon}</span> <span className="ml-[5px]">{tagline}</span>
           </div>
-          <div className='mt-3' data-testid='Card-heading'>
+          <div className="mt-3" data-testid="Card-heading">
             <Heading level={HeadingLevel.h1} typeStyle={HeadingTypeStyle.md}>
               {heading}
             </Heading>
           </div>
-          <div className='mt-2' data-testid='Card-desc'>
-            <Paragraph textColor={bg === 'bg-white' ? 'text-gray-600' : 'text-black'} typeStyle={ParagraphTypeStyle.sm}>
+          <div className="mt-2" data-testid="Card-desc">
+            <Paragraph
+              textColor={bg === 'bg-white' ? 'text-gray-600' : 'text-black'}
+              typeStyle={ParagraphTypeStyle.sm}
+            >
               {description}
             </Paragraph>
           </div>
-          <div className='flex w-full justify-end text-right' data-testid='Card-icon'>
-            <IconArrowUp className='w-[20px]' />
+          <div
+            className="flex w-full justify-end text-right"
+            data-testid="Card-icon"
+          >
+            <IconArrowUp className="w-[20px]" />
           </div>
         </div>
       </Link>
@@ -78,27 +86,32 @@ export default function Card({
   return (
     <div
       className={`h-140 w-full rounded border p-6 shadow-xl ${!bg && 'border-[#ad20e2]'} ${bg}`}
-      data-testid='Card-lg-bg'
+      data-testid="Card-lg-bg"
     >
       <div
         className={`flex w-min justify-between rounded-xl p-2 text-center text-xs text-black ${taglineBg}`}
-        data-testid='Card-lg-tagline'
+        data-testid="Card-lg-tagline"
       >
-        <span>{icon}</span> <span className='ml-[5px]'>{tagline}</span>
+        <span>{icon}</span> <span className="ml-[5px]">{tagline}</span>
       </div>
 
-      <div className='mt-10' data-testid='Card-heading-lg'>
-        <Heading level={HeadingLevel.h1} typeStyle={HeadingTypeStyle.lg} textColor={bg && 'text-white'}>
+      <div className="mt-10" data-testid="Card-heading-lg">
+        <Heading
+          level={HeadingLevel.h1}
+          typeStyle={HeadingTypeStyle.lg}
+          textColor={bg && 'text-white'}
+        >
           {heading}
         </Heading>
       </div>
-      <div className='mt-6' data-testid='Card-desc-lg'>
+      <div className="mt-6" data-testid="Card-desc-lg">
         <Paragraph textColor={bg && 'text-gray-400'}>{description}</Paragraph>
       </div>
-      <div className='mt-10'>
-        <Link href={link} data-testid='Card-link-lg'>
+      <div className="mt-10">
+        <Link href={link} data-testid="Card-link-lg">
           <div className={`flex ${btnBg} cursor-pointer`}>
-            <IconArrowUp className={`w-[20px] ${btnBg}`} /> <span className='ml-2 text-sm'>{btnText}</span>
+            <IconArrowUp className={`w-[20px] ${btnBg}`} />{' '}
+            <span className="ml-2 text-sm">{btnText}</span>
           </div>
         </Link>
       </div>
