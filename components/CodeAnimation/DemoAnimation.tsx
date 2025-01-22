230e73ebd7e890d38e531f2d9a3011d576f43de1
Vishvamsinh Vaghela <vaghelavishvamsinh11111@gmail.com>
2025-01-22 22:19:40 +0530

    lint fix again

--- components/CodeAnimation/DemoAnimation.tsx
+++ components/CodeAnimation/DemoAnimation.tsx
@@ -30,38 +30,35 @@ export default function DemoAnimation() {
   };
 
   return (
-    <div>
-      {/* eslint-disable-next-line */}
-    <div className='relative inset-x-1/2 -mx-[50vw] w-screen bg-blue-100 p-6 font-sans'>
-        <Container>
-          <h1 className='mb-2 text-center text-2xl font-medium'>Sneak Peek Into The Real Process</h1>
-          <p className='mb-6 text-center text-sm text-gray-600'>
-            One of our main goals is to improve the current state of Event
-            <br />
-            Driven Architecture (EDA!)
-          </p>
+    <div className='relative inset-x-1/2 mx-[-50vw] w-screen bg-blue-100 p-6 font-sans'>
+      <Container>
+        <h1 className='mb-2 text-center text-2xl font-medium'>Sneak Peek Into The Real Process</h1>
+        <p className='mb-6 text-center text-sm text-gray-600'>
+          One of our main goals is to improve the current state of Event
+          <br />
+          Driven Architecture (EDA!)
+        </p>
 
-          <div className='mb-4 flex flex-wrap overflow-hidden rounded-lg bg-black'>
-            {tabs.map((tab) => (
-              <button
-                key={tab}
-                className={`m-2 flex-1 rounded px-4 py-2 text-center text-sm transition-colors duration-200 ${
-                  activeTab === tab ? 'bg-white text-black' : 'bg-gray-700 text-gray-300 hover:bg-gray-800'
-                }`}
-                onClick={() => setActiveTab(tab)}
-              >
-                {tab}
-              </button>
-            ))}
-          </div>
+        <div className='mb-4 flex flex-wrap overflow-hidden rounded-lg bg-black'>
+          {tabs.map((tab) => (
+            <button
+              key={tab}
+              className={`m-2 flex-1 rounded px-4 py-2 text-center text-sm transition-colors duration-200 ${
+                activeTab === tab ? 'bg-white text-black' : 'bg-gray-700 text-gray-300 hover:bg-gray-800'
+              }`}
+              onClick={() => setActiveTab(tab)}
+            >
+              {tab}
+            </button>
+          ))}
+        </div>
 
-          <div className='min-h-[400px] rounded-lg bg-[#1a1b26] p-6 transition-all duration-200'>
-            <div className='max-w-full overflow-auto'>
-              <div className='overflow-x-auto whitespace-pre-wrap'>{renderContent()}</div>
-            </div>
+        <div className='min-h-[400px] rounded-lg bg-[#1a1b26] p-6 transition-all duration-200'>
+          <div className='max-w-full overflow-auto'>
+            <div className='overflow-x-auto whitespace-pre-wrap'>{renderContent()}</div>
           </div>
-        </Container>
-      </div>
+        </div>
+      </Container>
     </div>
   );
 }
