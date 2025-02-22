f0986ae8be1aabce377fe5134f20575e157b1586
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2025-02-22 18:33:41 +0530

    refactor: simplify category extraction logic in getUniqueCategories function

--- utils/getUniqueCategories.ts
+++ utils/getUniqueCategories.ts
@@ -9,16 +9,13 @@ import Expenses from '../config/finance/json-data/Expenses.json';
 export const getUniqueCategories = (): string[] => {
   const allCategories: string[] = [];
 
-  // eslint-disable-next-line no-restricted-syntax
-  for (const month in Expenses) {
-    if (Object.prototype.hasOwnProperty.call(Expenses, month)) {
-      Expenses[month as keyof typeof Expenses].forEach((entry: { Category: string }) => {
-        if (!allCategories.includes(entry.Category)) {
-          allCategories.push(entry.Category);
-        }
-      });
-    }
-  }
+  Object.keys(Expenses).forEach((month) => {
+    Expenses[month as keyof typeof Expenses].forEach((entry: { Category: string }) => {
+      if (!allCategories.includes(entry.Category)) {
+        allCategories.push(entry.Category);
+      }
+    });
+  });
 
   return allCategories;
 };
