cc6ca6d24e5b782ba717506cef60f4a62b062c6b
Aksshay88 <aksshaybala8846@gmail.com>
2025-02-27 18:42:24 +0530

    feat:added animated testimonial section

--- components/ui/animated-testimonials.tsx
+++ components/ui/animated-testimonials.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable */
 'use client';
 
 import { useEffect, useState } from 'react';
@@ -47,10 +48,10 @@ export const AnimatedTestimonials = ({
   };
 
   return (
-    <div className='max-w-sm md:max-w-4xl mx-auto antialiased font-sans px-4 md:px-8 lg:px-12 py-20'>
-      <div className='relative grid grid-cols-1 md:grid-cols-2 gap-20'>
+    <div className="max-w-sm md:max-w-4xl mx-auto antialiased font-sans px-4 md:px-8 lg:px-12 py-20">
+      <div className="relative grid grid-cols-1 md:grid-cols-2 gap-20">
         <div>
-          <div className='relative h-80 w-full'>
+          <div className="relative h-80 w-full">
             <AnimatePresence>
               {testimonials.map((testimonial, index) => (
                 <motion.div
@@ -59,27 +60,29 @@ export const AnimatedTestimonials = ({
                     opacity: 0,
                     scale: 0.9,
                     z: -100,
-                    rotate: randomRotateY()
+                    rotate: randomRotateY(),
                   }}
                   animate={{
                     opacity: isActive(index) ? 1 : 0.7,
                     scale: isActive(index) ? 1 : 0.95,
                     z: isActive(index) ? 0 : -100,
                     rotate: isActive(index) ? 0 : randomRotateY(),
-                    zIndex: isActive(index) ? 999 : testimonials.length + 2 - index,
-                    y: isActive(index) ? [0, -80, 0] : 0
+                    zIndex: isActive(index)
+                      ? 999
+                      : testimonials.length + 2 - index,
+                    y: isActive(index) ? [0, -80, 0] : 0,
                   }}
                   exit={{
                     opacity: 0,
                     scale: 0.9,
                     z: 100,
-                    rotate: randomRotateY()
+                    rotate: randomRotateY(),
                   }}
                   transition={{
                     duration: 0.4,
-                    ease: 'easeInOut'
+                    ease: 'easeInOut',
                   }}
-                  className='absolute inset-0 origin-bottom'
+                  className="absolute inset-0 origin-bottom"
                 >
                   <Image
                     src={testimonial.src}
@@ -87,7 +90,7 @@ export const AnimatedTestimonials = ({
                     width={500}
                     height={500}
                     draggable={false}
-                    className='size-full rounded-3xl object-cover object-center'
+                    className="size-full rounded-3xl object-cover object-center"
                     priority={isActive(index)}
                   />
                 </motion.div>
@@ -95,70 +98,70 @@ export const AnimatedTestimonials = ({
             </AnimatePresence>
           </div>
         </div>
-        <div className='flex flex-col justify-between py-4'>
+        <div className="flex flex-col justify-between py-4">
           <motion.div
             key={active}
             initial={{
               y: 20,
-              opacity: 0
+              opacity: 0,
             }}
             animate={{
               y: 0,
-              opacity: 1
+              opacity: 1,
             }}
             exit={{
               y: -20,
-              opacity: 0
+              opacity: 0,
             }}
             transition={{
               duration: 0.2,
-              ease: 'easeInOut'
+              ease: 'easeInOut',
             }}
           >
-            <h3 className='text-2xl font-bold text-black'>
+            <h3 className="text-2xl font-bold text-black">
               {testimonials[active].name}
             </h3>
-            <p className='text-sm font-semibold text-[#8851FB]'>
+            <p className="text-sm font-semibold text-[#8851FB]">
               {testimonials[active].designation}
             </p>
-            <motion.p className='text-lg font-semibold text-[#556061] mt-8'>
+            <motion.p className="text-lg font-semibold text-[#556061] mt-8">
               {testimonials[active].quote.split(' ').map((word, index) => (
                 <motion.span
                   key={index}
                   initial={{
                     filter: 'blur(10px)',
                     opacity: 0,
-                    y: 5
+                    y: 5,
                   }}
                   animate={{
                     filter: 'blur(0px)',
                     opacity: 1,
-                    y: 0
+                    y: 0,
                   }}
                   transition={{
                     duration: 0.2,
                     ease: 'easeInOut',
-                    delay: 0.02 * index
+                    delay: 0.02 * index,
                   }}
-                  className='inline-block'
+                  className="inline-block"
                 >
                   {word}&nbsp;
                 </motion.span>
               ))}
             </motion.p>
           </motion.div>
-          <div className='flex gap-4 pt-12 md:pt-0'>
+          <div className="flex gap-4 pt-12 md:pt-0">
             <button
               onClick={handlePrev}
-              className='h-7 w-7 rounded-full bg-[#A87EFC] flex items-center justify-center group/button'
+              className="h-7 w-7 rounded-full bg-[#A87EFC] flex items-center justify-center group/button"
             >
-              <IconArrowLeft className='h-5 w-5 text-white dark:text-white group-hover/button:rotate-12 transition-transform duration-300' />
+              <IconArrowLeft className="h-5 w-5 text-white dark:text-white group-hover/button:rotate-12 transition-transform duration-300" />
             </button>
             <button
               onClick={handleNext}
-              className='h-7 w-7 rounded-full bg-[#A87EFC] flex items-center justify-center group/button'
+              className="h-7 w-7 rounded-full bg-[#A87EFC] flex items-center justify-center group/button"
             >
-              <IconArrowRight className='h-5 w-5 text-white dark:text-white group-hover/button:rotate-12 transition-transform duration-300' />
+              <IconArrowRight className="h-5 w-5 text-white dark:text-white group-hover/button:rotate-12 transition-transform duration-300" />
             </button>
           </div>
         </div>
