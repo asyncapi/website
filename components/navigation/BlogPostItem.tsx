408a6495084c28d390f317c80253d25556c201ca
Nalin <nalindalal2004@gmail.com>
2025-03-02 18:25:28 +0530

    Solved 5.4;Improve card visibility by adding necessary effects and strokes when the user hovers over it.

--- components/navigation/BlogPostItem.tsx
+++ components/navigation/BlogPostItem.tsx
@@ -31,7 +31,7 @@ interface BlogPostItemProps {
  */
 export default forwardRef(function BlogPostItem(
   { post, className = '', id = '' }: BlogPostItemProps,
-  ref: Ref<HTMLLIElement> /** Reference object for the component. */
+  ref: Ref<HTMLLIElement> /** Reference object for the component. */,
 ) {
   let typeColors: [string, string] = ['bg-indigo-100', 'text-indigo-800'];
 
@@ -52,77 +52,77 @@ export default forwardRef(function BlogPostItem(
   }
 
   return (
-    <li className={`list-none rounded-lg ${className}`} ref={ref} id={id}>
-      <article className='h-full rounded-lg'>
+    <li
+      className={`list-none rounded-lg transition-all duration-300 ease-in-out hover:shadow-lg ${className}`}
+      ref={ref}
+      id={id}
+    >
+      <article className="h-full rounded-lg border border-gray-300 shadow-md hover:border-gray-400 hover:shadow-xl">
         <Link href={post.slug}>
-          <span
-            className={
-              'flex h-full cursor-pointer flex-col divide-y divide-gray-200 overflow-hidden rounded-lg border border-gray-200 shadow-md transition-all duration-300 ease-in-out hover:shadow-lg'
-            }
-            data-testid='BlogPostItem-Link'
-          >
+          <span className="flex overflow-hidden flex-col h-full rounded-lg divide-y divide-gray-200 cursor-pointer">
+            {/* Blog Image */}
             <img
-              className='h-48 w-full object-cover'
+              className="object-cover w-full h-48"
               src={post.cover}
-              alt=''
-              loading='lazy'
-              data-testid='BlogPostItem-Img'
+              alt={post.title}
+              loading="lazy"
             />
-            <div className='flex flex-1 flex-col justify-between bg-white p-6'>
-              <div className='flex-1'>
-                <Paragraph typeStyle={ParagraphTypeStyle.sm} textColor='text-indigo-500'>
+
+            {/* Blog Content */}
+            <div className="flex flex-col flex-1 justify-between p-6 bg-white md:p-8">
+              <div className="flex-1">
+                {/* Blog Type Badge */}
+                <Paragraph
+                  typeStyle={ParagraphTypeStyle.sm}
+                  textColor="text-indigo-500"
+                >
                   <span
                     className={`inline-flex items-center rounded-full px-3 py-0.5 ${typeColors[0]} ${typeColors[1]}`}
                   >
                     {post.type}
                   </span>
                 </Paragraph>
-                <span className='block'>
-                  <Heading level={HeadingLevel.h5} typeStyle={HeadingTypeStyle.smSemibold} className='mt-2'>
-                    {post.title}
-                  </Heading>
-                  <Paragraph typeStyle={ParagraphTypeStyle.sm} className='mt-3'>
-                    <TextTruncate element='span' line={4} text={post.excerpt} />
-                  </Paragraph>
-                </span>
+
+                {/* Blog Title */}
+                <Heading
+                  level={HeadingLevel.h5}
+                  typeStyle={HeadingTypeStyle.smSemibold}
+                  className="mt-3"
+                >
+                  {post.title}
+                </Heading>
+
+                {/* Blog Excerpt */}
+                <Paragraph
+                  typeStyle={ParagraphTypeStyle.sm}
+                  className="mt-4 text-gray-600"
+                >
+                  <TextTruncate element="span" line={4} text={post.excerpt} />
+                </Paragraph>
               </div>
-              <div className='mt-6 flex items-center'>
-                <div className='relative shrink-0'>
+
+              {/* Author & Date */}
+              <div className="flex items-center mt-6">
+                <div className="relative shrink-0">
                   <AuthorAvatars authors={post.authors} />
                 </div>
-                <div className='ml-3'>
-                  <Heading level={HeadingLevel.h3} typeStyle={HeadingTypeStyle.xsSemibold} textColor='text-gray-900'>
-                    <span>
-                      {post.authors
-                        .map((author, index) =>
-                          author.link ? (
-                            <button
-                              key={index}
-                              data-alt={author.name}
-                              className='cursor-pointer border-none bg-inherit p-0 hover:underline'
-                              onClick={(e) => {
-                                e.preventDefault();
-
-                                // Handle the click event, e.g., navigate to author.link
-                                window.open(author.link, '_blank');
-                              }}
-                            >
-                              {author.name}
-                            </button>
-                          ) : (
-                            author.name
-                          )
-                        )
-                        .reduce((prev, curr, index) => (
-                          <React.Fragment key={`author-${index}`}>
-                            {prev} & {curr}
-                          </React.Fragment>
-                        ))}
-                    </span>
+                <div className="ml-3">
+                  <Heading
+                    level={HeadingLevel.h3}
+                    typeStyle={HeadingTypeStyle.xsSemibold}
+                    textColor="text-gray-900"
+                  >
+                    {post.authors.map((author, index) => (
+                      <span key={index} className="hover:underline">
+                        {author.name}
+                      </span>
+                    ))}
                   </Heading>
-                  <Paragraph typeStyle={ParagraphTypeStyle.sm} className='flex'>
-                    <time dateTime={post.date}>{moment(post.date).format('MMMM D, YYYY')}</time>
-                    <span className='mx-1'>&middot;</span>
+                  <Paragraph typeStyle={ParagraphTypeStyle.sm} className="flex">
+                    <time dateTime={post.date}>
+                      {moment(post.date).format('MMMM D, YYYY')}
+                    </time>
+                    <span className="mx-1">&middot;</span>
                     <span>{post.readingTime} min read</span>
                   </Paragraph>
                 </div>
