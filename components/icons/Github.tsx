d346ade6c39e92cbc55890abec1a47ffd9795904
Aditya Bakshi <153073510+Aditya90456@users.noreply.github.com>
2025-03-05 19:40:54 +0530

    github

--- components/icons/Github.tsx
+++ components/icons/Github.tsx
@@ -6,8 +6,8 @@ import React from 'react';
  */
 export default function IconGithub({ className = '' }) {
   return (
-    <svg className={className} fill='currentColor' viewBox='0 0 45 45'>
-      <path d='https://www.flaticon.com/free-icon/github-sign_25657?term=github&page=1&position=2&origin=tag&related_id=25657' />
-    </svg>
+    
+    <Image
+      src="/images/icons/github.svg"  ></Image>
   );
 }
