782b5b8eed129e597e5941bf97e89a0598a8a5a9
Ansh Goyal <anshgoyal1704@gmail.com>
2025-03-08 11:08:31 +0530

    fix formatting

---
+++ next-i18next.config.ts
@@ -0,0 +1,14 @@
+
+export const i18nConfig = {
+  i18n: {
+    locales: ['en', 'de'],
+    defaultLocale: 'en',
+    namespaces: ['landing-page', 'common', 'tools'],
+    defaultNamespace: 'landing-page',
+    react: { useSuspense: false }
+  },
+  langMap: {
+    en: 'English',
+    de: 'Deutsch'
+  }
+};
