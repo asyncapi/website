00007f7504d21b1f9a7d24f1fc8a2fca80869343
Ansh Goyal <anshgoyal1704@gmail.com>
2025-03-08 11:12:03 +0530

    fix formatting

--- components/navigation/MobileNavMenu.tsx
+++ components/navigation/MobileNavMenu.tsx
@@ -35,7 +35,7 @@ export default function MobileNavMenu({
   onClickClose = () => {},
   uniqueLangs,
   currentLanguage,
-  changeLanguage,
+  changeLanguage
 }: MobileNavMenuProps) {
   const [open, setOpen] = useState<string | null>(null);
 
@@ -55,100 +55,67 @@ export default function MobileNavMenu({
   const { langMap } = i18nextConfig;
 
   return (
-    <div className="fixed inset-x-0 top-0 z-60 max-h-full origin-top-right overflow-y-auto py-2 transition lg:hidden">
-      <div className="rounded-lg shadow-lg">
-        <div className="shadow-xs divide-y-2 divide-gray-50 rounded-lg bg-white">
-          <div className="space-y-6 px-4 pb-6 pt-4">
-            <div className="flex items-center justify-between">
-              <Link
-                href="/"
-                className="cursor-pointer"
-                data-testid="MobileNav-Logo"
-              >
-                <AsyncAPILogo className="h-10 w-auto" />
+    <div className='fixed inset-x-0 top-0 z-60 max-h-full origin-top-right overflow-y-auto py-2 transition lg:hidden'>
+      <div className='rounded-lg shadow-lg'>
+        <div className='shadow-xs divide-y-2 divide-gray-50 rounded-lg bg-white'>
+          <div className='space-y-6 px-4 pb-6 pt-4'>
+            <div className='flex items-center justify-between'>
+              <Link href='/' className='cursor-pointer' data-testid='MobileNav-Logo'>
+                <AsyncAPILogo className='h-10 w-auto' />
               </Link>
-              <div
-                className="justify-content -mr-2 flex flex-row items-center"
-                data-testid="MobileNav-button"
-              >
+              <div className='justify-content -mr-2 flex flex-row items-center' data-testid='MobileNav-button'>
                 <SearchButton
-                  className="flex items-center space-x-2 rounded-md p-2 text-left text-gray-400 transition duration-150 ease-in-out hover:bg-gray-100 hover:text-gray-500 focus:bg-gray-100 focus:text-gray-500 focus:outline-none"
-                  aria-label="Open Search"
+                  className='flex items-center space-x-2 rounded-md p-2 text-left text-gray-400 transition duration-150 ease-in-out hover:bg-gray-100 hover:text-gray-500 focus:bg-gray-100 focus:text-gray-500 focus:outline-none'
+                  aria-label='Open Search'
                 >
                   <SearchIcon />
                 </SearchButton>
                 <button
                   onClick={onClickClose}
-                  type="button"
-                  className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 transition duration-150 ease-in-out hover:bg-gray-100 hover:text-gray-500 focus:bg-gray-100 focus:text-gray-500 focus:outline-none"
+                  type='button'
+                  className='inline-flex items-center justify-center rounded-md p-2 text-gray-400 transition duration-150 ease-in-out hover:bg-gray-100 hover:text-gray-500 focus:bg-gray-100 focus:text-gray-500 focus:outline-none'
                 >
-                  <svg
-                    className="size-6"
-                    stroke="currentColor"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M6 18L18 6M6 6l12 12"
-                    />
+                  <svg className='size-6' stroke='currentColor' fill='none' viewBox='0 0 24 24'>
+                    <path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M6 18L18 6M6 6l12 12' />
                   </svg>
                 </button>
               </div>
             </div>
           </div>
-          <div
-            className="space-y-2 px-5 py-2"
-            onClick={() => showMenu('learning')}
-            data-testid="MobileNav-docs"
-          >
-            <h4 className="flex justify-between font-medium text-gray-800">
+          <div className='space-y-2 px-5 py-2' onClick={() => showMenu('learning')} data-testid='MobileNav-docs'>
+            <h4 className='flex justify-between font-medium text-gray-800'>
               {' '}
-              <Link href="/docs" className="cursor-pointer flex">
+              <Link href='/docs' className='cursor-pointer flex'>
                 Docs
               </Link>
               <NavItemDropdown />
             </h4>
             {open === 'learning' && <MenuBlocks items={learningItems} />}
           </div>
-          <div
-            className="space-y-2 px-5 py-2"
-            onClick={() => showMenu('tooling')}
-            data-testid="MobileNav-tools"
-          >
-            <h4 className="flex justify-between font-medium text-gray-800">
+          <div className='space-y-2 px-5 py-2' onClick={() => showMenu('tooling')} data-testid='MobileNav-tools'>
+            <h4 className='flex justify-between font-medium text-gray-800'>
               {' '}
-              <Link href="/tools" className="cursor-pointer flex">
+              <Link href='/tools' className='cursor-pointer flex'>
                 Tools
               </Link>
               <NavItemDropdown />
             </h4>
             {open === 'tooling' && <MenuBlocks items={toolingItems} />}
           </div>
-          <div
-            className="space-y-2 px-5 py-2"
-            onClick={() => showMenu('community')}
-            data-testid="MobileNav-community"
-          >
-            <h4 className="flex justify-between font-medium text-gray-800">
-              <Link href="/community" className="cursor-pointer flex">
+          <div className='space-y-2 px-5 py-2' onClick={() => showMenu('community')} data-testid='MobileNav-community'>
+            <h4 className='flex justify-between font-medium text-gray-800'>
+              <Link href='/community' className='cursor-pointer flex'>
                 Community
               </Link>
               <NavItemDropdown />
             </h4>
             {open === 'community' && <MenuBlocks items={communityItems} />}
           </div>
-          <div
-            className="space-y-2 px-5 pt-2"
-            onClick={() => showMenu('others')}
-            data-testid="MobileNav-others"
-          >
-            <div className="grid gap-4">
+          <div className='space-y-2 px-5 pt-2' onClick={() => showMenu('others')} data-testid='MobileNav-others'>
+            <div className='grid gap-4'>
               <div>
-                <h4 className="mb-4 flex justify-between font-medium text-gray-800">
-                  <a className="cursor-pointer">Others</a>
+                <h4 className='mb-4 flex justify-between font-medium text-gray-800'>
+                  <a className='cursor-pointer'>Others</a>
                   <NavItemDropdown />
                 </h4>
                 {open === 'others' &&
@@ -157,9 +124,9 @@ export default function MobileNavMenu({
                       href={item.href}
                       key={index}
                       target={item.target || '_self'}
-                      rel="noopener noreferrer"
-                      className="mb-4 block rounded-lg py-1 text-base font-medium leading-6 text-gray-700 transition duration-150 ease-in-out hover:bg-gray-50"
-                      data-testid="MobileNav-others"
+                      rel='noopener noreferrer'
+                      className='mb-4 block rounded-lg py-1 text-base font-medium leading-6 text-gray-700 transition duration-150 ease-in-out hover:bg-gray-50'
+                      data-testid='MobileNav-others'
                     >
                       {item.text}
                     </Link>
@@ -167,14 +134,11 @@ export default function MobileNavMenu({
               </div>
             </div>
           </div>
-          <div
-            className="space-y-2 px-5 py-2"
-            onClick={() => showMenu('language')}
-          >
-            <div className="grid gap-4">
+          <div className='space-y-2 px-5 py-2' onClick={() => showMenu('language')}>
+            <div className='grid gap-4'>
               <div>
-                <h4 className="mb-4 flex justify-between font-medium text-gray-800">
-                  <a className="flex cursor-pointer items-center gap-x-2">
+                <h4 className='mb-4 flex justify-between font-medium text-gray-800'>
+                  <a className='flex cursor-pointer items-center gap-x-2'>
                     Language <IconLanguage />
                   </a>
                   <NavItemDropdown />
@@ -183,15 +147,11 @@ export default function MobileNavMenu({
                   uniqueLangs.map((lang) => (
                     <button
                       key={lang.key}
-                      onClick={() =>
-                        changeLanguage(lang.value.toLowerCase(), true)
-                      }
+                      onClick={() => changeLanguage(lang.value.toLowerCase(), true)}
                       className={`mb-4 ml-2 block w-full rounded-lg py-1 text-start text-sm font-medium leading-6 text-gray-700 transition duration-150 ease-in-out hover:bg-gray-50 ${currentLanguage.toLowerCase() === lang.text.toLowerCase() ? 'text-secondary-500' : ''}`}
-                      data-testid="MobileNav-language-item"
+                      data-testid='MobileNav-language-item'
                     >
-                      {langMap[
-                        lang.text.toLowerCase() as keyof typeof langMap
-                      ] || lang.text}
+                      {langMap[lang.text.toLowerCase() as keyof typeof langMap] || lang.text}
                     </button>
                   ))}
               </div>
