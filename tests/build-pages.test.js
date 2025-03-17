35d465e3223d3ca3e5f781e3939fc6475f8da38d
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2025-03-17 17:03:52 +0530

    refactor: remove istanbul ignore comments and add tests for file renaming logic in copyAndRenameFiles function

--- tests/build-pages.test.js
+++ tests/build-pages.test.js
@@ -46,6 +46,33 @@ describe('copyAndRenameFiles', () => {
     expect(nestedTargetFile).toBe('<Table><Tr><Td>Hello</Td></Tr></Table>');
   });
 
+  test('should not rename files with extensions other than .md', () => {
+    const fileContent = '<div>Hello</div>';
+    const testFile = path.join(SRC_DIR, 'test.txt');
+    const targetFile = path.join(TARGET_DIR, 'test.txt');
+    fs.writeFileSync(testFile, fileContent, 'utf8');
+
+    copyAndRenameFiles(SRC_DIR, TARGET_DIR);
+
+    expect(fs.existsSync(targetFile)).toBe(true);
+    expect(fs.readFileSync(targetFile, 'utf8')).toBe('<div>Hello</div>');
+    fs.unlinkSync(targetFile);
+  });
+
+  test('should handle non-directory and non-file entries', () => {
+    const nonDirectoryNonFile = {
+      name: 'test',
+      isDirectory: () => false,
+      isFile: () => false
+    };
+    const { readdirSync } = fs;
+    fs.readdirSync = () => [nonDirectoryNonFile];
+
+    copyAndRenameFiles(SRC_DIR, TARGET_DIR);
+
+    fs.readdirSync = readdirSync;
+  });
+
   test('should create a directory if it does not exist', () => {
     const NEW_TEST_DIR = 'testDir';
 
