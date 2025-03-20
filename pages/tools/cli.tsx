eef2be2d8ef3e98087be79fe6f59cd9800ecce06
Arijit Char <115190087+Arijit-Char@users.noreply.github.com>
2025-03-20 21:06:49 +0530

    Fixed: Page- tools/cli, Content- Example Description

--- pages/tools/cli.tsx
+++ pages/tools/cli.tsx
@@ -155,8 +155,7 @@ export default function CliPage() {
                 <div className='space-y-5'>
                   <div>
                     <Paragraph typeStyle={ParagraphTypeStyle.md} className='mb-4'>
-                      Create a new AsyncAPI file
-                    </Paragraph>
+                      Create a new AsyncAPI project, specification files, or templates for clients and applications.                    </Paragraph>
                     <CodeBlock
                       language='generator-cli'
                       textSizeClassName='text-sm'
