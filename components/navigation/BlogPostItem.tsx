b0774c3ec54fd2466249a0bb25f40134811d0fb7
Arijit Char <115190087+Arijit-Char@users.noreply.github.com>
2025-03-22 01:09:02 +0530

    Fixed: [FEATURE] <Social Sharing Buttons on Blogs> #3649

--- components/navigation/BlogPostItem.tsx
+++ components/navigation/BlogPostItem.tsx
@@ -13,6 +13,8 @@ import AuthorAvatars from '../AuthorAvatars';
 import Heading from '../typography/Heading';
 import Paragraph from '../typography/Paragraph';
 
+import { FacebookIcon, FacebookShareButton, LinkedinIcon, LinkedinShareButton, TwitterIcon, TwitterShareButton } from 'react-share';
+
 interface BlogPostItemProps {
   // eslint-disable-next-line prettier/prettier
 
@@ -50,6 +52,9 @@ export default forwardRef(function BlogPostItem(
       break;
     default:
   }
+  const shareUrl = `https://www.asyncapi.com${post.slug}`;
+  const shareText = `Check out this blog: ${post.title}`;
+  const hashTags = ['AsyncAPI', 'TechBlog', 'DevCommunity', 'TechTrends'];
 
   return (
     <li className={`list-none rounded-lg ${className}`} ref={ref} id={id}>
@@ -70,12 +75,23 @@ export default forwardRef(function BlogPostItem(
             />
             <div className='flex flex-1 flex-col justify-between bg-white p-6'>
               <div className='flex-1'>
-                <Paragraph typeStyle={ParagraphTypeStyle.sm} textColor='text-indigo-500'>
+                <Paragraph typeStyle={ParagraphTypeStyle.sm} textColor='text-indigo-500' className='flex items-center justify-around'>
                   <span
                     className={`inline-flex items-center rounded-full px-3 py-0.5 ${typeColors[0]} ${typeColors[1]}`}
                   >
                     {post.type}
                   </span>
+                  <div className='ml-2 flex space-x-2'>
+                    <TwitterShareButton url={shareUrl} title={shareText} hashtags={hashTags}>
+                      <TwitterIcon size={32} round />
+                    </TwitterShareButton>
+                    <LinkedinShareButton url={shareUrl} title={shareText} source='AsyncAPI Blog'>
+                      <LinkedinIcon size={32} round />
+                    </LinkedinShareButton>
+                    <FacebookShareButton url={shareUrl} title={shareText} hashtag='#AsyncAPI'>
+                      <FacebookIcon size={32} round />
+                    </FacebookShareButton>
+                  </div>
                 </Paragraph>
                 <span className='block'>
                   <Heading level={HeadingLevel.h5} typeStyle={HeadingTypeStyle.smSemibold} className='mt-2'>
