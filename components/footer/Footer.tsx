041227e4c08941c69694379023c6926fc526b09d
kimbohlovette <kimbohlovette@gmail.com>
2025-03-31 20:36:56 +0100

    update Email Us label to Email

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -70,7 +70,7 @@ export default function Footer() {
                         <path d='M22.5 6.908V6.75a3 3 0 0 0-3-3h-15a3 3 0 0 0-3 3v.158l9.714 5.978a1.5 1.5 0 0 0 1.572 0L22.5 6.908Z' />
                       </svg>
 
-                      <a href='mailto:press@asyncapi.io'>Email Us</a>
+                      <a href='mailto:press@asyncapi.io'>Email</a>
                     </div>
                   </li>
                 </ul>
