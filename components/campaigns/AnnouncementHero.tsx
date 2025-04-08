b97abc0fa2a17e607edaf0992c63727e129d33d6
Pushkar <git config --global user.email>
2025-04-08 18:28:50 +0530

    banner_fixed

--- components/campaigns/AnnouncementHero.tsx
+++ components/campaigns/AnnouncementHero.tsx
@@ -50,63 +50,61 @@ export default function AnnouncementHero({ className = '', small = false }: IAnn
 
   return (
     <Container as='section' padding='' className='text-center'>
-      <div className='relative flex flex-row items-center justify-center overflow-x-hidden md:gap-4'>
-        {numberOfVisibleBanners > 1 && (
-          <div
-            className={`absolute left-0 top-1/2 z-10 mb-2 flex size-8 -translate-y-1/2 cursor-pointer
-          items-center justify-center rounded-full bg-primary-500 opacity-50 hover:bg-primary-600 md:opacity-100`}
-            onClick={goToPrevious}
-          >
-            <ArrowLeft className='text-white' />
-          </div>
-        )}
-        <div className='relative flex w-5/6 flex-col items-center justify-center gap-2'>
-          <div className='relative flex min-h-72 w-full justify-center overflow-hidden lg:h-[17rem] lg:w-[38rem]'>
-            {visibleBanners.map((banner, index) => {
-              // Only render the active banner
-              const isVisible = index === activeIndex;
-
-              if (!isVisible) return null;
-
-              return (
-                <Banner
-                  key={index}
-                  title={banner.title}
-                  dateLocation={banner.dateLocation}
-                  cfaText={banner.cfaText}
-                  eventName={banner.eventName}
-                  cfpDeadline={banner.cfpDeadline}
-                  link={banner.link}
-                  city={banner.city}
-                  activeBanner={isVisible}
-                  className={className}
-                  small={small}
-                />
-              );
-            })}
-          </div>
-          <div className='m-auto flex justify-center'>
-            {visibleBanners.map((banner, index) => (
-              <div
-                key={index}
-                className={`mx-1 size-2 cursor-pointer rounded-full ${
-                  activeIndex === index ? 'bg-primary-500' : 'bg-gray-300'
-                }`}
-                onClick={() => goToIndex(index)}
-              />
-            ))}
-          </div>
-        </div>
-        {numberOfVisibleBanners > 1 && (
-          <div
-            className={`absolute right-0 top-1/2 z-10 mb-2 size-8 -translate-y-1/2 cursor-pointer
-                      rounded-full bg-primary-500 opacity-50 hover:bg-primary-600 md:opacity-100`}
-            onClick={goToNext}
-          >
-            <ArrowRight className='text-white' />
-          </div>
-        )}
+  <div className='relative flex flex-row items-center justify-center overflow-visible gap-2 sm:gap-4'>
+    {numberOfVisibleBanners > 1 && (
+      <div
+        className={`absolute left-[-1.5rem] top-1/2 z-30 flex h-8 w-8 -translate-y-1/2 cursor-pointer items-center justify-center rounded-full bg-primary-500 opacity-75 hover:bg-primary-600 sm:left-[-2rem] lg:left-[-2.5rem]`}
+        onClick={goToPrevious}
+      >
+        <ArrowLeft className='text-white' />
       </div>
-    </Container>
+    )}
+    <div className='relative flex w-full px-4 xs:w-11/12 sm:w-4/5 md:w-5/6 lg:w-3/4 flex-col items-center justify-center gap-4'>
+      <div className='relative flex min-h-72 w-full justify-center overflow-hidden sm:min-h-56 lg:min-h-64 lg:h-[20rem] lg:w-[42rem]'>
+        {visibleBanners.map((banner, index) => {
+          
+          const isVisible = index === activeIndex;
+
+          if (!isVisible) return null;
+
+          return (
+            <Banner
+              key={index}
+              title={banner.title}
+              dateLocation={banner.dateLocation}
+              cfaText={banner.cfaText}
+              eventName={banner.eventName}
+              cfpDeadline={banner.cfpDeadline}
+              link={banner.link}
+              city={banner.city}
+              activeBanner={isVisible}
+              className={className}
+              small={small}
+            />
+          );
+        })}
+      </div>
+      <div className='m-auto flex justify-center'>
+        {visibleBanners.map((banner, index) => (
+          <div
+            key={index}
+            className={`mx-1 h-2 w-2 cursor-pointer rounded-full ${
+              activeIndex === index ? 'bg-primary-500' : 'bg-gray-300'
+            }`}
+            onClick={() => goToIndex(index)}
+          />
+        ))}
+      </div>
+    </div>
+    {numberOfVisibleBanners > 1 && (
+      <div
+        className={`absolute right-[-1.5rem] top-1/2 z-30 flex h-8 w-8 -translate-y-1/2 cursor-pointer items-center justify-center rounded-full bg-primary-500 opacity-75 hover:bg-primary-600 sm:right-[-2rem] lg:right-[-2.5rem]`}
+        onClick={goToNext}
+      >
+        <ArrowRight className='text-white' />
+      </div>
+    )}
+  </div>
+</Container>
   );
 }
