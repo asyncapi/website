44c6bcac3976cf8dfb2a3c8a74cd634fc1e2d79c
Anushka <anushkasharan05@gmail.com>
2025-05-27 18:12:56 +0530

    feat: add Cypress for end-to-end testing

---
+++ cypress.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  defaultCommandTimeout: 8000,
+  video: true,
+  screenshotOnRunFailure: true,
+  e2e: {
+    screenshotsFolder: 'cypress/screenshots',
+    videosFolder: 'cypress/videos',
+    viewportWidth: 1280,
+    viewportHeight: 720,
+    setupNodeEvents(on, config) {
+      config.specPattern = ['cypress/*.cy.js'];
+      return config;
+    },
+    baseUrl: 'http://127.0.0.1:3000',
+    env: {
+      STARTUP_WAIT_MILLISECONDS: 30000,
+    },
+  },
+  retries: {
+    runMode: 1,
+    openMode: 1,
+  },
+});
