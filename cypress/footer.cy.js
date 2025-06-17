10c1c2682c9cd8ea01b74648656dafa82e516b3a
Anushka Sharan <anushkasharan05@gmail.com>
2025-06-17 23:22:31 +0530

    feat: footer e2e tests

---
+++ cypress/footer.cy.js
@@ -0,0 +1,33 @@
+import Footer from './pages/footer';
+
+describe('Footer Links Validation', () => {
+  const footer = new Footer();
+
+  beforeEach(() => {
+    footer.visit();
+  });
+
+  it('User verifies the initiative section', () => {
+    footer.initiativeLinks.forEach((link) => {
+      footer.verifyLink(link.href, link.text);
+    });
+  });
+
+  it('User verifies the social section', () => {
+    footer.socialLinks.forEach(({ href, text }) => {
+      footer.verifyLink(href, text);
+    });
+  });
+
+  it('User verifies the news section', () => {
+    footer.newsLinks.forEach((link) => {
+      footer.verifyLink(link.href, link.text);
+    });
+  });
+
+  it('User verifies Netlify link', () => {
+    footer.verifyLink('https://netlify.com', '');
+  });
+});
+
+
