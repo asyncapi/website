4db1a6f91e12a606736b92a1e85fc5bb3d3c4040
Sagar Kori <sagarkoriup11@gmail.com>
2025-06-22 23:32:32 +0530

    reconfigured the build pages page

--- scripts/build-pages.ts
+++ scripts/build-pages.ts
@@ -2,9 +2,6 @@ import type { PathLike } from 'fs';
 import fs from 'fs';
 import path from 'path';
 
-const SRC_DIR = 'markdown';
-const TARGET_DIR = 'pages';
-
 const capitalizeTags = ['table', 'tr', 'td', 'th', 'thead', 'tbody'];
 
 /**
@@ -16,7 +13,6 @@ export function ensureDirectoryExists(directory: PathLike) {
     fs.mkdirSync(directory, { recursive: true });
   }
 }
-ensureDirectoryExists(TARGET_DIR);
 
 /**
  * Capitalizes the first letter of JSX tag names in the provided content if they are in a predefined list.
@@ -46,6 +42,8 @@ export function capitalizeJsxTags(content: string): string {
  * @param targetDir - The path to the target directory where transformed files and directories are written.
  */
 export function copyAndRenameFiles(srcDir: string, targetDir: string) {
+  // Check if the specified path exists, if not then create it.
+  ensureDirectoryExists(targetDir);
   // Read all files and directories from source directory
   const entries = fs.readdirSync(srcDir, { withFileTypes: true });
 
@@ -79,5 +77,3 @@ export function copyAndRenameFiles(srcDir: string, targetDir: string) {
     }
   });
 }
-
-copyAndRenameFiles(SRC_DIR, TARGET_DIR);
