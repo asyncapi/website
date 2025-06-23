47eeb0b8109bac41b2de073268a599f1a63486d9
Sagar Kori <sagarkoriup11@gmail.com>
2025-06-23 10:20:38 +0530

    reverted the case studies changes

--- components/CaseStudyCard.tsx
+++ components/CaseStudyCard.tsx
@@ -1,26 +1,25 @@
 import React from 'react';
 
-import type { ICaseStudy } from '@/types/post';
+import type { ICaseStudies } from '@/types/post';
 import { ParagraphTypeStyle } from '@/types/typography/Paragraph';
 
 import Paragraph from './typography/Paragraph';
 
 interface ICaseStudyCardProps {
-  studies?: ICaseStudy[];
+  studies?: ICaseStudies;
 }
 
 /**
  * @description A component that displays a list of case studies in a card format
- * @param {ICaseStudy[]} props.studies - The list of case studies to display
+ * @param {ICaseStudies} props.studies - The list of case studies to display
  */
 export default function CaseStudyCard({ studies = [] }: ICaseStudyCardProps) {
   if (studies.length === 0) {
-    return null;
-  }
+	@@ -20,7 +20,7 @@ export default function CaseStudyCard({ studies = [] }: ICaseStudyCardProps) {
 
   return (
     <div className='flex flex-wrap gap-3 pt-10 justify-center lg:gap-8 lg:text-center'>
-      {studies.map((study: ICaseStudy, index: number) => (
+      {studies.map((study, index) => (
         <a key={index} className='lg:w-[30%]' href={`casestudies/${study.id}`}>
           <div
             className='h-full min-h-[300px] max-w-sm overflow-hidden rounded-md border border-gray-200 bg-white p-4'
