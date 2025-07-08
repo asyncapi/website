42f8834425659c9b5efe6f54fa7e93aa9a02661a
Anushka Sharan <anushkasharan05@gmail.com>
2025-07-08 22:51:04 +0530

    test: update e2e test files

--- cypress/pages/DocsPage.js
+++ cypress/pages/DocsPage.js
@@ -3,6 +3,12 @@ class DocsPage {
     cy.get('[data-testid="NavItem-Link"][href="/docs"]').click();
   }
 
+  verifyCardLinks(href){
+    cy.get(
+      `[href="${href}"] [data-testid="Docs-div-contents"] [data-testid="Paragraph-test"]`
+    ).should('have.attr', 'data-testid', 'Paragraph-test');
+  }
+  
   goToConceptsSection() {
     cy.get('a[href="/docs/concepts"] span').contains('Concepts').click();
     cy.get('#concepts').should('be.visible'); //Fixes a flaky test
@@ -16,8 +22,25 @@ class DocsPage {
     cy.get('a[href="/docs/tools"] span').contains('Tools').click();
   }
 
+  goToGuidesSection() {
+    cy.get('a[href="/docs/guides"] span').contains('Guides').click();
+  }
+
+  goToReferenceSection() {
+    cy.get('a[href="/docs/reference"] span').contains('Reference').click();
+  }
+
+  goToMigrationsSection(){
+    cy.get('a[href="/docs/migration"] span').contains('Migrations').click();
+  }
+
+  goToCommunitySection(){
+    cy.get('a[href="/docs/community"] span').contains('Community').click();
+  }
+
   verifyConceptSubsection({ href, label, heading }) {
     cy.get(`a[href="${href}"] span`).contains(label).click();
+
     cy.get('h1').should('contain.text', heading);
   }
 }
