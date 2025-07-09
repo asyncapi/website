9e68f768d6312fe8085097b806aa1b4cffc4dffc
Sagar Kori <sagarkoriup11@gmail.com>
2025-07-09 23:40:49 +0530

    removed index.ts file and removed the linting errors of build dashboard runner file

--- npm/runners/build-dashboard-runner.ts
+++ npm/runners/build-dashboard-runner.ts
@@ -1,16 +1,15 @@
-import { start } from "@/scripts/dashboard/build-dashboard";
-
 import { dirname, resolve } from 'path';
 import { fileURLToPath } from 'url';
+
+import { start } from '@/scripts/dashboard/build-dashboard';
 
 const currentFilePath = fileURLToPath(import.meta.url);
 const currentDirPath = dirname(currentFilePath);
 
 export async function runBuildDashboard() {
-    try {
-        await start(resolve(currentDirPath, '..', '..', 'dashboard.json'));
-    }
-    catch (error) {
-        throw error;
-    }
+  try {
+    await start(resolve(currentDirPath, '..', '..', 'dashboard.json'));
+  } catch (error) {
+    throw error;
+  }
 }
