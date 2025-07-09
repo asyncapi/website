e0d56724cc1f2bd7478861a6bef8130ccd490549
Anushka Sharan <anushkasharan05@gmail.com>
2025-07-09 14:13:42 +0530

    test: small changes

--- cypress/docspage.cy.js
+++ cypress/docspage.cy.js
@@ -11,40 +11,39 @@ beforeEach(() => {
   cy.fixture('docsSections').as('subsections');
 });
 
-describe('Docs Page Tests',()=>{
-  it('User navigates SideBar containing various sections',()=>{
+describe('Docs Page Tests', () => {
+  it('User navigates SideBar containing various sections', () => {
+    docsPage.verifyConceptSection();
 
-  docsPage.goToConceptsSection();
-  docsPage.goToTutorialsSection();
-  docsPage.goToToolsSection();
-  docsPage.goToGuidesSection();
-  docsPage.goToReferenceSection();
-  docsPage.goToMigrationsSection();
-  docsPage.goToCommunitySection();
+    cy.get('@subsections').then((subsections) => {
+      subsections.forEach((subsection) => {
+        docsPage.verifyConceptSubsection(subsection);
+      });
+    });
 
-  cy.get('@subsections').then((subsections) => {
-    subsections.forEach((subsection) => {
-      docsPage.verifyConceptSubsection(subsection);
+    docsPage.goToConceptsSection();
+    docsPage.goToTutorialsSection();
+    docsPage.goToToolsSection();
+    docsPage.goToGuidesSection();
+    docsPage.goToReferenceSection();
+    docsPage.goToMigrationsSection();
+    docsPage.goToCommunitySection();
+
+    it('User verifies Card Links present on the Docs Page', () => {
+      const cards = [
+        { href: '/docs/concepts' },
+        { href: '/docs/tutorials' },
+        { href: '/docs/guides' },
+        { href: '/docs/tools' },
+        { href: '/docs/reference' },
+        { href: '/docs/migration' },
+        { href: '/docs/community' },
+        { href: '/docs/reference/specification/v3.0.0-explorer' },
+      ];
+
+      cards.forEach((card) => {
+        docsPage.verifyCardLinks(card.href);
+      });
     });
   });
-});
-});
-
-
-it('User verifies Card Links present on the Docs Page', function() {
- const cards=[
-  {href:"/docs/concepts"},
-  {href:"/docs/tutorials"},
-  {href:"/docs/guides"},
-  {href:"/docs/tools"},
-  {href:"/docs/reference"},
-  {href:"/docs/migration"},
-  {href:"/docs/community"},
-  {href:"/docs/reference/specification/v3.0.0-explorer"}
-
- ]
-
- cards.forEach((card)=>{
-  docsPage.verifyCardLinks(card.href);
- })
 });
