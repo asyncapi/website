701d76e50ff7ecfea1f5b182afa37cd7f468c021
Anushka Sharan <anushkasharan05@gmail.com>
2025-07-10 10:08:18 +0530

    test: add e2e tests for tsc, slack and ambassadors page

---
+++ cypress/pages/tscpage.js
@@ -0,0 +1,32 @@
+
+class TSCPage {
+
+
+  hoverCommunityLink() {
+    cy.get('[data-testid="NavItem-Link"]').contains('Community').trigger('mouseover');
+  }
+  
+  fillNewsletterForm(name, email) {
+    cy.get('[data-testid="NewsletterSubscribe-text-input"]').type(name);
+    cy.get('[data-testid="NewsletterSubscribe-email-input"]').type(email);
+  }
+
+  submitNewsletter() {
+    cy.get('[data-testid="Button-main"]').click();
+  }
+
+  getSuccessMessage() {
+    return cy.get('[data-testid="Paragraph-test"]').contains(
+      `You'll receive an email whenever someone requests the TSC to vote.`
+    );
+  }
+
+  getFailureMessage() {
+    return cy.get('[data-testid="Paragraph-test"]').contains(
+      `Subscription failed, please let us know about it by submitting a bug`
+    );
+  }
+}
+
+export default TSCPage;
+
