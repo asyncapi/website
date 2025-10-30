0d62b5142d6fffc5bd6c3c124db4b4f30a5501ab
WillyEverGreen <saibalkawade10@gmail.com>
2025-10-30 13:28:42 +0530

    chore(i18n): revert manual i18n initialization in _app.tsx (use next-i18next)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -22,19 +22,19 @@ function MyApp({ Component, pageProps, router }: AppProps) {
     <AppContext.Provider value={{ path: router.asPath }}>
       {/* <MDXProvider components={mdxComponents}> */}
       <Head>
-        <script async defer src='https://buttons.github.io/buttons.js'></script>
+        <script async defer src="https://buttons.github.io/buttons.js"></script>
       </Head>
       <AlgoliaSearch>
-        <div className='flex min-h-screen flex-col'>
+        <div className="flex min-h-screen flex-col">
           <Banner />
           <StickyNavbar>
-            <NavBar className='mx-auto block max-w-screen-xl px-4 sm:px-6 lg:px-8' />
+            <NavBar className="mx-auto block max-w-screen-xl px-4 sm:px-6 lg:px-8" />
           </StickyNavbar>
           <Layout>
             <Component {...pageProps} />
             <ScrollButton />
           </Layout>
-          <div className='mt-auto'>
+          <div className="mt-auto">
             <Footer />
           </div>
         </div>
