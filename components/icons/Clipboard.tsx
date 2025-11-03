c0f4f3b0f98686b69940f5df7ae2045b3731cd8a
Shriya-Chauhan <auroralflower@gmail.com>
2025-11-03 11:30:41 +0530

    fix: update ambassadors page

--- components/icons/Clipboard.tsx
+++ components/icons/Clipboard.tsx
@@ -1,13 +1,12 @@
-import React from 'react';
-
-/* eslint-disable max-len */
-/**
- * @description Icons for asyncapi website
- */
 export default function IconClipboard({ className = '' }) {
   return (
-    <svg className={className || 'inline-block'} fill='currentColor' viewBox='0 0 20 20'>
-      <path d='M15.6 2l-1.2 3H5.6L4.4 2C3.629 2 3 2.629 3 3.4v15.2c0 .77.629 1.4 1.399 1.4h11.2c.77 0 1.4-.631 1.4-1.4V3.4C17 2.629 16.369 2 15.6 2zm-2 2l.9-2h-2.181L11.6 0H8.4l-.72 2H5.5l.899 2H13.6z' />
+    <svg className={className} fill='currentColor' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'>
+      <path d='M9 2a1 1 0 000 2h2a1 1 0 100-2H9z' />
+      <path
+        fillRule='evenodd'
+        d='M4 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v11a2 2 0 01-2 2H6a2 2 0 01-2-2V5zm3 4a1 1 0 000 2h.01a1 1 0 100-2H7zm3 0a1 1 0 000 2h3a1 1 0 100-2h-3zm-3 4a1 1 0 100 2h.01a1 1 0 100-2H7zm3 0a1 1 0 100 2h3a1 1 0 100-2h-3z'
+        clipRule='evenodd'
+      />
     </svg>
   );
 }
