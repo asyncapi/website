d1072e19eff08448ec333ec484f17eeb96199a6c
Shriya-Chauhan <auroralflower@gmail.com>
2025-11-03 11:30:41 +0530

    fix: update ambassadors page

---
+++ components/icons/Play.tsx
@@ -0,0 +1,10 @@
+export default function IconPlay({ className = '' }) {
+  return (
+    <svg className={className} fill='currentColor' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'>
+      <path d='M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z' />
+    </svg>
+  );
+}
+
+
+
