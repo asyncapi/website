62199908fdcde28f5d71c1514bf8ffc2d636cb4a
Anushka Sharan <anushkasharan05@gmail.com>
2025-11-03 13:25:57 +0530

    small change

--- cypress/pages/ambassadors.js
+++ cypress/pages/ambassadors.js
@@ -4,18 +4,12 @@ class AmbassadorsPage {
   }
 
   verifyKeySectionsAndLinks() {
-    // Main CTA button to program doc
+    
     cy.get(
       'a[href="https://github.com/asyncapi/community/blob/master/docs/050-mentorship-program/ambassador-program/AMBASSADOR_PROGRAM.md"]'
     ).should('be.visible');
-
-    // Intro video iframe
     cy.get('[data-testid="Ambassadors-Iframe"]').should('be.visible');
-
-    // Members grid exists
     cy.get('[data-testid="Ambassadors-members-main"]').should('be.visible');
-
-    // Bottom CTA buttons
     cy.get(
       'a[href="https://github.com/asyncapi/community/blob/master/AMBASSADOR_ORGANIZATION.md#are-you-interested-in-becoming-an-official-asyncapi-ambassador"]'
     ).should('be.visible');
@@ -23,7 +17,7 @@ class AmbassadorsPage {
   }
 
   verifyAmbassadorSocialLinks(name, links) {
-    // Find the card for the ambassador by their name, then check social anchors within it
+    
     cy.contains('[data-testid="Ambassadors-members-details"]', name)
       .closest('[data-testid="Ambassadors-members"]')
       .within(() => {
