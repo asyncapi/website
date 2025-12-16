20de86f51f632bed508f8e8124d3cfd8684eb8bf
Anushka Sharan <anushkasharan05@gmail.com>
2025-12-16 14:30:10 +0530

    refactor: remove unnecessary comments from verifyLinks function

--- cypress/support/helpers.js
+++ cypress/support/helpers.js
@@ -1,12 +1,3 @@
-/**
- * Helper function to verify multiple links using the same verification method
- * Reduces repetitive forEach loops in tests
- * 
- * @param {Array} linksArray - Array of link objects with url/href and label properties
- * @param {Function} verifyFn - The verification function to call for each link
- * @param {string} urlKey - The property name for the URL (default: 'url')
- * @param {string} labelKey - The property name for the label (default: 'label')
- */
 export function verifyLinks(linksArray, verifyFn, urlKey = 'url', labelKey = 'label') {
   linksArray.forEach((link) => {
     verifyFn(link[urlKey], link[labelKey]);
