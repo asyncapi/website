e03e5be40930b80dccef15bc64ecdff2ddcee5e9
Ram Shukla <codewithrex@gmail.com>
2025-12-26 15:52:18 +0530

    fix: correct formatting for linter in AccordionItem

--- components/Accordion/AccordionItem.tsx
+++ components/Accordion/AccordionItem.tsx
@@ -42,7 +42,10 @@ export default function AccordionItem({ itemIndex, title, content, isActive, set
     <div className='my-2 flex size-full flex-col gap-1 border border-gray-200 bg-white px-2'>
       <button className='flex h-8 w-full items-center justify-between rounded-sm py-2' onClick={handleClick}>
         <div className='font-body font-semibold text-gray-800 antialiased'>{title}</div>
-        <div className='transition-transform duration-300' style={{ transform: isActive ? 'rotate(180deg)' : 'rotate(0deg)' }}>
+        <div
+          className='transition-transform duration-300'
+          style={{ transform: isActive ? 'rotate(180deg)' : 'rotate(0deg)' }}
+        >
           <svg width='24' height='24' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'>
             <path
               d='M18 9.00005C18 9.00005 13.5811 15 12 15C10.4188 15 6 9 6 9'
