770a45daba2322b2eef6cd77ef270ef7ef25b519
Vaibhav Adarsh <vaibhavadarsh07@gmail.com>
2025-12-30 23:49:27 +0530

    fix(blog): prevent Next.js prefetching of static rss.xml
    Replaced TextLink with anchor tag for rss.xml to avoid invalid Next.js prefetch requests.

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -93,7 +93,7 @@ export default function BlogIndexPage() {
                 height='18px'
                 width='18px'
               />
-              <TextLink href='/rss.xml'> RSS Feed</TextLink>, too!
+              <a href='/rss.xml'> RSS Feed</a>, too!
             </Paragraph>
           </div>
           <div className='mx:64 mt-12 md:flex md:justify-center lg:justify-center'>
