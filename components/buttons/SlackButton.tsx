c32f4fbc4ebe2c6880990c42cda55dda39d62555
ika12345 <24bcs005@nith.ac.in>
2026-01-02 04:37:53 +0530

    chore: clean up SlackButton imports and formatting

--- components/buttons/SlackButton.tsx
+++ components/buttons/SlackButton.tsx
@@ -9,20 +9,22 @@ import Button from './Button';
 interface ISlackButtonProps extends IButtonDefaultProps {}
 
 /**
- * SlackButton component always opens AsyncAPI Slack invite in a new tab.
+ * @description The SlackButton component is a button that links to the AsyncAPI Slack channel.
  */
-export default function SlackButton({ text = 'Join on Slack', className = '' }: ISlackButtonProps) {
-  const href = 'https://www.asyncapi.com/slack-invite';
-
+export default function SlackButton({
+  text = 'Join on Slack',
+  className = '',
+  href = 'https://www.asyncapi.com/slack-invite',
+  target = '_blank',
+  iconPosition = ButtonIconPosition.LEFT
+}: ISlackButtonProps) {
   return (
     <Button
-      as='a'
       href={href}
       text={text}
       icon={<IconSlack className='-mt-1 inline-block size-6 p-0.5' />}
-      iconPosition={ButtonIconPosition.LEFT}
-      target='_blank'
-      rel='noopener noreferrer'
+      iconPosition={iconPosition}
+      target={target}
       className={className}
       data-testid='Slack-button'
       bgClassName='bg-slack hover:bg-slack-light'
