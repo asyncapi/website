741b37aa69d9977c9596ea566aa99f8715bfcd2b
Sam <samailahmad61@gmail.com>
2026-01-02 20:07:01 +0000

    fix: memory leak is fixed

--- components/tools/ToolsDashboard.tsx
+++ components/tools/ToolsDashboard.tsx
@@ -44,7 +44,7 @@ export default function ToolsDashboard() {
     return () => {
       document.removeEventListener('mousedown', checkIfClickOutside);
     };
-  });
+  }, [openFilter]);
 
   // useEffect function to enable the close Category dropdown Modal feature when clicked outside of the modal
   useEffect(() => {
@@ -59,7 +59,7 @@ export default function ToolsDashboard() {
     return () => {
       document.removeEventListener('mousedown', checkIfClickOutside);
     };
-  });
+  }, [openCategory]);
 
   // useMemo function to filter the tools according to the filters applied by the user
   const toolsList = useMemo(() => {
