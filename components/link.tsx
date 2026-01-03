8427e43a6f0840c159ac8ab355db7383a4cb1aba
SHUBHANSHU602 <shubhanshusingh602@gmail.com>
2026-01-04 02:34:39 +0530

    fix: apply prettier formatting to LinkText function

--- components/link.tsx
+++ components/link.tsx
@@ -42,7 +42,10 @@ export default function LinkComponent({
   // Detect current language based on the path or query parameter
   const slug = asPath.split('/')[1];
   const langSlug = languages.includes(slug) && slug;
-  const language: string = query.lang && typeof query.lang === 'string' ? query.lang : langSlug || defaultLanguage; // Ensure language is always a string
+  const language: string =
+    query.lang && typeof query.lang === 'string'
+      ? query.lang
+      : langSlug || defaultLanguage; // Ensure language is always a string
 
   let href = props.href || pathname;
 
@@ -50,7 +53,12 @@ export default function LinkComponent({
     If explicit href is provided, and the language-specific paths for the current language do not include the href,
     or if the href starts with "http", render a standard Link
   */
-  if ((props.href && i18nPaths[language] && !i18nPaths[language].includes(href)) || href.includes('http', 0)) {
+  if (
+    (props.href &&
+      i18nPaths[language] &&
+      !i18nPaths[language].includes(href)) ||
+    href.includes('http', 0)
+  ) {
     return (
       <Link href={href} target={target} rel={rel}>
         {children}
@@ -64,7 +72,9 @@ export default function LinkComponent({
     } else {
       // If the current path starts with "/404", update href to be the root path with the locale
       // Otherwise, replace "[lang]" placeholder with the locale
-      href = pathname.startsWith('/404') ? `/${locale}` : pathname.replace('[lang]', locale);
+      href = pathname.startsWith('/404')
+        ? `/${locale}`
+        : pathname.replace('[lang]', locale);
     }
   } else {
     // If no locale is provided, update the href with the current language or keep it as is
