ec327f387da82dd08a028d8f0634a609294b1943
ika12345 <24bcs005@nith.ac.in>
2026-01-04 19:33:24 +0530

    Restore slackbutton and Slack CTA to original behavior

--- components/buttons/SlackButton.tsx
+++ components/buttons/SlackButton.tsx
@@ -11,29 +11,28 @@ interface ISlackButtonProps extends IButtonDefaultProps {}
 /**
  * @description The SlackButton component is a button that links to the AsyncAPI Slack channel.
  * @param {string} props.text - The text to display on the button.
- * @param {string} props.href - The external Slack invite URL.
+ * @param {string} props.href - The href attribute for the anchor tag.
  * @param {string} props.target - The target attribute for the anchor tag.
  * @param {ButtonIconPosition} props.iconPosition - The position of the icon in the button.
  * @param {string} props.className - The class name to be applied to the button.
  */
-
 export default function SlackButton({
   text = 'Join on Slack',
-  className = '',
-  href = 'https://www.asyncapi.com/slack-invite',
+  href = '/slack-invite',
   target = '_blank',
-  iconPosition = ButtonIconPosition.LEFT
+  iconPosition = ButtonIconPosition.LEFT,
+  className = ''
 }: ISlackButtonProps) {
   return (
     <Button
-      href={href}
       text={text}
       icon={<IconSlack className='-mt-1 inline-block size-6 p-0.5' />}
+      href={href}
       iconPosition={iconPosition}
       target={target}
       className={className}
       data-testid='Slack-button'
-      bgClassName='bg-slack hover:bg-slack-light'
+      bgClassName='bg-slack  hover:bg-slack-light'
     />
   );
 }
