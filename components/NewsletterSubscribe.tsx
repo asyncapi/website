8753514a5f0cbc86e644b85127008d3cd458b715
Choudhary Khushboo GirdhariRam <khushboo@Choudharys-MacBook-Pro-M3.local>
2026-01-06 14:13:18 +0530

    chore: fix lint and formatting issues

--- components/NewsletterSubscribe.tsx
+++ components/NewsletterSubscribe.tsx
@@ -30,24 +30,13 @@ interface NewsletterSubscribeProps {
   errorSubtitle?: string;
 }
 
-/**
- * @description This component displays Newsletter Subscribe component.
- *
- * @param {NewsletterSubscribeProps} props - The props for the Newsletter Subscribe component.
- * @param {string} props.className - CSS class for styling the card.
- * @param {boolean} props.dark - If true, the theme of the component will be dark.
- * @param {string} props.title - The title of the Subscribe card.
- * @param {string} props.subtitle - The subtitle of the Subscribe card.
- * @param {string} props.type - The type of subscription.
- * @param {string} props.errorSubtitle - The error subtitle to be displayed.
- */
 export default function NewsletterSubscribe({
   className = 'p-8 text-center text-black',
   dark = false,
   title = 'Subscribe to our newsletter to receive news about AsyncAPI.',
   subtitle = 'We respect your inbox. No spam, promise ✌️',
   type = 'Newsletter',
-  errorSubtitle = 'Subscription failed, please let us know about it by submitting a bug',
+  errorSubtitle = 'Subscription failed, please let us know about it by submitting a bug'
 }: NewsletterSubscribeProps) {
   const [email, setEmail] = useState<string>('');
   const [name, setName] = useState<string>('');
@@ -68,18 +57,19 @@ export default function NewsletterSubscribe({
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     setStatus(FormStatus.LOADING);
     event.preventDefault();
+
     const data = {
       name,
       email,
-      interest: type,
+      interest: type
     };
 
     fetch('/.netlify/functions/newsletter_subscription', {
       method: 'POST',
       body: JSON.stringify(data),
       headers: {
-        'Content-Type': 'application/json',
-      },
+        'Content-Type': 'application/json'
+      }
     })
       .then(async (res) => {
         if (res.status === 200) {
@@ -103,55 +93,39 @@ export default function NewsletterSubscribe({
 
   if (status === FormStatus.SUCCESS) {
     return (
-      <div className={className} data-testid="NewsletterSubscribe-main">
-        <Heading
-          level={HeadingLevel.h3}
-          textColor={headTextColor}
-          typeStyle={HeadingTypeStyle.lg}
-          className="mb-4"
-        >
+      <div className={className} data-testid='NewsletterSubscribe-main'>
+        <Heading level={HeadingLevel.h3} textColor={headTextColor} typeStyle={HeadingTypeStyle.lg} className='mb-4'>
           {ready ? t('successTitle') : 'Thank you for subscribing!'}
         </Heading>
-        <Paragraph className="mb-8" textColor={paragraphTextColor}>
+        <Paragraph className='mb-8' textColor={paragraphTextColor}>
           {ready ? t('subtitle') : subtitle}
         </Paragraph>
       </div>
     );
   }
+
   if (status === FormStatus.SERVICE_UNAVAILABLE) {
     return (
-      <div className={className} data-testid="NewsletterSubscribe-main">
-        <Heading
-          level={HeadingLevel.h3}
-          textColor={headTextColor}
-          typeStyle={HeadingTypeStyle.lg}
-          className="mb-4"
-        >
+      <div className={className} data-testid='NewsletterSubscribe-main'>
+        <Heading level={HeadingLevel.h3} textColor={headTextColor} typeStyle={HeadingTypeStyle.lg} className='mb-4'>
           Newsletter service temporarily unavailable
         </Heading>
-        <Paragraph className="mb-8" textColor={paragraphTextColor}>
+        <Paragraph className='mb-8' textColor={paragraphTextColor}>
           Please try again later.
         </Paragraph>
       </div>
     );
   }
+
   if (status === FormStatus.ERROR) {
     return (
-      <div className={className} data-testid="NewsletterSubscribe-main">
-        <Heading
-          level={HeadingLevel.h3}
-          textColor={headTextColor}
-          typeStyle={HeadingTypeStyle.lg}
-          className="mb-4"
-        >
+      <div className={className} data-testid='NewsletterSubscribe-main'>
+        <Heading level={HeadingLevel.h3} textColor={headTextColor} typeStyle={HeadingTypeStyle.lg} className='mb-4'>
           {ready ? t('errorTitle') : 'Something went wrong'}
         </Heading>
-        <Paragraph className="mb-8" textColor={paragraphTextColor}>
+        <Paragraph className='mb-8' textColor={paragraphTextColor}>
           {ready ? t('errorSubtitle') : errorSubtitle}{' '}
-          <TextLink
-            href="https://github.com/asyncapi/website/issues/new?template=bug.md"
-            target="_blank"
-          >
+          <TextLink href='https://github.com/asyncapi/website/issues/new?template=bug.md' target='_blank'>
             {ready ? t('errorLinkText') : 'here'}
           </TextLink>
         </Paragraph>
@@ -160,39 +134,27 @@ export default function NewsletterSubscribe({
   }
 
   return (
-    <div className={className} data-testid="NewsletterSubscribe-main">
-      <Heading
-        level={HeadingLevel.h3}
-        textColor={headTextColor}
-        typeStyle={HeadingTypeStyle.lg}
-        className="mb-4"
-      >
+    <div className={className} data-testid='NewsletterSubscribe-main'>
+      <Heading level={HeadingLevel.h3} textColor={headTextColor} typeStyle={HeadingTypeStyle.lg} className='mb-4'>
         {ready ? t('title') : title}
       </Heading>
-      <Paragraph className="mb-8" textColor={paragraphTextColor}>
+      <Paragraph className='mb-8' textColor={paragraphTextColor}>
         {ready ? t('subtitle') : subtitle}
       </Paragraph>
       {status === FormStatus.LOADING ? (
-        <Loader
-          loaderText={'Waiting for response...'}
-          loaderIcon={<IconCircularLoader dark />}
-          dark={dark}
-        />
+        <Loader loaderText={'Waiting for response...'} loaderIcon={<IconCircularLoader dark />} dark={dark} />
       ) : (
-        <form
-          className="flex flex-col gap-4 md:flex-row"
-          onSubmit={handleSubmit}
-        >
+        <form className='flex flex-col gap-4 md:flex-row' onSubmit={handleSubmit}>
           <InputBox
             inputType={InputTypes.TEXT}
-            inputName="name"
+            inputName='name'
             placeholder={ready ? t('nameInput') : 'Your name'}
             inputValue={name}
             setInput={setName}
           />
           <InputBox
             inputType={InputTypes.EMAIL}
-            inputName="email"
+            inputName='email'
             placeholder={ready ? t('emailInput') : 'Your email'}
             inputValue={email}
             setInput={setEmail}
@@ -200,8 +162,8 @@ export default function NewsletterSubscribe({
           <Button
             type={ButtonType.SUBMIT}
             text={ready ? t('subscribeBtn') : 'Subscribe'}
-            className="mt-2 w-full md:mr-2 md:mt-0 md:flex-1"
-            href=""
+            className='mt-2 w-full md:mr-2 md:mt-0 md:flex-1'
+            href=''
           />
         </form>
       )}
