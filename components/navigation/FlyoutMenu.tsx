ef865df201aad4e488d808d96ea43b7ace03c477
successbyte <ehsanullahhaidary@gmail.com>
2026-01-09 23:20:14 +0430

    style: header dropdowns consistent shadow and border radius

--- components/navigation/FlyoutMenu.tsx
+++ components/navigation/FlyoutMenu.tsx
@@ -14,15 +14,11 @@ interface FlyoutProps {
 export default function Flyout({ items = [] }: FlyoutProps) {
   return (
     <div
-      className='absolute z-50 -ml-4 md:h-[80vh] overflow-x-auto w-screen max-w-md pt-3 md:ml-12 md:-translate-x-1/2 lg:left-1/2 lg:max-w-3xl lg:-translate-x-1/2'
-      data-testid='Flyout-main'
+      className="absolute z-50 bg-white rounded-lg -ml-4 md:max-h-[80vh] shadow-lg overflow-x-auto w-screen max-w-md pt-3 md:ml-12 md:-translate-x-1/2 lg:left-1/2 lg:max-w-3xl lg:-translate-x-1/2"
+      data-testid="Flyout-main"
     >
-      <div className='rounded-lg shadow-lg'>
-        <div className='shadow-xs overflow-hidden rounded-lg'>
-          <div className='relative z-20 grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8 lg:grid-cols-2'>
-            <MenuBlocks items={items} />
-          </div>
-        </div>
+      <div className="relative z-20 grid gap-6 px-5 py-6 sm:gap-8 sm:p-8 lg:grid-cols-2">
+        <MenuBlocks items={items} />
       </div>
     </div>
   );
