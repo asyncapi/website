a40c8c1cca857d6748285398ca17271e41ea3ed2
successbyte <ehsanullahhaidary@gmail.com>
2026-01-09 23:04:57 +0430

    fix: formatted FlyoutMenu

--- components/navigation/FlyoutMenu.tsx
+++ components/navigation/FlyoutMenu.tsx
@@ -14,12 +14,12 @@ interface FlyoutProps {
 export default function Flyout({ items = [] }: FlyoutProps) {
   return (
     <div
-      className='absolute z-50 bg-white rounded-lg -ml-4 md:max-h-[80vh] shadow-lg overflow-x-auto w-screen max-w-md pt-3 md:ml-12 md:-translate-x-1/2 lg:left-1/2 lg:max-w-3xl lg:-translate-x-1/2'
-      data-testid='Flyout-main'
+      className="absolute z-50 bg-white rounded-lg -ml-4 md:max-h-[80vh] shadow-lg overflow-x-auto w-screen max-w-md pt-3 md:ml-12 md:-translate-x-1/2 lg:left-1/2 lg:max-w-3xl lg:-translate-x-1/2"
+      data-testid="Flyout-main"
     >
-        <div className='relative z-20 grid gap-6 px-5 py-6 sm:gap-8 sm:p-8 lg:grid-cols-2'>
-          <MenuBlocks items={items} />
-        </div>
+      <div className="relative z-20 grid gap-6 px-5 py-6 sm:gap-8 sm:p-8 lg:grid-cols-2">
+        <MenuBlocks items={items} />
+      </div>
     </div>
   );
 }
