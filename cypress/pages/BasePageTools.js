2eeba2945ff730c43a7f83456db8c2e692756eed
Anushka Sharan <anushkasharan05@gmail.com>
2026-02-02 14:08:05 +0530

    test:  add e2e tests for dashboard and events pages

--- cypress/pages/BasePageTools.js
+++ cypress/pages/BasePageTools.js
@@ -3,12 +3,27 @@ class BasePage {
     cy.visit(path);
   }
 
+  verifyElementIsVisible(selector) {
+    cy.get(selector).should('be.visible');
+  }
+
+  verifyTextVisible(text) {
+    cy.contains(text).should('be.visible');
+  }
+
+  verifyLinkWithText(selector, text, expectedHrefPart) {
+    cy.contains(selector, text)
+      .should('be.visible')
+      .and('have.attr', 'href')
+      .and('include', expectedHrefPart);
+  }
+
   getHeaderText(selector, expectedText) {
     cy.get(selector).should('contain.text', expectedText);
   }
 
   getLink(href, text) {
-   return cy.contains(`a[href="${href}"]`, text)
+    return cy.contains(`a[href="${href}"]`, text)
       .should('be.visible')
       .and('have.attr', 'href', href);
   }
