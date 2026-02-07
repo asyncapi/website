1482343935569fb28dcbacf0d1145c9539d7d1ac
jagadish-7 <coderjagadish143@gmail.com>
2026-02-07 23:23:17 +0530

    fixes double border outline
    Signed-off-by: jagadish-7 <coderjagadish143@gmail.com>

--- components/form/Select.tsx
+++ components/form/Select.tsx
@@ -31,7 +31,7 @@ export default function Select({ className = '', onChange = () => {}, options, s
       data-testid='Select-form'
       onChange={handleOnChange}
       className={twMerge(
-        `form-select h-full pl-2 pr-8 inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500 ${className}`
+        `form-select h-full pl-2 pr-8 inline-flex justify-center rounded-md border border-gray-300 focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none ${className}`
       )}
       value={selected}
     >
