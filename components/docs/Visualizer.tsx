a0a93e8e077f6d40fcbfa1b46ec8012e480e9a4f
acethecreator <devaze007@gmail.com>
2026-03-10 14:32:34 +0100

    feat: use latest schyma version and support for v3.1

--- components/docs/Visualizer.tsx
+++ components/docs/Visualizer.tsx
@@ -4,18 +4,25 @@ import type { JSONSchema7Object } from 'json-schema';
 import React from 'react';
 import Schyma from 'schyma';
 
-import schema from '../../config/3.0.0.json';
-
-const typeSchema = schema as unknown as JSONSchema7Object;
+import schemav3 from '../../config/3.0.0.json';
+import schemav3_1 from '../../config/3.1.0.json';
 
 /**
  * @description This component renders the spec explorer.
  */
-function Visualizer() {
+
+interface VisualizerProps {
+  version: string;
+}
+function Visualizer({ version }: VisualizerProps) {
+  let typeSchema = schemav3 as unknown as JSONSchema7Object;
+  if (version === '3.1.0') {
+    typeSchema = schemav3_1 as unknown as JSONSchema7Object;
+  }
   return (
-    <div>
+    <div className="overflow-hidden">
       <Schyma
-        title='AsyncAPI Specification'
+        title="AsyncAPI Specification"
         description="The AsyncAPI Specification defines a set of fields that can be used in an
         AsyncAPI document to describe an application's API. The document may reference other files for
         additional details or shared fields, but it is typically a single,
