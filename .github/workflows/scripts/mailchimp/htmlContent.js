e42257f1fad8bb4ecd486a0ddc54b5473ec831c4
Prince Rajpoot <prince.rajpoot.20@gmail.com>
2026-04-05 01:38:55 +0530

    minor change

--- .github/workflows/scripts/mailchimp/htmlContent.js
+++ .github/workflows/scripts/mailchimp/htmlContent.js
@@ -424,7 +424,7 @@ Topic: <a href="${ link }" style="color:#007c89;font-weight:normal;text-decorati
                         <td valign="top" class="mcnTextContent" style="padding-top: 0;padding-right: 18px;padding-bottom: 9px;padding-left: 18px;mso-line-height-rule: exactly;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;word-break: break-word;color: #202020;font-family: Helvetica;font-size: 16px;line-height: 150%;text-align: left;">
                         
                             Cheers,<br>
-AsyncAPI Initiative
+<span style="color:#696969">AsyncAPI Initiative</span>
                         </td>
                     </tr>
                 </tbody></table>
