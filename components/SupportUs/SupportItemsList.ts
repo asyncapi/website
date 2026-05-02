d73034e75eb7664f46873d5258572d6cff122621
aeworxet <aeworxet@protonmail.com>
2026-05-02 20:59:58 +0300

    fix: address visual issues in Dark Theme (#5374)

--- components/SupportUs/SupportItemsList.ts
+++ components/SupportUs/SupportItemsList.ts
@@ -11,42 +11,42 @@ export const items: ISupportItemType[] = [
     href: 'https://slack.com/media-kit',
     imgSrc: '/img/supportus/slack.webp',
     imgTitle: 'Slack - Free Standard Subscription.',
-    imgClass: 'inline-block px-4 sm:h-10',
+    imgClass: 'inline-block px-4 sm:h-10 rounded-lg p-2 bg-white',
     section: 1
   },
   {
     href: 'https://www.netlify.com/',
     imgSrc: '/img/supportus/netlify.webp',
     imgTitle: 'Netlify - Free website deployment.',
-    imgClass: 'inline-block px-4 sm:h-10',
+    imgClass: 'inline-block px-4 sm:h-10 rounded-lg p-2 bg-white',
     section: 1
   },
   {
     href: 'https://sonarcloud.io/',
     imgSrc: '/img/supportus/sonarcloud.webp',
     imgTitle: 'Sonarcloud - Free tier for automated project scanning.',
-    imgClass: 'inline-block px-4 md:h-14',
+    imgClass: 'inline-block px-4 md:h-14 rounded-lg p-2 bg-white',
     section: 1
   },
   {
     href: 'https://confengine.com',
     imgSrc: '/img/supportus/confengine.png',
     imgTitle: 'Confengine - CFP platform sponsorship for AsyncAPI Conference.',
-    imgClass: 'inline-block px-4 sm:h-14 mt-4',
+    imgClass: 'inline-block px-4 sm:h-14 rounded-lg p-2 bg-white',
     section: 2
   },
   {
     href: 'https://www.digitalocean.com/press/',
     imgSrc: '/img/supportus/digitalocean.webp',
     imgTitle: 'DigitalOcean - 500 dollars on cloud services.',
-    imgClass: 'inline-block px-2 sm:h-8',
+    imgClass: 'inline-block px-4 sm:h-10 md:h-10 lg:h-10 rounded-lg p-2 bg-white',
     section: 2
   },
   {
     href: 'https://restream.io/',
     imgSrc: '/img/supportus/restream.webp',
     imgTitle: 'Restream - Free professional plan subscription.',
-    imgClass: 'inline-block px-4 sm:h-6',
+    imgClass: 'inline-block px-4 sm:h-10 md:h-10 lg:h-10 rounded-lg p-2 bg-white',
     section: 2
   }
 ];
--- components/sponsors/SilverSponsorsList.ts
+++ components/sponsors/SilverSponsorsList.ts
@@ -6,34 +6,34 @@ export const Silversponsors: SponsorType[] = [
     website: 'https://bump.sh/asyncapi?utm_source=asyncapi&utm_medium=referral&utm_campaign=sponsor',
     altText: 'OpenAPI & AsyncAPI API docs - Bump.sh',
     imageSrc: '/img/sponsors/bumpsh.svg',
-    imageClass: 'inline-block sm:h-9'
+    imageClass: 'inline-block sm:h-20'
   },
   {
     name: 'Svix',
     website: 'https://www.svix.com/',
     altText: 'Svix',
     imageSrc: '/img/sponsors/svix_logo.svg',
-    imageClass: 'inline-block sm:h-9'
+    imageClass: 'inline-block sm:h-20'
   },
   {
     name: 'Aklivity',
     website: 'https://www.aklivity.io/',
     altText: 'Aklivity',
     imageSrc: '/img/sponsors/aklivity_logo.png',
-    imageClass: 'inline-block sm:h-9'
+    imageClass: 'inline-block sm:h-20'
   },
   {
     name: 'SmartBear',
     website: 'https://smartbear.com',
     altText: 'SmartBear',
     imageSrc: '/img/sponsors/smartbear_logo.png',
-    imageClass: 'inline-block sm:h-9'
+    imageClass: 'inline-block sm:h-20'
   },
   {
     name: 'Route4Me',
     website: 'https://route4me.com',
     altText: 'Best Route Planning and Route Optimization Software',
     imageSrc: '/img/sponsors/route4me_logo.png',
-    imageClass: 'inline-block sm:h-9'
+    imageClass: 'inline-block sm:h-20'
   }
 ];
