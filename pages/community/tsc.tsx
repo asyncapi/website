a51c8ccb458a214a72cc0fd52a9880bc203f3b3c
DuskWarden <pawar96sameer@gmail.com>
2026-05-28 00:53:10 +0530

    fix: reduce extra vertical spacing between hero and info cards after stats removal

--- pages/community/tsc.tsx
+++ pages/community/tsc.tsx
@@ -110,7 +110,7 @@ export default function TSC() {
       </div>
 
       {/* Info Cards Section */}
-      <div className='bg-white dark:bg-dark-background py-16 sm:py-20'>
+      <div className='bg-white dark:bg-dark-background pt-8 pb-16 sm:pt-10 sm:pb-20'>
         <div className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8'>
           <div className='grid grid-cols-1 md:grid-cols-3 gap-8'>
             {/* What is TSC Card */}
