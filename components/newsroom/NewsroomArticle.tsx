ecdf1a7b87a7fb54476af75e30ad67113821634f
hasithasandunlakshan <hasiofficial2002@gmail.com>
2026-05-31 13:22:15 +0530

    feat: add dark mode support to newsroom article cards and dates

--- components/newsroom/NewsroomArticle.tsx
+++ components/newsroom/NewsroomArticle.tsx
@@ -22,14 +22,14 @@ export default function NewsroomArticl() {
       {articlesData.map((article: Article, index: number) => (
         <li key={index}>
           <a
-            className='mb-2 block rounded-md border border-gray-200 bg-white p-6 text-left shadow-md transition-all duration-300 ease-in-out hover:shadow-lg lg:w-full'
+            className='mb-2 block rounded-md border border-gray-200 dark:border-gray-800 bg-white dark:bg-dark-card p-6 text-left shadow-md transition-all duration-300 ease-in-out hover:shadow-lg lg:w-full'
             href={article.url}
             target='_blank'
             rel='noopener noreferrer'
             data-testid={`NewsroomArticle-${index}`}
           >
             <div>
-              <Paragraph typeStyle={ParagraphTypeStyle.sm} textColor='text-gray-600'>
+              <Paragraph typeStyle={ParagraphTypeStyle.sm} textColor='text-gray-600 dark:text-gray-400'>
                 {article.publishDate}
               </Paragraph>
               <Heading level={HeadingLevel.h4} typeStyle={HeadingTypeStyle.xsSemibold} className='mt-3'>
